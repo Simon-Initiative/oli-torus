1cf4ae5fa7b78889f5e82b1d4d77917b442bd521
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2020-03-11 14:45:58 -0400

    change ordering

--- priv/repo/migrations/20200310193550_accounts_authoring_delivery.exs
+++ priv/repo/migrations/20200310193550_accounts_authoring_delivery.exs
@@ -2,39 +2,6 @@ defmodule Oli.Repo.Migrations.CreateRoles do
   use Ecto.Migration
 
   def change do
-    # accounts
-
-    create table(:system_roles) do
-      timestamps()
-      add :type, :string
-    end
-
-    create table(:project_roles) do
-      timestamps()
-      add :type, :string
-    end
-
-    create table(:section_roles) do
-      timestamps()
-      add :type, :string
-    end
-
-    create table(:users_projects) do
-      timestamps()
-      add :user_id, references(:users)
-      add :project_id, references(:projects)
-      add :project_role_id, references(:project_roles)
-    end
-
-    create table(:users_sections) do
-      timestamps()
-      add :user_id, references(:users)
-      add :section_id, references(:sections)
-      add :section_role_id, references(:section_roles)
-    end
-
-    # authoring
-
     create table(:project_families) do
       timestamps()
       add :slug, :string
@@ -55,6 +22,55 @@ defmodule Oli.Repo.Migrations.CreateRoles do
       add :type, :string
     end
 
+    create table(:system_roles) do
+      timestamps()
+      add :type, :string
+    end
+
+    create table(:project_roles) do
+      timestamps()
+      add :type, :string
+    end
+
+    create table(:section_roles) do
+      timestamps()
+      add :type, :string
+    end
+
+    create table(:sections) do
+      timestamps()
+      add :title, :string
+      add :start_date, :date
+      add :end_date, :date
+      add :time_zone, :string
+      add :institution_id, references(:institutions)
+      add :open_and_free, :boolean
+      add :registration_open, :boolean
+    end
+
+    create table(:users_sections) do
+      timestamps()
+      add :user_id, references(:users)
+      add :section_id, references(:sections)
+      add :section_role_id, references(:section_roles)
+    end
+
+    create table(:users_projects) do
+      timestamps()
+      add :user_id, references(:users)
+      add :project_id, references(:projects)
+      add :project_role_id, references(:project_roles)
+    end
+
+    create table(:revisions) do
+      timestamps()
+      add :type, :string
+      add :md5, :string
+      add :revision_number, :integer
+      add :author_id, references(:users)
+      add :previous_revision_id, references(:revisions)
+    end
+
     create table(:resources) do
       timestamps()
       add :title, :string
@@ -68,27 +84,6 @@ defmodule Oli.Repo.Migrations.CreateRoles do
       timestamps()
       add :json, :string
       add :revision_id, references(:revisions)
-    end
-
-    create table(:revisions) do
-      timestamps()
-      add :type, :string
-      add :md5, :string
-      add :revision_number, :integer
-      add :author_id, references(:users)
-      add :previous_revision_id, references(:revisions)
-    end
-
-    # delivery
-    create table(:sections) do
-      timestamps()
-      add :title, :string
-      add :start_date, :date
-      add :end_date, :date
-      add :time_zone, :String
-      add :institution_id, references(:institutions)
-      add :open_and_free, :boolean
-      add :registration_open, :boolean
     end
   end
 end
