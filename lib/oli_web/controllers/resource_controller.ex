179f3d35db8288d8486cd6126f5833ce8a561725
Darren Siegel <siegel.darren@gmail.com>
2020-04-08 08:46:58 -0400

    stashing

--- lib/oli_web/controllers/resource_controller.ex
+++ lib/oli_web/controllers/resource_controller.ex
@@ -1,10 +1,11 @@
 defmodule OliWeb.ResourceController do
   use OliWeb, :controller
 
+  alias Oli.ResourceEditing
   import OliWeb.ProjectPlugs
 
   plug :fetch_project when action not in [:view, :update]
-  plug :authorize_project when action not in [:view, :update]
+  plug :authorize_project when action not in [:view]
 
   def view(conn, %{"project" => _project_id}) do
     render conn, "page.html", title: "Page", active: :page
@@ -14,8 +15,17 @@ defmodule OliWeb.ResourceController do
     render conn, "edit.html", title: "Resource Editor", active: :resource_editor
   end
 
-  def update(conn, %{"project" => _project_id, "resource" => _resource_id }) do
-    json conn, %{ "type" => "success"}
+  def update(conn, %{"project" => project_slug, "resource" => resource_slug }) do
+
+    current_author_id
+
+    case ResourceEditing.edit(project_slug, resource_slug, author, update) do
+      {:ok, _revision} -> json conn, %{ "type" => "success"}
+      {:error, {:lock_not_acquired}} -> json conn, %{ "type" => "lock_not_acquired"}
+      {:error, {:not_found}} -> json conn, %{ "type" => "not_found"}
+      _ -> json conn, %{ "type" => "error"}
+    end
+
   end
 
 end
