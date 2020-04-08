b57a7455c5667b186ad74f71022a2d5046085b7b
Darren Siegel <siegel.darren@gmail.com>
2020-04-08 07:46:25 -0400

    handle unused variables

--- lib/oli_web/controllers/lock_controller.ex
+++ lib/oli_web/controllers/lock_controller.ex
@@ -1,14 +1,12 @@
 defmodule OliWeb.LockController do
   use OliWeb, :controller
 
-  def acquire(conn, %{"project" => project_id, "resource" => resource_id, "user" => user_id }) do
-
+  def acquire(conn, %{"project" => _project_slug, "resource" => _resource_slug, "user" => _user }) do
     response = %{ "type" => "success"}
     json(conn, response)
   end
 
-  def release(conn, %{"project" => project_id, "resource" => resource_id, "user" => user_id}) do
-
+  def release(conn, %{"project" => _project_slug, "resource" => _resource_slug, "user" => _user}) do
     response = %{ "type" => "success"}
     json(conn, response)
   end
