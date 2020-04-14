7421909e28c6db931c7cf8889a773cf685be7096
Darren Siegel <siegel.darren@gmail.com>
2020-04-14 09:32:01 -0400

    add stub components

---
+++ assets/src/components/activities/multiple_choice/MultipleChoice.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const MultipleChoiceDelivery = () => {
+  return <p>Multiple Choice Delivery</p>;
+};
+
+export const MultipleChoiceAuthoring = () => {
+  return <p>Multiple Choice Authoring</p>;
+};
