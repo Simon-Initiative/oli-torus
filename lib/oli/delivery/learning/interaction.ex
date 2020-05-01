6c051fd9278704ee7b70d3a37a9784b3bab34113
Raphael Gachuhi <rgachuhi@cmu.edu>
2020-05-01 10:07:31 -0400

    comments

--- lib/oli/delivery/learning/interaction.ex
+++ lib/oli/delivery/learning/interaction.ex
@@ -7,7 +7,7 @@ defmodule Oli.Delivery.Learning.Interaction do
     field :name, :string
 
     belongs_to :problem_attempt, Oli.Delivery.Learning.ProblemAttempt
-    # This one to many relation tracks multiple answers at the same input before an official submit is issued by the learner
+    # This one-to-many relation tracks multiple answers at the same input before an official submit is issued by the learner
     # Example would be in a quiz where learner enters a value in an input field, we autosave that, later during the quiz
     # learner changes their mind and enters a new value in the same input field, we autosave that the new "current" response
     # In most use cases, one response prevalent
