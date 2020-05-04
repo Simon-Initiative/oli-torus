77f363a9a0bc7652a574f7df78b92f17957cd540
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2020-05-04 19:21:17 -0400

    test infra for content writer on client

--- assets/src/data/content/writers/html.ts
+++ assets/src/data/content/writers/html.ts
@@ -1,12 +1,16 @@
-import { WriterImpl } from './writer';
-import { ModelElement } from '../model';
+import { WriterImpl, Next } from './writer';
+import { ModelElement, Image, HeadingSix, Paragraph, HeadingOne,
+  HeadingTwo, HeadingThree, HeadingFour, HeadingFive, YouTube, Audio,
+  Table, TableRow, TableHeader, TableData, OrderedList, UnorderedList,
+  ListItem, Math, MathLine, Code, CodeLine, Blockquote, Hyperlink } from '../model';
 import { Text } from 'slate';
+import { WriterContext } from './context';
 
 export class HtmlParser implements WriterImpl {
   private escapeXml = (text: string) => decodeURI(encodeURI(text));
 
   private wrapWithMarks(text: string, textEntity: Text): string {
-    const supportedMarkTags: any = {
+    const supportedMarkTags: { [key: string]: string} = {
       em: 'em',
       strong: 'strong',
       mark: 'mark',
@@ -23,25 +27,19 @@ export class HtmlParser implements WriterImpl {
       .reverse().reduce((acc, mark) => `<${mark}>${acc}</${mark}>`, text);
   }
 
-  p = (context: any, next: any, x: any) => `<p>${next()}</p>\n`;
-  h1 = (context: any, next: any, x: any) => `<h1>${next()}</h1>\n`;
-  h2 = (context: any, next: any, x: any) => `<h2>${next()}</h2>\n`;
-  h3 = (context: any, next: any, x: any) => `<h3>${next()}</h3>\n`;
-  h4 = (context: any, next: any, x: any) => `<h4>${next()}</h4>\n`;
-  h5 = (context: any, next: any, x: any) => `<h5>${next()}</h5>\n`;
-  h6 = (context: any, next: any, x: any) => `<h6>${next()}</h6>\n`;
-  img = (context: any, x: any, attrs: any) => {
-    let heightWidth;
-    if (attrs.height && attrs.width) {
-      heightWidth = `height="${attrs.height}" width="${attrs.width}"`;
-    }
-
-    return`<img ${heightWidth}
+  p = (context: WriterContext, next: Next, x: Paragraph) => `<p>${next()}</p>\n`;
+  h1 = (context: WriterContext, next: Next, x: HeadingOne) => `<h1>${next()}</h1>\n`;
+  h2 = (context: WriterContext, next: Next, x: HeadingTwo) => `<h2>${next()}</h2>\n`;
+  h3 = (context: WriterContext, next: Next, x: HeadingThree) => `<h3>${next()}</h3>\n`;
+  h4 = (context: WriterContext, next: Next, x: HeadingFour) => `<h4>${next()}</h4>\n`;
+  h5 = (context: WriterContext, next: Next, x: HeadingFive) => `<h5>${next()}</h5>\n`;
+  h6 = (context: WriterContext, next: Next, x: HeadingSix) => `<h6>${next()}</h6>\n`;
+  img = (context: WriterContext, next: Next, attrs: Image) =>
+    `<img ${attrs.height && attrs.width && `height="${attrs.height}" width="${attrs.width}"` || ''}
       style="display: block; max-height: 500px; margin-left: auto; margin-right: auto;"
-      src="${attrs.src}" />\n`;
-  }
+      src="${attrs.src}" />\n`
 
-  youtube = (context: any, x: any, { src }: any) => `
+  youtube = (context: any, next: Next, { src }: YouTube) => `
     <iframe
       id='${src}'
       width='640'
@@ -51,25 +49,27 @@ export class HtmlParser implements WriterImpl {
       style='display: block; margin-left: auto; margin-right: auto;'>
     </iframe>
   `
-  audio = (context: any, x: any,  { src }: any) => `<audio src="${src}"/>\n`;
-  table = (context: any, next: any, x: any) => `<table>${next()}</table>\n`;
-  tr = (context: any, next: any, x: any) => `<tr>'${next()}</tr>\n`;
-  th = (context: any, next: any, x: any) => `<th>${next()}</th>\n`;
-  td = (context: any, next: any, x: any) => `<td>${next()}</td>\n`;
-  ol = (context: any, next: any, x: any) => `<ol>${next()}</ol>\n`;
-  ul = (context: any, next: any, x: any) => `<ul>${next()}</ul>\n`;
-  li = (context: any, next: any, x: any) => `<li>${next()}</li>\n`;
-  math = (context: any, next: any, x: any) => `<div>${next()}</div>\n`;
-  mathLine = (context: any, next: any, x: any) => `${next()}\n`;
-  code = (context: any, next: any, { language, startingLineNumberr, showNumbers }: any) =>
+  audio = (context: WriterContext, next: Next, { src }: Audio) => `<audio src="${src}"/>\n`;
+  table = (context: WriterContext, next: Next, x: Table) => `<table>${next()}</table>\n`;
+  tr = (context: WriterContext, next: Next, x: TableRow) => `<tr>'${next()}</tr>\n`;
+  th = (context: WriterContext, next: Next, x: TableHeader) => `<th>${next()}</th>\n`;
+  td = (context: WriterContext, next: Next, x: TableData) => `<td>${next()}</td>\n`;
+  ol = (context: WriterContext, next: Next, x: OrderedList) => `<ol>${next()}</ol>\n`;
+  ul = (context: WriterContext, next: Next, x: UnorderedList) => `<ul>${next()}</ul>\n`;
+  li = (context: WriterContext, next: Next, x: ListItem) => `<li>${next()}</li>\n`;
+  math = (context: WriterContext, next: Next, x: Math) => `<div>${next()}</div>\n`;
+  mathLine = (context: WriterContext, next: Next, x: MathLine) => `${next()}\n`;
+  code = (context: WriterContext, next: Next,
+    { language, startingLineNumberr, showNumbers }: Code) =>
     `<pre><code>${next()}</pre></code>\n`
-  codeLine = (context: any, next: any, x: any) => `${next()}\n`;
-  blockquote = (context: any, next: any, x: any) => `<blockquote>${next()}</blockquote>\n`;
-  a = (context: any, next: any,  { href } : any) => `<link href="${this.escapeXml(href)}">${next()}</link>\n`;
-  definition = (context: any, next: any, x: any) => `<extra>${next()}</extra>\n`;
-  text = (context: any, textEntity: any) =>
+  codeLine = (context: WriterContext, next: Next, x: CodeLine) => `${next()}\n`;
+  blockquote = (context: WriterContext, next: Next, x: Blockquote) =>
+    `<blockquote>${next()}</blockquote>\n`
+  a = (context: WriterContext, next: Next,  { href } : Hyperlink) =>
+    `<link href="${this.escapeXml(href)}">${next()}</link>\n`
+  text = (context: WriterContext, textEntity: Text) =>
     this.wrapWithMarks(this.escapeXml(textEntity.text), textEntity)
 
-  unsupported = (context: any, { type }: ModelElement) =>
+  unsupported = (context: WriterContext, { type }: ModelElement) =>
     `<div class="unsupported-element">Element type "${type}" is not supported</div>\n`
 }
