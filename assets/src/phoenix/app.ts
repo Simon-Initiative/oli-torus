ce6d1c5e96583df90dfa21cd0272429b2d19633a
Eli Knebel <eliknebel@gmail.com>
2020-06-15 15:45:26 -0400

    fix build issue

--- assets/src/phoenix/app.ts
+++ assets/src/phoenix/app.ts
@@ -1,10 +1,3 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-// import css from '../../css/app.css';
-
-require('../../css/app.css');
-
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in 'webpack.config.js'.
