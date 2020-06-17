adf87b89a678306d1b6578de0efa5102fd2ea168
Raphael Gachuhi <rgachuhi@cmu.edu>
2020-06-17 13:06:24 -0400

    added objectives page header

--- lib/oli_web/live/objectives/objective_render.ex
+++ lib/oli_web/live/objectives/objective_render.ex
@@ -25,7 +25,7 @@ defmodule OliWeb.Objectives.ObjectiveRender do
     </div>
     <% end %>
     <% @mode == :add_sub_objective -> %>
-    <div class="pt-1 mb-1"><%= @objective_mapping.revision.title %></div>
+    <div class="pt-1 mb-1"><span class="mr-1"><i class="fas fa-crosshairs fa-lg"></i></span><%= @objective_mapping.revision.title %></div>
      <%= live_component @socket, ObjectiveForm, changeset: @changeset,
                  project: @project, title_value: @title_value, slug_value: "", parent_slug_value: @parent_slug_value,
                  form_id: @form_id, place_holder: @place_holder, phx_disable_with: @phx_disable_with, button_text: @button_text, method: @method %>
