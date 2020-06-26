c129520b3873452f9090c8ae8022a2c16b44babe
Darren Siegel <siegel.darren@gmail.com>
2020-06-26 10:41:34 -0400

    update docs for ManualModal, explaing difference

--- lib/oli_web/live/common/manual_modal.ex
+++ lib/oli_web/live/common/manual_modal.ex
@@ -1,12 +1,19 @@
 defmodule OliWeb.Common.ManualModal do
 
   @moduledoc """
-  A reusable LivewComponent for a Bootstrap modal, one that is manually controlled via JavaScript.
+  A reusable LivewComponent for a Bootstrap modal, one that is manually controlled via JavaScript, and that
+  must be added and removed from a parent LiveView programmatically when it needs to be shown.
+
+  This component should be used in place of `OliWeb.Common.Modal` when there is a need to programmatically
+  vary the content that the modal displays from invocation to invocation.  The only way to do this
+  is to completely remove the component from the DOM and remount it with the new content.  Doing this, however,
+  forces the launching of the modal to be done via JavaScript (notice the phx-hook that is present on
+  the root div of this modal)
 
   Minimal example usage specifying only the required properties:
 
   ```
-  <%= live_component @socket, Modal, title: "Confirm your request", modal_id: "my_unique_modal_id", ok_action: "confirm" do %>
+  <%= live_component @socket, ManualModal, title: "Confirm your request", modal_id: "my_unique_modal_id", ok_action: "confirm" do %>
     <p class="mb-4">Are you sure you want to do this?</p>
   <% end %>
   ```
