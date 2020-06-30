fc78482992fae2f88ccfa750b572d538b5466da7
Darren Siegel <siegel.darren@gmail.com>
2020-06-30 08:03:35 -0400

    remove unnecessary comment prefix

--- lib/oli/analytics/datashop/messages/tool.ex
+++ lib/oli/analytics/datashop/messages/tool.ex
@@ -1,21 +1,21 @@
 
 defmodule Oli.Analytics.Datashop.Messages.Tool do
   @moduledoc """
-  # <tool_message context_message_id="student-p1-MAJOR-ARC-BFD-2-0">
-  #   <meta>
-  #     <user_id anonFlag="true">student-p1</user_id>
-  #     <session_id>student-p1-2006-07-22</session_id>
-  #     <time>2006-07-22 12:59:47 EST</time>
-  #     <time_zone>EST</time_zone>
-  #   </meta>
-  #   <problem_name>MAJOR-ARC-BFD-2-0</problem_name>
-  #   <semantic_event transaction_id="student-p1-MAJOR-ARC-BFD-2-0-2006-07-22-12-55-00-003s" name="ATTEMPT"/>
-  #   <event_descriptor>
-  #     <selection>(ARC-BD-MEASURE QUESTION1 REASON)</selection>
-  #     <action>INPUT-CELL-VALUE</action>
-  #     <input>given</input>
-  #   </event_descriptor>
-  # </tool_message>
+    <tool_message context_message_id="student-p1-MAJOR-ARC-BFD-2-0">
+      <meta>
+        <user_id anonFlag="true">student-p1</user_id>
+        <session_id>student-p1-2006-07-22</session_id>
+        <time>2006-07-22 12:59:47 EST</time>
+        <time_zone>EST</time_zone>
+      </meta>
+      <problem_name>MAJOR-ARC-BFD-2-0</problem_name>
+      <semantic_event transaction_id="student-p1-MAJOR-ARC-BFD-2-0-2006-07-22-12-55-00-003s" name="ATTEMPT"/>
+      <event_descriptor>
+        <selection>(ARC-BD-MEASURE QUESTION1 REASON)</selection>
+        <action>INPUT-CELL-VALUE</action>
+        <input>given</input>
+      </event_descriptor>
+    </tool_message>
   """
   # defstruct [:context_message_id, :meta_element, :problem_name, :transaction_id, :name, :selection, :action, :input]
   import XmlBuilder
--- lib/oli/analytics/datashop/messages/tutor.ex
+++ lib/oli/analytics/datashop/messages/tutor.ex
@@ -1,32 +1,32 @@
 
 defmodule Oli.Analytics.Datashop.Messages.Tutor do
   @moduledoc """
-  # <tutor_message context_message_id="mary-smith-MAJOR-ARC-BFD-2-0">
-  #   <meta>
-  #     <user_id >mary-smith</user_id>
-  #     <session_id>mary-smith-2006-07-22</session_id>
-  #     <time>2006-07-22 12:58:50 EST</time>
-  #     <time_zone>EST</time_zone>
-  #   </meta>
-  #   <problem_name>MAJOR-ARC-BFD-2-0</problem_name>
-  #   <semantic_event transaction_id="mary-smith-MAJOR-ARC-BFD-2-0-2006-07-22-12-55-00-001s" name="HINT_MSG" subtype="HELP-TEXT"/>
-  #   <event_descriptor>
-  #     <selection>(ARC-BD-MEASURE QUESTION1 ANSWER)</selection>
-  #     <action>INPUT-CELL-VALUE</action>
-  #   </event_descriptor>
-  #   <action_evaluation current_hint_number="1" total_hints_available="1" classification="HELP-TEXT">HINT</action_evaluation>
-  #   <tutor_advice> Some useful information is highlighted in the problem statement. .</tutor_advice>
-  # <skill>
-  #   <name>WORKING-WITH-ANGLES-OUTSIDE-A-CIRCLE</name>
-  #   <category/>
-  #   <model_name>Manual Model</model_name>
-  # </skill>
-  # <skill>
-  #   <name>WORKING-WITH-ARCS-THAT-FORM-A-CIRCLE</name>
-  #   <category/>
-  #   <model_name>Manual Model</model_name>
-  # </skill>
-  # </tutor_message>
+    <tutor_message context_message_id="mary-smith-MAJOR-ARC-BFD-2-0">
+      <meta>
+       <user_id >mary-smith</user_id>
+        <session_id>mary-smith-2006-07-22</session_id>
+        <time>2006-07-22 12:58:50 EST</time>
+        <time_zone>EST</time_zone>
+      </meta>
+      <problem_name>MAJOR-ARC-BFD-2-0</problem_name>
+      <semantic_event transaction_id="mary-smith-MAJOR-ARC-BFD-2-0-2006-07-22-12-55-00-001s" name="HINT_MSG" subtype="HELP-TEXT"/>
+      <event_descriptor>
+        <selection>(ARC-BD-MEASURE QUESTION1 ANSWER)</selection>
+        <action>INPUT-CELL-VALUE</action>
+      </event_descriptor>
+      <action_evaluation current_hint_number="1" total_hints_available="1" classification="HELP-TEXT">HINT</action_evaluation>
+      <tutor_advice> Some useful information is highlighted in the problem statement. .</tutor_advice>
+    <skill>
+      <name>WORKING-WITH-ANGLES-OUTSIDE-A-CIRCLE</name>
+      <category/>
+      <model_name>Manual Model</model_name>
+    </skill>
+    <skill>
+      <name>WORKING-WITH-ARCS-THAT-FORM-A-CIRCLE</name>
+      <category/>
+      <model_name>Manual Model</model_name>
+    </skill>
+   </tutor_message>
   """
 
   import XmlBuilder
