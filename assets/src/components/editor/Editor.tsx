c09e9137055f130c75e79a656d57da78a91891c5
Darren Siegel <siegel.darren@gmail.com>
2020-07-13 10:14:14 -0400

    table fixes

--- assets/src/components/editor/Editor.tsx
+++ assets/src/components/editor/Editor.tsx
@@ -1,14 +1,14 @@
-import React, { useMemo, useCallback, KeyboardEvent } from 'react';
+import React, { useMemo, useCallback, useEffect, useState, KeyboardEvent } from 'react';
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react';
-import { createEditor, Node, NodeEntry, Range, Editor as SlateEditor, Transforms, Path } from 'slate';
-import { create, Mark, ModelElement, schema, Paragraph, SchemaConfig } from 'data/content/model';
+import { createEditor, Node, Range, Transforms, Path } from 'slate';
+import { create, Mark, ModelElement, schema, Paragraph } from 'data/content/model';
 import { editorFor, markFor } from './editors';
 import { ToolbarItem, CommandContext } from './interfaces';
 import { FixedToolbar, HoveringToolbar } from './Toolbars';
 import { onKeyDown as listOnKeyDown } from './editors/Lists';
 import { onKeyDown as quoteOnKeyDown } from './editors/Blockquote';
 import { getRootOfText } from './utils';
-
+import { installNormalizer } from './normalizer';
 import guid from 'utils/guid';
 
 export type EditorProps = {
@@ -61,6 +61,15 @@ export const Editor = React.memo((props: EditorProps) => {
   const commandContext = props.commandContext;
 
   const editor = useMemo(() => withReact(createEditor()), []);
+  const [installed, setInstalled] = useState(false);
+
+  // Install the custom normalizer, only once
+  useEffect(() => {
+    if (!installed) {
+      installNormalizer(editor);
+      setInstalled(true);
+    }
+  }, [installed]);
 
   // Override isVoid to incorporate our schema's opinion on which
   // elements are void
@@ -81,63 +90,6 @@ export const Editor = React.memo((props: EditorProps) => {
     } catch (e) {
       return false;
     }
-  };
-
-  const { normalizeNode } = editor;
-  editor.normalizeNode = (entry: NodeEntry<Node>) => {
-
-    if ((editor as any).suspendNormalization) {
-      normalizeNode(entry);
-      return;
-    }
-
-    try {
-      const [node, path] = entry;
-
-      // Ensure that we always have a paragraph as the last node in
-      // the document, otherwise it can be impossible for a user
-      // to position their cursor after the last node
-      if (SlateEditor.isEditor(node)) {
-        const last = node.children[node.children.length - 1];
-
-        if (last.type !== 'p') {
-          Transforms.insertNodes(editor, create<Paragraph>(
-            { type: 'p', children: [{ text: '' }], id: guid() }),
-            { mode: 'highest', at: SlateEditor.end(editor, []) });
-        }
-        return; // Return here is necessary to enable multi-pass normalization
-
-      }
-
-      // Check this node's parent constraints
-      if (SlateEditor.isBlock(editor, node)) {
-        const [parent] = SlateEditor.parent(editor, path);
-        if (!SlateEditor.isEditor(parent)) {
-          const config : SchemaConfig = (schema as any)[parent.type as string];
-          if (!(config.validChildren as any)[node.type as string]) {
-            Transforms.removeNodes(editor, { at: path });
-            return; // Return here is necessary to enable multi-pass normalization
-          }
-
-        }
-      }
-
-      // Check the top-level constraints
-      if (SlateEditor.isBlock(editor, node) && !(schema as any)[node.type as string].isTopLevel) {
-        const [parent] = SlateEditor.parent(editor, path);
-        if (SlateEditor.isEditor(parent)) {
-          Transforms.removeNodes(editor, { at: path });
-          return; // Return here is necessary to enable multi-pass normalization
-        }
-      }
-
-    } catch (e) {
-      // tslint:disable-next-line
-      console.log(e);
-    }
-
-    normalizeNode(entry);
-
   };
 
   const renderElement = useCallback((props) => {
--- assets/src/components/editor/editors/Table.tsx
+++ assets/src/components/editor/editors/Table.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { ReactEditor, useFocused, useSelected, useSlate } from 'slate-react';
-import { Transforms, Editor, Path } from 'slate';
+import { Transforms, Node, Editor, Path, Editor as SlateEditor } from 'slate';
 import { updateModel, getEditMode } from './utils';
 import * as ContentModel from 'data/content/model';
 import { Command, CommandDesc } from '../interfaces';
@@ -19,6 +19,54 @@ const tr = (children: ContentModel.TableData[]) => ContentModel.create<ContentMo
 
 const table = (children: ContentModel.TableRow[]) => ContentModel.create<ContentModel.Table>(
   { type: 'table', children, id: guid() });
+
+export const normalize = (editor: ReactEditor, node: Node, path: Path) => {
+
+  if (node.type === 'table') {
+    // Ensure that the number of cells in each row is the same
+
+    // First get max count of cells in any row, and see if any rows
+    // have a different amount of cells.
+    let max = -1;
+    let anyDiffer = false;
+    (node.children as any).forEach((row: Node) => {
+      const children = row.children as any;
+      const count = children.length;
+
+      if (max === -1) {
+        max = count;
+      } else if (count !== max) {
+        anyDiffer = true;
+
+        if (count > max) {
+          max = count;
+        }
+      }
+    });
+
+
+    if (anyDiffer) {
+
+      (node.children as any).forEach((row: Node, index: number) => {
+        const children = row.children as any;
+        let count = children.length;
+
+        // Get a path to the first td element in this row
+        const thisPath = [...path, index, 0];
+
+        // Add as many empty td elements to bring this row back up to
+        // the max td count
+        while (count < max) {
+          Transforms.insertNodes(editor, td(''), { at: thisPath });
+          count = count + 1;
+        }
+
+      });
+    }
+
+  }
+
+};
 
 
 // The UI command for creating tables
@@ -247,10 +295,6 @@ type TableSettingsProps = {
   commandContext: CommandContext,
   editMode: boolean,
 };
-
-const toLink = (src: string) =>
-  'https://www.youtube.com/embed/' + (src === '' ? 'zHIIzcWqsP0' : src);
-
 
 const TableSettings = (props: TableSettingsProps) => {
 
