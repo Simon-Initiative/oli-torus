5634bc591f092d88bb2e7be66be606c25c8d409f
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2020-07-27 15:01:16 -0400

    Remove console logs

--- lib/oli_web/live/objectives/objectives.ex
+++ lib/oli_web/live/objectives/objectives.ex
@@ -257,15 +257,13 @@ defmodule OliWeb.Objectives.Objectives do
     container_slug = Map.get(objective_params, "parent_slug")
 
     socket = case ObjectiveEditor.add_new(with_atom_keys, socket.assigns.author, socket.assigns.project, container_slug) do
-      {:ok, _} ->
-        IO.inspect("Added new objective")
-        assign(socket, :changeset, Resources.change_revision(%Revision{}))
+      {:ok, _} -> socket
       {:error, %Ecto.Changeset{} = _changeset} ->
         socket
         |> put_flash(:error, "Could not create objective")
     end
 
-    {:noreply, IO.inspect(assign(socket, :edit, :none), label: "Socket")}
+    {:noreply, assign(socket, :edit, :none)}
   end
 
 
