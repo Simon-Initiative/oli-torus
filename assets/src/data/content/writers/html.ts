3ed93db596f1ce85abcb4dc525751537645e7c00
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2020-07-30 15:42:37 -0400

    Fix typo with html client-side rendering

--- assets/src/data/content/writers/html.ts
+++ assets/src/data/content/writers/html.ts
@@ -52,7 +52,7 @@ export class HtmlParser implements WriterImpl {
   style="display: block; margin-left: auto; margin-right: auto;"></iframe>`
   audio = (context: WriterContext, next: Next, { src }: Audio) => `<audio src="${src}"/>\n`;
   table = (context: WriterContext, next: Next, x: Table) => `<table>${next()}</table>\n`;
-  tr = (context: WriterContext, next: Next, x: TableRow) => `<tr>'${next()}</tr>\n`;
+  tr = (context: WriterContext, next: Next, x: TableRow) => `<tr>${next()}</tr>\n`;
   th = (context: WriterContext, next: Next, x: TableHeader) => `<th>${next()}</th>\n`;
   td = (context: WriterContext, next: Next, x: TableData) => `<td>${next()}</td>\n`;
   ol = (context: WriterContext, next: Next, x: OrderedList) => `<ol>${next()}</ol>\n`;
