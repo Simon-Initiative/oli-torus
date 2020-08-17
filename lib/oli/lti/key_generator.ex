8a659a54a877db0dc95c905b723e676d5e05cb5e
Eli Knebel <eliknebel@gmail.com>
2020-08-17 15:55:32 -0400

    add key generator

---
+++ lib/oli/lti/key_generator.ex
@@ -0,0 +1,14 @@
+defmodule Oli.Lti do
+
+  @doc """
+  Generates RSA public and private key pair to validate between Tool and the Platform
+
+  ## Examples
+    iex> key_generator()
+    %{ public_key: "...", private_key: "..." }
+
+  """
+  def key_generator() do
+
+  end
+end
