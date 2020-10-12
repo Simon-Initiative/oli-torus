18fe2f9ef9bc20a9ef41177b6d8e2c92ccfbd658
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2020-10-12 16:23:44 -0400

    make new breadcrumb liveview, rendered from project_header

---
+++ lib/oli_web/live/breadcrumb/Breadcrumbs.ex
@@ -0,0 +1,15 @@
+defmodule OliWeb.Breadcrumb.Breadcrumbs do
+
+  use Phoenix.LiveView
+
+  def mount(params, session, socket) do
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    """
+  end
+
+end
