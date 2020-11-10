e13a75322a30c401b50e8b7c8f0bedcc511c32bd
Eli Knebel <eliknebel@gmail.com>
2020-11-10 14:33:52 -0500

    separate PORT and HTTP_PORT config completely

--- config/dev.exs
+++ config/dev.exs
@@ -21,7 +21,7 @@ config :oli, Oli.Mailer,
 config :oli, OliWeb.Pow.Mailer,
   adapter: Bamboo.LocalAdapter
 
-force_ssl = case System.get_env("FORCE_SSL", "true") do
+force_ssl = case System.get_env("FORCE_SSL", "false") do
   "true" -> [rewrite_on: [:x_forwarded_proto]]
   _ -> false
 end
@@ -34,7 +34,7 @@ end
 # with webpack to recompile .js and .css sources.
 config :oli, OliWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("HTTP_PORT", System.get_env("PORT", "4000")))
+    port: String.to_integer(System.get_env("HTTP_PORT", "80"))
   ],
   url: [
     scheme: System.get_env("SCHEME", "https"),
