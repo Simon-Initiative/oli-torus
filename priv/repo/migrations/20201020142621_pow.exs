8dbaaa43805fcf64ea249030908d6849d95e2ce0
Eli Knebel <eliknebel@gmail.com>
2020-11-13 13:06:02 -0500

    remove user identity converstions

--- priv/repo/migrations/20201020142621_pow.exs
+++ priv/repo/migrations/20201020142621_pow.exs
@@ -15,8 +15,6 @@ defmodule Oli.Repo.Migrations.Pow do
 
     create unique_index(:user_identities, [:uid, :provider])
 
-    execute(&convert_author_providers_to_user_identities_up/0)
-
     # modify authors table for pow
     rename table(:authors), :first_name, to: :given_name
     rename table(:authors), :last_name, to: :family_name
@@ -58,45 +56,9 @@ defmodule Oli.Repo.Migrations.Pow do
     rename table(:authors), :given_name, to: :first_name
     rename table(:authors), :family_name, to: :last_name
 
-    execute(&convert_author_providers_to_user_identities_down/0)
-
     drop unique_index(:user_identities, [:uid, :provider])
 
     drop table(:user_identities)
   end
 
-  defp convert_author_providers_to_user_identities_up() do
-    flush()
-
-    authors = Oli.Repo.all(
-      from a in "authors",
-        where: a.provider != "identity",
-        select: %{id: a.id, provider: a.provider}
-    )
-
-    now = Timex.now()
-    user_identities = Enum.map(authors, fn author ->
-      %{
-        provider: author.provider,
-        user_id: author.id,
-        uid: "pending-#{UUID.uuid4()}",
-        inserted_at: now,
-        updated_at: now
-      }
-    end)
-
-    Oli.Repo.insert_all("user_identities", user_identities)
-  end
-
-  defp convert_author_providers_to_user_identities_down() do
-    user_identities = Oli.Repo.all(
-      from i in "user_identities",
-        select: %{user_id: i.user_id, provider: i.provider}
-    )
-
-    Enum.each(user_identities, fn i ->
-      from(a in "authors", where: a.id == ^i.user_id, select: %{id: a.id})
-      |> Oli.Repo.update_all(set: [provider: i.provider])
-    end)
-  end
 end
