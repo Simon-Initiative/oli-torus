79371f80dbbe569071783b0df7d1cf1e0791a690
Darren Siegel <siegel.darren@gmail.com>
2020-12-02 10:09:39 -0500

    remove unused alias

--- lib/oli_web/api_spec.ex
+++ lib/oli_web/api_spec.ex
@@ -1,5 +1,5 @@
 defmodule OliWeb.ApiSpec do
-  alias OpenApiSpex.{Components, Info, OpenApi, Paths, Server}
+  alias OpenApiSpex.{Info, OpenApi, Paths, Server}
   alias OliWeb.{Endpoint, Router}
   @behaviour OpenApi
 
