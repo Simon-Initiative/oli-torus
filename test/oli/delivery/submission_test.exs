5f406aa42406f03b2325f81fde68c84830be8584
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2021-01-22 10:41:29 -0500

    more tests

--- test/oli/delivery/submission_test.exs
+++ test/oli/delivery/submission_test.exs
@@ -29,8 +29,9 @@ defmodule Oli.Delivery.AttemptsSubmissionTest do
       |> Seeder.add_objective("objective one", :o1)
       |> Seeder.add_activity(%{title: "one", max_attempts: 2, content: content}, :activity)
       |> Seeder.add_user(%{}, :user1)
+      |> Seeder.add_user(%{}, :user2)
 
-      attrs = %{
+      Seeder.add_page(map, %{
         title: "page1",
         content: %{
           "model" => [
@@ -38,14 +39,82 @@ defmodule Oli.Delivery.AttemptsSubmissionTest do
           ]
         },
         objectives: %{"attached" => [Map.get(map, :o1).resource.id]}
-      }
-
-      Seeder.add_page(map, attrs, :page)
+      }, :page)
+      |> Seeder.add_page(%{
+        title: "page2",
+        content: %{
+          "model" => [
+            %{"type" => "activity-reference", "activity_id" => Map.get(map, :activity).revision.resource_id}
+          ]
+        },
+        objectives: %{"attached" => [Map.get(map, :o1).resource.id]},
+        graded: true,
+      }, :graded_page)
       |> Seeder.create_resource_attempt(%{attempt_number: 1}, :user1, :page, :attempt1)
       |> Seeder.create_activity_attempt(%{attempt_number: 1, transformed_model: content}, :activity, :attempt1, :activity_attempt1)
       |> Seeder.create_part_attempt(%{attempt_number: 1}, %Part{id: "1", responses: [], hints: []}, :activity_attempt1, :part1_attempt1)
 
+      |> Seeder.create_resource_attempt(%{attempt_number: 1}, :user1, :graded_page, :attempt2)
+      |> Seeder.create_activity_attempt(%{attempt_number: 1, transformed_model: content}, :activity, :attempt2, :activity_attempt2)
+      |> Seeder.create_part_attempt(%{attempt_number: 1}, %Part{id: "1", responses: [], hints: []}, :activity_attempt2, :part1_attempt2)
+
+      |> Seeder.create_resource_attempt(%{attempt_number: 1}, :user2, :graded_page, :attempt3)
+      |> Seeder.create_activity_attempt(%{attempt_number: 1, transformed_model: content}, :activity, :attempt3, :activity_attempt3)
+      |> Seeder.create_part_attempt(%{attempt_number: 1}, %Part{id: "1", responses: [], hints: []}, :activity_attempt3, :part1_attempt3)
+
     end
+
+    test "get_latest_resource_attempt gives an unevaluated attempt with 1 user", %{ page: %{ resource: resource }, user1: user1,
+    section: section, attempt1: resource_attempt1 } do
+      resource_attempt = Attempts.get_latest_resource_attempt(resource.id, section.context_id, user1.id)
+
+      assert resource_attempt == resource_attempt1
+      assert is_nil(resource_attempt.date_evaluated)
+      assert is_nil(resource_attempt.score)
+    end
+
+    test "get_latest_resource_attempt gives two different attempts with 2 users", %{ page: %{ resource: resource }, user1: user1,
+    user2: user2, section: section, attempt1: resource_attempt1, attempt2: resource_attempt2 } do
+      resource_attempt_user1 = Attempts.get_latest_resource_attempt(resource.id, section.context_id, user1.id)
+      resource_attempt_user2 = Attempts.get_latest_resource_attempt(resource.id, section.context_id, user2.id)
+
+      assert resource_attempt1 == resource_attempt_user1
+      assert resource_attempt2 == resource_attempt_user2
+
+      assert resource_attempt1 != resource_attempt2
+    end
+
+    test "determine_resource_attempt_state works for graded pages with 1 user", %{ graded_page: %{ revision: revision },
+      user1: user1, section: section, part1_attempt2: part_attempt, activity_attempt2: activity_attempt,
+      attempt2: resource_attempt1,
+    } do
+      activity_provider = &Oli.Delivery.ActivityProvider.provide/2
+
+      {:ok, {:in_progress, _resource_attempt}} = Attempts.determine_resource_attempt_state(
+        revision, section.context_id, user1.id, activity_provider)
+
+      part_inputs = [%{attempt_guid: part_attempt.attempt_guid, input: %StudentInput{input: "a"}}]
+
+      {:ok, _evals} = Attempts.submit_part_evaluations(
+        section.context_id, activity_attempt.attempt_guid, part_inputs)
+
+      IO.inspect(Attempts.submit_graded_page(section.context_id, resource_attempt1.attempt_guid))
+
+      {:ok, {:not_started, _resource_attempt}} = Attempts.determine_resource_attempt_state(
+        revision, section.context_id, user1.id, activity_provider)
+
+    end
+
+    test "determine_resource_attempt_state works for graded pages with 2 users" do
+
+    end
+
+
+
+    # IO.inspect(resource_attempt)
+
+    # get_resource_state(resource_revision, context_id, user_id, activity_provider) do
+
 
     test "processing a submission", %{ activity: %{ revision: activity_revision }, page: %{ revision: page_revision }, user1: user,
       part1_attempt1: part_attempt, section: section, activity_attempt1: activity_attempt} do
