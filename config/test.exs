18e8bd73a749469f703dd36ff194937db882c918
Eli Knebel <eliknebel@cmu.edu>
2021-03-02 09:33:54 -0500

    [BUG FIX] Replace HTTPoison with Oli.HTTP (#804)
    * replace HTTPoison with Oli.HTTP
    
    * add more lti launch unit tests

--- config/test.exs
+++ config/test.exs
@@ -33,6 +33,9 @@ config :oli, Oli.Mailer,
 config :oli, OliWeb.Pow.Mailer,
   adapter: Bamboo.TestAdapter
 
+config :lti_1p3,
+  http_client: Oli.Test.MockHTTP
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :oli, OliWeb.Endpoint,
