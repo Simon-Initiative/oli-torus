b47bf231696cc259be0b50a332aaabe0e5cdd4ca
Darren Siegel <siegel.darren@gmail.com>
2021-03-24 14:58:35 -0400

    add multiple projects, simple list test

--- test/oli/versioning/revision_tree/tree_test.exs
+++ test/oli/versioning/revision_tree/tree_test.exs
@@ -21,10 +21,38 @@ defmodule Oli.Versioning.RevisionTree.TreeTest do
       assert Enum.to_list(tree_nodes) |> length() == 5
 
       assert Map.get(tree_nodes, 1).revision.id == 1
-      assert  [%{revision: %{id: 2}}] = Map.get(tree_nodes, 1).children
+      assert [%{revision: %{id: 2}}] = Map.get(tree_nodes, 1).children
 
       assert Map.get(tree_nodes, 5).revision.id == 5
-      assert  [] = Map.get(tree_nodes, 5).children
+      assert [] = Map.get(tree_nodes, 5).children
+
+    end
+
+    test "it works on a simple linked list, multiple projects" do
+
+      # Constructs the following:
+      #
+      # 1-2-3-4-5
+      # ^       ^
+      # |       |
+      # proj2   proj1
+      #
+      # Simulating that proj2 forked from the initial revision, never edited that revision,
+      # and proj1 have several other revisions added afterwards
+
+      revisions = [{4, 5}, {1, 2}, {2, 3}, {3, 4}, {nil,1}] |> as_revisions
+      project1 = %{id: 1}
+      project2 = %{id: 2}
+
+      tree_nodes = Tree.build(revisions, [hd(revisions), List.last(revisions)], [project1, project2])
+
+      assert Enum.to_list(tree_nodes) |> length() == 5
+
+      assert Map.get(tree_nodes, 1).revision.id == 1
+      assert [%{revision: %{id: 2}}] = Map.get(tree_nodes, 1).children
+
+      assert Map.get(tree_nodes, 5).revision.id == 5
+      assert [] = Map.get(tree_nodes, 5).children
 
     end
 
@@ -44,14 +72,20 @@ defmodule Oli.Versioning.RevisionTree.TreeTest do
 
       assert Enum.to_list(tree_nodes) |> length() == 8
 
+      # Verify the root node
       assert Map.get(tree_nodes, 1).revision.id == 1
-      assert  [%{revision: %{id: 2}}] = Map.get(tree_nodes, 1).children
+      assert [%{revision: %{id: 2}}] = Map.get(tree_nodes, 1).children
 
+      # Verify the leaf nodes
       assert Map.get(tree_nodes, 5).revision.id == 5
-      assert  [] = Map.get(tree_nodes, 5).children
+      assert [] = Map.get(tree_nodes, 5).children
 
+      assert Map.get(tree_nodes, 8).revision.id == 8
+      assert [] = Map.get(tree_nodes, 8).children
+
+      # Verify the forked node
       node3 = Map.get(tree_nodes, 3)
-      assert length(node3.children) == 2
+      assert [%{revision: %{id: 4}}, %{revision: %{id: 6}}] = node3.children
 
     end
 
