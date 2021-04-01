89f5c9fbb73b95c8b93e4db25cdbdebccf5c8167
Eli Knebel <eliknebel@cmu.edu>
2021-04-01 09:19:36 -0400

    change delivery url to courses

--- assets/src/components/editing/models/link/utils.ts
+++ assets/src/components/editing/models/link/utils.ts
@@ -10,7 +10,7 @@ interface Uninitialized {
 
 export type LinkablePages = Uninitialized | Waiting | Persistence.PagesReceived;
 
-export const internalLinkPrefix = '/sections/link';
+export const internalLinkPrefix = '/courses/link';
 
 export const isInternalLink = (href: string) => href.startsWith(internalLinkPrefix);
 
