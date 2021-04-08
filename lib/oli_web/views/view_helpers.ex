332557de64b0309d1667961a2bf90f03221c636f
Eli Knebel <eliknebel@cmu.edu>
2021-04-08 16:38:36 -0400

    open link in new tab add external_link helper

--- lib/oli_web/views/view_helpers.ex
+++ lib/oli_web/views/view_helpers.ex
@@ -1,4 +1,6 @@
 defmodule OliWeb.ViewHelpers do
+  use Phoenix.HTML
+
   def is_admin?(%{:assigns => assigns}) do
     admin_role_id = Oli.Accounts.SystemRole.role_id().admin
     assigns.current_author.system_role_id == admin_role_id
@@ -6,5 +8,14 @@ defmodule OliWeb.ViewHelpers do
 
   def preview_mode(%{assigns: assigns} = _conn) do
     Map.get(assigns, :preview_mode, false)
+  end
+
+  @doc """
+  Renders a link with text and an external icon which opens in a new tab
+  """
+  def external_link(text, opts \\ []) do
+    link Keyword.merge([target: "_blank"], opts) do
+      [text, content_tag("i", "", class: "las la-external-link-alt ml-1")]
+    end
   end
 end
