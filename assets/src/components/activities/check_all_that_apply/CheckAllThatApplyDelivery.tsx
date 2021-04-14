e90ee499150881c17dcfc73a2f3ce5aa0a5eceb3
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2021-04-14 10:40:48 -0400

    fix merge issue

--- assets/src/components/activities/check_all_that_apply/CheckAllThatApplyDelivery.tsx
+++ assets/src/components/activities/check_all_that_apply/CheckAllThatApplyDelivery.tsx
@@ -93,8 +93,12 @@ const CheckAllThatApply = (props: DeliveryElementProps<CheckAllThatApplyModelSch
         response: { input: selectionToInput(undefined) },
       }])
       .then((response: EvaluationResponse) => {
-        if (response.evaluations.length > 0) {
-          const { score, out_of, feedback, error } = response.evaluations[0];
+        if (response.actions.length > 0) {
+
+          const action: ActivityTypes.FeedbackAction
+            = response.actions[0] as ActivityTypes.FeedbackAction;
+
+          const { score, out_of, feedback, error } = action;
           const parts = [Object.assign({}, attemptState.parts[0], { feedback, error })];
           const updated = Object.assign({}, attemptState, { score, outOf: out_of, parts });
           setAttemptState(updated);
