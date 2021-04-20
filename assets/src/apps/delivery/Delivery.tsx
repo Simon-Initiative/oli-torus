146aae4136d46b77e8dcee955cc145897626ed2a
Ben Sparks <bsparks@unicon.net>
2021-04-20 09:51:28 -0700

    cleanup unused code

--- assets/src/apps/delivery/Delivery.tsx
+++ assets/src/apps/delivery/Delivery.tsx
@@ -76,40 +76,6 @@ export const Delivery: React.FunctionComponent<DeliveryProps> = (
           <AdaptivePageView />
         </div>
       </div>
-      {/* <div>
-        <h3>Advanced Delivery Mode</h3>
-
-        <table className="table table-sm">
-          <thead>
-            <tr>
-              <th>Attribute</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Resource Id:</td>
-              <td>{props.resourceId}</td>
-            </tr>
-            <tr>
-              <td>Resource Slug:</td>
-              <td>{props.pageSlug}</td>
-            </tr>
-            <tr>
-              <td>User Id:</td>
-              <td>{props.userId}</td>
-            </tr>
-            <tr>
-              <td>Section Slug:</td>
-              <td>{props.sectionSlug}</td>
-            </tr>
-            <tr>
-              <td>Content:</td>
-              <td>{JSON.stringify(props.content)}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div> */}
     </Provider>
   );
 };
