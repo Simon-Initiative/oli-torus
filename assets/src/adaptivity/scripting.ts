268d103234109478d9752c90f33f2d3c28f1d454
Ben Sparks <bsparks@unicon.net>
2021-05-20 16:45:31 -0700

    init state support WIP

--- assets/src/adaptivity/scripting.ts
+++ assets/src/adaptivity/scripting.ts
@@ -88,3 +88,88 @@ export const getValues = (identifiers: string[], env?: Environment) => {
   const { result } = evalScript(script, env);
   return result;
 };
+
+export interface ApplyStateOperation {
+  id?: string;
+  target: string;
+  operator: string;
+  value: any;
+  type?: CapiVariableTypes;
+  targetType?: CapiVariableTypes;
+}
+
+export const applyState = (operation: ApplyStateOperation, env?: Environment): any => {
+  const targetKey = operation.target;
+  // FIXME: (converter fix model) init and mutate have 2 diff names for type
+  /* const opType = operation.type || operation.targetType || CapiVariableTypes.STRING; */
+  let script = `let {${targetKey}} `;
+  switch (operation.operator) {
+    case 'adding':
+    case '+':
+      script += `= {${targetKey}} + ${operation.value};`;
+      break;
+    case 'subtracting':
+    case '-':
+      script += `= {${targetKey}} - ${operation.value};`;
+      break;
+    case 'bind to':
+      // NOTE: once a value is bound, you can *never* set it other than through binding????
+      // at least right now otherwise it will just overwrite the binding
+      script += `&= {${operation.value}};`;
+      break;
+    case 'setting to':
+    case '=':
+      {
+        let newValue = operation.value;
+        if (typeof newValue === 'string') {
+          // SUPPORTED:
+          // {q:1541204522672:818|stage.FillInTheBlanks.Input 1.Value}
+          // {stage.foo.whatever}
+          // {stage.foo.bar}{session.blah} (multiples)
+          // {stage.vft.Score}*70 + {stage.vft.Map complete}*100 - {session.tutorialScore}
+          // round({stage.foo.something})
+          // PMP-705: {e:1617736969329:1|stage.HeatSourceSorting.Content Slots.Slot 1},{e:1617736969329:1|stage.HeatSourceSorting.Content Slots.Slot 2}
+          // PMP-705: [{e:1617736969329:1|stage.HeatSourceSorting.Content Slots.Slot 1},{e:1617736969329:1|stage.HeatSourceSorting.Content Slots.Slot 2}]
+          // NOT SUPPORTED:
+          // @latex7@latex
+          if (
+            newValue[0] === '[' &&
+            newValue[1] === '{' &&
+            (newValue.includes('},{') || newValue.includes('}, {'))
+          ) {
+            newValue = newValue.replace(/[[\]]+/g, '');
+          }
+          if (newValue[0] === '{' && newValue[1] !== '"') {
+            // BS: this assumes that if the string starts with {" that it's intended
+            // as a JSON string, not a script; possibly check opType to see if
+            // the intended type is a string or not to allow for creating hashes like {"foo": 3}
+            // that are NOT meant to be JSON strings
+            if (newValue.slice(-1) === '}' && newValue?.indexOf('\n') !== -1) {
+              newValue = JSON.stringify(newValue.replace(/"/g, '"').replace(/\n/g, ''));
+            }
+            // PMP-705 (DS): initState contains an array of variables set as a string
+            if (newValue.includes('},{') || newValue.includes('}, {')) {
+              newValue = JSON.stringify(newValue.split(',')).replace(/"/g, '');
+            }
+            script += `= ${newValue};`;
+          } else {
+            script += `= "${newValue.replace(/"/g, '\\"')}";`;
+          }
+        } else {
+          script += Array.isArray(newValue) ? `= ${JSON.stringify(newValue)}` : `= ${newValue}`;
+        }
+      }
+      break;
+    default:
+      console.warn(`Unknown applyState operator ${operation.operator}!`);
+      break;
+  }
+  const result = evalScript(script, env);
+  // console.log('APPLY STATE RESULTS: ', { script, result });
+  return result;
+};
+
+export const bulkApplyState = (operations: ApplyStateOperation[], env?: Environment): any[] => {
+  // need to apply one at a time, TODO: break on error?
+  return operations.map((op) => applyState(op, env));
+};
