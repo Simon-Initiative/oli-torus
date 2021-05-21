bdf2b1ea43f4192294abbf1c9dc690786f559b06
Darren Siegel <siegel.darren@gmail.com>
2021-05-21 19:26:51 -0400

    update popover

--- assets/src/components/editing/models/link/Editor.tsx
+++ assets/src/components/editing/models/link/Editor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Popover from 'react-tiny-popover';
+import { Popover } from 'react-tiny-popover';
 import * as ContentModel from 'data/content/model';
 import { updateModel } from 'components/editing/models/utils';
 import { EditorProps } from 'components/editing/models/interfaces';
@@ -8,7 +8,7 @@ import { EditLink } from 'components/editing/models/link/EditLink';
 import { DisplayLink } from 'components/editing/models/link/DisplayLink';
 import * as Persistence from 'data/persistence/resource';
 // eslint-disable-next-line
-export interface Props extends EditorProps<ContentModel.Hyperlink> { }
+export interface Props extends EditorProps<ContentModel.Hyperlink> {}
 
 export const LinkEditor = (props: Props) => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
@@ -35,33 +35,41 @@ export const LinkEditor = (props: Props) => {
       onClickOutside={() => setIsPopoverOpen(false)}
       isOpen={isPopoverOpen}
       padding={25}
-      position={['bottom', 'top', 'left', 'right']}
-      transitionDuration={0}
       content={() => {
         if (editLink && selectedPage && pages.type === 'success') {
-          return <EditLink
+          return (
+            <EditLink
+              href={model.href}
+              onEdit={onEdit}
+              pages={pages}
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          );
+        }
+        return (
+          <DisplayLink
+            setEditLink={setEditLink}
+            commandContext={props.commandContext}
             href={model.href}
-            onEdit={onEdit}
+            setPages={setPages}
             pages={pages}
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
-          />;
-        }
-        return <DisplayLink
-          setEditLink={setEditLink}
-          commandContext={props.commandContext}
-          href={model.href}
-          setPages={setPages}
-          pages={pages}
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-        />;
-      }}>
-      <a id={props.model.id} href="#"
-        className="inline-link" {...attributes} onClick={() => {
+          />
+        );
+      }}
+    >
+      <a
+        id={props.model.id}
+        href="#"
+        className="inline-link"
+        {...attributes}
+        onClick={() => {
           setIsPopoverOpen(true);
           setEditLink(false);
-        }}>
+        }}
+      >
         {children}
       </a>
     </Popover>
