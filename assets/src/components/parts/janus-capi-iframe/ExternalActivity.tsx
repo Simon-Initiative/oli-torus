126f479d072d9833638589ba15f85dc112278d14
Devesh Tiwari <dtiwari@ats360.com>
2021-06-04 12:32:32 +0530

    -- hide console log

--- assets/src/components/parts/janus-capi-iframe/ExternalActivity.tsx
+++ assets/src/components/parts/janus-capi-iframe/ExternalActivity.tsx
@@ -369,7 +369,7 @@ const ExternalActivity: React.FC<any> = (props) => {
   };
 
   const writeCapiLog = (msg: any, ...rest: any[]) => {
-    const boolWriteLog = false;
+    const boolWriteLog = true;
     let colorStyle = 'background: #222; color: #bada55';
     const [logStyle] = rest;
     const args = rest;
