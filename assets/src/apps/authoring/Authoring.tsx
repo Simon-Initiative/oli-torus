1425f7cd7671e349b30c98ec9fe93a902ff1dedf
David Suriano <dsuriano@unicon.net>
2021-06-09 17:34:09 -0400

    finished up basic 3 col grid structure w/ app launch

--- assets/src/apps/authoring/Authoring.tsx
+++ assets/src/apps/authoring/Authoring.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import HeaderNav from './HeaderNav';
+import { SidePanel } from './SidePanel';
 
 export interface AuthoringProps {
   isAdmin: boolean;
@@ -11,6 +12,8 @@ export interface AuthoringProps {
 export const Authoring: React.FC<AuthoringProps> = (props: AuthoringProps) => {
   const url = `/project/${props.projectSlug}/preview/${props.revisionSlug}`;
   const windowName = `preview-${props.projectSlug}`;
+  const authoringContainer = document.getElementById('advanced-authoring');
+  const [appState, setAppState] = useState<any>({ isVisible: false });
   const [panelState, setPanelState] = useState({ left: true, right: true, top: true });
 
   const PreviewButton = () => (
@@ -19,59 +22,96 @@ export const Authoring: React.FC<AuthoringProps> = (props: AuthoringProps) => {
     </a>
   );
 
-  // Prevents double scroll bars
   useEffect(() => {
-    document.body.classList.add('overflow-hidden');
+    if (appState.isVisible) {
+      document.body.classList.add('overflow-hidden'); // prevents double scroll bars
+      authoringContainer?.classList.remove('d-none');
+      setTimeout(() => {
+        authoringContainer?.classList.add('startup');
+      }, 50);
+    }
+    if (!appState.isVisible) {
+      document.body.classList.remove('overflow-hidden');
+      authoringContainer?.classList.remove('startup');
+      setTimeout(() => {
+        authoringContainer?.classList.add('d-none');
+      }, 350);
+    }
     return () => {
       document.body.classList.remove('overflow-hidden');
     };
-  }, []);
+  }, [appState.isVisible]);
 
   return (
-    <div className="advanced-authoring">
-      <HeaderNav content={props.content} isVisible={panelState.top} />
-      <section className={`aa-panel left-panel${panelState.left ? ' open' : ''}`}>
-        <p>left sidebar</p>
-      </section>
-      <section className="aa-stage">
-        <div className="aa-stage-inner">
-          <PreviewButton />
-          <h1>Main Content Stage</h1>
-          <div className="btn-group" role="group">
-            <button
-              onClick={() => setPanelState({ ...panelState, left: !panelState.left })}
-              type="button"
-              className="btn btn-secondary"
-            >
-              toggle left
-            </button>
-            <button
-              onClick={() => setPanelState({ ...panelState, right: !panelState.right })}
-              type="button"
-              className="btn btn-secondary"
-            >
-              toggle right
-            </button>
-            <button
-              onClick={() =>
-                setPanelState({
-                  right: !panelState.right,
-                  left: !panelState.left,
-                  top: !panelState.top,
-                })
-              }
-              type="button"
-              className="btn btn-secondary"
-            >
-              toggle all
-            </button>
+    <>
+      {!appState.isVisible && (
+        <button
+          onClick={() => setAppState({ ...appState, isVisible: true })}
+          type="button"
+          className="btn btn-primary"
+        >
+          🚀 Launch
+        </button>
+      )}
+      <div id="advanced-authoring" className={`advanced-authoring d-none`}>
+        <HeaderNav content={props.content} isVisible={panelState.top} />
+        <SidePanel
+          position="left"
+          panelState={panelState}
+          setPanelState={() => setPanelState({ ...panelState, left: !panelState.left })}
+        >
+          I am the left side panel.
+        </SidePanel>
+        <section className="aa-stage">
+          <div className="aa-stage-inner">
+            <PreviewButton />
+            <h1>Main Content Stage</h1>
+            <div className="btn-group" role="group">
+              <button
+                onClick={() =>
+                  setPanelState({
+                    right: false,
+                    left: false,
+                    top: false,
+                  })
+                }
+                type="button"
+                className="btn btn-secondary"
+              >
+                hide all
+              </button>
+              <button
+                onClick={() =>
+                  setPanelState({
+                    right: true,
+                    left: true,
+                    top: true,
+                  })
+                }
+                type="button"
+                className="btn btn-secondary"
+              >
+                show all
+              </button>
+              <button
+                onClick={() => setAppState({ ...appState, isVisible: false })}
+                type="button"
+                className="btn btn-secondary"
+              >
+                quit
+              </button>
+            </div>
           </div>
-        </div>
-        {/* <div>{JSON.stringify(props.content)}</div> */}
-      </section>
-      <section className={`aa-panel right-panel${panelState.right ? ' open' : ''}`}>
-        <p>right sidebar</p>
-      </section>
-    </div>
+          {/* <div>{JSON.stringify(props.content)}</div> */}
+        </section>
+        <SidePanel
+          position="right"
+          panelState={panelState}
+          setPanelState={() => setPanelState({ ...panelState, right: !panelState.right })}
+        >
+          I am the right side panel.
+        </SidePanel>
+      </div>
+    </>
   );
 };
--- assets/src/apps/authoring/HeaderNav.tsx
+++ assets/src/apps/authoring/HeaderNav.tsx
@@ -7,7 +7,6 @@ interface HeaderNavProps {
 
 const HeaderNav: React.FC<HeaderNavProps> = (props: HeaderNavProps) => {
   const { content, isVisible } = props;
-  console.log('🚀 > file: HeaderNav.tsx > line 10 > content', content);
   return (
     <nav className={`aa-header-nav top-panel${isVisible ? ' open' : ''}`}>{content.title}</nav>
   );
