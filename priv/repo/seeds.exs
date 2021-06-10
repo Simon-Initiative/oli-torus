4ab02a60bf619406a975a4420717c89abb89394a
Devesh Tiwari <dtiwari@ats360.com>
2021-06-10 14:17:45 +0530

    -- updated tag name for carousel

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -106,7 +106,7 @@ Oli.Registrar.register_local_part_components(
     "janus_audio",
     "janus_video",
     "janus_slider",
-    "janus_carousel",
+    "janus_image_carousel",
     "janus_mcq",
     "janus_popup",
     "janus_capi_iframe",
