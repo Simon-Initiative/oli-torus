f4856237701e0a670b8c5fd14a9df2d0e2dc72b0
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-06-11 21:46:17 +0530

    accordion and tabs in left and right panel in authoring tool (#1142)
    * accordion and tabs in left and right panel in authoring tool
    
    * removing unwanted parts and minor miodifications
    
    * formatting files

---
+++ assets/src/apps/authoring/TabStrip/TabStrip.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+const TabStrip: React.FC<any> = (props) => {
+  const tabs: [] = props.tabsData.tabs;
+  return (
+    <div>
+      <ul className="nav nav-tabs" role="tablist">
+        {tabs.map((tab: any, index: number) => (
+          <li className="nav-item">
+            <a
+              id={`${tab.id}-tab`}
+              data-toggle="tab"
+              role="tab"
+              aria-controls={`tab${tab.id}`}
+              aria-selected={index == 0}
+              className={'nav-link ' + (index == 0 ? 'active' : '')}
+              href={`#tab${tab.id}`}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="tab-content" id="myTabContent">
+        {tabs.map((tab: any, index: number) => (
+          <div
+            className={`tab-pane fade${index == 0 ? ' show active' : ''}`}
+            id={`tab${tab.id}`}
+            role="tabpanel"
+            aria-labelledby={`${tab.id}-tab`}
+          >
+            {tab.data}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TabStrip;
