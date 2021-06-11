61fdc52548ee9de103d6a7ff8ae1e7d886286446
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-06-11 21:37:13 +0530

    formatting files

--- assets/src/apps/authoring/TabStrip/TabStrip.tsx
+++ assets/src/apps/authoring/TabStrip/TabStrip.tsx
@@ -4,36 +4,40 @@
 
 import React from 'react';
 const TabStrip: React.FC<any> = (props) => {
-
   const tabs: [] = props.tabsData.tabs;
-  //console.log(props.data);
   return (
-<div>
-    <ul className='nav nav-tabs' role='tablist'>
-      {tabs.map((tab: any, index:number) => (
-        <li className='nav-item'>
-          <a id={`${tab.id}-tab`}
-            data-toggle='tab'
-            role='tab'
-            aria-controls={`tab${tab.id}` }
-            aria-selected={index == 0}
-           className={'nav-link ' + (index == 0? 'active':'')}
-           href={`#tab${tab.id}`}>{tab.title}</a>
-        </li>
-      ))}
-  </ul>
+    <div>
+      <ul className="nav nav-tabs" role="tablist">
+        {tabs.map((tab: any, index: number) => (
+          <li className="nav-item">
+            <a
+              id={`${tab.id}-tab`}
+              data-toggle="tab"
+              role="tab"
+              aria-controls={`tab${tab.id}`}
+              aria-selected={index == 0}
+              className={'nav-link ' + (index == 0 ? 'active' : '')}
+              href={`#tab${tab.id}`}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
 
-  <div className='tab-content' id='myTabContent'>
-    {tabs.map((tab: any, index: number) => (
-      <div className={`tab-pane fade${index == 0 ? ' show active':''}`}
-        id={`tab${tab.id}` }
-        role='tabpanel'
-        aria-labelledby={`${tab.id}-tab`}>
-          {tab.data}
+      <div className="tab-content" id="myTabContent">
+        {tabs.map((tab: any, index: number) => (
+          <div
+            className={`tab-pane fade${index == 0 ? ' show active' : ''}`}
+            id={`tab${tab.id}`}
+            role="tabpanel"
+            aria-labelledby={`${tab.id}-tab`}
+          >
+            {tab.data}
+          </div>
+        ))}
       </div>
-    ))}
     </div>
-</div>
   );
 };
 
