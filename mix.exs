a5b8b205c498c19879bb31083535172ca96f37f2
Eli Knebel <eliknebel@cmu.edu>
2021-06-15 10:17:52 -0400

    [RELEASE] Version 0.11.0 (#1158)
    * Update mix.exs
    
    * Update CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oli.MixProject do
   def project do
     [
       app: :oli,
-      version: "0.10.0",
+      version: "0.11.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
