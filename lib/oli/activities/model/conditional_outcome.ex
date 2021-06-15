d8343d5504489d1aa8e43ff1541a856ed83c3cc1
Ben Sparks <bsparks@unicon.net>
2021-06-15 09:57:41 -0700

    serialize the rest of needed rule properties

--- lib/oli/activities/model/conditional_outcome.ex
+++ lib/oli/activities/model/conditional_outcome.ex
@@ -1,14 +1,27 @@
 defmodule Oli.Activities.Model.ConditionalOutcome do
   @derive Jason.Encoder
-  defstruct [:id, :conditions, :event, :name]
+  defstruct [:id, :conditions, :event, :name, :disabled, :default, :correct, :priority]
 
-  def parse(%{"id" => id, "event" => event, "name" => name, "conditions" => conditions}) do
+  def parse(%{
+    "id" => id,
+    "event" => event,
+    "name" => name,
+    "conditions" => conditions,
+    "disabled" => disabled,
+    "default" => default,
+    "correct" => correct,
+    "priority" => priority,
+    }) do
     {:ok,
      %Oli.Activities.Model.ConditionalOutcome{
        id: id,
        event: event,
        name: name,
-       conditions: conditions
+       conditions: conditions,
+       disabled: disabled,
+       default: default,
+       correct: correct,
+       priority: priority
      }}
   end
 
