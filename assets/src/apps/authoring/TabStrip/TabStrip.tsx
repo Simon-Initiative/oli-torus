347694b6d79400719e7b6bd1db52cec95c48dc9b
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-06-16 21:32:49 +0530

    Styling accordion and Tabs to match the designs

--- assets/src/apps/authoring/TabStrip/TabStrip.tsx
+++ assets/src/apps/authoring/TabStrip/TabStrip.tsx
@@ -2,33 +2,43 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/prop-types */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 const TabStrip: React.FC<any> = (props) => {
-  const tabs: [] = props.tabsData.tabs;
+  const tabs: any[] = props.tabsData.tabs;
+  const clickHandler = (tabid: any) => {
+    setSelectedTab(tabid);
+  };
+  const [selectedTab, setSelectedTab] = useState<any>('');
+  useEffect(() => {
+    setSelectedTab(tabs[0].id);
+  }, []);
   return (
     <div>
       <ul className="nav nav-tabs" role="tablist">
-        {tabs.map((tab: any, index: number) => (
-          <li className="nav-item">
-            <a
-              id={`${tab.id}-tab`}
-              data-toggle="tab"
-              role="tab"
-              aria-controls={`tab${tab.id}`}
-              aria-selected={index == 0}
-              className={'nav-link ' + (index == 0 ? 'active' : '')}
-              href={`#tab${tab.id}`}
-            >
-              {tab.title}
-            </a>
+        {tabs.map((tab: any) => (
+          <li
+            role="tab"
+            id={`${tab.id}-tab`}
+            data-toggle="tab"
+            aria-controls={`tab${tab.id}`}
+            aria-selected={selectedTab == tab.id}
+            className={`nav-item ${
+              selectedTab == tab.id ? 'active font-weight-bold' : ''
+            } p-2`}
+            href={`#tab${tab.id}`}
+            onClick={() => {
+              clickHandler(tab.id);
+            }}
+          >
+            <span className={selectedTab == tab.id ? 'border-top' : ''}>{tab.title}</span>
           </li>
         ))}
       </ul>
 
       <div className="tab-content" id="myTabContent">
-        {tabs.map((tab: any, index: number) => (
+        {tabs.map((tab: any) => (
           <div
-            className={`tab-pane fade${index == 0 ? ' show active' : ''}`}
+            className={`tab-pane fade${selectedTab == tab.id ? ' show active' : ''}`}
             id={`tab${tab.id}`}
             role="tabpanel"
             aria-labelledby={`${tab.id}-tab`}
