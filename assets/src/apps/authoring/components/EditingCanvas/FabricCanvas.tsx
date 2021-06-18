01e3650e3c7ca23a2e9767d7b8ab9b157e67b4cb
Ben Sparks <bsparks@unicon.net>
2021-06-18 13:08:07 -0700

    update to teh center canvas

---
+++ assets/src/apps/authoring/components/EditingCanvas/FabricCanvas.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+const FabricCanvas: React.FC<any> = (props) => {
+  return <canvas className="aa-canvas-inner"></canvas>;
+};
+
+export default FabricCanvas;
