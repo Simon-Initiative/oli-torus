d01681f14968401f79d902b108b3d506df060364
Ben Sparks <bsparks@unicon.net>
2021-06-18 16:05:33 -0700

    make properties more like json schema

--- assets/src/apps/authoring/components/PropertyEditor/PropertyEditor.tsx
+++ assets/src/apps/authoring/components/PropertyEditor/PropertyEditor.tsx
@@ -2,26 +2,49 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
+interface SchemaOptions {
+  label?: string;
+  input_width?: string | number;
+}
+interface Schema {
+  type: string;
+  description?: string;
+  format?: string;
+  options: SchemaOptions;
+  default?: any;
+  properties?: Record<string, Schema>;
+  items?: Schema;
+}
 interface PropertyEditorProps {
-  schema: any;
+  schema: Schema;
 }
 
 const PropertyEditor: React.FC<PropertyEditorProps> = ({ schema }) => {
-  const inputs = Object.keys(schema).map((schemaKey) => {
-    const {
-      description,
-      format,
-      options,
-      type,
-      default: defaultValue,
-    } = schema[schemaKey];
+  const rootProps = schema.properties || {};
+  // for now just using properties, but going to need to make a recursive schema renderer
+  const inputs = Object.keys(rootProps).map((schemaKey) => {
+    const { description, format, options, type, default: defaultValue } = rootProps[schemaKey];
 
     const label = options?.label || schemaKey;
+
+    // TODO: base different editors off of type
+    let controlType = 'text';
+    if (type === 'string') {
+      if (format === 'email') {
+        controlType = 'email';
+      }
+    }
+    if (type === 'number') {
+      controlType = 'number';
+    }
+    if (type === 'boolean') {
+      // render checkbox instead
+    }
 
     return (
       <Form.Group key={schemaKey} controlId={`form_${schemaKey}`}>
         <Form.Label>{label}</Form.Label>
-        <Form.Control type="text" placeholder={`Enter ${label}`} />
+        <Form.Control type={controlType} placeholder={`Enter ${label}`} />
         {description ? <Form.Text className="text-muted">{description}</Form.Text> : null}
       </Form.Group>
     );
--- assets/src/apps/authoring/components/PropertyEditor/schemas/screen.ts
+++ assets/src/apps/authoring/components/PropertyEditor/schemas/screen.ts
@@ -1,15 +1,18 @@
 const screenSchema = {
-  customCssClass: {
-    type: 'string',
-    options: { input_width: '500px' },
+  type: 'object',
+  properties: {
+    customCssClass: {
+      type: 'string',
+      options: { input_width: '500px' },
+    },
+    showCheckBtn: {
+      type: 'boolean',
+      format: 'checkbox',
+      default: true,
+    },
+    width: { type: 'number' },
+    height: { type: 'number' },
   },
-  showCheckBtn: {
-    type: 'boolean',
-    format: 'checkbox',
-    default: true,
-  },
-  width: { type: 'number' },
-  height: { type: 'number' },
 };
 
 export default screenSchema;
