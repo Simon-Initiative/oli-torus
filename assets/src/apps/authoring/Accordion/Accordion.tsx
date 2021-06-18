a0a32f669fd9d3ae9ae29fb93f2686ed24f172d9
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-06-18 22:14:59 +0530

    Feature/styling accordion and tab (#1164)
    * Styling accordion and Tabs to match the designs
    
    * Update TabStrip.tsx
    
    * removing unwanted references
    
    * Update Accordion.tsx
    
    * Update Accordion.tsx

--- assets/src/apps/authoring/Accordion/Accordion.tsx
+++ assets/src/apps/authoring/Accordion/Accordion.tsx
@@ -1,37 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable react/jsx-key */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/prop-types */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 const Accordion: React.FC<any> = (props) => {
   const tabs: [] = props.tabsData.tabs;
+  const clickHandler = (tabid: any) => {
+    if (expandedCards.includes(tabid))
+      setExpandedCards(expandedCards.filter((i: any) => i !== tabid));
+    else setExpandedCards([...expandedCards, tabid]);
+  };
+  const [expandedCards, setExpandedCards] = useState<any>([]);
+  useEffect(() => {
+    const tabsId = tabs.map((tab: any) => tab.id);
+    setExpandedCards(tabsId);
+  }, []);
   return (
-    <div className="accordion">
+    <div className='accordion'>
       {tabs.map((tab: any) => (
-        <div className="card">
-          <div id={tab.id} className="d-flex justify-content-between">
-            <span
-              className="col-10"
-              data-toggle="collapse"
+        <div id={`card${tab.id}`} className='card'>
+          <div id={tab.id} className='d-flex justify-content-between py-2 border-bottom'>
+            <div
+              className='col-10 font-weight-bold'
+              data-toggle='collapse'
               data-target={`#collapse${tab.id}`}
-              aria-expanded="true"
+              aria-expanded='true'
               aria-controls={`collapse${tab.id}`}
+              onClick={() => {
+                clickHandler(tab.id);
+              }}
             >
+              {expandedCards.includes(tab.id) ? (
+                <i className='fa fa-angle-down my-1 mr-2'></i>
+              ) : (
+                <i className='fa fa-angle-right my-1 mr-2'></i>
+              )}
               {tab.title}
-            </span>
-            <button type="button" className="btn btn-info some-button col-2">
-              <span> + </span>
-            </button>
+            </div>
+            <i className='fa fa-plus col-2 my-1'></i>
           </div>
 
-          <div id={`collapse${tab.id}`} className={'collapse show'} aria-labelledby={tab.id}>
-            <div className="card-body">
-              <ul className="list-group list-group-flush">
-                {tab.data.map((item: any) => (
-                  <li className="list-group-item">{item}</li>
-                ))}
-              </ul>
-            </div>
+          <div id={`collapse${tab.id}`} className='collapse show' aria-labelledby={tab.id}>
+            <ul style={{listStyleType: 'none'}}>
+              {tab.data.map((item: any,index: number) => (
+                <li id={`li${index}`}>{item}</li>
+              ))}
+            </ul>
           </div>
         </div>
       ))}
