b1b7ab089998acb3b059aec0cc39d92fe18376af
David Suriano <dsuriano@unicon.net>
2021-06-22 18:04:51 -0400

    cleaned up header nav styles

--- assets/src/apps/authoring/components/HeaderNav.tsx
+++ assets/src/apps/authoring/components/HeaderNav.tsx
@@ -8,15 +8,6 @@ interface HeaderNavProps {
   isVisible: boolean;
 }
 
-const PreviewButton: React.FC<{ url: string; windowName?: string }> = (props) => (
-  <a
-    className="btn btn-sm btn-outline-primary"
-    onClick={() => window.open(props.url, props.windowName)}
-  >
-    Preview <i className="las la-external-link-alt ml-1"></i>
-  </a>
-);
-
 const HeaderNav: React.FC<HeaderNavProps> = (props: HeaderNavProps) => {
   const { content, isVisible } = props;
   const projectSlug = useSelector(selectProjectSlug);
@@ -26,56 +17,55 @@ const HeaderNav: React.FC<HeaderNavProps> = (props: HeaderNavProps) => {
   const url = `/authoring/project/${projectSlug}/preview/${revisionSlug}`;
   const windowName = `preview-${projectSlug}`;
   return (
-    <nav className={`aa-header-nav top-panel${isVisible ? ' open' : ''} d-flex justify-content-between`}
-          style={{alignItems:'center'}}>
+    <nav
+      className={`aa-header-nav top-panel${
+        isVisible ? ' open' : ''
+      } d-flex justify-content-between`}
+      style={{ alignItems: 'center' }}
+    >
       <div>
-        <div className="btn-group" role="group">
-          <div className="pr-3 border-right">
-            INFINISCOPE{/* Place for Logo */}
-          </div>
-          <div className="pl-3">
-            {content.title}
-          </div>
+        <div className="btn-group align-items-center" role="group">
+          <div className="pr-3 border-right">INFINISCOPE{/* Place for Logo */}</div>
+          <div className="pl-3">{content.title}</div>
         </div>
       </div>
       <div className="btn-toolbar" role="toolbar">
-        <div className="btn-group pr-3 border-right" role="group">
-          <div className="px-2">
+        <div className="btn-group pr-3 border-right align-items-center" role="group">
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-text.svg`}></img>
-          </div>
-          <div className="px-2">
+          </button>
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-image.svg`}></img>
-          </div>
-          <div className="px-2">
+          </button>
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-video.svg`}></img>
-          </div>
+          </button>
         </div>
-        <div className="btn-group px-3 border-right" role="group">
-          <div className="px-2">
+        <div className="btn-group px-3 border-right align-items-center" role="group">
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-navButton.svg`}></img>
-          </div>
-          <div className="px-2">
+          </button>
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-multiChoice.svg`}></img>
-          </div>
-          <div className="px-2">
+          </button>
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-userInput.svg`}></img>
-          </div>
+          </button>
         </div>
-        <div className="btn-group pl-3" role="group">
-          <div className="px-2">
+        <div className="btn-group pl-3 align-items-center" role="group">
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-componentList.svg`}></img>
-          </div>
+          </button>
         </div>
       </div>
       <div>
-        <div className="btn-group" role="group" aria-label="Third group">
-          <div className="px-2">
+        <div className="btn-group align-items-center" role="group" aria-label="Third group">
+          <button className="px-2 btn btn-link" onClick={() => window.open(url, windowName)}>
             <img src={`${paths.images}/icons/icon-preview.svg`}></img>
-            <PreviewButton url={url} windowName={windowName} />
-          </div>
-          <div className="px-2">
+          </button>
+          <button className="px-2 btn btn-link" disabled>
             <img src={`${paths.images}/icons/icon-publish.svg`}></img>
-          </div>
+          </button>
         </div>
       </div>
     </nav>
