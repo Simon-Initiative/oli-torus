d2b0014d3aea4c51725705ecfdd772158fe03ffa
Ben Sparks <bsparks@unicon.net>
2021-06-22 16:57:42 -0700

    more robust outcome parsing

--- lib/oli/activities/model/conditional_outcome.ex
+++ lib/oli/activities/model/conditional_outcome.ex
@@ -2,35 +2,29 @@ defmodule Oli.Activities.Model.ConditionalOutcome do
   @derive Jason.Encoder
   defstruct [:id, :conditions, :event, :name, :disabled, :default, :correct, :priority]
 
-  def parse(%{
-    "id" => id,
-    "event" => event,
-    "name" => name,
-    "conditions" => conditions,
-    "disabled" => disabled,
-    "default" => default,
-    "correct" => correct,
-    "priority" => priority,
-    }) do
+  def parse(outcome) when is_map(outcome) do
     {:ok,
      %Oli.Activities.Model.ConditionalOutcome{
-        id: id,
-        event: event,
-        name: name,
-        conditions: conditions,
-        disabled: disabled,
-        default: default,
-        correct: correct,
-        priority: priority
+        id: Map.get(outcome, "id"),
+        event: Map.get(outcome, "event"),
+        name: Map.get(outcome, "name"),
+        conditions: Map.get(outcome, "conditions"),
+        disabled: Map.get(outcome, "disabled", false),
+        default: Map.get(outcome, "default", false),
+        correct: Map.get(outcome, "correct", false),
+        priority: Map.get(outcome, "priority", 1),
      }}
   end
 
   def parse(outcomes) when is_list(outcomes) do
+    if Enum.empty?(outcomes) do
+      []
+    end
     Enum.map(outcomes, &parse/1)
     |> Oli.Activities.ParseUtils.items_or_errors()
   end
 
-  def parse(_) do
+  def parse(bad_input) do
     {:error, "invalid conditional outcome"}
   end
 end
