a3fb5bb4c0749c3dae7c1da7ca263983165694ec
Darren Siegel <siegel.darren@gmail.com>
2021-06-23 21:09:42 -0400

    fine tune toast animation

--- assets/src/components/resource/resourceEditor/UndoToasts.tsx
+++ assets/src/components/resource/resourceEditor/UndoToasts.tsx
@@ -11,7 +11,16 @@ export const UndoToasts = (props: UndoToastsProps) => {
   const toasts = props.undoables.toArray().map((u) => {
     const [key, action] = u;
     return (
-      <CSSTransition key={key} in={true} out={500} timeout={500} classNames="undotoast">
+      <CSSTransition
+        key={key}
+        in={true}
+        timeout={{
+          appear: 500,
+          enter: 500,
+          exit: 50,
+        }}
+        classNames="undotoast"
+      >
         <div key={key} className="undotoast">
           <div className="toast-body d-flex justify-content-between">
             <span className="undo-toast-desc">{action.undoable.description}</span>
