eb33ec38d2739bf90620ca4dc1d75ceb9ebc0ef6
Darren Siegel <siegel.darren@gmail.com>
2021-06-24 16:39:26 -0400

    restore splitchunks optimization for vendor.js

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -103,7 +103,29 @@ module.exports = (env, options) => ({
   devtool: 'source-map',
   optimization: {
     minimize: process.env.NODE_ENV == 'production',
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    chunkIds: "named",
+		splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   entry: populateEntries(),
   output: {
