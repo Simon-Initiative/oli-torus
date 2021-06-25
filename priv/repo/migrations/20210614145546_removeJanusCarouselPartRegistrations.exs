d2af8e69b240e8e0cb9f319f5cf92ab0c60f9eec
Devesh Tiwari <dtiwari@ats360.com>
2021-06-25 20:47:58 +0530

    bug fix

--- priv/repo/migrations/20210614145546_removeJanusCarouselPartRegistrations.exs
+++ priv/repo/migrations/20210614145546_removeJanusCarouselPartRegistrations.exs
@@ -9,10 +9,10 @@ defmodule Oli.Repo.Migrations.RemoveJanusCarousel do
     from(p in "part_component_registrations",
       where: delivery_element = 'janus-carousel'
     )
-    |> Oli.Repo.update_all(set: [slug:"janus_image_carousel",
+    |> Oli.Repo.update_all(set: [slug: "janus_image_carousel",
     delivery_element: "janus-image-carousel",
     authoring_element: "janus-image-carousel",
-    authoring_script:"janus_image_carousel_authoring.js",
-    delivery_script:"janus_image_carousel_delivery.js"])
+    authoring_script: "janus_image_carousel_authoring.js",
+    delivery_script: "janus_image_carousel_delivery.js"])
   end
 end
