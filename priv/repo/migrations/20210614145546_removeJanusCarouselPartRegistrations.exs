1f1a5af3e05d26998fdbdaf0e47547a05f4f153f
Devesh Tiwari <dtiwari@ats360.com>
2021-06-25 20:56:45 +0530

    more changes

--- priv/repo/migrations/20210614145546_removeJanusCarouselPartRegistrations.exs
+++ priv/repo/migrations/20210614145546_removeJanusCarouselPartRegistrations.exs
@@ -1,18 +1,10 @@
 defmodule Oli.Repo.Migrations.RemoveJanusCarousel do
   use Ecto.Migration
 
-  import Ecto.Query, warn: false
-
   def change do
-    flush()
+    execute "DELETE FROM part_component_registrations
+      WHERE delivery_element = 'janus-carousel';"
 
-    from(p in "part_component_registrations",
-      where: delivery_element == 'janus-carousel'
-    )
-    |> Oli.Repo.update_all(set: [slug: "janus_image_carousel",
-    delivery_element: "janus-image-carousel",
-    authoring_element: "janus-image-carousel",
-    authoring_script: "janus_image_carousel_authoring.js",
-    delivery_script: "janus_image_carousel_delivery.js"])
+      flush()
   end
 end
