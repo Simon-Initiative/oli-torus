93b12e02f1ebe104825ff6634a999442312d4f1c
Devesh Tiwari <dtiwari@ats360.com>
2021-06-30 18:13:54 +0530

    Initial check-in for histroy navigation

--- assets/src/apps/delivery/layouts/deck/components/HistoryNavigation.tsx
+++ assets/src/apps/delivery/layouts/deck/components/HistoryNavigation.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 import { setRestartLesson } from '../../../store/features/adaptivity/slice';
 import React, { Fragment, useState } from 'react';
-import { useDispatch } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import HistoryPanel from './HistoryPanel';
+import { selectCurrentActivityId } from '../../../store/features/activities/slice';
+import { selectEnableHistory } from '../../../store/features/page/slice';
+import {
+  navigateToNextActivity,
+  navigateToPrevActivity,
+} from '../../../store/features/groups/actions/deck';
+import { selectSequence } from '../../../store/features/groups/selectors/deck';
 const HistoryNavigation: React.FC = () => {
-  const enableHistory = false;
-  // const currentPage: AdaptivePage = useSelector(selectCurrentPage);
-  // enableHistory = !!currentPage?.custom?.enableHistory;
-
-  //const currentEnsemble: SequenceEntry = useSelector(selectCurrentEnsemble);
-
+  const currentActivityId = useSelector(selectCurrentActivityId);
+  const isHistoryModeOn = true; //useSelector(selectEnableHistory);
   const [minimized, setMinimized] = useState(true);
+  const sequences = useSelector(selectSequence);
 
   const dispatch = useDispatch();
 
@@ -18,86 +22,85 @@ const HistoryNavigation: React.FC = () => {
     dispatch(setRestartLesson({ restartLesson: true }));
   };
 
-  // const nextHandler = () => {
-  //   dispatch(navigateToNextEnsemble());
-  // };
+  const nextHandler = () => {
+    dispatch(navigateToNextActivity());
+  };
 
-  // const prevHandler = () => {
-  //   dispatch(navigateToPrevEnsemble());
-  // };
+  const prevHandler = () => {
+    dispatch(navigateToPrevActivity());
+  };
 
-  // const minimizeHandler = () => {
-  //   setMinimized(!minimized);
-  // };
+  const minimizeHandler = () => {
+    setMinimized(!minimized);
+  };
 
   // TODO this is actually driven by the student's history IF you are a student
-  // and the toc otherwise
-  // const historyItems =
-  //     currentPage.sequence
-  //         .filter((entry) => !entry.custom?.isLayer)
-  //         .map((entry) => {
-  //             return {
-  //                 id: entry.id,
-  //                 // TODO: pull all ensembles in sequence and get their names?
-  //                 // maybe need a history state to track this instead!
-  //                 name: entry.custom?.sequenceName || entry.id,
-  //             };
-  //         }) || [];
-  // const currentEnsembleIndex = historyItems.findIndex(
-  //     (item) => item.id === currentEnsemble.id
-  // );
-  // const isFirst = currentEnsembleIndex === 0;
-  // const isLast = currentEnsembleIndex === historyItems.length - 1;
+  //and the toc otherwise
+  const historyItems =
+    sequences
+      ?.filter((sequence) => !sequence?.custom?.isLayer)
+      .map((entry: any) => {
+        return {
+          id: entry.custom?.sequenceId,
+          // TODO: pull all ensembles in sequence and get their names?
+          // maybe need a history state to track this instead!
+          name: entry.custom?.sequenceName || entry.id,
+        };
+      }) || [];
+
+  const currentEnsembleIndex = historyItems.findIndex((item: any) => item.id === currentActivityId);
+  const isFirst = currentEnsembleIndex === 0;
+  const isLast = currentEnsembleIndex === historyItems.length - 1;
 
   return (
     <Fragment>
-      {/* {enableHistory ? (
-                <div className="historyStepView pullLeftInCheckContainer">
-                    <div className="historyStepContainer">
-                        <button
-                            onClick={prevHandler}
-                            className="backBtn historyStepButton"
-                            aria-label="Previous screen"
-                            disabled={isFirst}
-                        >
-                            <span className="icon-chevron-left" />
-                        </button>
-                        <button
-                            onClick={nextHandler}
-                            className="nextBtn historyStepButton"
-                            aria-label="Next screen"
-                            disabled={isLast}
-                        >
-                            <span className="icon-chevron-right" />
-                        </button>
-                    </div>
-                </div>
-            ) : null} */}
+      {isHistoryModeOn ? (
+        <div className="historyStepView pullLeftInCheckContainer">
+          <div className="historyStepContainer">
+            <button
+              onClick={prevHandler}
+              className="backBtn historyStepButton"
+              aria-label="Previous screen"
+              disabled={isFirst}
+            >
+              <span className="icon-chevron-left" />
+            </button>
+            <button
+              onClick={nextHandler}
+              className="nextBtn historyStepButton"
+              aria-label="Next screen"
+              disabled={isLast}
+            >
+              <span className="icon-chevron-right" />
+            </button>
+          </div>
+        </div>
+      ) : null}
       <div
         className={[
           'navigationContainer',
-          enableHistory ? undefined : 'pullLeftInCheckContainer',
+          isHistoryModeOn ? undefined : 'pullLeftInCheckContainer',
         ].join(' ')}
       >
         <aside className={minimized ? 'minimized' : undefined}>
-          {/* {enableHistory ? (
-                        <Fragment>
-                            <button
-                                onClick={minimizeHandler}
-                                className="navigationToggle"
-                                aria-label="Show lesson history"
-                                aria-haspopup="true"
-                                aria-controls="theme-history-panel"
-                                aria-pressed="false"
-                            />
+          {isHistoryModeOn ? (
+            <Fragment>
+              <button
+                onClick={minimizeHandler}
+                className="navigationToggle"
+                aria-label="Show lesson history"
+                aria-haspopup="true"
+                aria-controls="theme-history-panel"
+                aria-pressed="false"
+              />
 
-                            <HistoryPanel
-                                items={historyItems}
-                                onMinimize={minimizeHandler}
-                                onRestart={restartHandler}
-                            />
-                        </Fragment>
-                    )  */}
+              <HistoryPanel
+                items={historyItems}
+                onMinimize={minimizeHandler}
+                onRestart={restartHandler}
+              />
+            </Fragment>
+          ) : null}
           <button onClick={restartHandler} className="theme-no-history-restart">
             <span>
               <div className="theme-no-history-restart__icon" />
---
+++ assets/src/apps/delivery/layouts/deck/components/HistoryPanel.tsx
@@ -0,0 +1,93 @@
+import {
+  selectCurrentActivityId,
+  selectCurrentActivityContent,
+} from '../../../store/features/activities/slice';
+import { selectPageContent } from '../../../store/features/page/slice';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { navigateToActivity } from '../../../store/features/groups/actions/deck';
+
+interface HistoryEntry {
+  id: string;
+  name: string;
+  timestamp?: any; // not sure if this is needed
+  current?: boolean;
+  selected?: boolean;
+}
+
+interface HistoryPanelProps {
+  items: HistoryEntry[];
+  onMinimize: any; // function?
+  onRestart: any; // function
+}
+
+const HistoryPanel: React.FC<HistoryPanelProps> = ({ items, onMinimize, onRestart }) => {
+  const dispatch = useDispatch();
+
+  const currentPage = useSelector(selectPageContent);
+  const currentActivityId = useSelector(selectCurrentActivityId);
+  const currentActivity = useSelector(selectCurrentActivityContent);
+
+  const itemClickHandler = (item: HistoryEntry) => {
+    dispatch(navigateToActivity(item.id));
+  };
+
+  const getItemClasses = (item: HistoryEntry) => {
+    const currentClass = 'history-element-current';
+    const selectedClass = 'history-element-selected';
+    const classes = ['history-element'];
+    // TODO: current is a feature of history of the actual current screen and not the history mode screen?
+    if (item.id === currentActivityId) {
+      classes.push(currentClass);
+    }
+    if (item.id === currentActivityId) {
+      classes.push(selectedClass);
+    }
+    return classes.join(' ');
+  };
+
+  return (
+    <div
+      id="theme-history-panel"
+      className="theme-history"
+      aria-role="alertdialog"
+      aria-hidden="false"
+      aria-label="Lesson history"
+    >
+      <div className="theme-history__title">
+        <span>Lesson History</span>
+        <button
+          onClick={onMinimize}
+          className="theme-history__close-btn"
+          aria-label="Minimize lesson history"
+        >
+          <span>
+            <div className="theme-history__close-icon" />
+          </span>
+        </button>
+      </div>
+      <nav className="theme-history__nav">
+        {items.map((item) => (
+          <button
+            key={item.id}
+            className={getItemClasses(item)}
+            onClick={() => itemClickHandler(item)}
+          >
+            <div className="history-element__screenName">{item.name}</div>
+            <div className="history-element__timestamp">{item.timestamp}</div>
+          </button>
+        ))}
+      </nav>
+      <div className="theme-history__footer">
+        <button onClick={onRestart} className="theme-history__restart">
+          <span>
+            <div className="theme-history__restart-icon" />
+            <span className="theme-history__restart-label">Restart Lesson</span>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryPanel;
