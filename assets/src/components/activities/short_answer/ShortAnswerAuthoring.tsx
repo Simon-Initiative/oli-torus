aa7d29a75b4b7496a181fd7421e33fb73b8660b6
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2021-06-29 21:59:52 -0400

    initial

--- assets/src/components/activities/short_answer/ShortAnswerAuthoring.tsx
+++ assets/src/components/activities/short_answer/ShortAnswerAuthoring.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AuthoringElement, AuthoringElementProps } from '../AuthoringElement';
+import {
+  AuthoringElement,
+  AuthoringElementProps,
+  AuthoringElementProvider,
+  useAuthoringElementContext,
+} from '../AuthoringElement';
 import { ShortAnswerModelSchema, InputType } from './schema';
 import * as ActivityTypes from '../types';
 import { Stem } from '../common/Stem';
@@ -10,7 +15,10 @@ import { ShortAnswerActions } from './actions';
 import { ModalDisplay } from 'components/modal/ModalDisplay';
 import { Provider } from 'react-redux';
 import { configureStore } from 'state/store';
-import produce from 'immer';
+import { TabbedNavigation } from 'components/tabbed_navigation/Tabs';
+import { StemAuthoring } from 'components/activities/common/stem/StemAuthoring';
+import { StemActions } from 'components/activities/common/authoring/actions/stemActions';
+import { HintsAuthoringConnected } from 'components/activities/common/hints/authoring/HintsAuthoringConnected';
 
 const store = configureStore();
 
@@ -53,10 +61,67 @@ export const InputTypeDropdown = ({ onChange, editMode, inputType }: InputTypeDr
 };
 
 const ShortAnswer = (props: AuthoringElementProps<ShortAnswerModelSchema>) => {
-  const dispatch = (action: any) => {
-    const nextModel = produce(props.model, (draftState) => action(draftState));
-    props.onEdit(nextModel);
-  };
+  const { dispatch } = useAuthoringElementContext();
+  return (
+    <>
+      <TabbedNavigation.Tabs>
+        <TabbedNavigation.Tab label="Question">
+          <StemAuthoring
+            stem={props.model.stem}
+            onEdit={(content) => dispatch(StemActions.editStemAndPreviewText(content))}
+          />
+          <InputTypeDropdown
+            editMode={props.editMode}
+            inputType={props.model.inputType}
+            onChange={(inputType) => dispatch(ShortAnswerActions.setInputType(inputType))}
+          />
+        </TabbedNavigation.Tab>
+        <TabbedNavigation.Tab label="Answer Key">
+          {/* <AnswerKeyAuthoring
+            stem={props.model.stem}
+            choices={props.model.choices}
+            selectedChoiceIds={getCorrectChoiceIds(props.model)}
+            selectedIcon={<Checkbox.Correct />}
+            unselectedIcon={<Checkbox.Unchecked />}
+            onSelectChoiceId={(id) => dispatch(CATAActions.toggleChoiceCorrectness(id))}
+          /> */}
+          {/* <SimpleFeedback
+            correctResponse={getCorrectResponse(props.model)}
+            incorrectResponse={getIncorrectResponse(props.model)}
+            update={(id, content) => dispatch(ResponseActions.editResponseFeedback(id, content))}
+          /> */}
+
+          {/* {isTargetedCATA(props.model) && (
+            <TargetedFeedback
+              choices={props.model.choices}
+              targetedMappings={getTargetedResponseMappings(props.model)}
+              toggleChoice={(choiceId, mapping) => {
+                dispatch(
+                  CATAActions.editTargetedFeedbackChoices(
+                    mapping.response.id,
+                    mapping.choiceIds.includes(choiceId)
+                      ? mapping.choiceIds.filter((id) => id !== choiceId)
+                      : mapping.choiceIds.concat(choiceId),
+                  ),
+                );
+              }}
+              updateResponse={(id, content) =>
+                dispatch(ResponseActions.editResponseFeedback(id, content))
+              }
+              addTargetedResponse={() => dispatch(CATAActions.addTargetedFeedback())}
+              unselectedIcon={<Checkbox.Unchecked />}
+              selectedIcon={<Checkbox.Checked />}
+              onRemove={(id) => dispatch(CATAActions.removeTargetedFeedback(id))}
+            />
+          )} */}
+        </TabbedNavigation.Tab>
+
+        <TabbedNavigation.Tab label="Hints">
+          <HintsAuthoringConnected />
+        </TabbedNavigation.Tab>
+      </TabbedNavigation.Tabs>
+    </>
+  );
 
   const sharedProps = {
     model: props.model,
@@ -65,11 +130,6 @@ const ShortAnswer = (props: AuthoringElementProps<ShortAnswerModelSchema>) => {
 
   return (
     <div>
-      <InputTypeDropdown
-        editMode={props.editMode}
-        inputType={props.model.inputType}
-        onChange={(inputType) => dispatch(ShortAnswerActions.setInputType(inputType))}
-      />
       <Stem
         projectSlug={props.projectSlug}
         editMode={props.editMode}
@@ -100,7 +160,9 @@ export class ShortAnswerAuthoring extends AuthoringElement<ShortAnswerModelSchem
   render(mountPoint: HTMLDivElement, props: AuthoringElementProps<ShortAnswerModelSchema>) {
     ReactDOM.render(
       <Provider store={store}>
-        <ShortAnswer {...props} />
+        <AuthoringElementProvider {...props}>
+          <ShortAnswer {...props} />
+        </AuthoringElementProvider>
         <ModalDisplay />
       </Provider>,
       mountPoint,
