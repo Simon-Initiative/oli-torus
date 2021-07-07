37a78587c2dfad45417ca3afd29a20b785f4c4a6
David Suriano <dsuriano@users.noreply.github.com>
2021-07-07 16:52:09 -0400

    add screen/layer to sequence, no persist (#1235)

--- assets/src/apps/authoring/components/SequenceEditor/SequenceEditor.tsx
+++ assets/src/apps/authoring/components/SequenceEditor/SequenceEditor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Accordion, ListGroup, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import guid from 'utils/guid';
 import {
   selectCurrentActivityId,
   setCurrentActivityId,
@@ -11,11 +12,178 @@ import {
 } from '../../../delivery/store/features/groups/actions/sequence';
 import { selectSequence } from '../../../delivery/store/features/groups/selectors/deck';
 import ContextAwareToggle from '../Accordion/ContextAwareToggle';
+import { selectCurrentGroup } from '../../../delivery/store/features/groups/slice';
+import { createNew as createNewActivity } from '../../../authoring/store/activities/actions/createNew';
+import { addSequenceItem } from '../../store/groups/layouts/deck/actions/addSequenceItem';
 
 const SequenceEditor: React.FC<any> = (props) => {
   const dispatch = useDispatch();
   const currentActivityId = useSelector(selectCurrentActivityId);
   const sequence = useSelector(selectSequence);
+  const currentGroup = useSelector(selectCurrentGroup);
+
+  const handleItemClick = (entry: SequenceEntry<SequenceEntryChild>) => {
+    dispatch(setCurrentActivityId({ activityId: entry.activitySlug }));
+  };
+
+  const handleItemAdd = async (
+    parentItem: SequenceEntry<SequenceEntryChild> | undefined,
+    isLayer = false,
+  ) => {
+    const { payload: newActivity } = await dispatch<any>(createNewActivity());
+    let layerRef: string | undefined;
+
+    if (parentItem) {
+      layerRef = parentItem.custom.sequenceId;
+    }
+    const newSequenceEntry = {
+      type: 'activity-reference',
+      activitySlug: newActivity.id,
+      custom: {
+        isLayer,
+        layerRef,
+        sequenceId: `${newActivity.id}_${guid()}`,
+        sequenceName: `New ${layerRef ? 'Child' : ''}${isLayer ? 'Layer' : 'Screen'}`,
+      },
+    };
+    await dispatch(
+      addSequenceItem({
+        sequence: sequence,
+        item: newSequenceEntry,
+        group: currentGroup,
+      }),
+    );
+  };
+
+  const SequenceItemContextMenu = (props: any) => {
+    const { id } = props;
+
+    return (
+      <div className="dropdown">
+        <button
+          className="dropdown-toggle aa-context-menu-trigger btn btn-link p-0 px-1"
+          type="button"
+          id={`sequence-item-${id}-context-trigger`}
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          onClick={(e) => {
+            e.stopPropagation();
+            ($(`#sequence-item-${id}-context-trigger`) as any).dropdown('toggle');
+          }}
+        >
+          <i className="fas fa-ellipsis-v" />
+        </button>
+        <div
+          id={`sequence-item-${id}-context-menu`}
+          className="dropdown-menu"
+          aria-labelledby={`sequence-item-${id}-context-trigger`}
+        >
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-desktop mr-2" /> Add Subscreen
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-layer-group mr-2" /> Add Layer
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-exchange-alt mr-2" /> Convert to Layer
+          </button>
+          <button
+            className="dropdown-item text-danger"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-trash mr-2" /> Delete
+          </button>
+          <div className="dropdown-divider"></div>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-arrow-up mr-2" /> Move Up
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-arrow-down mr-2" /> Move Down
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-arrow-right mr-2" /> Move Right
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-arrow-left mr-2" /> Move Left
+          </button>
+          <div className="dropdown-divider"></div>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-copy mr-2" /> Copy
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-paste mr-2" /> Paste as Child
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={(e) => {
+              e.stopPropagation();
+              ($(`#sequence-item-${id}-context-menu`) as any).dropdown('toggle');
+            }}
+          >
+            <i className="fas fa-paste mr-2" /> Paste as Sibling
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   const handleItemClick = (entry: SequenceEntry<SequenceEntryChild>) => {
     dispatch(setCurrentActivityId({ activityId: entry.activitySlug }));
@@ -37,11 +205,40 @@ const SequenceEditor: React.FC<any> = (props) => {
             </Tooltip>
           }
         >
-          <span>
-            <button className="btn btn-link p-0">
+          <div className="dropdown">
+            <button
+              className="dropdown-toggle btn btn-link p-0"
+              type="button"
+              id={`sequence-add`}
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
               <i className="fa fa-plus" />
             </button>
-          </span>
+            <div
+              id={`sequence-add-contextMenu`}
+              className="dropdown-menu"
+              aria-labelledby={`sequence-add-contextMenu`}
+            >
+              <button
+                className="dropdown-item"
+                onClick={() => {
+                  handleItemAdd(undefined);
+                }}
+              >
+                <i className="fas fa-desktop mr-2" /> Screen
+              </button>
+              <button
+                className="dropdown-item"
+                onClick={() => {
+                  handleItemAdd(undefined, true);
+                }}
+              >
+                <i className="fas fa-layer-group mr-2" /> Layer
+              </button>
+            </div>
+          </div>
         </OverlayTrigger>
       </div>
       <Accordion.Collapse eventKey="0">
@@ -53,8 +250,10 @@ const SequenceEditor: React.FC<any> = (props) => {
               key={entry.custom.sequenceId}
               active={entry.activitySlug === currentActivityId}
               onClick={() => handleItemClick(entry)}
+              tabIndex={0}
             >
               {entry.custom.sequenceName}
+              <SequenceItemContextMenu id={entry.activitySlug} />
             </ListGroup.Item>
           ))}
         </ListGroup>
