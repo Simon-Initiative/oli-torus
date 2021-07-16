8427b31be74000049fc99cf258eb578c9d36e4c5
schowkekarats360 <schowkekar@ats360.com>
2021-07-16 21:20:50 +0530

    Update ColorPickerWidget.tsx

--- assets/src/apps/authoring/components/PropertyEditor/custom/ColorPickerWidget.tsx
+++ assets/src/apps/authoring/components/PropertyEditor/custom/ColorPickerWidget.tsx
@@ -5,21 +5,16 @@ interface ColorPickerProps {
 }
 
 const getColorValueString = (value: any) => {
-  return (
-    value.rgb.a && value.rgb.a != 0 ?
-      `rgba(${value.rgb.r},${value.rgb.g},${value.rgb.b},${value.rgb.a})`
-      :
-      `rgb(${value.rgb.r},${value.rgb.g},${value.rgb.b})`
-  );
-}
+  return `rgba(${value.rgb.r},${value.rgb.g},${value.rgb.b},${value.rgb.a})`;
+};
 
 const getRGBColorValue = (value: any) => {
   if (value) {
-    const parts = value.replace('rgba(', '').replace('rgb(','').replace(')', '').split(',');
-    return { r: parts[0], g: parts[1], b: parts[2], a: parts.length > 3 ? parts[3] : 100 }
+    const parts = value.replace('rgba(', '').replace('rgb(', '').replace(')', '').split(',');
+    return { r: parts[0], g: parts[1], b: parts[2], a: parts.length > 3 ? parts[3] : 100 };
   }
   return { r: 255, g: 255, b: 255, a: 0 };
-}
+};
 
 const ColorPickerWidget: React.FC<ColorPickerProps> = (props: any) => {
   const color = getRGBColorValue(props.value);
@@ -27,14 +22,14 @@ const ColorPickerWidget: React.FC<ColorPickerProps> = (props: any) => {
 
   const handleColorBoxClick = () => {
     setDisplayPicker(true);
-    document.addEventListener("mousedown", handleClick);
+    document.addEventListener('mousedown', handleClick);
   };
   const handleClick = (event: any) => {
     if (pickerRef.current && !pickerRef.current.contains(event.target)) {
       setDisplayPicker(false);
-      document.removeEventListener("mousedown", handleClick);
+      document.removeEventListener('mousedown', handleClick);
     }
-  }
+  };
   const pickerRef = useRef<HTMLDivElement>(null);
   const colorDiv: CSSProperties = {
     width: '36px',
@@ -42,28 +37,28 @@ const ColorPickerWidget: React.FC<ColorPickerProps> = (props: any) => {
     borderRadius: '2px',
     border: '1px solid black',
     background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
-  }
+  };
   const popup: CSSProperties = {
     position: 'absolute',
     zIndex: 2,
     left: 0,
-    top: '25px'
-  }
+    top: '25px',
+  };
   return (
-    <div className="d-flex justify-content-between" >
+    <div className="d-flex justify-content-between">
       <span className="form-label">{props.label}</span>
       <div>
         <div style={colorDiv} onClick={handleColorBoxClick}></div>
-        {displayPicker ?
+        {displayPicker ? (
           <div style={popup} ref={pickerRef}>
             <SketchPicker
               color={color}
-              onChangeComplete={color => {
+              onChangeComplete={(color) => {
                 props.onChange(getColorValueString(color));
               }}
-            /></div>
-          : null
-        }
+            />
+          </div>
+        ) : null}
       </div>
     </div>
   );
