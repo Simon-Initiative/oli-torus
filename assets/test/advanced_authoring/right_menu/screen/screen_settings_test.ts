c7059555c414ccbae6e9e51bc380f53c73cc5e5a
schowkekarats360 <schowkekar@ats360.com>
2021-07-28 12:55:55 +0530

    multiple screen data versions

--- assets/test/advanced_authoring/right_menu/screen/screen_settings_test.ts
+++ assets/test/advanced_authoring/right_menu/screen/screen_settings_test.ts
@@ -2,13 +2,23 @@ import {
   transformScreenModeltoSchema,
   transformScreenSchematoModel,
 } from 'apps/authoring/components/PropertyEditor/schemas/screen';
-import { screen, transformedSchema } from './screen_mocks';
+import { screen, screen1, transformedSchema } from './screen_mocks';
 
 describe('transforming screen structure to screen schema and back to structure', () => {
   it('should transform activity structure to screen schema', () => {
     const schema = transformScreenModeltoSchema(screen);
+    console.log(schema);
     const changes = transformScreenSchematoModel(schema);
     console.log('changes', changes);
     expect(changes).toMatchObject(transformedSchema);
   });
+
+  it('should transform activity structure to screen schema', () => {
+    const schema = transformScreenModeltoSchema(screen1);
+    console.log(schema);
+    const changes = transformScreenSchematoModel(schema);
+    console.log('changes', changes);
+    expect(changes).toMatchObject(transformedSchema);
+  });
+
 });
