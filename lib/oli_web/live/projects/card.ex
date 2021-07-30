0f1c098c0508838af110bf2b8466386cb3d6472c
Eli Knebel <eliknebel@gmail.com>
2021-07-29 21:56:30 -0400

    add example image to projects card links

--- lib/oli_web/live/projects/card.ex
+++ lib/oli_web/live/projects/card.ex
@@ -6,11 +6,12 @@ defmodule OliWeb.Projects.Card do
     ~L"""
     <div class="col my-1">
       <a class="course-card-link card h-100 mb-4" href="<%= Routes.project_path(OliWeb.Endpoint, :overview, @project)%>">
+        <img src="<%= Routes.static_path(@socket, "/images/course_default.jpg") %>" class="card-img-top" alt="course image">
         <div class="card-body">
           <h5 class="card-title"><%= @project.title  %></h5>
           <div class="d-flex justify-content-between align-items-center">
             <p class="card-text"><%= @project.description %></p>
-            <span class="authors d-flex align-items-center"><%= @author_count %> <i class="material-icons-outlined">person</i>
+            <span class="authors d-flex align-items-center"><%= @author_count %> <i class="las la-user la-lg ml-1"></i>
           </div>
         </div>
       </a>
