b0915be699cd7a93767cf4a1cf1b9d7143a946e4
Devesh Tiwari <dtiwari@ats360.com>
2021-08-03 12:14:32 +0530

    PMP-1653

--- assets/src/adaptivity/operators/equality.ts
+++ assets/src/adaptivity/operators/equality.ts
@@ -20,7 +20,7 @@ export const isEqual = (factValue: any, value: any): boolean => {
     if (Array.isArray(value)) {
       // ** We are doing this for the cases where factValue comes [2 , 5] but the values comes as ['2','5'] */
       // ** DT - making sure that value is of array type else value.map() will throw error. */
-      compareValue = parseArrayString(factValue).sort();
+      compareValue = parseArrayString(value).sort();
     }
 
     // ** DT - Sorting both arrays. depending upon user selection in UI the array sometimes comes
