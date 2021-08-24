82c33004d9b9697bf0c192b36c44e481daa10603
schowkekarats360 <schowkekar@ats360.com>
2021-08-24 18:11:06 +0530

    PMP-1779

--- assets/src/components/parts/janus-audio/schema.ts
+++ assets/src/components/parts/janus-audio/schema.ts
@@ -30,13 +30,13 @@ export const schema = {
     default: false,
   },
   startTime: {
-    title: 'Start time',
+    title: 'Start time(secs)',
     type: 'number',
     description: 'specifies the start time of the audio',
     default: 0,
   },
   endTime: {
-    title: 'End time',
+    title: 'End time(secs)',
     type: 'number',
     description: 'specifies the end time of the audio',
     default: 0,
