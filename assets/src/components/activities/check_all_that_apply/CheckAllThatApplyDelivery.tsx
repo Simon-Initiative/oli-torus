7eb826ce6328820af82f6347902e36d205fdd862
Darren Siegel <siegel.darren@gmail.com>
2021-08-24 11:01:58 -0400

    revert cata changes

--- assets/src/components/activities/check_all_that_apply/CheckAllThatApplyDelivery.tsx
+++ assets/src/components/activities/check_all_that_apply/CheckAllThatApplyDelivery.tsx
@@ -39,17 +39,6 @@ export const CheckAllThatApplyComponent: React.FC = () => {
 
   useEffect(() => {
     dispatch(initializeState(activityState, initialSelection(activityState)));
-
-    setTimeout(() => {
-      if (activityState.parts[0].response === null) {
-        onSaveActivity(activityState.attemptGuid, [
-          {
-            attemptGuid: activityState.parts[0].attemptGuid,
-            response: { input: '' },
-          },
-        ]);
-      }
-    }, 0);
   }, []);
 
   // First render initializes state
