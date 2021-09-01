71ca80bd447aacfe6dfdc9f48a7e38987903fbfe
schowkekarats360 <schowkekar@ats360.com>
2021-09-01 14:54:46 +0530

    Update component_settings_test.ts

--- assets/test/advanced_authoring/right_menu/component/component_settings_test.ts
+++ assets/test/advanced_authoring/right_menu/component/component_settings_test.ts
@@ -7,9 +7,7 @@ import { componentData, transformedSchema } from './component_mocks';
 describe('transforming component data structure to schema and back to data structure', () => {
   it('should transform component data structure to schema and back to structure after changes', () => {
     const model = transformModelToSchema(componentData);
-    console.log(model);
     const changes = transformSchemaToModel(model);
-    console.log(changes);
     expect(changes).toMatchObject(transformedSchema);
     expect(changes.custom).toMatchObject(transformedSchema.custom);
   });
