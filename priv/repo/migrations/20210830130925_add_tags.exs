3065a4d7fa44d288e907a5e37ca97202dba10987
Darren Siegel <siegel.darren@gmail.com>
2021-09-01 07:53:33 -0400

    add index to remove a seq scan from selection queries

--- priv/repo/migrations/20210830130925_add_tags.exs
+++ priv/repo/migrations/20210830130925_add_tags.exs
@@ -5,6 +5,10 @@ defmodule Oli.Repo.Migrations.AddTags do
     alter table(:revisions) do
       add :tags, {:array, :id}, default: [], null: false
     end
+
+    create unique_index(:published_resources, [:publication_id, :revision_id],
+             name: :index_published_resources_pub_rev
+           )
   end
 
 end
