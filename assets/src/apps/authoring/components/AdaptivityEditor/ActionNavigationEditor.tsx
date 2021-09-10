a770ec0c6aa82c44d9a696b3e3062417dee04b06
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-09-10 21:52:50 +0530

    [JAN-619 | PMP-1807] Screen picket in navigate-to field (#1499)
    * PMP-1807
    
    * Update ActionNavigationEditor.tsx
    
    * PMP-1807
    
    * hide some console spam
    
    Co-authored-by: Ben Sparks <bsparks@unicon.net>

--- assets/src/apps/authoring/components/AdaptivityEditor/ActionNavigationEditor.tsx
+++ assets/src/apps/authoring/components/AdaptivityEditor/ActionNavigationEditor.tsx
@@ -1,7 +1,16 @@
 import { NavigationAction, NavigationActionParams } from 'apps/authoring/types';
+import {
+  findInSequence,
+  getHierarchy,
+  SequenceEntry,
+  SequenceEntryChild,
+} from 'apps/delivery/store/features/groups/actions/sequence';
+import { selectSequence } from 'apps/delivery/store/features/groups/selectors/deck';
 import React, { useState } from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import guid from 'utils/guid';
+import ScreenDropdownTemplate from '../PropertyEditor/custom/ScreenDropdownTemplate';
 
 interface ActionNavigationEditorProps {
   action: NavigationAction;
@@ -11,13 +20,15 @@ interface ActionNavigationEditorProps {
 
 const ActionNavigationEditor: React.FC<ActionNavigationEditorProps> = (props) => {
   const { action, onChange, onDelete } = props;
-  const [target, setTarget] = useState(action?.params?.target || '');
+  const sequence = useSelector(selectSequence);
+  const selectedSequence = findInSequence(sequence, action?.params?.target);
+  const [target, setTarget] = useState(selectedSequence?.custom.sequenceId || 'next');
   const uuid = guid();
 
-  const handleTargetChange = (e: any) => {
-    const currentVal = e.target.value;
-    setTarget(currentVal);
-    onChange({ target: currentVal });
+  const onChangeHandler = (sequenceId: string) => {
+    // console.log('onChange picker', sequenceId);
+    onChange({ target: sequenceId || 'next' });
+    setTarget(sequenceId || 'next');
   };
 
   return (
@@ -32,32 +43,31 @@ const ActionNavigationEditor: React.FC<ActionNavigationEditorProps> = (props) =>
             Navigate To
           </div>
         </div>
-        <input
-          type="text"
-          className="form-control form-control-sm"
+        <ScreenDropdownTemplate
           id={`action-navigation-${uuid}`}
-          placeholder="SequenceId"
+          label=""
           value={target}
-          onChange={(e) => setTarget(e.target.value)}
-          onBlur={(e) => handleTargetChange(e)}
-          title={target}
+          onChange={onChangeHandler}
+          dropDownCSSClass="adaptivityDropdown form-control"
+          buttonCSSClass="form-control-sm"
         />
+
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 150, hide: 150 }}
+          overlay={
+            <Tooltip id="button-tooltip" style={{ fontSize: '12px' }}>
+              Delete Action
+            </Tooltip>
+          }
+        >
+          <span>
+            <button className="btn btn-link p-0 ml-1" onClick={() => onDelete(action)}>
+              <i className="fa fa-trash-alt" />
+            </button>
+          </span>
+        </OverlayTrigger>
       </div>
-      <OverlayTrigger
-        placement="top"
-        delay={{ show: 150, hide: 150 }}
-        overlay={
-          <Tooltip id="button-tooltip" style={{ fontSize: '12px' }}>
-            Delete Action
-          </Tooltip>
-        }
-      >
-        <span>
-          <button className="btn btn-link p-0 ml-1" onClick={() => onDelete(action)}>
-            <i className="fa fa-trash-alt" />
-          </button>
-        </span>
-      </OverlayTrigger>
     </div>
   );
 };
