52d2315c57ac041e8d4f22a486b2f24ae8c273a1
schowkekarats360 <74950354+schowkekarats360@users.noreply.github.com>
2021-09-10 20:31:20 +0530

    PMP-1804, PMP-1802 (#1505)

--- assets/src/components/parts/janus-slider/SliderAuthor.tsx
+++ assets/src/components/parts/janus-slider/SliderAuthor.tsx
@@ -1,17 +1,44 @@
 import { AuthorPartComponentProps } from 'components/parts/types/parts';
 import React, { CSSProperties, useEffect } from 'react';
 import { SliderModel } from './schema';
+import './Slider.scss';
 
 const SliderAuthor: React.FC<AuthorPartComponentProps<SliderModel>> = (props) => {
-  const { model } = props;
+  const { id, model } = props;
 
-  const { x, y, z, width } = model;
+  const {
+    x,
+    y,
+    z,
+    height,
+    width,
+    customCssClass,
+    label,
+    maximum = 1,
+    minimum = 0,
+    snapInterval,
+    showValueLabels,
+    showLabel,
+    invertScale,
+  } = model;
+
   const styles: CSSProperties = {
     width,
     zIndex: z,
     backgroundColor: 'magenta',
     overflow: 'hidden',
     fontWeight: 'bold',
+  };
+  const inputStyles: CSSProperties = {
+    width: '100%',
+    height: `${height}px`,
+    zIndex: z,
+    direction: invertScale ? 'rtl' : 'ltr',
+  };
+  const divStyles: CSSProperties = {
+    width: '100%',
+    display: `flex`,
+    flexDirection: 'row',
   };
 
   useEffect(() => {
@@ -20,8 +47,30 @@ const SliderAuthor: React.FC<AuthorPartComponentProps<SliderModel>> = (props) =>
   }, []);
 
   return (
-    <div style={styles}>
-      <p>Slider</p>
+    <div data-janus-type={tagName} style={styles} className={`slider`}>
+      <div className="sliderInner">
+        {showValueLabels && <label htmlFor={id}>{invertScale ? maximum : minimum}</label>}
+        <div className="rangeWrap">
+          <div style={divStyles}>
+            <input
+              disabled={true}
+              style={inputStyles}
+              min={minimum}
+              max={maximum}
+              type={'range'}
+              step={snapInterval}
+              className={` slider ` + customCssClass}
+              id={id}
+            />
+          </div>
+        </div>
+        {showValueLabels && <label htmlFor={id}>{invertScale ? minimum : maximum}</label>}
+      </div>
+      {showLabel && (
+        <label className="input-label" htmlFor={id}>
+          {label}
+        </label>
+      )}
     </div>
   );
 };
