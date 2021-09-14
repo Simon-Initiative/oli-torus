17d40dad86ef26bcfdc3410b38588dd0ed4e8a05
Eli Knebel <eliknebel@gmail.com>
2021-09-14 11:22:04 -0400

    fix an issue where no collaborators for a project crashes the projects view update changelog and version for hotfix

--- test/oli_web/live/projects_live_test.exs
+++ test/oli_web/live/projects_live_test.exs
@@ -1,8 +1,12 @@
-defmodule OliWeb.Projects.StateLogicTest do
-  use Oli.DataCase
+defmodule OliWeb.Projects.ProjectsLiveTest do
+  use OliWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
   alias Oli.Authoring.Course
   alias Oli.Accounts
   alias OliWeb.Projects.State
+  alias Oli.Seeder
 
   def merge_changes(changes, state) do
     Map.merge(state, Enum.reduce(changes, %{}, fn {k, v}, m -> Map.put(m, k, v) end))
@@ -40,5 +44,35 @@ defmodule OliWeb.Projects.StateLogicTest do
       assert state.sort_order == "desc"
       assert hd(state.projects).title == "Zebra"
     end
+  end
+
+  describe "projects live" do
+    setup [:setup_author_project]
+
+    test "loads correctly when there are no collaborators for a project", %{
+      conn: conn,
+      map: %{
+        author: author,
+        author2: author2
+      }
+    } do
+      {:ok, _author} = Accounts.delete_author(author)
+      {:ok, _author} = Accounts.delete_author(author2)
+
+      conn = get(conn, "/authoring/projects")
+
+      {:ok, _view, _} = live(conn)
+    end
+  end
+
+  defp setup_author_project(%{conn: conn}) do
+    map = Seeder.base_project_with_resource2()
+    admin = author_fixture(%{system_role_id: Oli.Accounts.SystemRole.role_id().admin})
+
+    conn =
+      Plug.Test.init_test_session(conn, lti_session: nil)
+      |> Pow.Plug.assign_current_user(admin, OliWeb.Pow.PowHelpers.get_pow_config(:author))
+
+    {:ok, conn: conn, map: map}
   end
 end
