5b71db75f791364deba26d0ddb4812b9de332255
Ben Sparks <bsparks@unicon.net>
2021-09-20 12:05:30 -0700

    factor windo piece out to separate comp

--- assets/src/components/parts/janus-popup/PopupAuthor.tsx
+++ assets/src/components/parts/janus-popup/PopupAuthor.tsx
@@ -1,10 +1,15 @@
 import { AuthorPartComponentProps } from 'components/parts/types/parts';
-import React, { CSSProperties, useEffect } from 'react';
+import React, { CSSProperties, useEffect, useState } from 'react';
 import { getIconSrc } from './GetIcon';
+import PopupWindow from './PopupWindow';
 import { PopupModel } from './schema';
+import { ContextProps } from './types';
 
 const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
   const { id, model } = props;
+
+  const [context, setContext] = useState<ContextProps>({ currentActivity: '', mode: '' });
+  const [showWindow, setShowWindow] = useState(false);
 
   const {
     x,
@@ -21,6 +26,26 @@ const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
     popup,
     description,
   } = model;
+
+  // need to offset the window position by the position of the parent element
+  // since it's a child of the parent element and not the activity (screen) directly
+  const offsetWindowConfig = {
+    ...popup.custom,
+    x: popup.custom.x - (x || 0),
+    y: popup.custom.y - (y || 0),
+  };
+
+  const [windowConfig, setWindowConfig] = useState<any>(offsetWindowConfig);
+  const [windowParts, setWindowParts] = useState<any[]>(popup.partsLayout || []);
+
+  useEffect(() => {
+    setWindowConfig(offsetWindowConfig);
+    setWindowParts(popup.partsLayout || []);
+  }, [props.model.popup]);
+
+  const handleWindowClose = () => {
+    setShowWindow(false);
+  };
 
   const iconSrc = getIconSrc(iconURL, defaultURL);
 
@@ -40,24 +65,37 @@ const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
   }, []);
 
   return (
-    <input
-      role="button"
-      draggable="false"
-      {...(iconSrc
-        ? {
-            src: iconSrc,
-            type: 'image',
-            alt: description,
-          }
-        : {
-            type: 'button',
-          })}
-      className={`info-icon`}
-      aria-controls={id}
-      aria-haspopup="true"
-      aria-label={description}
-      style={styles}
-    />
+    <React.Fragment>
+      <input
+        role="button"
+        draggable="false"
+        {...(iconSrc
+          ? {
+              src: iconSrc,
+              type: 'image',
+              alt: description,
+            }
+          : {
+              type: 'button',
+            })}
+        className={`info-icon`}
+        onDoubleClick={() => {
+          setShowWindow(true);
+        }}
+        aria-controls={id}
+        aria-haspopup="true"
+        aria-label={description}
+        style={styles}
+      />
+      {showWindow && (
+        <PopupWindow
+          config={windowConfig}
+          parts={windowParts}
+          context={context}
+          onClose={handleWindowClose}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
