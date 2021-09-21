f3512133b875d3157e1742d5e7f8ed4578d96252
Darren Siegel <siegel.darren@gmail.com>
2021-09-21 15:14:54 -0400

    open api codes for payment endpoint

--- lib/oli_web/controllers/api/payment_controller.ex
+++ lib/oli_web/controllers/api/payment_controller.ex
@@ -3,12 +3,80 @@ defmodule OliWeb.Api.PaymentController do
   Endpoint for payment code bulk request.
   """
 
+  alias OpenApiSpex.Schema
+
   alias Oli.Delivery.Paywall
   alias Oli.Delivery.Paywall.Payment
   import OliWeb.Api.Helpers
 
   use OliWeb, :controller
+  use OpenApiSpex.Controller
 
+  defmodule BatchPaymentCodeRequest do
+    require OpenApiSpex
+
+    OpenApiSpex.schema(%{
+      title: "Payment code batch request",
+      description: "The request body to create a batch of payment codes",
+      type: :object,
+      properties: %{
+        product_slug: %Schema{
+          type: :string,
+          description: "Product identifier to create codes against"
+        },
+        batch_size: %Schema{
+          type: :integer,
+          description: "The number of codes to create, from 1 to 500"
+        }
+      },
+      required: [:product_slug, :batch_size],
+      example: %{
+        "product_slug" => "intro_biology_cmu",
+        "batch_size" => 50
+      }
+    })
+  end
+
+  defmodule PaymentCodeBatch do
+    require OpenApiSpex
+
+    OpenApiSpex.schema(%{
+      title: "Batch Payment Code Creation Response",
+      description: "A collection of payment codes",
+      type: :object,
+      properties: %{
+        codes: %Schema{
+          type: :list,
+          description: "List of the generated payment codes, as strings"
+        },
+        result: %Schema{type: :string, description: "success"}
+      },
+      required: [:codes, :result],
+      example: %{
+        "result" => "success",
+        "codes" => [
+          "XKT-3RTH",
+          "4DM-99NM",
+          "1S3-WKRP",
+          "59V-WRTP",
+          "HM7-N34P"
+        ]
+      }
+    })
+  end
+
+  @doc """
+  Create a batch of payment codes for a specific product.
+  """
+  @doc parameters: [],
+       request_body:
+         {"Request body for making a payment code batch request", "application/json",
+          OliWeb.Api.PaymentController.BatchPaymentCodeRequest, required: true},
+       responses: %{
+         200 =>
+           {"Media Item Upload Response", "application/json",
+            OliWeb.Api.PaymentController.PaymentCodeBatch}
+       }
   def new(conn, %{"product_slug" => product_slug, "batch_size" => batch_size}) do
     if is_valid_api_key?(conn, &Oli.Interop.validate_for_payments/1) do
       case Paywall.create_payment_codes(product_slug, batch_size) do
