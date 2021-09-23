edf8b03de4ac9dbb6e5014bf1ab4c45abf864546
schowkekarats360 <schowkekar@ats360.com>
2021-09-23 19:12:36 +0530

    Update bankProps.ts

--- assets/src/apps/authoring/components/PropertyEditor/schemas/bankProps.ts
+++ assets/src/apps/authoring/components/PropertyEditor/schemas/bankProps.ts
@@ -32,6 +32,7 @@ const BankPropsSchema: JSONSchema7 = {
       type: 'object',
       properties: {
         backgroundColor: { type: 'string', title: 'Background Color' },
+        borderColor: { type: 'string', title: 'Border Color' },
         borderRadius: { type: 'string', title: 'Border Radius' },
         borderStyle: { type: 'string', title: 'Border Style' },
         borderWidth: { type: 'string', title: 'Border Width' },
