da9c1d918e54a5864697311017e13021466ac456
Devesh Tiwari <dtiwari@ats360.com>
2021-09-23 11:52:25 +0530

    code clean-up

--- assets/src/components/parts/janus-capi-iframe/CapiIframeAuthor.tsx
+++ assets/src/components/parts/janus-capi-iframe/CapiIframeAuthor.tsx
@@ -8,17 +8,16 @@ import { CapiVariable } from '../../../adaptivity/capi';
 import CapiVariablePicker from './CapiVariablePicker';
 
 const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (props) => {
-  const { onCancelConfigure, onSaveConfigure } = props;
+  const { model, configuremode, onCancelConfigure, onSaveConfigure } = props;
+  const { x, y, z, width, height, src, configData } = model;
+  const id: string = props.id;
   const [simFrame, setSimFrame] = useState<HTMLIFrameElement>();
   const messageListener = useRef<any>(null);
-  const { model, configuremode } = props;
   const [inConfigureMode, setInConfigureMode] = useState<boolean>(parseBoolean(configuremode));
-  const { x, y, z, width, height, src, configData } = model;
   const [configClicked, setconfigClicked] = useState(false);
-  const [key, setKey] = useState(props.id);
   const [ready, setReady] = useState<boolean>(false);
-  const id: string = props.id;
   const [internalState, setInternalState] = useState<any>([]);
+
   const styles: CSSProperties = {
     width,
     height,
@@ -52,53 +51,39 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
     authToken: string;
     version?: string;
     config: any;
-    /*
-      config:
-          context: "VIEWER"
-          lessonAttempt: 0
-          lessonId: 109535
-          questionId: "q:1540493294565:499"
-          servicesBaseUrl: "https://api.smartsparrow.com"
-          userData:
-              givenName: "Ben"
-              id: "4261745"
-              surname: "Sparks"
-  */
   }
-  const getCleanSimLife = () => ({
-    // these 2 are mysterious
-    key: '',
-    simId: '',
-    // ...
-    handshakeMade: false,
-    handshake: {
-      // should be of type CapiHandshake
-      requestToken: '',
-      authToken: props.id,
-      config: {},
-    },
-    init: false, // initial setup complete; this might be init state?
-    ready: false,
-    currentState: [],
-    ownerActivityId: 0,
-  });
-
-  const [simLife, setSimLife] = useState(getCleanSimLife());
   interface CapiMessage {
     handshake: CapiHandshake;
     options?: any; // ?? dunno
     type: JanusCAPIRequestTypes;
     values: any; // usually array, but sometimes more?
   }
-  useEffect(() => {
-    setInConfigureMode(parseBoolean(configuremode));
-  }, [configuremode]);
+
+  const getCleanSimLife = () => ({
+    key: '',
+    simId: '',
+    handshakeMade: false,
+    handshake: {
+      requestToken: '',
+      authToken: props.id,
+      config: {},
+    },
+    init: false,
+    ready: false,
+    currentState: [],
+    ownerActivityId: 0,
+  });
+
+  const [simLife, setSimLife] = useState(getCleanSimLife());
   const frameRef = (frame: HTMLIFrameElement) => {
-    /* console.log('%c DEBUG FRAME REF CALLBACK', 'background: darkred; color: #fff;', { frame }); */
     if (frame) {
       setSimFrame(frame);
     }
   };
+
+  useEffect(() => {
+    setInConfigureMode(parseBoolean(configuremode));
+  }, [configuremode]);
 
   const initialize = useCallback(async (pModel) => {
     const initResult = await props.onInit({
@@ -111,13 +96,18 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
   useEffect(() => {
     initialize(model);
   }, []);
+
   useEffect(() => {
     // all activities *must* emit onReady
     props.onReady({ id: `${props.id}` });
   }, [ready]);
+
+  //Methods for CAPI listener starts from her. Eventually this will move to a common place where authroing and delivery both can share the same JS file
+
   const sendToIframe = (data: any) => {
     simFrame?.contentWindow?.postMessage(JSON.stringify(data), '*');
   };
+
   const sendFormedResponse = (
     handshake: CapiHandshake,
     options: any,
@@ -191,6 +181,7 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
 
     return;
   };
+
   useEffect(() => {
     if (!simFrame) {
       return;
@@ -247,10 +238,9 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
     };
   }, [simFrame]);
 
+  //Methods for CAPI listener End her
+
   const handleValueChangeFromModal = (changedVar: any) => {
-    //const filterVars = createCapiObjectFromStateVars(changedVar);
-    //sendFormedResponse(simLife.handshake, {}, JanusCAPIRequestTypes.VALUE_CHANGE, changedVar);
-    //setInConfigureMode(false);
     const finalConfigData = internalState.map((variable: CapiVariable) => {
       if (variable.key === changedVar.key) {
         variable.value = changedVar.value;
@@ -259,11 +249,11 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
     });
     setInternalState(finalConfigData);
   };
+
   const handleEditorSave = (changeOperations: any) => {
     if (!inConfigureMode) {
       return;
     }
-    console.log('handleEditorSave called', { changeOperations });
     setconfigClicked(false);
     setInConfigureMode(false);
     const modelClone = clone(model);
@@ -273,19 +263,19 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
       id,
       snapshot: modelClone,
     });
-    setKey(`${props.id}_${configClicked}`);
   };
 
   const handleEditorCancel = () => {
     if (!inConfigureMode) {
       return;
     } // not mine
-    // console.log('TF EDITOR CANCEL');
     setInConfigureMode(false);
     onCancelConfigure({ id });
     setconfigClicked(false);
-    setKey(`${props.id}_${configClicked}`);
   };
+
+  //inConfigureMode = true means user has clicked on the Edit button that opens modal for updating the CAPI variables.
+  //configClicked = true means user has clicked on the link to load the capi in auhtoring mode.
 
   const renderIt = inConfigureMode ? (
     ReactDOM.createPortal(
@@ -301,7 +291,7 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
   ) : (
     <React.Fragment>
       <div style={styles}>
-        {configClicked && (
+        {configClicked ? (
           <iframe
             ref={frameRef}
             style={{ height: '100%', width: '100%' }}
@@ -309,8 +299,7 @@ const CapiIframeAuthor: React.FC<AuthorPartComponentProps<CapiIframeModel>> = (p
             src={props.model.src}
             scrolling={props.type?.toLowerCase() === 'janus-capi-iframe' ? 'no' : ''}
           />
-        )}
-        {!configClicked && (
+        ) : (
           <div className="container h-100">
             <div className="row h-100 justify-content-center align-items-center">
               <div>
