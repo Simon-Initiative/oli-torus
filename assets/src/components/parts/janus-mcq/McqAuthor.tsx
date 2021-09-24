99edc289c1b74e2a029b06ba0bf992e53b9027e8
schowkekarats360 <schowkekar@ats360.com>
2021-09-24 18:00:44 +0530

    PMP-1803

--- assets/src/components/parts/janus-mcq/McqAuthor.tsx
+++ assets/src/components/parts/janus-mcq/McqAuthor.tsx
@@ -1,11 +1,23 @@
 import { AuthorPartComponentProps } from 'components/parts/types/parts';
 import React, { CSSProperties, useEffect } from 'react';
+import { MCQItem } from './MultipleChoiceQuestion';
 import { McqModel } from './schema';
 
 const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
-  const { model } = props;
+  const { id, model } = props;
 
-  const { x, y, z, width } = model;
+  const {
+    x = 0,
+    y = 0,
+    z = 0,
+    width,
+    multipleSelection,
+    mcqItems,
+    customCssClass,
+    layoutType,
+    height,
+    overrideHeight = false,
+  } = model;
   const styles: CSSProperties = {
     width,
     zIndex: z,
@@ -19,9 +31,43 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
     props.onReady({ id: `${props.id}` });
   }, []);
 
+  const options: any[] = mcqItems?.map((item: any, index: number) => ({
+    ...item,
+    index: index,
+    value: index + 1,
+  }));
+
+  let columns = 1;
+  if (customCssClass === 'two-columns') {
+    columns = 2;
+  }
+  if (customCssClass === 'three-columns') {
+    columns = 3;
+  }
+  if (customCssClass === 'four-columns') {
+    columns = 4;
+  }
+
   return (
-    <div style={styles}>
-      <p>MCQ</p>
+    <div data-janus-type={tagName} style={styles} className={`mcq-input`}>
+      {options?.map((item, index) => (
+        <MCQItem
+          index={index}
+          key={`${id}-item-${index}`}
+          totalItems={options.length}
+          layoutType={layoutType}
+          itemId={`${id}-item-${index}`}
+          groupId={`mcq-${id}`}
+          val={item.value}
+          {...item}
+          x={0}
+          y={0}
+          overrideHeight={overrideHeight}
+          disabled={false}
+          multipleSelection={multipleSelection}
+          columns={columns}
+        />
+      ))}
     </div>
   );
 };
