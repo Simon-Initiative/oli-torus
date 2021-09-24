b98bd606a5a152331572809274f030ed2e6b024a
Darren Siegel <siegel.darren@gmail.com>
2021-09-24 10:04:11 -0400

    migrate to surface based sortable table

--- lib/oli_web/live/products/listing.ex
+++ lib/oli_web/live/products/listing.ex
@@ -1,14 +1,13 @@
 defmodule OliWeb.Products.Listing do
-  use Surface.LiveComponent
-
+  use Surface.Component
+  alias OliWeb.Common.SortableTable.Table
   alias OliWeb.Common.Paging
-  alias Surface.Components.Link
-  alias OliWeb.Router.Helpers, as: Routes
 
-  prop products, :list, required: true
   prop total_count, :integer, required: true
-  prop offset, :integer, required: true
   prop limit, :integer, required: true
+  prop offset, :integer, required: true
+  prop table_model, :struct, required: true
+  prop sort, :event, required: true
   prop page_change, :event, required: true
 
   def render(assigns) do
@@ -16,48 +15,7 @@ defmodule OliWeb.Products.Listing do
     <div>
       {#if @total_count > 0}
         <Paging id="header_paging" total_count={@total_count} offset={@offset} limit={@limit} click={@page_change}/>
-
-        <table class="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>Product Title</th><th>Status</th><th>Required Payment</th><th>Base Project</th><th>Created</th>
-            </tr>
-          </thead>
-          <tbody>
-          {#for product <- Enum.slice(@products, @offset, @limit)}
-            <tr>
-              <td>
-                <Link
-                  label={product.title}
-                  to={Routes.live_path(@socket, OliWeb.Products.DetailsView, product.slug)}
-                />
-              </td>
-              <td>{product.status}</td>
-              <td>{
-                if product.requires_payment do
-                  case Money.to_string(product.amount) do
-                    {:ok, m} -> m
-                    _ -> "Yes"
-                  end
-                else
-                  "None"
-                end
-              }
-              </td>
-              <td>
-                <Link
-                  label={product.base_project.title}
-                  to={Routes.project_path(@socket, :overview, product.base_project.slug)}
-                />
-              </td>
-              <td>
-                {Timex.format!(product.inserted_at, "{relative}", :relative)}
-              </td>
-            </tr>
-          {/for}
-          </tbody>
-        </table>
-
+        <Table model={@table_model} sort={@sort}/>
         <Paging id="footer_paging" total_count={@total_count} offset={@offset} limit={@limit} click={@page_change}/>
       {#else}
         <p>No products exist</p>
--- lib/oli_web/live/products/products_view.ex
+++ lib/oli_web/live/products/products_view.ex
@@ -7,48 +7,50 @@ defmodule OliWeb.Products.ProductsView do
   alias Oli.Authoring.Course
   alias Oli.Accounts.Author
   alias Oli.Delivery.Sections.Blueprint
+  alias OliWeb.Common.Table.SortableTableModel
+  alias OliWeb.Router.Helpers, as: Routes
 
-  data creation_title, :string, default: ""
-  data breadcrumbs, :any, default: [Breadcrumb.new(%{full_title: "Course Products"})]
-  data products, :list, default: []
-  data total_count, :integer, default: 0
-  data offset, :integer, default: 0
-  data limit, :integer, default: 20
   prop is_admin_view, :boolean
   prop project, :any
   prop author, :any
+  prop breadcrumbs, :any, default: [Breadcrumb.new(%{full_title: "Course Products"})]
   data title, :string, default: "Course Products"
+
+  data creation_title, :string, default: ""
+  data products, :list, default: []
+  data tabel_model, :struct
+  data total_count, :integer, default: 0
+  data offset, :integer, default: 0
+  data limit, :integer, default: 20
 
   def mount(%{"project_id" => project_slug}, %{"current_author_id" => author_id}, socket) do
     author = Repo.get(Author, author_id)
     project = Course.get_project_by_slug(project_slug)
-
     products = Blueprint.list_for_project(project)
-    total_count = length(products)
 
-    {:ok,
-     assign(socket,
-       is_admin_view: false,
-       author: author,
-       project: project,
-       products: products,
-       total_count: total_count
-     )}
+    mount_as(author, false, products, project, socket)
   end
 
   def mount(_, %{"current_author_id" => author_id}, socket) do
     author = Repo.get(Author, author_id)
 
     products = Blueprint.list()
+    mount_as(author, true, products, nil, socket)
+  end
+
+  defp mount_as(author, is_admin_view, products, project, socket) do
     total_count = length(products)
+
+    {:ok, table_model} = OliWeb.Products.ProductsTableModel.new(products)
 
     {:ok,
      assign(socket,
-       is_admin_view: true,
+       is_admin_view: is_admin_view,
        author: author,
-       project: nil,
+       project: project,
        products: products,
-       total_count: total_count
+       total_count: total_count,
+       table_model: table_model
      )}
   end
 
@@ -62,7 +64,13 @@ defmodule OliWeb.Products.ProductsView do
         <hr/>
       {/if}
 
-      <Listing id="listing" products={@products} total_count={@total_count} offset={@offset} limit={@limit} page_change="page_change"/>
+      <Listing
+        table_model={@table_model}
+        total_count={@total_count}
+        offset={@offset}
+        limit={@limit}
+        sort="sort"
+        page_change="page_change"/>
 
     </div>
 
@@ -73,8 +81,31 @@ defmodule OliWeb.Products.ProductsView do
     {:noreply, assign(socket, creation_title: title)}
   end
 
-  def handle_event("page_change", %{"offset" => offset, "limit" => limit}, socket) do
-    {:noreply, assign(socket, offset: String.to_integer(offset), limit: String.to_integer(limit))}
+  def handle_event("page_change", %{"offset" => offset}, socket) do
+    {:noreply,
+     push_patch(socket,
+       to:
+         Routes.live_path(
+           socket,
+           __MODULE__,
+           get_patch_params(socket.assigns.table_model, String.to_integer(offset))
+         )
+     )}
+  end
+
+  def handle_event("sort", %{"sort_by" => sort_by}, socket) do
+    table_model =
+      SortableTableModel.update_sort_params(
+        socket.assigns.table_model,
+        String.to_existing_atom(sort_by)
+      )
+
+    offset = socket.assigns.offset
+
+    {:noreply,
+     push_patch(socket,
+       to: Routes.live_path(socket, __MODULE__, get_patch_params(table_model, offset))
+     )}
   end
 
   def handle_event("create", _, socket) do
@@ -91,5 +122,33 @@ defmodule OliWeb.Products.ProductsView do
       {:error, _} ->
         {:noreply, put_flash(socket, :error, "Could not create product")}
     end
+  end
+
+  defp get_patch_params(table_model, offset) do
+    Map.merge(%{offset: offset}, SortableTableModel.to_params(table_model))
+  end
+
+  def handle_params(params, _, socket) do
+    offset =
+      case params["offset"] do
+        nil ->
+          0
+
+        offset ->
+          case Integer.parse(offset) do
+            {num, _} -> num
+            _ -> 0
+          end
+      end
+
+    tabel_model =
+      Map.put(
+        socket.assigns.table_model,
+        :rows,
+        Enum.slice(socket.assigns.products, offset, socket.assigns.limit)
+      )
+      |> SortableTableModel.update_from_params(params)
+
+    {:noreply, assign(socket, table_model: tabel_model, offset: offset)}
   end
 end
