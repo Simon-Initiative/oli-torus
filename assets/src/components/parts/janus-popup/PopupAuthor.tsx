379facfbc1680bcd91ae12294bad24a65a4e64dd
Ben Sparks <384189+bsparks@users.noreply.github.com>
2021-09-30 08:41:57 -0700

    [FEATURE] Popup & Feedback Authoring Part 1 (#1594)
    * cleanup console spam
    
    * factor windo piece out to separate comp
    
    * adjust z values for popup
    
    * WIP popup layout designer
    
    * start to add style to designer
    
    * WIP popup designer
    
    * factor out LayoutEditor for reuse
    
    * factor out layout editor from adaptive author
    
    * can open and close popup editor, move things around and add new parts
    
    * five min feedback...
    
    * WIP refactor for feedback authoring
    
    * almost able to save feedback changes
    
    * enable saving of feedback changes
    
    * temp disable configure for popup until feature is ready

--- assets/src/components/parts/janus-popup/PopupAuthor.tsx
+++ assets/src/components/parts/janus-popup/PopupAuthor.tsx
@@ -1,10 +1,24 @@
-import { AuthorPartComponentProps } from 'components/parts/types/parts';
-import React, { CSSProperties, useEffect } from 'react';
+import { AnyPartComponent, AuthorPartComponentProps } from 'components/parts/types/parts';
+import React, { CSSProperties, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { parseBoolean } from 'utils/common';
 import { getIconSrc } from './GetIcon';
+import PopupWindow from './PopupWindow';
+import PopupWindowDesigner from './PopupWindowDesigner';
 import { PopupModel } from './schema';
+import { ContextProps } from './types';
 
 const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
-  const { id, model } = props;
+  const { id, model, configuremode, onConfigure, onCancelConfigure, onSaveConfigure } = props;
+
+  const [inConfigureMode, setInConfigureMode] = useState<boolean>(parseBoolean(configuremode));
+  useEffect(() => {
+    // console.log('PopupAuthor configuremode changed!!', configuremode);
+    setInConfigureMode(parseBoolean(configuremode));
+  }, [configuremode]);
+
+  const [context, setContext] = useState<ContextProps>({ currentActivity: '', mode: '' });
+  const [showWindow, setShowWindow] = useState(false);
 
   const {
     x,
@@ -21,6 +35,27 @@ const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
     popup,
     description,
   } = model;
+
+  // need to offset the window position by the position of the parent element
+  // since it's a child of the parent element and not the activity (screen) directly
+  const offsetWindowConfig = {
+    ...popup.custom,
+    x: popup.custom.x - (x || 0),
+    y: popup.custom.y - (y || 0),
+    z: Math.max(z || 0, popup.custom.z || 0),
+  };
+
+  const [windowConfig, setWindowConfig] = useState<any>(offsetWindowConfig);
+  const [windowParts, setWindowParts] = useState<any[]>(popup.partsLayout || []);
+
+  useEffect(() => {
+    setWindowConfig(offsetWindowConfig);
+    setWindowParts(popup.partsLayout || []);
+  }, [props.model.popup]);
+
+  const handleWindowClose = () => {
+    setShowWindow(false);
+  };
 
   const iconSrc = getIconSrc(iconURL, defaultURL);
 
@@ -39,25 +74,61 @@ const PopupAuthor: React.FC<AuthorPartComponentProps<PopupModel>> = (props) => {
     props.onReady({ id: `${props.id}` });
   }, []);
 
+  const handleDesignerSave = (parts: AnyPartComponent[]) => {
+    console.log('POPUP AUTHOR DESIGNER SAVE', parts);
+  };
+
+  const handleDesignerCancel = () => {
+    onConfigure({ id: `${props.id}`, configure: false });
+    setInConfigureMode(false);
+  };
+
+  const Designer = () => {
+    // console.log('PopupAuthor: Designer', props.portal);
+    return ReactDOM.createPortal(
+      <PopupWindowDesigner
+        config={windowConfig}
+        parts={windowParts}
+        onSave={handleDesignerSave}
+        onCancel={handleDesignerCancel}
+      />,
+      document.getElementById(props.portal) as Element,
+    );
+  };
+
   return (
-    <input
-      role="button"
-      draggable="false"
-      {...(iconSrc
-        ? {
-            src: iconSrc,
-            type: 'image',
-            alt: description,
-          }
-        : {
-            type: 'button',
-          })}
-      className={`info-icon`}
-      aria-controls={id}
-      aria-haspopup="true"
-      aria-label={description}
-      style={styles}
-    />
+    <React.Fragment>
+      {inConfigureMode && <Designer />}
+      <input
+        role="button"
+        draggable="false"
+        {...(iconSrc
+          ? {
+              src: iconSrc,
+              type: 'image',
+              alt: description,
+            }
+          : {
+              type: 'button',
+            })}
+        className={`info-icon`}
+        onDoubleClick={() => {
+          setShowWindow(true);
+        }}
+        aria-controls={id}
+        aria-haspopup="true"
+        aria-label={description}
+        style={styles}
+      />
+      {showWindow && (
+        <PopupWindow
+          config={windowConfig}
+          parts={windowParts}
+          context={context}
+          onClose={handleWindowClose}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
