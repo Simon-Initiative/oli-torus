b109455d38d728c4c0bbec7f692e4d93f7cd8583
Darren Siegel <siegel.darren@gmail.com>
2021-10-04 12:46:31 -0400

    apply changes to select_source lv

--- lib/oli_web/live/delivery/select_source.ex
+++ lib/oli_web/live/delivery/select_source.ex
@@ -19,6 +19,7 @@ defmodule OliWeb.Delivery.SelectSource do
   data offset, :integer, default: 0
   data limit, :integer, default: 20
   data filter, :string, default: ""
+  data applied_filter, :string, default: ""
 
   @table_filter_fn &OliWeb.Delivery.SelectSource.filter_rows/2
   @table_push_patch_path &OliWeb.Delivery.SelectSource.live_path/2
@@ -83,12 +84,12 @@ defmodule OliWeb.Delivery.SelectSource do
     ~F"""
     <div>
 
-      <Filter id="filter" change={"change_filter"} reset="reset_filter"/>
+      <Filter apply={"apply_filter"} change={"change_filter"} reset="reset_filter"/>
 
       <div class="mb-3"/>
 
       <Listing
-        filter={@filter}
+        filter={@applied_filter}
         table_model={@table_model}
         total_count={@total_count}
         offset={@offset}
