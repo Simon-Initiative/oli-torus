6294fe113a997cf33c41fde3829c763da268fd80
Devesh Tiwari <70621864+dtiwarATS@users.noreply.github.com>
2021-10-04 20:23:28 +0530

    PMP-1933 | JAN-744 (#1611)

--- assets/src/components/parts/janus-popup/Popup.tsx
+++ assets/src/components/parts/janus-popup/Popup.tsx
@@ -55,10 +55,6 @@ const Popup: React.FC<PartComponentProps<PopupModel>> = (props) => {
     if (isOpen !== undefined) {
       setShowPopup(isOpen);
     }
-    const openByDefault: boolean | undefined = currentStateSnapshot[`stage.${id}.openByDefault`];
-    if (openByDefault !== undefined) {
-      setShowPopup(openByDefault);
-    }
     const isVisible = currentStateSnapshot[`stage.${id}.visible`];
     if (isVisible !== undefined) {
       setPopupVisible(isVisible);
