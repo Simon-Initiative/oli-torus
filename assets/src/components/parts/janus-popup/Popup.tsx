236ed745136ca5aa351914710006b8e97545eec6
Devesh Tiwari <70621864+dtiwarATS@users.noreply.github.com>
2021-10-12 20:41:44 +0530

    [BUG] Popup Issues (#1669)
    * Refactoring Pop-up implementation
    
    * more changes
    
    * PR Suggestions

--- assets/src/components/parts/janus-popup/Popup.tsx
+++ assets/src/components/parts/janus-popup/Popup.tsx
@@ -251,6 +251,15 @@ const Popup: React.FC<PartComponentProps<PopupModel>> = (props) => {
       ],
     });
   };
+  useEffect(() => {
+    const popupModalZ = config?.z || 1000;
+    const zIndexIcon = z || 0;
+    const finalZIndex = Math.max(zIndexIcon + popupModalZ, popupModalZ);
+    const modifiedData = { zIndex: { value: finalZIndex } };
+    if (finalZIndex) {
+      props.onResize({ id: `${id}`, settings: modifiedData });
+    }
+  }, [showPopup, model]);
   const handlePartInit = () => {
     return initSnapshot;
   };
