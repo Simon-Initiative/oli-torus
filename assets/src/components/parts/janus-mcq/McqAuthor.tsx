5d59a692e4b7e52326b8b26b4772f0ad25fd3b97
Devesh Tiwari <dtiwari@ats360.com>
2021-10-27 11:04:02 +0530

    More changes

--- assets/src/components/parts/janus-mcq/McqAuthor.tsx
+++ assets/src/components/parts/janus-mcq/McqAuthor.tsx
@@ -6,6 +6,10 @@ import { MarkupTree } from '../janus-text-flow/TextFlow';
 import { MCQItem } from './MultipleChoiceQuestion';
 import { McqModel } from './schema';
 import { registerEditor, tagName as quillEditorTagName } from '../janus-text-flow/QuillEditor';
+import {
+  NotificationType,
+  subscribeToNotification,
+} from 'apps/delivery/components/NotificationContext';
 
 // eslint-disable-next-line react/display-name
 const Editor: React.FC<any> = React.memo(({ html, tree, portal }) => {
@@ -43,20 +47,20 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
     width,
   };
   const [inConfigureMode, setInConfigureMode] = useState<boolean>(parseBoolean(configuremode));
+  const [textNodes, setTextNodes] = useState<any[]>([]);
   const [windowModel, setWindowModel] = useState<any>(model);
   const [ready, setReady] = useState<boolean>(false);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
   useEffect(() => {
-    /* console.log('MCQAuthor configuremode changed!!', configuremode); */
     setInConfigureMode(parseBoolean(configuremode));
   }, [configuremode]);
 
   const handleNotificationSave = useCallback(async () => {
     const modelClone = clone(model);
-    modelClone.popup = windowModel;
-    // console.log('PA:NOTIFYSAVE', { id, modelClone, windowModel });
+    mcqItems[currentIndex].nodes = textNodes;
     await onSaveConfigure({ id, snapshot: modelClone });
     setInConfigureMode(false);
-  }, [windowModel, model]);
+  }, [model, textNodes, currentIndex, mcqItems]);
 
   const [portalEl, setPortalEl] = useState<HTMLElement | null>(null);
   const initialize = useCallback(async (pModel) => {
@@ -67,8 +71,6 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
     initialize(model);
   }, []);
   useEffect(() => {
-    console.log({ portal: props.portal });
-
     // timeout to give modal a moment to load
     setTimeout(() => {
       const el = document.getElementById(props.portal);
@@ -78,10 +80,7 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
       }
     }, 10);
   }, [inConfigureMode, props.portal]);
-  const handleScreenAuthorChange = (changedScreen: any) => {
-    /* console.log('POPUP AUTHOR SCREEN AUTHOR CHANGE', changedScreen); */
-    /* setWindowModel(changedScreen); */
-  };
+
   useEffect(() => {
     // all activities *must* emit onReady
     props.onReady({ id: `${props.id}` });
@@ -92,12 +91,75 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
   }, []);
 
   useEffect(() => {
+    if (!props.notify) {
+      return;
+    }
+    const notificationsHandled = [
+      NotificationType.CONFIGURE,
+      NotificationType.CONFIGURE_SAVE,
+      NotificationType.CONFIGURE_CANCEL,
+    ];
+    const notifications = notificationsHandled.map((notificationType: NotificationType) => {
+      const handler = (payload: any) => {
+        /* console.log(`${notificationType.toString()} notification event [PopupAuthor]`, payload); */
+        if (!payload) {
+          // if we don't have anything, we won't even have an id to know who it's for
+          // for these events we need something, it's not for *all* of them
+          return;
+        }
+        switch (notificationType) {
+          case NotificationType.CONFIGURE:
+            {
+              const { partId, configure } = payload;
+              if (partId === id) {
+                /* console.log('TF:NotificationType.CONFIGURE', { partId, configure }); */
+                // if it's not us, then we shouldn't be configuring
+                setInConfigureMode(configure);
+                if (configure) {
+                  onConfigure({ id, configure, context: { fullscreen: false } });
+                }
+              }
+            }
+            break;
+          case NotificationType.CONFIGURE_SAVE:
+            {
+              const { id: partId } = payload;
+              if (partId === id) {
+                /*
+                console.log('TF:NotificationType.CONFIGURE_SAVE', { partId, payload }); */
+                handleNotificationSave();
+              }
+            }
+            break;
+          case NotificationType.CONFIGURE_CANCEL:
+            {
+              const { id: partId } = payload;
+              if (partId === id) {
+                /* console.log('TF:NotificationType.CONFIGURE_CANCEL', { partId }); */
+                setInConfigureMode(false);
+              }
+            }
+            break;
+        }
+      };
+      const unsub = subscribeToNotification(props.notify, notificationType, handler);
+      return unsub;
+    });
+    return () => {
+      notifications.forEach((unsub) => {
+        unsub();
+      });
+    };
+  }, [props.notify, handleNotificationSave]);
+
+  useEffect(() => {
     const handleEditorSave = (e: any) => {
+      /* console.log('TF EDITOR SAVE'); */
       if (!inConfigureMode) {
         return;
       } // not mine
       const { payload, callback } = e.detail;
-      // console.log('TF EDITOR SAVE', { payload, callback, props });
+      /* console.log('TF EDITOR SAVE', { payload, callback, props }); */
       const modelClone = clone(model);
       modelClone.nodes = payload;
       // optimistic update
@@ -122,8 +184,8 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
         return;
       } // not mine
       const { payload, callback } = e.detail;
-      console.log('MCQ EDITOR CHANGE', { payload, callback });
-      //setTextNodes(payload.value);
+      /* console.log('MCQ EDITOR CHANGE', { payload, callback }); */
+      setTextNodes(payload.value);
     };
 
     if (inConfigureMode) {
@@ -162,6 +224,7 @@ const McqAuthor: React.FC<AuthorPartComponentProps<McqModel>> = (props) => {
     } else if (Array.isArray(mcqItems[index].nodes)) {
       setTree(mcqItems[index].nodes);
     }
+    setTextNodes(mcqItems[index].nodes);
     onConfigure({ id, configure: true, context: { fullscreen: false } });
     setInConfigureMode(true);
   };
