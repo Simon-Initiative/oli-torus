7f79754b7863114043b413328c258adf0ab1e028
Devesh Tiwari <dtiwari@ats360.com>
2021-10-28 11:29:08 +0530

    PMP-1219 | JAN-124

--- assets/src/apps/delivery/components/ActivityRenderer.tsx
+++ assets/src/apps/delivery/components/ActivityRenderer.tsx
@@ -56,6 +56,11 @@ const defaultHandler = async () => {
 // because of events and function references, we need to store state outside of the function
 const sharedAttemptStateMap = new Map();
 
+const AllAttemptStateList: {
+  activityId: string | undefined;
+  attemptGuid: string;
+  attempt: unknown;
+}[] = [];
 // the activity renderer should be capable of handling *any* activity type, not just adaptive
 // most events should be simply bubbled up to the layout renderer for handling
 const ActivityRenderer: React.FC<ActivityRendererProps> = ({
@@ -226,7 +231,12 @@ const ActivityRenderer: React.FC<ActivityRendererProps> = ({
       let isForMe = false;
 
       const currentAttempt = sharedAttemptStateMap.get(activity.id);
-      if (attemptGuid === currentAttempt.attemptGuid) {
+      const currentActivityAllAttempt = AllAttemptStateList.filter(
+        (activityAttempt) =>
+          activityAttempt.activityId === activity.id && activityAttempt.attemptGuid === attemptGuid,
+      );
+
+      if (attemptGuid === currentAttempt.attemptGuid || currentActivityAllAttempt?.length) {
         /* console.log('EVENT FOR ME', { e, activity, attempt }); */
         isForMe = true;
       }
@@ -295,6 +305,11 @@ const ActivityRenderer: React.FC<ActivityRendererProps> = ({
       const currentAttempt = sharedAttemptStateMap.get(activity.id);
       if (currentAttempt.activityId === lastCheckResults.attempt.activityId) {
         sharedAttemptStateMap.set(activity.id, lastCheckResults.attempt);
+        AllAttemptStateList.push({
+          activityId: activity?.id,
+          attemptGuid: lastCheckResults.attempt.attemptGuid,
+          attempt: lastCheckResults.attempt,
+        });
       }
       notifyCheckComplete(lastCheckResults);
     }
