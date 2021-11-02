6e3eccefa28d704c5a47b5a0d0801f4dc65c2960
Eli Knebel <eliknebel@cmu.edu>
2021-11-02 20:40:01 +0000

    remove db constraint change query to take first result

--- priv/repo/migrations/20211102141706_ensure_unique_active_section.exs
+++
@@ -1,8 +0,0 @@
-defmodule Oli.Repo.Migrations.EnsureUniqueActiveSection do
-  use Ecto.Migration
-
-  def change do
-    # guarantee there is only ever one active section with a given context id
-    create unique_index(:sections, [:context_id], where: "status = 'active'", name: :sections_active_context_id_unique_index)
-  end
-end
