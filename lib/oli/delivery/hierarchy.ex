f1b213e23ccf7ef9f32abd2c2e0a03a198a49e66
Darren Siegel <siegel.darren@gmail.com>
2021-11-10 17:41:19 -0500

    implement the link map construction

--- lib/oli/delivery/hierarchy.ex
+++ lib/oli/delivery/hierarchy.ex
@@ -1,13 +1,11 @@
 defmodule Oli.Delivery.Hierarchy do
   @moduledoc """
   A module for hierarchy and HierarchyNode operations and utilities
-
   A delivery hierarchy is the main structure in which a course curriculum is organized
   to be delivered. It is mainly persisted through section resource records. A hierarchy is
   also a generic in-memory representation of a curriculum which can be passed into
   delivery-centric functions from an authoring context, in which case the hierarchy could
   be ephemeral and section_resources are empty (e.g. course preview)
-
   See also HierarchyNode for more details
   """
   import Oli.Utils
@@ -226,46 +224,6 @@ defmodule Oli.Delivery.Hierarchy do
   end
 
   @doc """
-  Given a hierarchy node, this function "flattens" all nodes below into a list, in the order that
-  a student would encounter the resources working linearly through a course.
-
-  As an example, consider the followign hierarchy:
-
-  --Unit 1
-  ----Module 1
-  ------Page A
-  ------Page B
-  --Unit 2
-  ----Moudule 2
-  ------Page C
-
-  The above would be flattened to:
-  Unit 1
-  Module 1
-  Page A
-  Page B
-  Unit 2
-  Module 2
-  Page C
-
-  """
-  def flatten(%HierarchyNode{} = root) do
-    flatten_helper(root, [], [])
-    |> Enum.reverse()
-  end
-
-  defp flatten_helper(%HierarchyNode{children: children}, flattened_nodes, current_ancestors) do
-    Enum.reduce(children, flattened_nodes, fn node, acc ->
-      node = %{node | ancestors: current_ancestors}
-
-      case Oli.Resources.ResourceType.get_type_by_id(node.revision.resource_type_id) do
-        "container" -> flatten_helper(node, [node | acc], current_ancestors ++ [node])
-        _ -> [node | acc]
-      end
-    end)
-  end
-
-  @doc """
   Given a course section hierarchy, this function builds a "navigation link map" for a course section that is
   used during delivery to efficiently render the "Previous" and "Next" page links.
 
@@ -303,7 +261,136 @@ defmodule Oli.Delivery.Hierarchy do
   determining prev and next links.
   """
   def build_navigation_link_map(%HierarchyNode{} = root) do
+    {map, _} =
+      flatten(root)
+      |> Enum.filter(fn node ->
+        Oli.Resources.ResourceType.get_type_by_id(node.revision.resource_type_id) == "page"
+      end)
+      |> Enum.reduce({%{}, nil}, fn node, {map, previous} ->
+        this_id = Integer.to_string(node.revision.resource_id)
+
+        this_entry = %{
+          "slug" => node.revision.slug,
+          "title" => node.revision.title,
+          "prev" =>
+            case previous do
+              nil -> nil
+              _ -> previous
+            end,
+          "next" => nil
+        }
+
+        map =
+          case previous do
+            nil ->
+              map
+
+            id ->
+              previous_entry = Map.get(map, id)
+              updated = Map.merge(previous_entry, %{"next" => this_id})
+              Map.put(map, id, updated)
+          end
+
+        {Map.put(map, this_id, this_entry), this_id}
+      end)
+
+    map
   end
+
+  @doc """
+  Given a hierarchy node, this function "flattens" all nodes below into a list, in the order that
+  a student would encounter the resources working linearly through a course.
+  As an example, consider the following hierarchy:
+  --Unit 1
+  ----Module 1
+  ------Page A
+  ------Page B
+  --Unit 2
+  ----Moudule 2
+  ------Page C
+  The above would be flattened to:
+  Unit 1
+  Module 1
+  Page A
+  Page B
+  Unit 2
+  Module 2
+  Page C
+  """
+  def flatten(%HierarchyNode{} = root) do
+    flatten_helper(root, [], [])
+    |> Enum.reverse()
+  end
+
+  defp flatten_helper(%HierarchyNode{children: children}, flattened_nodes, current_ancestors) do
+    Enum.reduce(children, flattened_nodes, fn node, acc ->
+      node = %{node | ancestors: current_ancestors}
+
+      case Oli.Resources.ResourceType.get_type_by_id(node.revision.resource_type_id) do
+        "container" -> flatten_helper(node, [node | acc], current_ancestors ++ [node])
+        _ -> [node | acc]
+      end
+    end)
+  end
+
+  @doc """
+  Builds a map that contains a list of gated ancestor resource_ids for each node. The list will
+  contain the resource_id of the node itself, if that resource is gated.
+  The keys of the map will be returned as strings, but the list of resource_ids themselves
+  will be integers. This is done this way for consistency because when this map gets
+  persisted to a the Postgres JSON datatype, these keys will be stored to strings.
+  Takes 2 parameters. First is the root of the hierarchy. Second is a gated_resource_id_map
+  which is a map containing the resource_ids of gated resources as keys (the value is ignored)
+  For example, consider the following hierarchy:
+  --Unit 1 (gated: true, resource_id: 1)
+  ----Module 1 (gated: true, resource_id: 2)
+  ------Page A (gated: true, resource_id: 3)
+  ------Page B (gated: false, resource_id: 4)
+  --Unit 2 (gated: false, resource_id: 5)
+  ----Moudule 2 (gated: false, resource_id: 6)
+  ------Page C (gated: true, resource_id: 7)
+  The following ancestry map will be returned:
+  %{
+    "1" => [1],
+    "2" => [2, 1],
+    "3" => [3, 2 ,1],
+    "4" => [2, 1],
+    "7" => [7]
+  }
+  """
+  def gated_ancestry_map(%HierarchyNode{} = root, %{} = gated_resource_id_map) do
+    gated_ancestry_map(root, %{}, [], gated_resource_id_map)
+  end
+
+  defp gated_ancestry_map(
+         %HierarchyNode{resource_id: resource_id, children: children},
+         index_map,
+         gated_ancestors,
+         gated_resource_id_map
+       ) do
+    # if this node is gated, then we add it to the list of gated ancestors
+    gated_ancestors =
+      if Map.has_key?(gated_resource_id_map, resource_id) do
+        [resource_id | gated_ancestors]
+      else
+        gated_ancestors
+      end
+
+    # if any ancestors are gated (including the current node), then add it to the index
+    index_map =
+      if Enum.empty?(gated_ancestors) == false do
+        Map.put(index_map, ensure_string(resource_id), gated_ancestors)
+      else
+        index_map
+      end
+
+    Enum.reduce(children, index_map, fn node, acc ->
+      gated_ancestry_map(node, acc, gated_ancestors, gated_resource_id_map)
+    end)
+  end
+
+  defp ensure_string(maybe_str) when is_binary(maybe_str), do: maybe_str
+  defp ensure_string(maybe_str) when is_integer(maybe_str), do: Integer.to_string(maybe_str)
 
   @doc """
   Debugging utility to inspect a hierarchy without all the noise. Choose which keys
