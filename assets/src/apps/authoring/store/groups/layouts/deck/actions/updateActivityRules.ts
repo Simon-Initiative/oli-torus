5877b70884286e7117aea40aff73d6d6a3b9d346
Ben Sparks <bsparks@unicon.net>
2021-11-23 16:52:58 -0700

    initial part to find references in rules

--- assets/src/apps/authoring/store/groups/layouts/deck/actions/updateActivityRules.ts
+++ assets/src/apps/authoring/store/groups/layouts/deck/actions/updateActivityRules.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { extractAllExpressionsFromText } from 'adaptivity/scripting';
 import { inferTypeFromOperatorAndValue } from 'apps/authoring/components/AdaptivityEditor/AdaptiveItemOptions';
+import { findInSequence } from 'apps/delivery/store/features/groups/actions/sequence';
 import { BulkActivityUpdate, bulkEdit } from 'data/persistence/activity';
 import { isEqual } from 'lodash';
 import { clone } from 'utils/common';
@@ -31,13 +33,40 @@ const updateNestedConditions = (conditions: any) => {
   });
 };
 
+const findReferencedActivitiesInConditions = (conditions: any) => {
+  const referencedActivities: Set<string> = new Set();
+
+  conditions.forEach((condition: any) => {
+    if (condition.fact && condition.fact.indexOf('|stage.') !== -1) {
+      const referencedSequenceId = condition.fact.split('|stage.')[0];
+      referencedActivities.add(referencedSequenceId);
+    }
+    if (typeof condition.value === 'string' && condition.value.indexOf('|stage.') !== -1) {
+      // value could have more than one reference inside it
+      const exprs = extractAllExpressionsFromText(condition.value);
+      exprs.forEach((expr: string) => {
+        if (expr.indexOf('|stage.') !== -1) {
+          const referencedSequenceId = expr.split('|stage.')[0];
+          referencedActivities.add(referencedSequenceId);
+        }
+      });
+    }
+    if (condition.any || condition.all) {
+      const childRefs = findReferencedActivitiesInConditions(condition.any || condition.all);
+      childRefs.forEach((ref) => referencedActivities.add(ref));
+    }
+  });
+
+  return Array.from(referencedActivities);
+};
+
 export const updateActivityRules = createAsyncThunk(
   `${GroupsSlice}/updateActivityRules`,
   async (deck: any, { dispatch, getState }) => {
     const rootState = getState() as any;
     const activitiesToUpdate: any[] = [];
 
-    /* console.log('RULE UPDATE', deck); */
+    console.log(`UPDATE RULES for ${deck.children.length} activities`, deck);
     deck.children.forEach((child: any) => {
       const childActivity = selectActivityById(rootState, child.resourceId);
 
@@ -51,6 +80,8 @@ export const updateActivityRules = createAsyncThunk(
       const activityRules = childActivity?.authoring.rules || [];
       const activityRulesClone = clone(activityRules);
 
+      const referencedSequenceIds: string[] = [];
+
       // ensure that all conditions and condition blocks are assigned an id
       activityRulesClone.forEach((rule: any) => {
         const { conditions } = rule;
@@ -61,17 +92,49 @@ export const updateActivityRules = createAsyncThunk(
           rootCondition.id = `b:${guid()}`;
         }
         updateNestedConditions(conditionsToUpdate);
+        referencedSequenceIds.push(...findReferencedActivitiesInConditions(conditionsToUpdate));
         rule.conditions = rootCondition;
       });
 
       const childActivityClone = clone(childActivity);
+      const referencedActivityIds: number[] = Array.from(new Set(referencedSequenceIds))
+        .map((id) => {
+          const sequenceItem = findInSequence(deck.children, id);
+          if (sequenceItem) {
+            return sequenceItem.resourceId;
+          } else {
+            console.warn(
+              `[updateActivityRules] could not find referenced activity ${id} in sequence`,
+              deck,
+            );
+          }
+        })
+        .filter((id) => id) as number[];
+      if (!referencedActivityIds.includes(childActivity.id as number)) {
+        referencedActivityIds.unshift(childActivity.id as number);
+      }
+      if (
+        !isEqual(
+          childActivityClone.authoring.activitiesRequiredForEvaluation,
+          referencedActivityIds,
+        )
+      ) {
+        console.log('RULE REFS: ', referencedActivityIds);
+        childActivityClone.authoring.activitiesRequiredForEvaluation = referencedActivityIds;
+        activitiesToUpdate.push(childActivityClone);
+      }
       childActivityClone.authoring.rules = activityRulesClone;
       /* console.log('CLONE RULES', { childActivityClone, childActivity }); */
       if (!isEqual(childActivity.authoring.rules, childActivityClone.authoring.rules)) {
         /* console.log('CLONE IS DIFFERENT!'); */
-        activitiesToUpdate.push(childActivityClone);
+        if (activitiesToUpdate.indexOf(childActivityClone) === -1) {
+          activitiesToUpdate.push(childActivityClone);
+        }
       }
     });
+
+    console.log(`${activitiesToUpdate.length} ACTIVITIES TO UPDATE: `, activitiesToUpdate);
+
     if (activitiesToUpdate.length) {
       dispatch(upsertActivities({ activities: activitiesToUpdate }));
       // TODO: write to server
