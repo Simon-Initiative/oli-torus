f828db1b775ff7a3ecca741fe1c18449adfc824e
Darren Siegel <siegel.darren@gmail.com>
2021-12-02 15:43:29 -0500

    fix copy_listener problems

--- assets/src/hooks/copy_listener.ts
+++ assets/src/hooks/copy_listener.ts
@@ -1,12 +1,17 @@
 export const CopyListener = {
   mounted() {
+    const clipTargetSelector = this.el.dataset['clipboardTarget'];
+    const el = this.el;
+    const originalHTML = this.el.innerHTML;
+
     this.el.addEventListener('click', (_e: any) => {
-      const targetText = document.querySelector(this.el.dataset['clipboard-target'])?.value;
-      const el = this.el;
+      const targetText = document.querySelector(clipTargetSelector)?.value;
 
       navigator.clipboard.writeText(targetText).then(function () {
         el.innerHTML = 'Copied!';
-        setTimeout(() => (el.innerHtml = '<i class="lar la-clipboard"></i> Copy'), 5000);
+        setTimeout(() => {
+          el.innerHTML = originalHTML;
+        }, 5000);
       });
     });
   },
