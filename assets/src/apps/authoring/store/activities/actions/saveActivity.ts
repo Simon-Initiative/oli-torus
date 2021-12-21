c6896709317db227adc5fca6e0743ffe3fe0e50b
Ben Sparks <bsparks@unicon.net>
2021-12-21 16:49:16 -0700

    update score when related screen is changed

--- assets/src/apps/authoring/store/activities/actions/saveActivity.ts
+++ assets/src/apps/authoring/store/activities/actions/saveActivity.ts
@@ -1,15 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { selectSequence } from 'apps/delivery/store/features/groups/selectors/deck';
 import { ActivityModelSchema } from 'components/activities/types';
 import { ObjectiveMap } from 'data/content/activity';
 import { ActivityUpdate, BulkActivityUpdate, bulkEdit, edit } from 'data/persistence/activity';
 import {
   ActivitiesSlice,
   IActivity,
+  selectActivityById,
+  selectAllActivities,
   upsertActivities,
   upsertActivity,
 } from '../../../../delivery/store/features/activities/slice';
 import { selectProjectSlug, selectReadOnly } from '../../app/slice';
-import { selectResourceId } from '../../page/slice';
+import { savePage } from '../../page/actions/savePage';
+import { selectResourceId, selectState as selectCurrentPage, updatePage } from '../../page/slice';
 
 export const saveActivity = createAsyncThunk(
   `${ActivitiesSlice}/saveActivity`,
@@ -38,6 +42,33 @@ export const saveActivity = createAsyncThunk(
         false,
       );
       /* console.log('EDIT SAVE RESULTS', { editResults }); */
+      const currentPage = selectCurrentPage(rootState);
+      if (!currentPage.custom.scoreFixed) {
+        // need to check if this update to an activity affects the total score
+        const oldActivityData = selectActivityById(rootState, activity.resourceId as number);
+        if (activity.content?.custom.maxScore !== oldActivityData?.content?.custom.maxScore) {
+          const allActivities = selectAllActivities(rootState);
+          const sequence = selectSequence(rootState);
+          const totalScore = sequence.reduce((acc, sequenceItem) => {
+            if (sequenceItem.custom.isLayer || sequenceItem.custom.isBank) {
+              return acc;
+            }
+            if (sequenceItem.resourceId === activity.resourceId) {
+              return acc + activity.content?.custom.maxScore;
+            }
+            const currActivity = allActivities.find((a) => a.id === sequenceItem.resourceId);
+            if (!currActivity) {
+              return acc;
+            }
+            return acc + (currActivity?.content?.custom?.maxScore || 0);
+          }, 0);
+          if (currentPage.custom.totalScore !== totalScore) {
+            console.log('updating total score', { totalScore, currentPage, activity });
+            await dispatch(savePage({ custom: { ...currentPage.custom, totalScore } }));
+            await dispatch(updatePage({ custom: { ...currentPage.custom, totalScore } }));
+          }
+        }
+      }
     }
 
     await dispatch(upsertActivity({ activity }));
