a8d5412ed87216ee68aa28462eb5f897a0529814
Ben Sparks <384189+bsparks@users.noreply.github.com>
2022-01-04 12:22:22 -0700

    sync approach using bindings (#2050)

--- assets/src/components/parts/janus-video/delivery-entry.ts
+++ assets/src/components/parts/janus-video/delivery-entry.ts
@@ -5,6 +5,7 @@ import {
   customEvents as apiCustomEvents,
   observedAttributes as apiObservedAttributes,
 } from '../partsApi';
+import { adaptivitySchema } from './schema';
 import Video from './Video';
 
 const observedAttributes: string[] = [...apiObservedAttributes];
@@ -13,4 +14,7 @@ const customEvents: any = { ...apiCustomEvents };
 register(Video, manifest.delivery.element, observedAttributes, {
   customEvents,
   shadow: false,
+  customApi: {
+    getAdaptivitySchema: async () => adaptivitySchema,
+  },
 });
