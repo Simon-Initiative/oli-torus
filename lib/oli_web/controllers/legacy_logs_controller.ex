144692e1fcf54a0cc7b7a0077a8afe340462b36c
Raphael Gachuhi <rgachuhi@cmu.edu>
2022-01-05 08:33:43 -0500

    ensure commits

--- lib/oli_web/controllers/legacy_logs_controller.ex
+++ lib/oli_web/controllers/legacy_logs_controller.ex
@@ -8,11 +8,9 @@ defmodule OliWeb.LegacyLogsController do
     doc = Map.get(conn.assigns, :raw_body)
     activity_attempt_guid = to_string(xpath(doc, ~x"//*/@external_object_id"))
     action = to_string(xpath(doc, ~x"//*/@action_id"))
-#    IO.inspect activity_attempt_guid
-#    IO.inspect action
 
-    Oli.Delivery.CustomActivityLogs.queue_or_create_activity_log(activity_attempt_guid, action, to_string(doc))
-#    Oli.Delivery.CustomLogs.Worker.perform_now(activity_attempt_guid, action, to_string(doc))
+#    Oli.Delivery.CustomActivityLogs.queue_or_create_activity_log(activity_attempt_guid, action, to_string(doc))
+    Oli.Delivery.CustomLogs.Worker.perform_now(activity_attempt_guid, action, to_string(doc))
 
     conn
     |> put_resp_content_type("text/xml")
