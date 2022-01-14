b27cb319c4a02e3a109d47298caa11ea17242fd1
James Rissler <risslerjames@gmail.com>
2022-01-14 07:33:09 -0600

    [BUG FIX] Update AWS REGION after deploy findings (#2099)

--- config/config.exs
+++ config/config.exs
@@ -138,7 +138,7 @@ config :lti_1p3,
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
-  region: System.get_env("AWS_REGION", "us-east-1")
+  region: [{:system, "AWS_REGION"}, :instance_role]
 
 # Configures Elixir's Logger
 config :logger, :console,
