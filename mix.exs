cb110cb7770ded5dd7f58a24239d5687d1483eb6
Mauricio Cassola <mauricio@wyeworks.com>
2022-01-17 14:41:41 -0300

    [MER-511] Add libcluster

--- mix.exs
+++ mix.exs
@@ -160,6 +160,7 @@ defmodule Oli.MixProject do
       {:jose, "~> 1.10"},
       {:lti_1p3, "~> 0.4.2"},
       {:lti_1p3_ecto_provider, "~> 0.3.1"},
+      {:libcluster, "~> 3.3"},
       {:mime, "~> 1.2"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:mox, "~> 0.5", only: [:test, :hound]},
