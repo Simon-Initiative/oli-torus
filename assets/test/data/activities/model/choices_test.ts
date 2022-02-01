df398dd2ad58797fe48f4b99d36f88e85e659cc2
Darren Siegel <siegel.darren@gmail.com>
2022-02-01 09:22:07 -0500

    ensure numbers as strings work

--- assets/test/data/activities/model/choices_test.ts
+++ assets/test/data/activities/model/choices_test.ts
@@ -4,12 +4,13 @@ describe('choices test', () => {
   const model = {
     stem: { content: { model: [{ type: 'p', children: [{ text: 'test' }] }] } },
     choices: [
-      { id: 'yes', content: { model: [{ type: 'p', children: [{ text: 'test' }] }] } },
+      { id: '111', content: { model: [{ type: 'p', children: [{ text: 'test' }] }] } },
       { id: 'no', content: { model: [{ type: 'p', children: [{ text: 'test' }] }] } },
     ],
   };
 
   it('finds one', () => {
     expect(Choices.getOne(model, 'no')).toEqual(model.choices[1]);
+    expect(Choices.getOne(model, '111')).toEqual(model.choices[0]);
   });
 });
