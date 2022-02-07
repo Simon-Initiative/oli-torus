dbdace5e5c792cb44c5c5e58c6586bffe1250962
Devesh Tiwari <dtiwari@ats360.com>
2022-02-07 10:17:08 +0530

    PR Changes

--- assets/src/adaptivity/capi.ts
+++ assets/src/adaptivity/capi.ts
@@ -1,5 +1,10 @@
 import { ConditionProperties } from 'json-rules-engine';
-import { isStringArray, parseArray, parseBoolean, parseNumString } from 'utils/common';
+import {
+  isStringArray,
+  parseArray,
+  parseArrayWithoutStringConversion,
+  parseBoolean,
+} from 'utils/common';
 
 export enum CapiVariableTypes {
   NUMBER = 1,
@@ -66,7 +71,7 @@ export const coerceCapiValue = (
       return String(parseBoolean(value)); // for some reason these need to be strings
     case CapiVariableTypes.ARRAY:
     case CapiVariableTypes.ARRAY_POINT:
-      return shouldConvertNumbers ? parseArray(value) : parseArrayWithStringConversion(value);
+      return shouldConvertNumbers ? parseArray(value) : parseArrayWithoutStringConversion(value);
     default:
       return `${value}`;
   }
@@ -106,64 +111,6 @@ export interface JanusConditionProperties extends ConditionProperties {
   id: string;
   type?: CapiVariableTypes;
 }
-export const parseArrayWithStringConversion = (val: unknown): unknown[] => {
-  if (Array.isArray(val)) {
-    return val;
-  }
-
-  if (isStringArray(val)) {
-    try {
-      // its possible that we just get arrays of numbers which this should
-      // work fine for or even a normal stringified array
-      const json = JSON.parse(val as string);
-      if (Array.isArray(json)) {
-        return json.map((item: any) => {
-          return typeof item === 'number' ? item.toString() : item;
-        });
-      }
-    } catch (err) {
-      // guess it wasn't valid, now we'll try to parse it
-    }
-    const inner = (val as string).substring(1, (val as string).length - 1);
-    const isNested = isStringArray(inner);
-    if (isNested) {
-      // NOTE this will only support ONE level of nesting
-      // otherwise the comma will break it again
-      // maybe there is some better regex way
-      // tagging them with newline just for something to target for the split
-      const innerEls = inner
-        .replace(/\], \[/g, '],\n[')
-        .replace(/\],\[/g, '],\n[')
-        .split(/,\n/g);
-      return innerEls.map(parseArrayWithStringConversion);
-    } else {
-      const elements = inner.split(',');
-      const isEmpty = elements.length === 1 && elements[0] === '';
-      const parsedArray = isEmpty ? [] : elements;
-      if (isNested) {
-        return parsedArray.map(parseArrayWithStringConversion);
-      }
-      return parsedArray.map((element) => {
-        if (typeof element !== 'string') return element;
-        if (element.match(/^\s+$/)) {
-          return element;
-        } else {
-          return element.trim();
-        }
-      });
-    }
-  }
-
-  if (!val) {
-    return [];
-  } else if (typeof val === 'string') {
-    return val.split(',');
-  }
-  // if we hit this, it was something WAY off
-  const err = new Error('not a valid array');
-  throw err;
-};
-
 export class CapiVariable {
   public key: string;
   public type: CapiVariableTypes;
