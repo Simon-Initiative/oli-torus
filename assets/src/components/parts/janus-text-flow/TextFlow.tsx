0040a0fb225f32050431f6e8986c60a684583f39
Devesh Tiwari <dtiwari@ats360.com>
2022-02-21 13:08:27 +0530

    PMP-2509 | JAN-1350

--- assets/src/components/parts/janus-text-flow/TextFlow.tsx
+++ assets/src/components/parts/janus-text-flow/TextFlow.tsx
@@ -229,9 +229,12 @@ const TextFlow: React.FC<PartComponentProps<TextFlowModel>> = (props: any) => {
 
       let bgColor = 'transparent';
       if (palette.fillColor >= 0) {
-        bgColor = chroma(palette.fillColor || 0)
-          .alpha(palette.fillAlpha || 0)
-          .css();
+        const color = chroma(palette.fillColor || 0).alpha(palette.fillAlpha || 0);
+        // for few use cases, chroma(palette.fillColor || 0).alpha(palette.fillAlpha || 0) was sometimes getting evaluated to a numeric number(i.e. 1)
+        //hence 1.css() was throwing error as it is not a supported fuction for numbers.
+        if (typeof color === 'object') {
+          bgColor = color.css();
+        }
       }
       styles.backgroundColor = bgColor;
     }
