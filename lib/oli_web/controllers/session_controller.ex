5b085f4a5df53d50dcbf5930e7d2bde18c7dd116
Mauricio Cassola <mauricio@wyeworks.com>
2022-02-24 14:34:22 -0300

    [MER-761] Remove current id from session when signout

--- lib/oli_web/controllers/session_controller.ex
+++ lib/oli_web/controllers/session_controller.ex
@@ -5,19 +5,22 @@ defmodule OliWeb.SessionController do
 
   plug :require_authenticated when action in [:signout]
 
-  @session_data_to_delete [:dismissed_messages]
+  @shared_session_data_to_delete [:dismissed_messages]
 
   def signout(conn, %{"type" => type}) do
     conn
     |> use_pow_config(String.to_atom(type))
     |> Pow.Plug.delete()
-    |> delete_session_data()
+    |> delete_session_data(type)
     |> redirect(to: Routes.static_page_path(conn, :index))
   end
 
-  defp delete_session_data(conn) do
-    Enum.reduce(@session_data_to_delete, conn, fn field, acc_conn ->
+  defp delete_session_data(conn, type) do
+    Enum.reduce(session_data_to_delete(type), conn, fn field, acc_conn ->
       delete_session(acc_conn, field)
     end)
   end
+
+  defp session_data_to_delete(type),
+    do: @shared_session_data_to_delete ++ [String.to_atom("current_#{type}_id")]
 end
