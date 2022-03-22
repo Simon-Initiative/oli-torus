550a61e4a72b9297f888fcd0396863b310bd4403
Eli Knebel <eliknebel@gmail.com>
2022-03-22 09:04:12 -0400

    [BUG FIX] Remix renumbering (#2348)
    * WIP
    
    * fix an issue where remix doesnt properly update numberings in section resources

--- test/oli/resources/numbering_test.exs
+++ test/oli/resources/numbering_test.exs
@@ -6,6 +6,7 @@ defmodule Oli.Resources.NumberingTest do
   alias Oli.Delivery.Sections
   alias Oli.Publishing
   alias Oli.Publishing.DeliveryResolver
+  alias Oli.Delivery.Hierarchy
 
   describe "container numbering" do
     setup do
@@ -107,6 +108,67 @@ defmodule Oli.Resources.NumberingTest do
       path_titles = Enum.map(path_nodes, fn n -> n.revision.title end)
 
       assert path_titles == ["Root Container", "Unit 1", "Module 2", "Section 1", "Page 2"]
+    end
+
+    test "renumber_hierarchy/1", %{project: project, institution: institution} do
+      # Publish the current state of our test project
+      {:ok, pub1} = Publishing.publish_project(project, "some changes")
+
+      {:ok, section} =
+        Sections.create_section(%{
+          title: "1",
+          timezone: "1",
+          registration_open: true,
+          context_id: UUID.uuid4(),
+          institution_id: institution.id,
+          base_project_id: project.id
+        })
+        |> then(fn {:ok, section} -> section end)
+        |> Sections.create_section_resources(pub1)
+
+      hierarchy = DeliveryResolver.full_hierarchy(section.slug)
+
+      page_1_node = hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)
+      section_2_node = hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(1) |> Map.get(:children) |> Enum.at(1)
+
+      hierarchy = Hierarchy.move_node(hierarchy, page_1_node, section_2_node.uuid)
+
+      module_2_node = hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)
+      unit_2_node = hierarchy.children |> Enum.at(1)
+
+      hierarchy = Hierarchy.move_node(hierarchy, module_2_node, unit_2_node.uuid)
+        |> Hierarchy.finalize()
+
+      assert hierarchy.numbering.level == 0
+      assert hierarchy.numbering.index == 1
+
+      assert (hierarchy.children |> Enum.at(0)).numbering.level == 1
+      assert (hierarchy.children |> Enum.at(0)).numbering.index == 1
+
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.level == 2
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.index == 1
+
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.index == 1
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(1)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(1)).numbering.index == 2
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(2)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(2)).numbering.index == 3
+
+      assert (hierarchy.children |> Enum.at(1)).numbering.level == 1
+      assert (hierarchy.children |> Enum.at(1)).numbering.index == 2
+
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.index == 4
+
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.level == 4
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(0)).numbering.index == 1
+
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(1)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(1)).numbering.index == 5
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(2)).numbering.level == 3
+      assert (hierarchy.children |> Enum.at(1) |> Map.get(:children) |> Enum.at(0) |> Map.get(:children) |> Enum.at(2)).numbering.index == 6
+
     end
   end
 end
