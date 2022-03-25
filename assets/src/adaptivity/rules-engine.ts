4e13084bfe04ffcbf3f84445fd3a3e16842e463c
Devesh Tiwari <dtiwari@ats360.com>
2022-03-25 12:50:01 +0530

    Handling Expression

--- assets/src/adaptivity/rules-engine.ts
+++ assets/src/adaptivity/rules-engine.ts
@@ -303,9 +303,19 @@ export const getReferencedKeysInConditions = (conditions: any) => {
     ) {
       // value could have more than one reference inside it
       const exprs = extractAllExpressionsFromText(condition.value);
+      const expressions = condition.value.match(/{([^{^}]+)}/g);
       exprs.forEach((expr: string) => {
         if (expr.search(/app\.|variables\.|stage\.|session\./) !== -1) {
           references.add(expr);
+        }
+      });
+      expressions.forEach((expr: string) => {
+        if (expr.search(/app\.|variables\.|stage\.|session\./) !== -1) {
+          //we should remove the {}
+          const actualExp = expr.substring(1, expr.length - 1);
+          if (!references.has(actualExp)) {
+            references.add(expr.substring(1, expr.length - 1));
+          }
         }
       });
     }
