1f9cdd8486f57349ac8919559e4fa9086cf2529f
Marc Hughes <marc.hughes@argos.education>
2022-05-05 10:51:35 -0400

    [ENHANCEMENT] [MER-636] New JS app embedding / XSS fix (#2482)
    * [ENHANCEMENT] [MER-636] Refactor js app embedding to use react-phoenix
    
    * [ENHANCEMENT] [MER-636] Import ReactPhoenix.ClientSide into our views
    
    * [ENHANCEMENT] [MER-636] Clean up ActivityController route & page that no longer functions

--- assets/src/apps/PageEditorApp.tsx
+++ assets/src/apps/PageEditorApp.tsx
@@ -1,38 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import PageEditor from './page-editor/PageEditor';
-import { ModalDisplay } from 'components/modal/ModalDisplay';
-import { Provider } from 'react-redux';
-import { Maybe } from 'tsmonad';
-import { configureStore } from 'state/store';
-import { b64DecodeUnicode } from 'utils/decode';
 
-let store = configureStore();
+import { registerApplication } from './app';
 
-window.oliMountApplication = (mountPoint: any, paramString: any) => {
-  const params = JSON.parse(b64DecodeUnicode(paramString));
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <PageEditor {...params} />
-      <ModalDisplay />
-    </Provider>,
-    mountPoint,
-  );
-};
-
-window.store = {
-  configureStore: (json: any) => {
-    store = configureStore(json);
-  },
-};
-
-// Expose other libraries to server-side rendered templates
-window.Maybe = Maybe;
-declare global {
-  interface Window {
-    Maybe: typeof Maybe;
-    store: { configureStore: (json: any) => void };
-    oliMountApplication: (mountPoint: any, paramString: any) => void;
-  }
-}
+registerApplication('PageEditor', PageEditor);
