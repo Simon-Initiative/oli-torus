abca090518d78e367b98c040fddc468be20ea638
Mauricio Cassola <mauricio@wyeworks.com>
2022-05-16 14:01:53 -0300

    Revert "[MER-947] Order curriculum items equal to add materials (#2472)"
    This reverts commit d65bb1c0f3d610f836b9cf2ad37c4d28e20c2b0a.

--- lib/oli_web/live/common/hierarchy/hierarchy_picker.ex
+++ lib/oli_web/live/common/hierarchy/hierarchy_picker.ex
@@ -290,7 +290,7 @@ defmodule OliWeb.Common.Hierarchy.HierarchyPicker do
     end
   end
 
-  def sort_items(children, assigns) do
+  defp sort_items(children, assigns) do
     case assigns do
       %{sort_items_fn: sort_items_fn} when sort_items_fn != nil ->
         sort_items_fn.(children)
