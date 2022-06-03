10299262b5878aea99cd33a80253ae77c0eb2cc5
Darren Siegel <siegel.darren@gmail.com>
2022-06-03 12:28:37 -0400

    changelog, cleanup

--- lib/oli/delivery/attempts/activity_lifecycle/persistence.ex
+++ lib/oli/delivery/attempts/activity_lifecycle/persistence.ex
@@ -134,9 +134,6 @@ defmodule Oli.Delivery.Attempts.ActivityLifecycle.Persistence do
         query
       end
 
-    IO.inspect("HEREREREREREE")
-    IO.inspect(input)
-
     case Repo.update_all(
            query,
            set: [
