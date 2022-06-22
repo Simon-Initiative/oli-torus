44c3d49a4330780ce2c26c9ef94b4c9d034aea56
Darren Siegel <siegel.darren@gmail.com>
2022-06-22 13:22:24 -0400

    [BUG FIX] remove setting of project_id during migration (#2716)

--- priv/repo/migrations/20220616184621_add_project_to_snapshots.exs
+++ priv/repo/migrations/20220616184621_add_project_to_snapshots.exs
@@ -3,15 +3,7 @@ defmodule Oli.Repo.Migrations.AddProjectToSnapshots do
 
   def change do
     alter table(:snapshots) do
-      add :project_id, references(:projects, on_delete: :nothing)
-    end
-
-    if direction() == :up do
-      flush()
-
-      execute """
-      UPDATE snapshots SET project_id = sect.base_project_id FROM snapshots snap LEFT JOIN sections sect ON snap.section_id = sect.id;
-      """
+      add :project_id, references(:projects, on_delete: :nothing), null: true, default: nil
     end
   end
 end
