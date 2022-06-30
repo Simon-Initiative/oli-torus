135555937f2115c955d77ff275e00b1ec9781d99
Julián Montes de Oca <julian@wyeworks.com>
2022-06-30 11:50:22 -0300

    [MER-1288] Add project ids to snapshots (#2755)

---
+++ priv/repo/migrations/20220629134948_update_snapshots_add_project_ids.exs
@@ -0,0 +1,28 @@
+defmodule Oli.Repo.Migrations.UpdateSnapshotsAddProjectIds do
+  use Ecto.Migration
+
+  def up do
+    drop index(:snapshots, [:objective_id])
+    drop index(:snapshots, [:activity_id])
+    drop index(:snapshots, [:section_id])
+    drop index(:snapshots, [:part_attempt_id, :objective_id], name: :snapshot_unique_part)
+
+    execute """
+      UPDATE snapshots
+      SET project_id = sect.base_project_id
+      FROM sections sect WHERE sect.id = snapshots.section_id;
+      """
+
+    create index(:snapshots, [:objective_id])
+    create index(:snapshots, [:activity_id])
+    create index(:snapshots, [:section_id])
+    create unique_index(:snapshots, [:part_attempt_id, :objective_id], name: :snapshot_unique_part)
+  end
+
+  def down do
+    execute """
+      UPDATE snapshots
+      SET project_id = NULL
+      """
+  end
+end
