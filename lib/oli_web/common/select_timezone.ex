72558ec3802dda5552519dc8ccc230745fcb57e2
Gastón Abellá <gaston@wyeworks.com>
2022-07-01 10:21:24 -0300

    [MER-1293] Fix tests and default browser scenario

--- lib/oli_web/common/select_timezone.ex
+++ lib/oli_web/common/select_timezone.ex
@@ -4,12 +4,12 @@ defmodule OliWeb.Common.SelectTimezone do
   import Phoenix.HTML.Form
 
   alias Oli.Predefined
-  alias OliWeb.Common.SessionContext
+  alias OliWeb.Common.{FormatDateTime, SessionContext}
   alias OliWeb.Router.Helpers, as: Routes
 
-  def render(assigns) do
-    context = SessionContext.init(assigns.conn)
-    browser_timezone = Plug.Conn.get_session(assigns.conn, "browser_timezone")
+  def render(%{conn: conn} = assigns) do
+    browser_timezone = Plug.Conn.get_session(conn, "browser_timezone")
+    context = SessionContext.init(conn)
 
     timezones =
       Enum.map(Predefined.timezones(), fn
@@ -36,9 +36,18 @@ defmodule OliWeb.Common.SelectTimezone do
       <%= form_for @conn, Routes.static_page_path(@conn, :update_timezone), [id: "timezone-form"], fn f -> %>
         <%= hidden_input f, :redirect_to, id: "hidden-redirect-to" %>
         <div class="form-label-group">
-          <%= select f, :timezone, timezones, onchange: "submitForm()", selected: context.local_tz || "Etc/Greenwich", class: "form-control dropdown-select", required: true %>
+          <%= select f, :timezone, timezones, onchange: "submitForm()", selected: selected_timezone(context.local_tz), class: "form-control dropdown-select", required: true %>
         </div>
       <% end %>
     """
+  end
+
+  defp selected_timezone(timezone) do
+    default = FormatDateTime.default_timezone()
+
+    case timezone do
+      ^default -> "Etc/Greenwich"
+      timezone -> timezone
+    end
   end
 end
