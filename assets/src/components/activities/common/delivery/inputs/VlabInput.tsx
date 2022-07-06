745cfac38ab8d887f0b5e348b318f44efbd31023
David H Brown <dhbrown@dhba.com>
2022-07-06 11:57:13 -0700

    Simplified structure and better styling of VlabInput

--- assets/src/components/activities/common/delivery/inputs/VlabInput.tsx
+++ assets/src/components/activities/common/delivery/inputs/VlabInput.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
 interface Props {
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
   value: string | number;
   disabled?: boolean;
-  placeholder?: string;
 }
 export const VlabInput: React.FC<Props> = (props) => {
   return (
     <input
-      placeholder="Vlab Value" //{props.placeholder}
-      readOnly="true"
-      type="number"
-      aria-label="answer submission textbox"
-      className="form-control"
-      onChange={props.onChange}
+      type="hidden"
       value={props.value}
       disabled={typeof props.disabled === 'boolean' ? props.disabled : false}
     />
