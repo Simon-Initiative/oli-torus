1553656cd06005c161059d2527608ebd728993c7
David H Brown <dhbrown@dhba.com>
2022-07-08 18:23:27 -0700

    VlabConfigTab

---
+++ assets/src/components/activities/vlab/sections/VlabConfigTab.tsx
@@ -0,0 +1,39 @@
+import { useAuthoringElementContext } from 'components/activities/AuthoringElement';
+import { MultiInputActions } from 'components/activities/vlab/actions';
+import { Dropdown, VlabInput, VlabSchema } from 'components/activities/vlab/schema';
+import { Card } from 'components/misc/Card';
+import React from 'react';
+import { ReactEditor } from 'slate-react';
+
+interface Props {
+  editor: ReactEditor & Editor;
+}
+
+const configList = ['assignmentPath'];
+
+export const VlabConfigTab: React.FC<Props> = (props) => {
+  const { model, dispatch } = useAuthoringElementContext<VlabSchema>();
+
+  return (
+    <Card.Card>
+      <Card.Title>Vlab Configuration</Card.Title>
+      <Card.Content>
+        <>
+          {configList.map((config, i) => (
+            <div key={i}>
+              <label>
+                <input
+                  type="text"
+                  name="config"
+                  value={model.configuration[config]}
+                  onChange={(e) => dispatch(MultiInputActions.setVlabConfiguration(e.target.value))}
+                />
+                {config}
+              </label>
+            </div>
+          ))}
+        </>
+      </Card.Content>
+    </Card.Card>
+  );
+};
