132fbcd8e3f844ac1182ca011dc4b35a364ea6e5
Mauricio Cassola <mauricio@wyeworks.com>
2022-07-12 17:34:48 -0300

    [MER-1324] Replace get_user to get_author (#2798)

--- test/oli_web/live/curriculum/container_test.exs
+++ test/oli_web/live/curriculum/container_test.exs
@@ -1,11 +1,39 @@
 defmodule OliWeb.Curriculum.ContainerLiveTest do
   use OliWeb.ConnCase
+
   alias Oli.Seeder
+  alias Oli.Publishing
   alias Oli.Publishing.AuthoringResolver
 
+  import Oli.Factory
   import Phoenix.ConnTest
   import Phoenix.LiveViewTest
+
   @endpoint OliWeb.Endpoint
+
+  describe "cannot access when is not logged in" do
+    test "redirect to new session when accessing the container view", %{conn: conn} do
+      project = insert(:project)
+
+      redirect_path = "/authoring/session/new?request_path=%2Fauthoring%2Fproject%2F#{project.slug}%2Fcurriculum"
+
+      {:error, {:redirect, %{to: ^redirect_path}}} =
+        live(conn, Routes.container_path(@endpoint, :index, project.slug))
+    end
+  end
+
+  describe "cannot access when is not an author" do
+    setup [:user_conn]
+
+    test "redirect to new session when accessing the container view", %{conn: conn} do
+      project = insert(:project)
+
+      redirect_path = "/authoring/session/new?request_path=%2Fauthoring%2Fproject%2F#{project.slug}%2Fcurriculum"
+
+      {:error, {:redirect, %{to: ^redirect_path}}} =
+        live(conn, Routes.container_path(@endpoint, :index, project.slug))
+    end
+  end
 
   describe "container live test" do
     setup [:setup_session]
@@ -41,6 +69,25 @@ defmodule OliWeb.Curriculum.ContainerLiveTest do
 
       assert view |> element("##{Integer.to_string(page1.id)}") |> has_element?()
       assert view |> element("##{Integer.to_string(page2.id)}") |> has_element?()
+    end
+
+    test "shows the author name editing the page correctly", %{
+      conn: conn,
+      project: project,
+      map: %{
+        published_resource1: published_resource1
+      }
+    } do
+      editing_author = insert(:author)
+
+      Publishing.update_published_resource(published_resource1, %{
+        locked_by_id: editing_author.id,
+        lock_updated_at: now()
+      })
+
+      {:ok, view, _} = live(conn, Routes.container_path(@endpoint, :index, project.slug))
+
+      assert has_element?(view, "span", "#{editing_author.name} is editing")
     end
   end
 
