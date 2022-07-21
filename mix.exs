a2f3b4f28d16f093541a67845beb22bccd3eb999
Eli Knebel <eliknebel@cmu.edu>
2022-07-21 15:53:17 -0400

    changelog and version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oli.MixProject do
   def project do
     [
       app: :oli,
-      version: "0.21.2",
+      version: "0.21.3",
       elixir: "~> 1.13.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
