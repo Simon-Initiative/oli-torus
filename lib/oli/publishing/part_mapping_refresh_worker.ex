198bdc8849fe97771dce0f3ee392bec9e0201657
Julian <julian@wyeworks.com>
2022-07-28 17:56:31 -0300

    [MER-1323] Make sure that only one node in the cluster is running the oban queue

--- lib/oli/publishing/part_mapping_refresh_worker.ex
+++ lib/oli/publishing/part_mapping_refresh_worker.ex
@@ -3,6 +3,9 @@ defmodule Oli.Publishing.PartMappingRefreshWorker do
     An Oban worker for refreshing the part_mapping materialized view.
     The view must be refreshed at least once after a Publication update is made.
     Since the complete view is refreshed, we avoid duplicate jobs in the queue, and only queue one max job if there's one being executed.
+
+    In Oli.Application the part_mapping_refresh queue is setup to be only configured in one node of the cluster. This is to ensure that only one node
+    is trying to refresh the view at any time.
   """
 
   use Oban.Worker,
@@ -17,7 +20,7 @@ defmodule Oli.Publishing.PartMappingRefreshWorker do
   require Logger
 
   def create() do
-    case Oli.Publishing.PartMappingRefreshWorker.new(%{})
+    case new(%{})
          |> Oban.insert() do
       {:ok, job} ->
         {:ok, job}
