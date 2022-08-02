2840264ded568d7d97f3f4f314e580bfc3a49c24
Mauricio Cassola <mauricio@wyeworks.com>
2022-08-02 18:13:32 -0300

    [MER-1355] Add tests

--- test/oli/delivery/attempts_test.exs
+++ test/oli/delivery/attempts_test.exs
@@ -448,7 +448,7 @@ defmodule Oli.Delivery.AttemptsTest do
     test "get graded resource accesses where the last lms sync failed - returns empty when no failed sync exists" do
       user = insert(:user)
 
-      {:ok, section: section, page_revision: page_revision} = section_with_assessment(%{})
+      {:ok, section: section, unit_one_revision: _unit_one_revision, page_revision: page_revision} = section_with_assessment(%{})
       last_grade_update = insert(:lms_grade_update)
 
       insert(:resource_access,
@@ -466,7 +466,7 @@ defmodule Oli.Delivery.AttemptsTest do
       user1 = insert(:user)
       user2 = insert(:user)
 
-      {:ok, section: section, page_revision: page_revision} = section_with_assessment(%{})
+      {:ok, section: section, unit_one_revision: _unit_one_revision, page_revision: page_revision} = section_with_assessment(%{})
       last_successful_grade_update = insert(:lms_grade_update)
       last_grade_update = insert(:lms_grade_update)
 
