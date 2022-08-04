bbd6784af9d2677dcfe430410b320ef142d9c58f
Darren Siegel <siegel.darren@gmail.com>
2022-08-04 09:52:51 -0400

    add test case for all requiring inherited state

--- test/oli/delivery/attempts/retake_mode_test.exs
+++ test/oli/delivery/attempts/retake_mode_test.exs
@@ -110,6 +110,71 @@ defmodule Oli.Delivery.Attempts.PageLifecycle.RetakeModeTest do
       assert Map.get(part_map, "1").response == nil
     end
 
+    test "targeted retake mode for all activities", %{
+      p1: p1,
+      user1: user,
+      section: section,
+      a1: a1,
+      a2: a2,
+      publication: pub
+    } do
+      Attempts.track_access(p1.resource.id, section.id, user.id)
+
+      activity_provider = &Oli.Delivery.ActivityProvider.provide/4
+      datashop_session_id = UUID.uuid4()
+
+      {:ok, resource_attempt} =
+        Hierarchy.create(%VisitContext{
+          latest_resource_attempt: nil,
+          page_revision: p1.revision,
+          section_slug: section.slug,
+          datashop_session_id: datashop_session_id,
+          user_id: user.id,
+          activity_provider: activity_provider,
+          blacklisted_activity_ids: [],
+          publication_id: pub.id
+        })
+
+      attempts = Hierarchy.get_latest_attempts(resource_attempt.id)
+      {attempt, part_map} = Map.get(attempts, a1.resource.id)
+
+      Attempts.update_activity_attempt(attempt, %{score: 1, out_of: 1})
+
+      Map.get(part_map, "1")
+      |> Attempts.update_part_attempt(%{score: 1, out_of: 1, response: %{input: "this was right"}})
+
+      {attempt, part_map} = Map.get(attempts, a2.resource.id)
+      Attempts.update_activity_attempt(attempt, %{score: 1, out_of: 1})
+
+      Map.get(part_map, "1")
+      |> Attempts.update_part_attempt(%{
+        score: 1,
+        out_of: 1,
+        response: %{input: "this was right, also"}
+      })
+
+      {:ok, resource_attempt2} =
+        Hierarchy.create(%VisitContext{
+          latest_resource_attempt: resource_attempt,
+          page_revision: p1.revision,
+          section_slug: section.slug,
+          datashop_session_id: datashop_session_id,
+          user_id: user.id,
+          activity_provider: activity_provider,
+          blacklisted_activity_ids: [],
+          publication_id: pub.id
+        })
+
+      attempts = Hierarchy.get_latest_attempts(resource_attempt2.id)
+
+      # Ensure that the new part attempt records carried forward the state of only the correct
+      # activity attempt from the previous resource attempt
+      {_, part_map} = Map.get(attempts, a1.resource.id)
+      assert Map.get(part_map, "1").response == %{"input" => "this was right"}
+      {_, part_map} = Map.get(attempts, a2.resource.id)
+      assert Map.get(part_map, "1").response == %{"input" => "this was right, also"}
+    end
+
     test "targeted retake mode disabled when page revision changes", %{
       p1: p1,
       p2: p2,
