a2719441b5941ff01781f0f6bc2eaa275db68875
Marc Hughes <marc.hughes@argos.education>
2022-08-11 13:28:54 -0400

    [BUG] [MER-1403] [#2918] Limit list of extensions to valid values.

--- lib/oli_web/common/mathjax_script.ex
+++ lib/oli_web/common/mathjax_script.ex
@@ -9,7 +9,40 @@ defmodule OliWeb.Common.MathJaxScript do
         inlineMath: [ ["\\(","\\)"] ],
         displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
         processEscapes: true,
-        packages: ['base', 'ams', 'noerrors', 'noundefined', 'require']
+        packages: ['base', 'ams', 'noerrors', 'noundefined', 'require'],
+        require: {
+          defaultAllow: false,
+          allow: {
+            "action": true,
+            "amscd": true,
+            "bbox": true,
+            "boldsymbol": true,
+            "braket": true,
+            "bussproofs": true,
+            "cancel": true,
+            "cases": true,
+            "centernot": true,
+            "color": true,
+            "colortbl": true,
+            "colorv2": true,
+            "configmacros": true,
+            "empheq": true,
+            "enclose": true,
+            "extpfeil": true,
+            "gensymb": true,
+            "html": true,
+            "mathtools": true,
+            "mhchem": true,
+            "physics": true,
+            "setoptions": true,
+            "tagformat": true,
+            "textcomp": true,
+            "textmacros": true,
+            "unicode": true,
+            "upgreek": true,
+            "verb": true
+          }
+        }
       },
       options: {
         ignoreHtmlClass: 'tex2jax_ignore',
