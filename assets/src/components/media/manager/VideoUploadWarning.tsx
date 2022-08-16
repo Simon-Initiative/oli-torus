e7330ec90361fca8304f89bf022791bf7e4027b1
Marc Hughes <marc.hughes@argos.education>
2022-08-16 13:22:13 -0400

    [MER-1409] [GH#2929] Increase max upload size

--- assets/src/components/media/manager/VideoUploadWarning.tsx
+++ assets/src/components/media/manager/VideoUploadWarning.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { MaxFileUploadSizeMB } from './upload';
 
 export const VideoUploadWarning = () => (
   <div className="alert alert-info show" role="alert">
-    File uploads have a 20mb limit. We recommend uploading your video to YouTube and then embedding
-    that within your lesson for most uses.
+    File uploads have a {MaxFileUploadSizeMB}mb limit. We recommend uploading your video to YouTube
+    and then embedding that within your lesson for most uses.
   </div>
 );
