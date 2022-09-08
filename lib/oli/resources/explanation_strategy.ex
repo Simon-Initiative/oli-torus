fd634f968dba6ed37e330120de307a95faaa3c18
Eli Knebel <eliknebel@cmu.edu>
2022-09-08 19:02:52 -0400

    add explanation strategy options ui

--- lib/oli/resources/explanation_strategy.ex
+++ lib/oli/resources/explanation_strategy.ex
@@ -2,6 +2,14 @@ defmodule Oli.Resources.ExplanationStrategy do
   use Ecto.Schema
   import Ecto.Changeset
 
+  def types() do
+    [
+      :none,
+      :after_max_resource_attempts_exhausted,
+      :after_set_num_attempts
+    ]
+  end
+
   @derive Jason.Encoder
   @primary_key false
   embedded_schema do
@@ -15,7 +23,14 @@ defmodule Oli.Resources.ExplanationStrategy do
   end
 
   @doc false
-  def changeset(explanation_strategy, attrs \\ %{}) do
+  def changeset(explanation_strategy, attrs \\ %{})
+
+  def changeset(explanation_strategy, %Oli.Resources.ExplanationStrategy{} = attrs) do
+    # if attrs is given as a struct, then convert it to a map in order to process
+    changeset(explanation_strategy, Map.from_struct(attrs))
+  end
+
+  def changeset(explanation_strategy, attrs) do
     explanation_strategy
     |> cast(attrs, [:type, :set_num_attempts])
     |> validate_required([:type])
