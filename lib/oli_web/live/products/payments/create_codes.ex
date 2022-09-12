1fcf34b887a237879e757b314f116a850dc35294
Santiago Simoncelli <simoncellisantiago@gmail.com>
2022-09-12 09:51:52 -0300

    [MER-744] Refactor CreateCodes component & payments view

--- lib/oli_web/live/products/payments/create_codes.ex
+++ lib/oli_web/live/products/payments/create_codes.ex
@@ -7,32 +7,37 @@ defmodule OliWeb.Products.Payments.CreateCodes do
   prop change, :event, required: true
   prop disabled, :boolean, required: true
   data something, :any, default: true
+  prop enabled_download, :boolean, required: true
 
   def render(assigns) do
     ~F"""
-    <div>
+    <div class="d-flex justify-content-between align-items-center">
       <div class="form-inline">
 
         <p>Download a new batch of payment codes:</p>
-        <input class="ml-2 form-control form-control-sm" disabled={@disabled} type="number" value={@count} style="width: 90px;" :on-blur={@change} :on-keyup={@change}/>
+        <input class="ml-2 form-control form-control-sm" disabled={@disabled} type="number" value={@count} style="width: 90px;" :on-blur={@change} :on-keyup={@change} :on-click={@change}/>
 
-        <a class="btn btn-primary btn-sm ml-1" href={route_or_disabled(assigns)}>Create</a>
+        <button class="btn btn-primary btn-sm ml-1" :on-click={@click}>Create</button>
 
       </div>
       <div>
-        <small class="text-muted">You will need to refresh this page to see these newly created codes</small>
+        <a class={"btn btn-outline-primary btn-sm ml-1" <> if @enabled_download, do: "", else: " disabled"} href={route_or_disabled(assigns)} style="font-size: 0.6rem;">Download last created</a>
       </div>
+
     </div>
     """
   end
 
   defp route_or_disabled(assigns) do
-    case assigns.disabled do
-      true ->
-        "#"
-
-      _ ->
-        Routes.payment_path(OliWeb.Endpoint, :download_codes, assigns.product_slug, assigns.count)
+    if assigns.enabled_download do
+      Routes.payment_path(
+        OliWeb.Endpoint,
+        :download_codes_generated,
+        assigns.product_slug,
+        count: assigns.count
+      )
+    else
+      "#"
     end
   end
 end
--- lib/oli_web/live/products/payments_view.ex
+++ lib/oli_web/live/products/payments_view.ex
@@ -1,5 +1,6 @@
 defmodule OliWeb.Products.PaymentsView do
   use Surface.LiveView
+  use OliWeb.Common.SortableTable.TableHandlers
 
   alias(OliWeb.Common.Filter)
 
@@ -21,6 +22,7 @@ defmodule OliWeb.Products.PaymentsView do
   data limit, :integer, default: 20
   data query, :string, default: ""
   data applied_query, :string, default: ""
+  data enabled_download, :boolean, default: false
 
   @table_filter_fn &OliWeb.Products.PaymentsView.filter_rows/3
   @table_push_patch_path &OliWeb.Products.PaymentsView.live_path/2
@@ -51,20 +53,7 @@ defmodule OliWeb.Products.PaymentsView do
   def mount(%{"product_id" => product_slug}, session, socket) do
     context = SessionContext.init(session)
 
-    payments =
-      Oli.Delivery.Paywall.list_payments(product_slug)
-      |> Enum.map(fn element ->
-        Map.put(
-          element,
-          :code,
-          if is_nil(element.payment.code) do
-            ""
-          else
-            Oli.Delivery.Paywall.Payment.to_human_readable(element.payment.code)
-          end
-        )
-        |> Map.put(:unique_id, element.payment.id)
-      end)
+    payments = list_payments(product_slug)
 
     total_count = length(payments)
 
@@ -85,7 +74,7 @@ defmodule OliWeb.Products.PaymentsView do
     ~F"""
     <div>
 
-      <CreateCodes id="create_codes" disabled={!@product.requires_payment} count={@code_count} product_slug={@product_slug} click="create_codes" change="change_count"/>
+      <CreateCodes id="create_codes" disabled={!@product.requires_payment} count={@code_count} product_slug={@product_slug} enabled_download={@enabled_download} click="create" change="change_count"/>
 
       <hr class="mt-5 mb-5"/>
 
@@ -107,6 +96,55 @@ defmodule OliWeb.Products.PaymentsView do
     """
   end
 
+  defp list_payments(product_slug) do
+    Oli.Delivery.Paywall.list_payments(product_slug)
+    |> Enum.map(fn element ->
+      Map.put(
+        element,
+        :code,
+        if is_nil(element.payment.code) do
+          ""
+        else
+          Oli.Delivery.Paywall.Payment.to_human_readable(element.payment.code)
+        end
+      )
+      |> Map.put(:unique_id, element.payment.id)
+    end)
+  end
+
+  def handle_event("create", _, socket) do
+    case Oli.Delivery.Paywall.create_payment_codes(
+           socket.assigns.product_slug,
+           socket.assigns.code_count
+         ) do
+      {:ok, _} ->
+        payments = list_payments(socket.assigns.product_slug)
+
+        {:ok, table_model} =
+          OliWeb.Products.Payments.TableModel.new(
+            payments,
+            socket.assigns.context
+          )
+
+        total_count = length(payments)
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Payment codes successfully added.")
+         |> assign(
+           payments: payments,
+           total_count: total_count,
+           table_model: table_model,
+           enabled_download: true
+         )}
+
+      _ ->
+        {:noreply,
+         socket
+         |> put_flash(:error, "Code payments couldn't be added.")}
+    end
+  end
+
   def handle_event("change_count", %{"value" => count}, socket) do
     count =
       case String.to_integer(count) do
@@ -114,8 +152,6 @@ defmodule OliWeb.Products.PaymentsView do
         _ -> 1
       end
 
-    {:noreply, assign(socket, code_count: count)}
+    {:noreply, assign(socket, code_count: count, enabled_download: false)}
   end
-
-  use OliWeb.Common.SortableTable.TableHandlers
 end
