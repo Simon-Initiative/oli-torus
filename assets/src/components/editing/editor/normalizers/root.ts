601489837c60a709e5b54195029e8dd80b348c92
Eli Knebel <eliknebel@cmu.edu>
2022-09-27 11:34:48 -0400

    restore root normalization logic, fixed by #3025

--- assets/src/components/editing/editor/normalizers/root.ts
+++ assets/src/components/editing/editor/normalizers/root.ts
@@ -1,13 +1,29 @@
 import { Model } from 'data/content/model/elements/factories';
-import { Editor, Path, Transforms } from 'slate';
+import { Editor, Element, Path, Transforms, Descendant } from 'slate';
+
+const blockTexts = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+const isBlockText = (e: Descendant) => Element.isElement(e) && blockTexts.includes(e.type);
 
 export const normalize = (editor: Editor, node: Editor, _path: Path) => {
-  // Ensure that we always have a paragraph in the document
-  if (node.children.length === 0) {
+  // Ensure that we always have a paragraph/heading as the first and last
+  // nodes in the document, otherwise it can be impossible for a
+  // user to position their cursor
+  const first = node.children[0];
+  const last = node.children[node.children.length - 1];
+  if (!isBlockText(first)) {
+    Transforms.insertNodes(editor, Model.p(), {
+      mode: 'highest',
+      at: Editor.start(editor, []),
+    });
+    console.warn('Normalizing content: Inserted paragraph at start of document');
+    return;
+  }
+  if (!isBlockText(last)) {
     Transforms.insertNodes(editor, Model.p(), {
       mode: 'highest',
       at: Editor.end(editor, []),
     });
+    console.warn('Normalizing content: Inserted paragraph at end of document');
+    return;
   }
-  return;
 };
