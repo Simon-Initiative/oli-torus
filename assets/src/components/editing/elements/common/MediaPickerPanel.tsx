fa45135ebe438c8ba33cbccf486f1179c085b442
Marc Hughes <marc.hughes@argos.education>
2022-10-03 14:59:54 -0400

    [BUG] Fixed video settings browse dialogs.

---
+++ assets/src/components/editing/elements/common/MediaPickerPanel.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useState } from 'react';
+import { MIMETYPE_FILTERS, SELECTION_TYPES } from '../../../media/manager/MediaManager';
+import { UrlOrUpload } from '../../../media/UrlOrUpload';
+import { SlideOutPanel } from './SlideOutPanel';
+
+export interface MediaInfo {
+  url: string;
+  mimeType?: string;
+}
+
+interface Props {
+  projectSlug: string;
+  mimeFilter?: string[];
+  selectionType?: SELECTION_TYPES;
+  initialSelectionPaths?: string[];
+  onMediaChange: (media: MediaInfo[]) => void;
+  open: boolean;
+}
+
+/**
+ * Component suitable for selecting an image from the media library, especially from within
+ * an already open modal when we don't want to open modals on top of other modals. (which doesn't
+ * work well with bootstrap style modals).
+ *
+ */
+export const MediaPickerPanel: React.FC<Props> = ({
+  projectSlug,
+  onMediaChange,
+  mimeFilter = MIMETYPE_FILTERS.IMAGE,
+  selectionType = SELECTION_TYPES.SINGLE,
+  initialSelectionPaths = [],
+  open,
+}) => {
+  const [url, setUrl] = useState(initialSelectionPaths.length > 0 ? initialSelectionPaths[0] : '');
+
+  const onUrlChange = useCallback((url) => {
+    setUrl(url);
+  }, []);
+
+  const onOk = useCallback(() => {
+    onMediaChange([{ url, mimeType: 'image/png' }]);
+  }, [onMediaChange, url]);
+
+  return (
+    <SlideOutPanel open={open}>
+      <UrlOrUpload
+        onUrlChange={onUrlChange}
+        onMediaSelectionChange={onMediaChange}
+        projectSlug={projectSlug}
+        mimeFilter={mimeFilter}
+        selectionType={selectionType}
+        initialSelectionPaths={initialSelectionPaths}
+      >
+        <button className="btn" onClick={onOk}>
+          OK
+        </button>
+      </UrlOrUpload>
+    </SlideOutPanel>
+  );
+};
