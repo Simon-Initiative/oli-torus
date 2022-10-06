d616eed367aa0989cc5ab185eeeb9dabc5654587
Darren Siegel <siegel.darren@gmail.com>
2022-10-06 09:23:59 -0400

    per part submission mode

--- assets/src/components/activities/multi_input/utils.tsx
+++ assets/src/components/activities/multi_input/utils.tsx
@@ -45,6 +45,7 @@ export const defaultModel = (): MultiInputSchema => {
     stem: multiInputStem(input),
     choices: [],
     inputs: [{ inputType: 'text', id: input.id, partId: '1' }],
+    partSubmission: false,
     authoring: {
       parts: [makePart(Responses.forTextInput(), [makeHint('')], '1')],
       targeted: [],
