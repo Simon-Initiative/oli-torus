e3dac473d7c5069d3bd5dfa510074125c48a76d7
Marc Hughes <marc.hughes@argos.education>
2022-10-14 08:36:48 -0400

    [FEATURE] Added closed caption support to video player. [MER-1498]

--- assets/src/components/video_player/VideoPlayer.tsx
+++ assets/src/components/video_player/VideoPlayer.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef } from 'react';
-import { Video } from '../../data/content/model/elements/types';
+import React, { ReactNode, useCallback, useRef } from 'react';
+import * as ContentModel from '../../data/content/model/elements/types';
 
 import {
   BigPlayButton,
@@ -18,6 +18,7 @@ import { InitialPlayButton } from './InitialPlayButton';
 import { PlayButton } from './VideoPlayButton';
 import { FullScreenButton } from './VideoFullScreenButton';
 import { useCommandTarget } from '../editing/elements/command_button/useCommandTarget';
+import { ClosedCaptionButton } from './ClosedCaptionButton';
 
 const startEndCueRegex = /startcuepoint=([0-9.]+);endcuepoint=([0-9.]+)/;
 const startCueRegex = /startcuepoint=([0-9.]+)/;
@@ -45,7 +46,7 @@ export const parseVideoPlayCommand = (command: string) => {
   return { start: -1, end: -1 };
 };
 
-const isValidSize = (video: Video) =>
+const isValidSize = (video: ContentModel.Video) =>
   video.width && video.height && video.width > 0 && video.height > 0;
 
 interface VideoInterface {
@@ -53,68 +54,83 @@ interface VideoInterface {
   seek: (time: number) => void;
 }
 
-export const VideoPlayer: React.FC<{ video: Video }> = React.memo(({ video }) => {
-  const playerRef = useRef(null);
-  const pauseAtPosition = useRef(-1);
-  const sizeAttributes = isValidSize(video)
-    ? { width: video.width, height: video.height, fluid: false }
-    : { fluid: true };
+export const VideoPlayer: React.FC<{ video: ContentModel.Video; children: ReactNode }> = React.memo(
+  ({ video, children }) => {
+    const playerRef = useRef(null);
+    const pauseAtPosition = useRef(-1);
+    const sizeAttributes = isValidSize(video)
+      ? { width: video.width, height: video.height, fluid: false }
+      : { fluid: true };
 
-  const onPlayer = useCallback((player) => {
-    playerRef.current = player;
+    const hasCaptions = video.captions && video.captions.length > 0;
 
-    if (!player) {
-      return;
-    }
-    // This handles stopping at the correct point if a cue-point command previously came in with an end-timestamp set.
-    player.subscribeToStateChange((state: PlayerState) => {
-      if (
-        pauseAtPosition.current > 0 &&
-        state.hasStarted &&
-        state.currentTime >= pauseAtPosition.current
-      ) {
-        pauseAtPosition.current = -1;
-        player.pause();
+    const onPlayer = useCallback((player) => {
+      playerRef.current = player;
+
+      if (!player) {
+        return;
       }
-    });
-  }, []);
+      // This handles stopping at the correct point if a cue-point command previously came in with an end-timestamp set.
+      player.subscribeToStateChange((state: PlayerState) => {
+        if (
+          pauseAtPosition.current > 0 &&
+          state.hasStarted &&
+          state.currentTime >= pauseAtPosition.current
+        ) {
+          pauseAtPosition.current = -1;
+          player.pause();
+        }
+      });
+    }, []);
 
-  const onCommandReceived = useCallback((message: string) => {
-    if (!playerRef.current) return;
-    const player = playerRef.current as VideoInterface;
-    const { start, end } = parseVideoPlayCommand(message);
-    pauseAtPosition.current = end;
-    if (start >= 0) {
-      player.seek(start);
-      player.play();
-    }
-  }, []);
+    const onCommandReceived = useCallback((message: string) => {
+      if (!playerRef.current) return;
+      const player = playerRef.current as VideoInterface;
+      const { start, end } = parseVideoPlayCommand(message);
+      pauseAtPosition.current = end;
+      if (start >= 0) {
+        player.seek(start);
+        player.play();
+      }
+    }, []);
 
-  useCommandTarget(video.id, onCommandReceived);
+    useCommandTarget(video.id, onCommandReceived);
 
-  return (
-    <div className="video-player">
-      <Player poster={video.poster} {...sizeAttributes} ref={onPlayer}>
-        {/* Hide the video-react big play button so we can render our own that fits with our icon styles */}
-        <BigPlayButton className="big-play-button-hide" />
-        <InitialPlayButton />
+    return (
+      <div className="video-player">
+        <Player poster={video.poster} {...sizeAttributes} ref={onPlayer} crossOrigin="anonymous">
+          {/* Hide the video-react big play button so we can render our own that fits with our icon styles */}
+          <BigPlayButton className="big-play-button-hide" />
+          <InitialPlayButton />
 
-        {video.src.map((src) => (
-          <source key={src.url} src={src.url} type={src.contenttype} />
-        ))}
-        <ControlBar disableDefaultControls={true} autoHide={true} className="control-bar">
-          <PlayButton key="play-toggle" order={1} />
-          <MuteButton key="volume-menu-button" order={4} />
-          <CurrentTimeDisplay key="current-time-display" order={5.1} />
-          <TimeDivider key="time-divider" order={5.2} />
-          <DurationDisplay key="duration-display" order={5.3} />
-          <ProgressControl key="progress-control" order={6} />
-          <PlaybackRateMenuButton key="playback-spoeed" order={8} />
-          <FullScreenButton key="fullscreen-toggle" order={9} />
-        </ControlBar>
-      </Player>
-    </div>
-  );
-});
+          {video.src.map((src) => (
+            <source key={src.url} src={src.url} type={src.contenttype} />
+          ))}
+          {video.captions?.map((caption, idx) => (
+            <track
+              key={idx}
+              src={caption.src}
+              kind="captions"
+              label={caption.label}
+              srcLang={caption.language_code}
+            />
+          ))}
+          <ControlBar disableDefaultControls={true} autoHide={true} className="control-bar">
+            <PlayButton key="play-toggle" order={1} />
+            <MuteButton key="volume-menu-button" order={4} />
+            <CurrentTimeDisplay key="current-time-display" order={5.1} />
+            <TimeDivider key="time-divider" order={5.2} />
+            <DurationDisplay key="duration-display" order={5.3} />
+            <ProgressControl key="progress-control" order={6} />
+            <PlaybackRateMenuButton key="playback-spoeed" order={8} />
+            <FullScreenButton key="fullscreen-toggle" order={9} />
+            {hasCaptions && <ClosedCaptionButton order={10} />}
+          </ControlBar>
+        </Player>
+        {children}
+      </div>
+    );
+  },
+);
 
 VideoPlayer.displayName = 'VideoPlayer';
