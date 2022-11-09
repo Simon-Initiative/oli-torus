54173a1f36a37ff31215749d9e0b8cbb574286d4
Eli Knebel <eliknebel@cmu.edu>
2022-11-09 17:57:53 -0500

    make publish active idempotent

--- lib/oli/utils/appsignal.ex
+++ lib/oli/utils/appsignal.ex
@@ -9,19 +9,29 @@ defmodule Oli.Utils.Appsignal do
   |> Oli.Utils.Appsignal()
   ```
   """
-  def capture_error(e) when is_atom(e) do
+  def capture_error(e, metadata \\ nil)
+
+  def capture_error(e, metadata) when is_atom(e) do
     Atom.to_string(e)
-    |> capture_error()
+    |> capture_error(metadata)
   end
 
-  def capture_error(e) when is_binary(e) do
+  def capture_error(e, metadata) when is_binary(e) do
     try do
       raise e
     catch
       kind, reason ->
-        Appsignal.send_error(kind, reason, __STACKTRACE__)
+        case metadata do
+          nil ->
+            Appsignal.send_error(kind, reason, __STACKTRACE__)
+
+          metadata ->
+            Appsignal.send_error(kind, reason, __STACKTRACE__, fn span ->
+              Appsignal.Span.set_attribute(span, "metadata", metadata)
+            end)
+        end
     end
   end
 
-  def capture_error({e}), do: capture_error(e)
+  def capture_error({e}, metadata), do: capture_error(e, metadata)
 end
