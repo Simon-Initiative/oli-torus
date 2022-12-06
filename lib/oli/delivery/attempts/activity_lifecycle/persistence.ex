815a7b61a480b6acd1a42105690a1f9231678b9e
Darren Siegel <siegel.darren@gmail.com>
2022-12-06 15:14:36 -0500

    [PERFORMANCE] Streamline submit activity [MER-1592] (#3164)
    * stashing
    
    * bulk persistence call

--- lib/oli/delivery/attempts/activity_lifecycle/persistence.ex
+++ lib/oli/delivery/attempts/activity_lifecycle/persistence.ex
@@ -4,12 +4,8 @@ defmodule Oli.Delivery.Attempts.ActivityLifecycle.Persistence do
 
   alias Oli.Delivery.Evaluation.Actions.{
     FeedbackAction,
-    NavigationAction,
-    StateUpdateAction,
     SubmissionAction
   }
-
-  alias Oli.Delivery.Attempts.Core.PartAttempt
 
   @moduledoc """
   Routines for persisting evaluations for part attempts.
@@ -30,151 +26,90 @@ defmodule Oli.Delivery.Attempts.ActivityLifecycle.Persistence do
   def persist_evaluations({:ok, evaluations}, part_inputs, roll_up_fn, datashop_session_id) do
     evaluated_inputs = Enum.zip(part_inputs, evaluations)
 
-    case Enum.reduce_while(
-           evaluated_inputs,
-           {:ok, false, []},
-           persist_single_evaluation_with_datashop_session_fn(datashop_session_id)
-         ) do
-      {:ok, _, results} -> roll_up_fn.({:ok, results})
-      error -> error
-    end
-  end
-
-  def persist_evaluations(
-        {:ok, evaluations},
-        part_inputs,
-        roll_up_fn,
-        replace,
-        datashop_session_id
-      ) do
-    case replace do
-      false ->
-        persist_evaluations({:ok, evaluations}, part_inputs, roll_up_fn, datashop_session_id)
-
-      true ->
-        evaluated_inputs = Enum.zip(part_inputs, evaluations)
-
-        case Enum.reduce_while(
-               evaluated_inputs,
-               {:ok, replace, []},
-               persist_single_evaluation_with_datashop_session_fn(datashop_session_id)
-             ) do
-          {:ok, _, results} -> roll_up_fn.({:ok, results})
-          error -> error
-        end
-    end
-  end
-
-  defp persist_single_evaluation_with_datashop_session_fn(datashop_session_id) do
-    &persist_single_evaluation(&1, &2, datashop_session_id)
-  end
-
-  # Persist the result of a single evaluation for a single part_input submission.
-  defp persist_single_evaluation({_, {:error, error}}, _, _), do: {:halt, {:error, error}}
-
-  defp persist_single_evaluation(
-         {_, {:ok, %NavigationAction{} = action_result}},
-         {:ok, replace, results},
-         _datashop_session_id
-       ) do
-    {:cont, {:ok, replace, results ++ [action_result]}}
-  end
-
-  defp persist_single_evaluation(
-         {_, {:ok, %StateUpdateAction{} = action_result}},
-         {:ok, replace, results},
-         _datashop_session_id
-       ) do
-    {:cont, {:ok, replace, results ++ [action_result]}}
-  end
-
-  defp persist_single_evaluation(
-         {%{attempt_guid: attempt_guid, input: input},
-          {:ok,
-           %FeedbackAction{
-             feedback: feedback,
-             score: score,
-             out_of: out_of
-           } = feedback_action}},
-         {:ok, replace, results},
-         datashop_session_id
-       ) do
-    now = DateTime.utc_now()
-
-    query =
-      from(p in PartAttempt,
-        where: p.attempt_guid == ^attempt_guid
+    update_values = Enum.map(evaluated_inputs, fn pair -> attrs_for(pair, datashop_session_id) end)
+    |> Enum.filter(fn attrs -> !is_nil(attrs) end)
+    |> Enum.map(fn pa ->
+      """
+      (
+        '#{pa.attempt_guid}',
+        #{handle_json(pa.response)},
+        '#{Atom.to_string(pa.lifecycle_state)}',
+        #{null_or_now(pa.date_evaluated)},
+        #{null_or_now(pa.date_submitted)},
+        #{handle_num(pa.score)},
+        #{handle_num(pa.out_of)},
+        #{handle_json(pa.feedback)},
+        '#{pa.datashop_session_id}'
       )
+      """
+    end)
+    |> Enum.join(",")
 
-    query =
-      if replace === false do
-        where(query, [p], is_nil(p.date_evaluated))
-      else
-        query
-      end
+    sql = """
+      UPDATE part_attempts
+      SET
+        response = batch_values.response,
+        lifecycle_state = batch_values.lifecycle_state,
+        date_evaluated = batch_values.date_evaluated,
+        date_submitted = batch_values.date_submitted,
+        score = batch_values.score,
+        out_of = batch_values.out_of,
+        feedback = batch_values.feedback,
+        datashop_session_id = batch_values.datashop_session_id,
+        updated_at = NOW()
+      FROM (
+          VALUES
+          #{update_values}
+      ) AS batch_values (attempt_guid, response, lifecycle_state, date_evaluated, date_submitted, score, out_of, feedback, datashop_session_id)
+      WHERE part_attempts.attempt_guid = batch_values.attempt_guid
+    """
 
-    case Repo.update_all(
-           query,
-           set: [
-             response: input,
-             lifecycle_state: :evaluated,
-             date_evaluated: now,
-             date_submitted: now,
-             score: score,
-             out_of: out_of,
-             feedback: feedback,
-             datashop_session_id: datashop_session_id
-           ]
-         ) do
-      nil ->
-        {:halt, {:error, :error}}
-
-      {1, _} ->
-        {:cont, {:ok, replace, results ++ [feedback_action]}}
-
-      _ ->
-        {:halt, {:error, :error}}
+    case Ecto.Adapters.SQL.query(Repo, sql, []) do
+      {:ok, _} -> roll_up_fn.({:ok, Enum.map(evaluations, fn {:ok, action} -> action end)})
+      e -> e
     end
   end
 
-  defp persist_single_evaluation(
-         {%{attempt_guid: attempt_guid, input: input},
-          {:ok, %SubmissionAction{} = submission_action}},
-         {:ok, replace, results},
-         datashop_session_id
-       ) do
-    now = DateTime.utc_now()
-
-    query =
-      from(p in PartAttempt,
-        where: p.attempt_guid == ^attempt_guid
-      )
-
-    query =
-      if replace === false do
-        where(query, [p], is_nil(p.date_evaluated))
-      else
-        query
-      end
-
-    case Repo.update_all(
-           query,
-           set: [
-             response: input,
-             lifecycle_state: :submitted,
-             date_evaluated: nil,
-             date_submitted: now,
-             datashop_session_id: datashop_session_id
-           ]
-         ) do
-      nil ->
-        {:halt, {:error, :error}}
-
-      {1, _} ->
-        {:cont, {:ok, replace, results ++ [submission_action]}}
-
-      _ ->
-        {:halt, {:error, :error}}
-    end
+  defp attrs_for({%{attempt_guid: attempt_guid, input: input}, {:ok, %FeedbackAction{
+    feedback: feedback,
+    score: score,
+    out_of: out_of}}}, datashop_session_id) do
+    %{
+      attempt_guid: attempt_guid,
+      response: input,
+      lifecycle_state: :evaluated,
+      date_evaluated: true,
+      date_submitted: true,
+      score: score,
+      out_of: out_of,
+      feedback: feedback,
+      datashop_session_id: datashop_session_id
+    }
   end
+
+  defp attrs_for({%{attempt_guid: attempt_guid, input: input}, {:ok, %SubmissionAction{}}}, datashop_session_id) do
+    %{
+      attempt_guid: attempt_guid,
+      response: input,
+      lifecycle_state: :submitted,
+      date_evaluated: nil,
+      date_submitted: true,
+      score: nil,
+      out_of: nil,
+      feedback: nil,
+      datashop_session_id: datashop_session_id
+    }
+  end
+
+  defp attrs_for(_, _), do: nil
+
+  defp handle_num(nil), do: "NULL::double precision"
+  defp handle_num(v), do: "#{v}"
+
+  defp handle_json(nil), do: "NULL::JSONB"
+  defp handle_json(map), do: "'#{Jason.encode!(map)}'::JSONB"
+
+  defp null_or_now(nil), do: "NULL::timestamp"
+  defp null_or_now(_), do: "NOW()"
+
 end
