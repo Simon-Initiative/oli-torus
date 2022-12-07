d8cc0526f533af205d20b0b3e7857d2e60d48ec1
Anders Weinstein <andersweinstein@users.noreply.github.com>
2022-12-07 13:58:05 -0500

    [FEATURE] working hotspot delivery w/temp authoring (#3163)
    * working hotspot delivery w/temp authoring
    
    * Auto format
    
    * remove log stmts & unused imports
    
    Co-authored-by: Anders Weinstein <andersw@cs.cmu.edu>

---
+++ assets/src/components/activities/image_hotspot/schema.ts
@@ -0,0 +1,59 @@
+import {
+  ActivityModelSchema,
+  Stem,
+  Part,
+  ChoiceIdsToResponseId,
+  Transformation,
+  Choice,
+  RichText,
+  makeChoice,
+} from '../types';
+import { ID } from 'data/content/model/other';
+import { CATACompatible } from '../check_all_that_apply/actions';
+
+export class Hotspot implements Choice {
+  id: ID;
+  content: RichText;
+  coords: number[];
+  title?: string;
+}
+
+export function getShape(hotspot: Hotspot): string | undefined {
+  const n = hotspot.coords.length;
+  if (n === 3) return 'circle';
+  if (n === 4) return 'rect';
+  if (n >= 6 && n % 2 === 0) return 'poly';
+  return undefined;
+}
+
+export function makeHotspot(): Hotspot {
+  const hotspot = new Hotspot();
+  const choice = makeChoice('');
+  hotspot.id = choice.id;
+  hotspot.content = choice.content;
+  hotspot.coords = [];
+  return hotspot;
+}
+
+export interface ImageHotspotModelSchema extends ActivityModelSchema, CATACompatible {
+  stem: Stem;
+  imageURL: string | undefined;
+  height?: number;
+  width?: number;
+  choices: Hotspot[];
+  multiple: boolean;
+  authoring: {
+    // If multiple is true, we must provide a CATA model, in which response rules are complex and
+    // correct holds an association list of correct choice ids to the matching response id
+    correct: ChoiceIdsToResponseId;
+    targeted: ChoiceIdsToResponseId[];
+    parts: Part[];
+    transformations: Transformation[];
+    previewText: string;
+  };
+}
+
+export interface ModelEditorProps {
+  model: ImageHotspotModelSchema;
+  editMode: boolean;
+}
