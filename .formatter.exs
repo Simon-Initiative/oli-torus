af4ad473122dcb217e1ad6d1dc5f11bd12916a6f
Mauricio Cassola <mauricio@wyeworks.com>
2022-12-20 14:03:51 -0300

    Add format for heex templates

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 [
   import_deps: [:ecto, :phoenix, :surface],
   surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  subdirectories: ["priv/*/migrations"],
+  plugins: [Phoenix.LiveView.HTMLFormatter]
 ]
