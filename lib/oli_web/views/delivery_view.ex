3ba09c5d95f839618550fa86e4bef64a86a64607
Eli Knebel <eliknebel@cmu.edu>
2023-01-08 16:35:01 -0500

    fix account dropdown fix client side modals adjust logo and dropdown profile pic

--- lib/oli_web/views/delivery_view.ex
+++ lib/oli_web/views/delivery_view.ex
@@ -193,7 +193,7 @@ defmodule OliWeb.DeliveryView do
       _picture ->
         ~H"""
         <div class="user-icon">
-          <img src={@current_user.picture} class="rounded-circle" />
+          <img src={@current_user.picture} referrerpolicy="no-referrer" class="rounded-full" />
         </div>
         """
     end
@@ -202,7 +202,7 @@ defmodule OliWeb.DeliveryView do
   def user_icon(assigns) do
     ~H"""
     <div class="user-icon">
-      <div class="user-img rounded-circle">
+      <div class="user-img rounded-full">
         <span class="material-icons text-secondary">account_circle</span>
       </div>
     </div>
