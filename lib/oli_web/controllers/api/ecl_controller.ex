f97be198583ccc9badea4412d26f2a7dbfa0e375
Darren Siegel <siegel.darren@gmail.com>
2023-01-24 09:25:53 -0500

    add timing output

--- lib/oli_web/controllers/api/ecl_controller.ex
+++ lib/oli_web/controllers/api/ecl_controller.ex
@@ -8,9 +8,17 @@ defmodule OliWeb.Api.ECLController do
     user_name = System.get_env("ECL_USERNAME", "")
     password = System.get_env("ECL_PASSWORD", "")
 
+    # Login
+    mark = :os.system_time(:millisecond)
     auth_token = Constellation.login(user_name, password)
+    time = :os.system_time(:millisecond) - mark
+    IO.inspect("Constellation.login: #{time}ms")
 
+    # Execute
+    mark = :os.system_time(:millisecond)
     result = Constellation.execute_sll_expression(auth_token, code)
+    time = :os.system_time(:millisecond) - mark
+    IO.inspect("Constellation.execute_sll_expression: #{time}ms")
 
     json(conn, %{"result" => result})
   end
