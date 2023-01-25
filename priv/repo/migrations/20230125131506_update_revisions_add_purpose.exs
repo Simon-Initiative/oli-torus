97287a3667aa2d6c2eb32c8db829c2530b7641d6
Augusto Alonso <augusto1024@gmail.com>
2023-01-25 16:10:42 -0300

    Replace up/down migrations for change migration

--- priv/repo/migrations/20230125131506_update_revisions_add_purpose.exs
+++ priv/repo/migrations/20230125131506_update_revisions_add_purpose.exs
@@ -1,28 +1,10 @@
 defmodule Oli.Repo.Migrations.UpdateRevisionsAddPurpose do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:revisions) do
-      add :purpose, :string, default: "foundation"
-      add :relates_to, {:array, :id}, default: []
-    end
-
-    execute """
-    UPDATE revisions
-    SET purpose = 'foundation', relates_to=array[]::integer[];
-    """
-
-    alter table(:revisions) do
-      modify :purpose, :string, default: "foundation", null: false
-      modify :relates_to, {:array, :id}, default: [], null: false
-    end
-  end
-
-
-  def down do
-    alter table(:revisions) do
-      remove :purpose
-      remove :tags
+      add :purpose, :string, default: "foundation", null: false
+      add :relates_to, {:array, :id}, default: [], null: false
     end
   end
 end
