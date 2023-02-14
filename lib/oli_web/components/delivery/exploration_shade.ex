10e656905c9c07ba11b68d1f4be73e5e353578ea
Augusto Alonso <augusto1024@gmail.com>
2023-02-14 09:31:45 -0300

    Add notification badge to exploration acitivities window shade

--- lib/oli_web/components/delivery/exploration_shade.ex
+++ lib/oli_web/components/delivery/exploration_shade.ex
@@ -2,12 +2,18 @@ defmodule OliWeb.Components.Delivery.ExplorationShade do
   use Phoenix.Component
 
   def exploration_shade(assigns) do
+    assigns = assign(assigns, exploration_pages: Map.get(assigns, :exploration_pages, nil))
     ~H"""
       <div class="bg-delivery-header text-white border-b border-slate-300">
         <div class="container mx-auto md:px-10 py-2 flex flex-row justify-between text-sm md:text-lg">
-          <h4 class="p-2 md:p-0 md:leading-loose">
-            Your Exploration Activities
-          </h4>
+          <div class="flex items-center gap-2">
+            <h4 class="p-2 md:p-0 md:leading-loose">
+              Your Exploration Activities
+            </h4>
+            <%= if not is_nil(@exploration_pages) and length(@exploration_pages) > 0 do %>
+              <span class="badge badge-info rounded-full"><%= length(@exploration_pages) %></span>
+            <% end %>
+          </div>
 
           <div>
             <button class="btn group" data-bs-toggle="collapse" data-bs-target="#collapseExploration" aria-expanded="false" aria-controls="collapseExploration">
@@ -16,7 +22,14 @@ defmodule OliWeb.Components.Delivery.ExplorationShade do
           </div>
         </div>
         <div class="collapse container mx-auto md:px-10 py-2" id="collapseExploration">
-          EXAMPLE EXPLORATION CONTENT
+          <%= if not is_nil(@exploration_pages) and length(@exploration_pages) > 0 do %>
+            <!-- TODO: Add the expandable components implemented in MER-1674 once it gets merged -->
+            <%= for page <- @exploration_pages |> IO.inspect() do %>
+              <p><%= page.title %></p>
+            <% end %>
+          <% else %>
+              <p class="italic">There are no explorations related to this page</p>
+          <% end %>
         </div>
       </div>
     """
