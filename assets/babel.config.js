56f26754acdb1ceb96d71a934f29036eabbf3853
Marc Hughes <marc.hughes@gmail.com>
2023-02-14 20:53:44 -0500

    [FEATURE] Feature/storybook (#3259)
    * [docs] Add Storybook to document react components.
    
    * [docs] Add storybook build into actions.

--- assets/babel.config.js
+++ assets/babel.config.js
@@ -4,5 +4,5 @@ module.exports = {
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [],
 };
