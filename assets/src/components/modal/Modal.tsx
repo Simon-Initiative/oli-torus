645e75dc1ff5dc06d5c124ead64ff36e82039c4a
Eli Knebel <eliknebel@cmu.edu>
2023-02-16 16:59:49 -0500

    remove console log and comments

--- assets/src/components/modal/Modal.tsx
+++ assets/src/components/modal/Modal.tsx
@@ -1,7 +1,6 @@
 import React, { PropsWithChildren, useEffect, useRef } from 'react';
 import { classNames } from 'utils/classNames';
 import { valueOr } from 'utils/common';
-import { lockScroll, unlockScroll } from './utils';
 
 interface Modal {
   modal: any;
@@ -44,10 +43,7 @@ export const Modal = (props: PropsWithChildren<ModalProps>) => {
     if (modal.current) {
       const currentModal = modal.current;
 
-      console.log('show modal', currentModal);
-
       (window as any).$(currentModal).modal('show');
-      // const scrollPosition = lockScroll();
 
       $(currentModal).on('hidden.bs.modal', (e) => {
         onCancel(e);
@@ -55,7 +51,6 @@ export const Modal = (props: PropsWithChildren<ModalProps>) => {
 
       return () => {
         (window as any).$(currentModal).modal('hide');
-        // unlockScroll(scrollPosition);
       };
     }
   }, []);
