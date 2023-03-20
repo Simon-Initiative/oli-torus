ac76f59c115e283d25572bcb6a7ed24b7dd1332d
Darren Siegel <siegel.darren@gmail.com>
2023-03-20 11:57:52 -0400

    [MER-1814] fix alternatives previewing (#3344)

--- lib/oli/authoring/editing/page_editor.ex
+++ lib/oli/authoring/editing/page_editor.ex
@@ -276,10 +276,11 @@ defmodule Oli.Authoring.Editing.PageEditor do
            mode: mode,
            activity_map: activities,
            resource_summary_fn: &Resources.resource_summary(&1, project_slug, AuthoringResolver),
-           alternatives_groups_fn: &Resources.alternatives_groups(&1, AuthoringResolver),
+           alternatives_groups_fn: fn -> Resources.alternatives_groups(project_slug, AuthoringResolver) end,
            alternatives_selector_fn: &Resources.Alternatives.select/2,
            extrinsic_read_section_fn: &Oli.Delivery.ExtrinsicState.read_section/3,
            project_slug: project_slug,
+           section_slug: project_slug,
            bib_app_params: Keyword.get(options, :bib_app_params, []),
            learning_language: attributes.learning_language
          } do
