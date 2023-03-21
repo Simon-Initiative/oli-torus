0dcc8b78e4160da0ee8ac2e6d4166be3519e94ca
Eli Knebel <eliknebel@cmu.edu>
2023-03-21 16:23:28 -0400

    publish dark mode fixes

--- lib/oli_web/live/projects/lti_connect_instructions.ex
+++ lib/oli_web/live/projects/lti_connect_instructions.ex
@@ -10,14 +10,15 @@ defmodule OliWeb.Projects.LtiConnectInstructions do
     ~F"""
       <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id={@id} tabindex="-1" aria-labelledby="exampleModalLgLabel" aria-modal="true" role="dialog" phx-hook="ModalLaunch">
         <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
-          <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+          <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none">
             <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-              <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLgLabel">
+              <h5 class="text-xl font-medium leading-normal" id="exampleModalLgLabel">
                 Connect to LMS
               </h5>
               <button type="button"
-                class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                class="btn-close box-content p-1 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:opacity-75 hover:no-underline"
                 data-bs-dismiss="modal" aria-label="Close">
+                <i class="fa-solid fa-xmark fa-xl"></i>
               </button>
             </div>
 
