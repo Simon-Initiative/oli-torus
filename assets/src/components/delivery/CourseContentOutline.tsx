af607f0d0ffc1b639a86a72f48d8acc8418ca65d
Eli Knebel <eliknebel@cmu.edu>
2023-03-28 09:02:31 -0400

    [ENHANCEMENT] Preview course outline support (#3372)
    * add course outline functional support for authoring and instructor preview
    
    * fix code block styles
    
    * Auto format
    
    * fix instructor preview links
    
    ---------
    
    Co-authored-by: eliknebel <eliknebel@users.noreply.github.com>

--- assets/src/components/delivery/CourseContentOutline.tsx
+++ assets/src/components/delivery/CourseContentOutline.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react';
 import { classNames } from 'utils/classNames';
 
+type MaybeSlug = string | null;
+
 interface CourseContentOutlineProps {
-  sectionSlug: string;
+  sectionSlug: MaybeSlug;
+  projectSlug: MaybeSlug;
   hierarchy: any;
+  isPreview?: boolean;
 }
 
-export const CourseContentOutline = ({ sectionSlug, hierarchy }: CourseContentOutlineProps) => {
-  const items = flatten({ type: 'root', ...hierarchy }, sectionSlug);
+export const CourseContentOutline = ({
+  sectionSlug,
+  projectSlug,
+  hierarchy,
+  isPreview,
+}: CourseContentOutlineProps) => {
+  const items = flatten({ ...hierarchy, type: 'root' }, sectionSlug, projectSlug, isPreview);
   const active = items.find((i: FlattenedItem) => i.isActive);
   const activeContainerSlug = active?.containerSlug;
 
@@ -31,6 +40,8 @@ export const CourseContentOutline = ({ sectionSlug, hierarchy }: CourseContentOu
             key={pageItemProps.id}
             {...pageItemProps}
             sectionSlug={sectionSlug}
+            projectSlug={projectSlug}
+            isPreview={isPreview}
             activeContainerSlug={activeContainerSlug}
           />
         ))}
@@ -51,13 +62,18 @@ type FlattenedItem = {
 
 const flatten = (
   item: HierarchyItem | Root,
-  sectionSlug: string,
+  sectionSlug: MaybeSlug,
+  projectSlug: MaybeSlug,
+  isPreview: boolean | undefined,
   containerSlug?: string | undefined,
   level = 0,
 ): FlattenedItem[] =>
   item.type === 'root'
     ? item.children.reduce(
-        (acc, c) => [...acc, ...flatten(c, sectionSlug, containerSlug, level + 1)],
+        (acc, c) => [
+          ...acc,
+          ...flatten(c, sectionSlug, projectSlug, isPreview, containerSlug, level + 1),
+        ],
         [],
       )
     : item.type === 'container'
@@ -69,12 +85,12 @@ const flatten = (
           slug: item.slug,
           level,
           containerSlug: item.slug,
-          isActive: isCurrentUrl(sectionSlug, item.type, item.slug),
+          isActive: isCurrentUrl(sectionSlug, projectSlug, item.type, item.slug, isPreview),
         },
         ...item.children.reduce(
           (acc: FlattenedItem[], c: HierarchyItem) => [
             ...acc,
-            ...flatten(c, sectionSlug, item.slug, level + 1),
+            ...flatten(c, sectionSlug, projectSlug, isPreview, item.slug, level + 1),
           ],
           [],
         ),
@@ -87,7 +103,7 @@ const flatten = (
           slug: item.slug,
           level,
           containerSlug,
-          isActive: isCurrentUrl(sectionSlug, item.type, item.slug),
+          isActive: isCurrentUrl(sectionSlug, projectSlug, item.type, item.slug, isPreview),
         },
       ];
 
@@ -113,16 +129,34 @@ interface Page {
 
 type HierarchyItem = Container | Page;
 
-const url = (sectionSlug: string, type: string, slug: string) =>
-  `/sections/${sectionSlug}/${type}/${slug}`;
+const url = (
+  sectionSlug: MaybeSlug,
+  projectSlug: MaybeSlug,
+  type: string,
+  slug: string,
+  isPreview: boolean | undefined,
+) =>
+  sectionSlug
+    ? isPreview
+      ? `/sections/${sectionSlug}/preview/${type}/${slug}`
+      : `/sections/${sectionSlug}/${type}/${slug}`
+    : `/authoring/project/${projectSlug}/preview/${slug}`;
 
-const isCurrentUrl = (sectionSlug: string, type: string, slug: string) => {
-  return window.location.href.endsWith(url(sectionSlug, type, slug));
+const isCurrentUrl = (
+  sectionSlug: MaybeSlug,
+  projectSlug: MaybeSlug,
+  type: string,
+  slug: string,
+  isPreview: boolean | undefined,
+) => {
+  return window.location.href.endsWith(url(sectionSlug, projectSlug, type, slug, isPreview));
 };
 
 interface PageItemProps extends FlattenedItem {
   activeContainerSlug: string | undefined;
-  sectionSlug: string;
+  sectionSlug: MaybeSlug;
+  projectSlug: MaybeSlug;
+  isPreview: boolean | undefined;
 }
 
 const PageItem = ({
@@ -131,18 +165,20 @@ const PageItem = ({
   slug,
   level,
   sectionSlug,
+  projectSlug,
   containerSlug,
   activeContainerSlug,
+  isPreview,
 }: PageItemProps) => (
   <a
     id={`outline-item-${slug}`}
-    href={url(sectionSlug, type, slug)}
+    href={url(sectionSlug, projectSlug, type, slug, isPreview)}
     className={classNames(
       'block p-3 border-l-[8px] text-current hover:text-delivery-primary hover:no-underline',
       activeContainerSlug &&
         (containerSlug === activeContainerSlug || slug == activeContainerSlug) &&
         'border-delivery-primary-100',
-      isCurrentUrl(sectionSlug, type, slug)
+      isCurrentUrl(sectionSlug, projectSlug, type, slug, isPreview)
         ? '!border-delivery-primary bg-delivery-primary-50 dark:bg-delivery-primary-800'
         : 'border-transparent',
     )}
