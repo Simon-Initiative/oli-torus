4ec5a1a135b0710bc44d5f48246448633a971541
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-04-03 15:56:02 -0300

    fix duplicated header after rebase

--- lib/oli_web/live/delivery/instructor_dashboard/instructor_dashboard_live.ex
+++ lib/oli_web/live/delivery/instructor_dashboard/instructor_dashboard_live.ex
@@ -31,10 +31,8 @@ defmodule OliWeb.Delivery.InstructorDashboard.InstructorDashboardLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <InstructorDashboard.main_layout {assigns}>
       <InstructorDashboard.tabs active_tab={@active_tab} section_slug={@section_slug} preview_mode={@preview_mode} />
       <%= render_tab(assigns) %>
-    </InstructorDashboard.main_layout>
     """
   end
 
