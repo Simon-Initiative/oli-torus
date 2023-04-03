849628bb091539c1ab1cb467fe4e8035c1409c7e
Darren Siegel <siegel.darren@gmail.com>
2023-04-03 10:31:17 -0400

    fix test

--- lib/oli_web/components/delivery/discussion_activity/discussion_activity.ex
+++ lib/oli_web/components/delivery/discussion_activity/discussion_activity.ex
@@ -222,6 +222,7 @@ defmodule OliWeb.Components.Delivery.DiscussionActivity do
           )
 
         discussion_table_model = Map.put(discussion_table_model, :rows, rows)
+        |> Map.put(:data, %{section_slug: section_slug, target: socket.assigns.myself})
 
         assign(socket,
           discussion_table_model: discussion_table_model,
--- test/oli_web/live/delivery/instructor_dashboard/assignments_tab_test.exs
+++ test/oli_web/live/delivery/instructor_dashboard/assignments_tab_test.exs
@@ -60,7 +60,7 @@ defmodule OliWeb.Delivery.InstructorDashboard.AssignmentsTabTest do
     } do
       Sections.enroll(instructor.id, section.id, [ContextRoles.get_role(:context_instructor)])
 
-      {:ok, view, _html} = live(conn, live_view_assignments_route(section.slug))
+      {:ok, view, html} = live(conn, live_view_assignments_route(section.slug))
 
       # Assignments tab is the selected one
       assert has_element?(
@@ -70,7 +70,7 @@ defmodule OliWeb.Delivery.InstructorDashboard.AssignmentsTabTest do
              )
 
       # Assignments tab content gets rendered
-      assert has_element?(view, ~s{div}, "Not available yet")
+      assert String.contains?(html, "Not available yet")
     end
   end
 end
