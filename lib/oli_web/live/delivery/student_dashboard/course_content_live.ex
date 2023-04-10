c1519de8107b99ff4fadf4f09d80ba1394baddf0
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-04-10 09:21:16 -0300

    breadcrumbs navigation

--- lib/oli_web/live/delivery/student_dashboard/course_content_live.ex
+++ lib/oli_web/live/delivery/student_dashboard/course_content_live.ex
@@ -1,7 +1,6 @@
 defmodule OliWeb.Delivery.StudentDashboard.CourseContentLive do
   use OliWeb, :live_view
 
-  alias OliWeb.Components.Delivery.Buttons
   alias Oli.Delivery.Sections
 
   @impl Phoenix.LiveView
@@ -10,9 +9,19 @@ defmodule OliWeb.Delivery.StudentDashboard.CourseContentLive do
       Sections.get_section_by_slug(section_slug)
       |> Oli.Repo.preload([:base_project, :root_section_resource])
 
-    hierarchy = Sections.build_hierarchy(section)
+    hierarchy = %{"children" => Sections.build_hierarchy(section).children}
+    current_position = 0
+    current_level = 0
 
-    {:ok, assign(socket, hierarchy: hierarchy)}
+    {:ok,
+     assign(socket,
+       hierarchy: hierarchy,
+       current_level_nodes: hierarchy["children"],
+       current_position: current_position,
+       current_level: current_level,
+       section_slug: section_slug,
+       breadcrumbs_tree: [{current_level, current_position, "Curriculum"}]
+     )}
   end
 
   @impl Phoenix.LiveView
@@ -25,31 +34,219 @@ defmodule OliWeb.Delivery.StudentDashboard.CourseContentLive do
           <span class="text-xs">Find all your course content, material, assignments and class activities here.</span>
         </section>
         <section class="flex flex-row justify-between p-9">
-          <button>left arrow</button>
+          <div class="text-xs absolute -mt-7"><%= render_breadcrumbs(%{breadcrumbs_tree: @breadcrumbs_tree}) %></div>
+          <button phx-click="previous_node" class={if @current_position == 0, do: "grayscale pointer-events-none"}>
+            <i class="fa-regular fa-circle-left text-primary"></i>
+          </button>
           <div class="flex flex-col">
-            <h4 class="text-base font-semibold mx-auto">Unit 1: Composition of Substances and Solutions</h4>
+            <h4 class="text-base font-semibold mx-auto"><%= get_current_node(@current_level_nodes, @current_position)["title"] || "Curriculum" %> </h4>
             <div class="flex items-center justify-center space-x-3">
-              <span class="uppercase text-xs">Unit 1 overall progress</span>
+              <span class="uppercase text-xs">overall progress</span>
               <div class="w-40 rounded-full bg-gray-200 h-2">
-                <div class="rounded-full bg-primary h-2" style={"width: #{30}%"}></div>
+                <div class="rounded-full bg-primary h-2" style={"width: #{Enum.random(0..100)}%"}></div>
               </div>
             </div>
           </div>
-          <button>right arrow</button>
+          <button phx-click="next_node" class={if @current_position + 1 == length(@current_level_nodes), do: "grayscale pointer-events-none"}>
+            <i class="fa-regular fa-circle-right text-primary"></i>
+          </button>
         </section>
-        <%= for resource <- [1,2,3] do %>
+        <%= for {resource, index} <- get_current_node(@current_level_nodes, @current_position)["children"] |> Enum.with_index() do %>
           <section class="flex flex-row w-full p-9">
             <div class="flex flex-col mr-4">
-              <h4 class="text-base font-semibold"><%= "1.0 Intro to Chemistry 101: test #{resource}" %></h4>
+              <h4 class={"text-base font-semibold #{if resource["type"] == "container", do: "underline cursor-pointer"}"} phx-click="go_down" phx-value-resource_id={resource["id"]} phx-value-selected_resource_index={index} phx-value-resource_type={resource["type"]}><%= resource["title"] %></h4>
               <span class="text-xs">Estimated completion time: 20 mins</span>
             </div>
             <span class="w-80 text-center text-xs bg-gray-200 px-3 py-2 rounded-sm ml-auto mr-4">Due by 10-03-2023</span>
-            <Buttons.button class="h-10">Open</Buttons.button>
+            <button class="torus-button primary h-10" phx-click="open_resource" phx-value-resource_slug={resource["slug"]} phx-value-resource_type={resource["type"]}>Open</button>
           </section>
         <% end %>
       </div>
-
     </div>
     """
   end
+
+  def render_breadcrumbs(%{breadcrumbs_tree: []}), do: nil
+
+  def render_breadcrumbs(assigns) do
+    ~H"""
+    <div class="flex flex-row space-x-2 divide-x divide-gray-100 dark:divide-gray-700">
+      <%= for {target_level, target_position, text} <- @breadcrumbs_tree do %>
+        <button
+          phx-click="breadcrumb-navigate"
+          phx-value-target_level={target_level}
+          phx-value-target_position={target_position}>
+          <%= text %>
+        </button>
+      <% end %>
+    </div>
+    """
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("next_node", _params, socket)
+      when length(socket.assigns.current_level_nodes) == socket.assigns.current_position,
+      do: {:noreply, socket}
+
+  @impl Phoenix.LiveView
+  def handle_event(
+        "next_node",
+        _params,
+        %{assigns: %{breadcrumbs_tree: current_breadcrumbs_tree}} = socket
+      ) do
+    {current_level, current_position, text} = List.last(current_breadcrumbs_tree)
+
+    updated_breadcrumbs_tree =
+      List.replace_at(
+        current_breadcrumbs_tree,
+        length(current_breadcrumbs_tree) - 1,
+        {current_level, current_position + 1, text}
+      )
+
+    socket =
+      socket
+      |> update(:current_position, &(&1 + 1))
+      |> assign(:breadcrumbs_tree, updated_breadcrumbs_tree)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("previous_node", _params, socket) when socket.assigns.current_position == 0,
+    do: {:noreply, socket}
+
+  @impl Phoenix.LiveView
+  def handle_event(
+        "previous_node",
+        _params,
+        %{assigns: %{breadcrumbs_tree: current_breadcrumbs_tree}} = socket
+      ) do
+    {current_level, current_position, text} = List.last(current_breadcrumbs_tree)
+
+    updated_breadcrumbs_tree =
+      List.replace_at(
+        current_breadcrumbs_tree,
+        length(current_breadcrumbs_tree) - 1,
+        {current_level, current_position - 1, text}
+      )
+
+    socket =
+      socket
+      |> update(:current_position, &(&1 - 1))
+      |> assign(:breadcrumbs_tree, updated_breadcrumbs_tree)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("go_down", %{"resource_type" => "page"}, socket), do: {:noreply, socket}
+
+  @impl Phoenix.LiveView
+  def handle_event("go_down", %{"selected_resource_index" => selected_resource_index}, socket) do
+    current_node =
+      get_current_node(socket.assigns.current_level_nodes, socket.assigns.current_position)
+
+    selected_resource_index = String.to_integer(selected_resource_index)
+
+    breadcrumbs_tree =
+      socket.assigns.breadcrumbs_tree ++
+        [{socket.assigns.current_level + 1, selected_resource_index, current_node["title"]}]
+
+    socket =
+      socket
+      |> update(:current_level, &(&1 + 1))
+      |> assign(:current_position, selected_resource_index)
+      |> assign(:current_level_nodes, current_node["children"])
+      |> assign(:breadcrumbs_tree, breadcrumbs_tree)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event(
+        "breadcrumb-navigate",
+        %{"target_level" => target_level, "target_position" => target_position},
+        socket
+      ) do
+    breadcrumbs_tree =
+      update_breadcrumbs_tree(
+        socket.assigns.breadcrumbs_tree,
+        String.to_integer(target_level),
+        String.to_integer(target_position)
+      )
+
+    current_level_nodes = get_current_level_nodes(breadcrumbs_tree, socket.assigns.hierarchy)
+
+    {level, current_position, _text} = List.last(breadcrumbs_tree)
+
+    socket =
+      socket
+      |> assign(:breadcrumbs_tree, breadcrumbs_tree)
+      |> assign(:current_level, level + 1)
+      |> assign(:current_position, current_position)
+      |> assign(:current_level_nodes, current_level_nodes)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event(
+        "open_resource",
+        %{"resource_slug" => resource_slug, "resource_type" => resource_type},
+        socket
+      ) do
+    {:noreply,
+     redirect(socket,
+       to:
+         Routes.page_delivery_path(
+           socket,
+           String.to_existing_atom(resource_type),
+           socket.assigns[:section_slug],
+           resource_slug
+         )
+     )}
+  end
+
+  defp update_breadcrumbs_tree(breadcrumbs_tree, 0, _target_position),
+    do: [hd(breadcrumbs_tree)]
+
+  defp update_breadcrumbs_tree(breadcrumbs_tree, target_level, target_position) do
+    {true, breadcrumbs_tree} =
+      Enum.reduce(breadcrumbs_tree, {false, []}, fn b, acc ->
+        {level, position, _text} = b
+        {found, breadcrumbs_tree} = acc
+
+        if level == target_level and position == target_position and found == false do
+          {true, breadcrumbs_tree}
+        else
+          if found == true do
+            {true, breadcrumbs_tree}
+          else
+            {false, [b | breadcrumbs_tree]}
+          end
+        end
+      end)
+
+    Enum.reverse(breadcrumbs_tree)
+  end
+
+  defp get_current_level_nodes(breadcrumbs_tree, hierarchy) do
+    {0, current_level_nodes} =
+      breadcrumbs_tree
+      |> Enum.reduce({length(breadcrumbs_tree) - 1, hierarchy["children"]}, fn b, acc ->
+        {steps_remaining, hierarchy} = acc
+        {_level, position, _text} = b
+
+        if steps_remaining == 0 do
+          {0, hierarchy}
+        else
+          {steps_remaining - 1, Enum.fetch!(hierarchy, position)["children"]}
+        end
+      end)
+
+    current_level_nodes
+  end
+
+  defp get_current_node(current_level_nodes, current_position),
+    do: Enum.fetch!(current_level_nodes, current_position)
 end
