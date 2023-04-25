629ce5f8fefc559919deb545e04909947fe5a9ed
Santiago Simoncelli <simoncellisantiago@gmail.com>
2023-04-25 17:15:10 -0300

    [MER-1725] Adds progress table model

---
+++ lib/oli_web/components/delivery/progress/student_progress_table_model.ex
@@ -0,0 +1,113 @@
+defmodule OliWeb.Components.Delivery.StudentProgressTabelModel do
+  use Surface.LiveComponent
+  alias OliWeb.Common.Table.{ColumnSpec, SortableTableModel}
+  alias OliWeb.Router.Helpers, as: Routes
+  alias OliWeb.Progress.ResourceTitle
+
+  @moduledoc """
+  This table model displays various pieces of "progress" information for all course resources in a
+  course section, for a specific student.
+
+  To construct this rows of this model, this implementation takes a flattened list of hierarchy nodes
+  and maps those to the specific row structure that is needed, pulling in information from a %ResourceAccess{}
+  struct.  A resource access record only exists though if that student has visited the resource at least
+  once.
+  """
+
+  @doc """
+  Takes a list of %HierarchyNode{}, a map of resource_ids to %ResourceAccess{} structs, and the
+  section and user to construct the table model.
+  """
+  def new(rows, section_slug, student_id, context) do
+    SortableTableModel.new(
+      rows: rows,
+      column_specs: [
+        %ColumnSpec{
+          name: :index,
+          label: "Order",
+          td_class: "text-center pl-0",
+          th_class: "!text-center"
+        },
+        %ColumnSpec{
+          name: :title,
+          label: "Resource Title",
+          render_fn: &__MODULE__.custom_render/3
+        },
+        %ColumnSpec{
+          name: :type,
+          label: "Type"
+        },
+        %ColumnSpec{
+          name: :score,
+          label: "Score",
+          render_fn: &__MODULE__.custom_render/3,
+          sortable: false,
+          td_class: "text-center",
+          th_class: "!text-center"
+        },
+        %ColumnSpec{
+          name: :number_attempts,
+          label: "# Attempts",
+          render_fn: &__MODULE__.custom_render/3,
+          sortable: false,
+          td_class: "text-center",
+          th_class: "!text-center"
+        },
+        %ColumnSpec{
+          name: :number_accesses,
+          label: "# Accesses",
+          render_fn: &__MODULE__.custom_render/3,
+          sortable: false,
+          td_class: "text-center",
+          th_class: "!text-center"
+        },
+        %ColumnSpec{
+          name: :inserted_at,
+          label: "First Visited",
+          render_fn: &OliWeb.Common.Table.Common.render_date/3,
+          sortable: false
+        },
+        %ColumnSpec{
+          name: :updated_at,
+          label: "Last Visited",
+          render_fn: &OliWeb.Common.Table.Common.render_date/3,
+          sortable: false
+        }
+      ],
+      event_suffix: "",
+      id_field: [:index],
+      data: %{
+        section_slug: section_slug,
+        user_id: student_id,
+        context: context
+      }
+    )
+  end
+
+  def custom_render(assigns, row, %ColumnSpec{name: :title}) do
+    ~F"""
+      <ResourceTitle
+        node={row.node}
+        url={Routes.live_path(OliWeb.Endpoint, OliWeb.Progress.StudentResourceView, assigns.section_slug, assigns.user_id, row.resource_id)}
+      />
+    """
+  end
+
+  def custom_render(_assigns, row, %ColumnSpec{name: :score}) do
+    if row.type == "Graded" and !is_nil(row.score), do: "#{row.score} / #{row.out_of}", else: ""
+  end
+
+  def custom_render(_assigns, row, %ColumnSpec{name: :number_accesses}) do
+    if row.number_accesses == 0, do: "", else: row.number_accesses
+  end
+
+  def custom_render(_assigns, row, %ColumnSpec{name: :number_attempts}) do
+    if row.number_attempts == 0, do: "", else: row.number_attempts
+  end
+
+  def render(assigns) do
+    ~F"""
+      <div>nothing</div>
+    """
+  end
+end
