6aece9d4b5481d5dc2d643eeb5f59a3f55a9297b
Santiago Simoncelli <simoncellisantiago@gmail.com>
2023-04-26 16:20:33 -0300

    [FEATURE] [MER-1725] Student progress details tab (#3443)
    * [MER-1725] Adds progress component
    
    * [MER-1725] Adds progress table model
    
    * [MER-1725] Fix date format in table model
    
    * [MER-1725] Adds tests

--- test/oli_web/live/delivery/student_dashboard/components/progress_tab_test.exs
+++ test/oli_web/live/delivery/student_dashboard/components/progress_tab_test.exs
@@ -8,7 +8,7 @@ defmodule OliWeb.Delivery.StudentDashboard.Components.ProgressTabTest do
   alias Lti_1p3.Tool.ContextRoles
   alias Oli.Delivery.Sections
 
-  defp live_view_students_dashboard_route(
+  defp live_view_students_progress_route(
          section_slug,
          student_id,
          tab,
@@ -31,25 +31,420 @@ defmodule OliWeb.Delivery.StudentDashboard.Components.ProgressTabTest do
     %{student: student}
   end
 
-  describe "Progress tab" do
-    setup [:instructor_conn, :section_with_assessment, :enrolled_student_and_instructor]
-
-    test "gets rendered correctly", %{
+  defp insert_resource_access(page1, page3, page5, section, user1) do
+    # Page 1
+    insert(:resource_access,
+      user: user1,
       section: section,
+      resource: page1.resource,
+      score: 5,
+      out_of: 10
+    )
+
+    # Page 3
+    insert(:resource_access,
+      user: user1,
+      section: section,
+      resource: page3.resource,
+      score: 7,
+      out_of: 10
+    )
+
+    # Page 5
+    insert(:resource_access,
+      user: user1,
+      section: section,
+      resource: page5.resource,
+      score: 2,
+      out_of: 10
+    )
+  end
+
+  describe "user" do
+    test "cannot access page when it is not logged in", %{conn: conn} do
+      section = insert(:section)
+      student = insert(:user)
+
+      redirect_path =
+        "/session/new?request_path=%2Fsections%2F#{section.slug}%2Fstudent_dashboard%2F#{student.id}%2Fprogress"
+
+      assert {:error, {:redirect, %{to: ^redirect_path}}} =
+               live(
+                 conn,
+                 live_view_students_progress_route(section.slug, student.id, :progress)
+               )
+    end
+  end
+
+  describe "student" do
+    setup [:user_conn]
+
+    test "cannot access page", %{user: user, conn: conn} do
+      section = insert(:section)
+      redirect_path = "/unauthorized"
+
+      assert {:error, {:redirect, %{to: ^redirect_path}}} =
+               live(
+                 conn,
+                 live_view_students_progress_route(section.slug, user.id, :progress)
+               )
+    end
+  end
+
+  describe "instructor" do
+    setup [:instructor_conn, :section_without_pages]
+
+    test "cannot access page if not enrolled to section", %{
       conn: conn,
-      student: student
+      section: section
     } do
+      student = insert(:user)
+      redirect_path = "/unauthorized"
+
+      assert {:error, {:redirect, %{to: ^redirect_path}}} =
+               live(
+                 conn,
+                 live_view_students_progress_route(section.slug, student.id, :progress)
+               )
+    end
+
+    test "can access page if enrolled to section", %{
+      conn: conn,
+      section: section,
+      instructor: instructor
+    } do
+      student = insert(:user)
+      Sections.enroll(instructor.id, section.id, [ContextRoles.get_role(:context_instructor)])
+
       {:ok, view, _html} =
-        live(conn, live_view_students_dashboard_route(section.slug, student.id, :progress))
+        live(
+          conn,
+          live_view_students_progress_route(section.slug, student.id, :progress)
+        )
 
       # Progress tab is the selected one
       assert has_element?(
                view,
-               ~s{a[href="#{live_view_students_dashboard_route(section.slug, student.id, :progress)}"].border-b-2},
+               ~s{a[href="#{live_view_students_progress_route(section.slug, student.id, :progress)}"].border-b-2},
                "Progress"
              )
 
-      assert has_element?(view, "p", "Not available yet")
+      # Progress tab content gets rendered
+      assert has_element?(view, "h6", "There are no progress to show")
+    end
+  end
+
+  describe "Progress tab" do
+    setup [:instructor_conn, :section_with_gating_conditions, :enrolled_student_and_instructor]
+
+    test "gets rendered correctly", %{
+      section: section,
+      conn: conn,
+      student: student,
+      graded_page_1: graded_page_1,
+      graded_page_2: graded_page_2
+    } do
+      {:ok, view, _html} =
+        live(conn, live_view_students_progress_route(section.slug, student.id, :progress))
+
+      # Progress tab is the selected one
+      assert has_element?(
+               view,
+               ~s{a[href="#{live_view_students_progress_route(section.slug, student.id, :progress)}"].border-b-2},
+               "Progress"
+             )
+
+      assert has_element?(view, "a", graded_page_1.title)
+      assert has_element?(view, "a", graded_page_2.title)
+    end
+
+    test "applies searching", %{
+      conn: conn,
+      section: section,
+      student: student,
+      graded_page_1: graded_page_1,
+      graded_page_2: graded_page_2,
+      graded_page_3: graded_page_3
+    } do
+      insert_resource_access(
+        graded_page_1,
+        graded_page_2,
+        graded_page_3,
+        section,
+        student
+      )
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress
+          )
+        )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_1.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_2.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_3.title}"
+             )
+
+      # searching by student
+      params = %{
+        text_search: graded_page_2.title
+      }
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress,
+            params
+          )
+        )
+
+      refute has_element?(
+               view,
+               "div",
+               "#{graded_page_1.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_2.title}"
+             )
+
+      refute has_element?(
+               view,
+               "div",
+               "#{graded_page_3.title}"
+             )
+    end
+
+    test "applies sorting", %{
+      conn: conn,
+      section: section,
+      student: student,
+      graded_page_1: graded_page_1,
+      graded_page_2: graded_page_2,
+      graded_page_3: graded_page_3,
+      graded_page_4: graded_page_4,
+      graded_page_5: graded_page_5,
+      graded_page_6: graded_page_6
+    } do
+      insert_resource_access(
+        graded_page_1,
+        graded_page_2,
+        graded_page_3,
+        section,
+        student
+      )
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress
+          )
+        )
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:first-child")
+             |> render() =~ graded_page_1.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(2)")
+             |> render() =~ graded_page_2.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(3)")
+             |> render() =~ graded_page_3.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(4)")
+             |> render() =~ graded_page_4.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(5)")
+             |> render() =~ graded_page_5.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:last-child")
+             |> render() =~ graded_page_6.title
+
+      ## sorting by student
+      params = %{
+        sort_order: :desc
+      }
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress,
+            params
+          )
+        )
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:last-child")
+             |> render() =~ graded_page_1.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(5)")
+             |> render() =~ graded_page_2.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(4)")
+             |> render() =~ graded_page_3.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(3)")
+             |> render() =~ graded_page_4.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:nth-child(2)")
+             |> render() =~ graded_page_5.title
+
+      assert view
+             |> element("table.instructor_dashboard_table > tbody > tr:first-child")
+             |> render() =~ graded_page_6.title
+    end
+
+    test "applies pagination", %{
+      conn: conn,
+      section: section,
+      student: student,
+      graded_page_1: graded_page_1,
+      graded_page_2: graded_page_2,
+      graded_page_3: graded_page_3
+    } do
+      insert_resource_access(
+        graded_page_1,
+        graded_page_2,
+        graded_page_3,
+        section,
+        student
+      )
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress
+          )
+        )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_1.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_2.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_3.title}"
+             )
+
+      ## aplies limit
+      params = %{
+        limit: 2,
+        sort_order: "asc"
+      }
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress,
+            params
+          )
+        )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_1.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_2.title}"
+             )
+
+      refute has_element?(
+               view,
+               "div",
+               "#{graded_page_3.title}"
+             )
+
+      ## aplies pagination
+      params = %{
+        limit: 2,
+        offset: 2,
+        sort_order: "asc"
+      }
+
+      {:ok, view, _html} =
+        live(
+          conn,
+          live_view_students_progress_route(
+            section.slug,
+            student.id,
+            :progress,
+            params
+          )
+        )
+
+      refute has_element?(
+               view,
+               "div",
+               "#{graded_page_1.title}"
+             )
+
+      refute has_element?(
+               view,
+               "div",
+               "#{graded_page_2.title}"
+             )
+
+      assert has_element?(
+               view,
+               "div",
+               "#{graded_page_3.title}"
+             )
     end
   end
 end
