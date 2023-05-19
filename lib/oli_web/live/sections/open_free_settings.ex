e416f8e3dba13c95ab099faca88ad13e349b8cc6
Darren Siegel <siegel.darren@gmail.com>
2023-05-19 09:26:57 -0400

    [ENHANCEMENT] Ensure editing of start end dates available to both LTI and open and free [MER-1984] [MER-1953] (#3549)
    * always be able to edit start and end dates
    
    * ensure start end date editing always available

--- lib/oli_web/live/sections/open_free_settings.ex
+++ lib/oli_web/live/sections/open_free_settings.ex
@@ -2,8 +2,7 @@ defmodule OliWeb.Sections.OpenFreeSettings do
   use OliWeb, :surface_component
 
   alias Surface.Components.Field
-  alias Surface.Components.Form.{Field, Label, DateTimeLocalInput, Checkbox, ErrorTag}
-  alias OliWeb.Common.FormatDateTime
+  alias Surface.Components.Form.{Field, Label, Checkbox}
   alias OliWeb.Common.Properties.Group
   import Ecto.Changeset
 
@@ -13,16 +12,6 @@ defmodule OliWeb.Sections.OpenFreeSettings do
   prop context, :struct, required: true
 
   def render(assigns) do
-    start_date =
-      assigns.changeset
-      |> Ecto.Changeset.get_field(:start_date)
-      |> FormatDateTime.convert_datetime(assigns.context)
-
-    end_date =
-      assigns.changeset
-      |> Ecto.Changeset.get_field(:end_date)
-      |> FormatDateTime.convert_datetime(assigns.context)
-
     ~F"""
     <Group label="Direct Delivery" description="Direct Delivery section settings">
       <Field name={:registration_open} class="form-check">
@@ -40,16 +29,6 @@ defmodule OliWeb.Sections.OpenFreeSettings do
         <Label class="form-check-label">Omit student email verification</Label>
       </Field>
 
-      <div class="form-row mt-4">
-        <Field name={:start_date} class="mr-3 form-label-group">
-          <div class="d-flex justify-content-between"><Label/><ErrorTag class="help-block"/></div>
-          <DateTimeLocalInput class="form-control" value={start_date} opts={disabled: @disabled}/>
-        </Field>
-        <Field name={:end_date} class="form-label-group">
-          <div class="d-flex justify-content-between"><Label/><ErrorTag class="help-block"/></div>
-          <DateTimeLocalInput class="form-control" value={end_date} opts={disabled: @disabled}/>
-        </Field>
-      </div>
       <div class="form-row">
         <small class="text-nowrap form-text text-muted">
           Timezone: {@context.local_tz}
