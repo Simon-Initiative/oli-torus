e004465fca3c043c01843486949451ab5d0f2469
Eli Knebel <eliknebel@cmu.edu>
2023-06-29 11:52:25 -0400

    upgrade react storybook to v7

--- assets/.storybook/main.js
+++ assets/.storybook/main.js
@@ -1,29 +1,34 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-scss',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass'),
+        },
+      },
+    },
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
   staticDirs: ['./temp'],
   webpackFinal: async (config) => {
     config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
-
     const sassOptions = {
       sassOptions: {
         includePaths: [path.join(__dirname, '../styles')],
         quietDeps: true,
       },
     };
-
     config.module.rules = config.module.rules.map((oldRule) => {
       // This injects the sassOptions above into the sass loader so our imports from styles/ work correctly
       if (!oldRule.use) return oldRule;
@@ -43,5 +48,8 @@ module.exports = {
 
     // console.info(JSON.stringify(config.module.rules, null, 2));
     return config;
+  },
+  docs: {
+    autodocs: true,
   },
 };
