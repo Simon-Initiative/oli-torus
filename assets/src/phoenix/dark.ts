a1f35a35ff76cbc48e30b395907e271b49429e8c
Eli Knebel <eliknebel@cmu.edu>
2023-07-11 11:52:14 -0400

    [BUG FIX] [MER-2275] V24 dark mode tweaks (#3886)
    * fix several dark mode issues
    fix an issue with instructor preview links
    
    * fix tests

--- assets/src/phoenix/dark.ts
+++ assets/src/phoenix/dark.ts
@@ -1,17 +1,45 @@
 import { addDarkModeListener, isDarkMode } from 'utils/browser';
+import theme from '../../tailwind.theme';
+
+function findOrCreateElement(selector: string): Element {
+  const metaThemeColor = document.querySelector(selector);
+  if (!metaThemeColor) {
+    document.appendChild(document.createElement('meta')).setAttribute('name', 'theme-color');
+  }
+
+  return document.querySelector(selector) as Element;
+}
+
+// Set the <meta name="theme-color"> tag for mobile browsers to render correct colors
+function setMetaThemeColor(dark: boolean) {
+  const metaThemeColor = findOrCreateElement('meta[name=theme-color]');
+
+  const lightColor = theme['colors']['delivery']['body']['DEFAULT'];
+  const darkColor = theme['colors']['delivery']['body']['dark']['DEFAULT'];
+
+  if (dark) {
+    metaThemeColor.setAttribute('content', darkColor);
+  } else {
+    metaThemeColor.setAttribute('content', lightColor);
+  }
+}
 
 if ((!('theme' in localStorage) && isDarkMode()) || localStorage.theme === 'dark') {
   document.documentElement.classList.add('dark');
+  setMetaThemeColor(true);
 } else {
   document.documentElement.classList.remove('dark');
+  setMetaThemeColor(false);
 }
 
 addDarkModeListener((mode) => {
   if (!('theme' in localStorage)) {
     if (mode === 'dark') {
       document.documentElement.classList.add('dark');
+      setMetaThemeColor(true);
     } else {
       document.documentElement.classList.remove('dark');
+      setMetaThemeColor(false);
     }
   }
 });
