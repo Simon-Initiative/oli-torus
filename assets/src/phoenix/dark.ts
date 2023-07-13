4793455909765790ac967cd843ef8ad439e08a7a
Eli Knebel <eliknebel@cmu.edu>
2023-07-13 14:21:40 -0400

    remove gray background from captions fix an issue with dark mode on load

--- assets/src/phoenix/dark.ts
+++ assets/src/phoenix/dark.ts
@@ -4,7 +4,7 @@ import theme from '../../tailwind.theme';
 function findOrCreateElement(selector: string): Element {
   const metaThemeColor = document.querySelector(selector);
   if (!metaThemeColor) {
-    document.appendChild(document.createElement('meta')).setAttribute('name', 'theme-color');
+    document.head.appendChild(document.createElement('meta')).setAttribute('name', 'theme-color');
   }
 
   return document.querySelector(selector) as Element;
@@ -32,19 +32,19 @@ if ((!('theme' in localStorage) && isDarkMode()) || localStorage.theme === 'dark
   setMetaThemeColor(false);
 }
 
-addDarkModeListener((mode) => {
-  if (!('theme' in localStorage)) {
-    if (mode === 'dark') {
-      document.documentElement.classList.add('dark');
-      setMetaThemeColor(true);
-    } else {
-      document.documentElement.classList.remove('dark');
-      setMetaThemeColor(false);
-    }
-  }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+  addDarkModeListener((mode) => {
+    if (!('theme' in localStorage)) {
+      if (mode === 'dark') {
+        document.documentElement.classList.add('dark');
+        setMetaThemeColor(true);
+      } else {
+        document.documentElement.classList.remove('dark');
+        setMetaThemeColor(false);
+      }
+    }
+  });
+
   if (isDarkMode()) {
     [].slice
       .call(document.querySelectorAll('.g-recaptcha'))
