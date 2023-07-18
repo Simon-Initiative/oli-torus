39b036f798b7e02e14e486140bd1ceb7b2c9330b
Devesh Tiwari <dtiwari@ats360.com>
2023-07-18 11:01:05 +0530

    Update RestartLessonDialog.tsx

--- assets/src/apps/delivery/layouts/deck/RestartLessonDialog.tsx
+++ assets/src/apps/delivery/layouts/deck/RestartLessonDialog.tsx
@@ -113,7 +113,6 @@ const RestartLessonDialog: React.FC<RestartLessonDialogProps> = ({ onRestart }) 
       >
         <div className="modal-header">
           <h3>Restart Lesson</h3>
-
           <button
             type="button"
             className="close"
