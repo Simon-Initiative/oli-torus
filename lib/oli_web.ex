ca4982397a9d1d350314372d423f8b170a3e1efc
Eli Knebel <eliknebel@cmu.edu>
2023-07-20 13:55:46 -0400

    fix remaining compiler errors

--- lib/oli_web.ex
+++ lib/oli_web.ex
@@ -119,7 +119,6 @@ defmodule OliWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
-      # import OliWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
