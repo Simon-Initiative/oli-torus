209aab43ad8a7a41ddf9bf4ffc38c65a7a693b5d
Darren Siegel <siegel.darren@gmail.com>
2023-07-27 15:48:46 -0400

    fix the test

--- lib/oli_web/live/delivery/student_onboarding/explorations.ex
+++ lib/oli_web/live/delivery/student_onboarding/explorations.ex
@@ -7,7 +7,7 @@ defmodule OliWeb.Delivery.StudentOnboarding.Explorations do
         <h2>Exploration Activities</h2>
         <span class="text-gray-500 text-sm">Explorations dig into how the course subject matter affects you</span>
         <img class="aspect-video w-full" src="/images/exploration.gif" />
-        <p class="mt-14">You&apos;ll have access to both simulations and digital versions of tools used in the real world to help you explore the topics brought up in the course from a real-world perspective.</p>
+        <p class="mt-14">You will have access to both simulations and digital versions of tools used in the real world to help you explore the topics brought up in the course from a real-world perspective.</p>
       </div>
     """
   end
