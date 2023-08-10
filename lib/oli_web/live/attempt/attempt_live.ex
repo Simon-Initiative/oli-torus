dbf811af5b551127d82df62d68bcbcf8adc09161
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-08-10 11:13:45 -0300

    migrate OliWeb.Attempt.AttemptLive

--- lib/oli_web/live/attempt/attempt_live.ex
+++ lib/oli_web/live/attempt/attempt_live.ex
@@ -2,7 +2,7 @@ defmodule OliWeb.Attempt.AttemptLive do
   import Ecto.Query, warn: false
   alias Oli.Repo
 
-  use Surface.LiveView, layout: {OliWeb.LayoutView, :live}
+  use OliWeb, :live_view
 
   alias OliWeb.Common.Breadcrumb
   alias OliWeb.Common.SortableTable.Table
@@ -12,17 +12,6 @@ defmodule OliWeb.Attempt.AttemptLive do
   alias Oli.Delivery.Attempts.PageLifecycle.Broadcaster
   alias OliWeb.Attempt.TableModel
   alias Oli.Delivery.Attempts.Core.{ActivityAttempt, ResourceAttempt}
-  @retain_count 100
-
-  data retain_count, :integer, default: @retain_count
-  data breadcrumbs, :any
-  data title, :string, default: "Debug Attempt"
-  data section, :any, default: nil
-  data table_model, :any, default: []
-  data updates, :list, default: []
-  data total_count, :integer, default: 0
-  data content, :any, default: nil
-  data selected, :any, default: nil
 
   def set_breadcrumbs(type, section, guid) do
     type
@@ -63,67 +52,48 @@ defmodule OliWeb.Attempt.AttemptLive do
            total_count: Enum.count(attempts),
            updates: [],
            section: section,
-           attempts: attempts
+           attempts: attempts,
+           selected: nil,
+           content: nil
          )}
     end
   end
 
   def render(assigns) do
-    ~F"""
+    ~H"""
     <div style="display: flex; flex-direction: row;">
       <div style="flex-grow: 4;">
-        <Table.render model={@table_model} select="select" sort="sort"/>
+        <Table.render model={@table_model} select="select" sort="sort" />
       </div>
       <div style="display: flex; flex-direction: column;">
-        {render_parts(assigns)}
+        <table :if={@selected}>
+          <tr>
+            <th>Part id</th>
+            <th>Attempt#</th>
+            <th>State</th>
+            <th>Score</th>
+            <th>Out of</th>
+          </tr>
+          <%= for p <- @selected.part_attempts do %>
+            <tr>
+              <td><%= p.part_id %></td>
+              <td><%= p.attempt_number %></td>
+              <td><%= p.lifecycle_state %></td>
+              <td><%= p.score %></td>
+              <td><%= p.out_of %></td>
+            </tr>
+          <% end %>
+        </table>
         <div style="margin-top: 30px; background: #DDDDDD;">
           <code>
-          <pre>
-          {render_content(assigns)}
-          </pre>
+            <pre :if={@content}>
+              <%= Jason.encode!(assigns.content, pretty: true) %>
+            </pre>
           </code>
         </div>
       </div>
     </div>
     """
-  end
-
-  def render_content(assigns) do
-    case assigns.content do
-      nil -> ""
-      _ -> Jason.encode!(assigns.content, pretty: true)
-    end
-  end
-
-  def render_parts(assigns) do
-    case assigns.selected do
-      nil ->
-        ""
-
-      _ ->
-        ~F"""
-
-            <table>
-              <tr>
-                <th>Part id</th>
-                <th>Attempt#</th>
-                <th>State</th>
-                <th>Score</th>
-                <th>Out of</th>
-              </tr>
-              {#for p <- @selected.part_attempts}
-                <tr>
-                  <td>{p.part_id}</td>
-                  <td>{p.attempt_number}</td>
-                  <td>{p.lifecycle_state}</td>
-                  <td>{p.score}</td>
-                  <td>{p.out_of}</td>
-                </tr>
-              {/for}
-            </table>
-
-        """
-    end
   end
 
   def handle_event("select", %{"id" => id} = _p, socket) do
