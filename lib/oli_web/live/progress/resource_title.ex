d587306dccf48154e3a32e7a9ddb27790d6e4fe0
Santiago Simoncelli <simoncellisantiago@gmail.com>
2023-08-11 12:01:04 -0300

    Migrate OliWeb.Progress.ResourceTitle

--- lib/oli_web/live/progress/resource_title.ex
+++ lib/oli_web/live/progress/resource_title.ex
@@ -1,31 +1,39 @@
 defmodule OliWeb.Progress.ResourceTitle do
-  use Surface.Component
+  use OliWeb, :html
 
   @moduledoc """
   Display the title of a resource, with a breadcrumb-like header above it indicating the
   path within the curriculum to this resource.
   """
 
-  prop node, :any, required: true
-  prop url, :string, required: true
+  attr :node, :any, required: true
+  attr :url, :string, required: true
 
   def render(assigns) do
     length = length(assigns.node.ancestors)
     numbering = assigns.node.numbering
 
-    ~F"""
+    assigns = assign(assigns, length: length, numbering: numbering)
+
+    ~H"""
     <div>
       <div>
         <small class="text-muted">
-          {#for {ancestor, index} <- Enum.with_index(@node.ancestors)}
-            <span>{Oli.Resources.Numbering.container_type_label(%Oli.Resources.Numbering{numbering | level: ancestor.section_resource.numbering_level, index: ancestor.section_resource.numbering_index})} {ancestor.section_resource.numbering_index}</span>
-            {#if index + 1 < length}
+          <%= for {ancestor, index} <- Enum.with_index(@node.ancestors) do %>
+            <span>
+              <%= Oli.Resources.Numbering.container_type_label(%Oli.Resources.Numbering{
+                @numbering
+                | level: ancestor.section_resource.numbering_level,
+                  index: ancestor.section_resource.numbering_index
+              }) %> <%= ancestor.section_resource.numbering_index %>
+            </span>
+            <%= if index + 1 < @length do %>
               <span> / </span>
-            {/if}
-          {/for}
+            <% end %>
+          <% end %>
         </small>
       </div>
-      <a href={@url}>{@node.revision.title}</a>
+      <a href={@url}><%= @node.revision.title %></a>
     </div>
     """
   end
