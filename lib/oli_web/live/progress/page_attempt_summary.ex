a9e0ea0bb54f2eb5da38bc2fc5bc0dd236a0b4db
Santiago Simoncelli <simoncellisantiago@gmail.com>
2023-08-11 10:50:16 -0300

    Migrate lib/oli_web/live/progress/page_attempt_summary.ex

--- lib/oli_web/live/progress/page_attempt_summary.ex
+++ lib/oli_web/live/progress/page_attempt_summary.ex
@@ -1,12 +1,12 @@
 defmodule OliWeb.Progress.PageAttemptSummary do
-  use OliWeb, :surface_component
+  use OliWeb, :live_component
   alias OliWeb.Router.Helpers, as: Routes
   alias OliWeb.Common.Utils
 
-  prop(attempt, :struct, required: true)
-  prop(section, :struct, required: true)
-  prop(ctx, :struct, required: true)
-  prop(revision, :struct, required: true)
+  attr(:attempt, :map, required: true)
+  attr(:section, :map, required: true)
+  attr(:ctx, :map, required: true)
+  attr(:revision, :map, required: true)
 
   @spec render(
           atom
@@ -21,54 +21,104 @@ defmodule OliWeb.Progress.PageAttemptSummary do
         %{revision: %{graded: graded}, attempt: %{attempt_guid: guid, lifecycle_state: :active}} =
           assigns
       ) do
-    ~F"""
+    ~H"""
     <div class="list-group-item list-group-action flex-column align-items-start">
-      <a href={Routes.instructor_review_path(OliWeb.Endpoint, :review_attempt, @section.slug, @attempt.attempt_guid)} class="block">
+      <a
+        href={
+          Routes.instructor_review_path(
+            OliWeb.Endpoint,
+            :review_attempt,
+            @section.slug,
+            @attempt.attempt_guid
+          )
+        }
+        class="block"
+      >
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">Attempt {@attempt.attempt_number}</h5>
+          <h5 class="mb-1">Attempt <%= @attempt.attempt_number %></h5>
           <span>Not Submitted Yet</span>
         </div>
-        <p class="mb-1">Started: {Utils.render_date(@attempt, :inserted_at, @ctx)}</p>
-        <small class="text-muted">Time elapsed: {duration(@attempt.inserted_at, DateTime.utc_now())}.</small>
+        <p class="mb-1">Started: <%= Utils.render_date(@attempt, :inserted_at, @ctx) %></p>
+        <small class="text-muted">
+          Time elapsed: <%= duration(@attempt.inserted_at, DateTime.utc_now()) %>.
+        </small>
       </a>
-      {#if graded}
-        <button class="btn btn-danger btn-sm" phx-click="submit_attempt" phx-value-guid={guid}>Submit Attempt on Behalf of Student</button>
-      {/if}
+      <%= if graded do %>
+        <button class="btn btn-danger btn-sm" phx-click="submit_attempt" phx-value-guid={guid}>
+          Submit Attempt on Behalf of Student
+        </button>
+      <% end %>
     </div>
     """
   end
 
   def do_render(%{attempt: %{lifecycle_state: :evaluated}} = assigns) do
-    ~F"""
+    ~H"""
     <div class="list-group-item list-group-action flex-column align-items-start">
-      <a href={Routes.instructor_review_path(OliWeb.Endpoint, :review_attempt, @section.slug, @attempt.attempt_guid)} class="block">
+      <a
+        href={
+          Routes.instructor_review_path(
+            OliWeb.Endpoint,
+            :review_attempt,
+            @section.slug,
+            @attempt.attempt_guid
+          )
+        }
+        class="block"
+      >
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">Attempt {@attempt.attempt_number}</h5>
-          <span>{Utils.format_score(@attempt.score)} / {@attempt.out_of}</span>
+          <h5 class="mb-1">Attempt <%= @attempt.attempt_number %></h5>
+          <span><%= Utils.format_score(@attempt.score) %> / <%= @attempt.out_of %></span>
         </div>
         <div class="d-flex flex-row">
-          {#if @attempt.was_late}
+          <%= if @attempt.was_late do %>
             <.badge variant={:danger}>LATE</.badge>
-          {/if}
-          <p class="mb-1 text-muted">Submitted: {Utils.render_date(@attempt, :date_evaluated, @ctx)} ({Utils.render_relative_date(@attempt, :date_evaluated, @ctx)})</p>
+          <% end %>
+          <p class="mb-1 text-muted">
+            Submitted: <%= Utils.render_date(@attempt, :date_evaluated, @ctx) %> (<%= Utils.render_relative_date(
+              @attempt,
+              :date_evaluated,
+              @ctx
+            ) %>)
+          </p>
         </div>
 
-        <small class="text-muted">Time elapsed: {duration(@attempt.inserted_at, @attempt.date_evaluated)}.</small>
-        </a>
+        <small class="text-muted">
+          Time elapsed: <%= duration(@attempt.inserted_at, @attempt.date_evaluated) %>.
+        </small>
+      </a>
     </div>
     """
   end
 
   def do_render(%{attempt: %{lifecycle_state: :submitted}} = assigns) do
-    ~F"""
+    ~H"""
     <div class="list-group-item list-group-action flex-column align-items-start">
-      <a href={Routes.instructor_review_path(OliWeb.Endpoint, :review_attempt, @section.slug, @attempt.attempt_guid)} class="block">
+      <a
+        href={
+          Routes.instructor_review_path(
+            OliWeb.Endpoint,
+            :review_attempt,
+            @section.slug,
+            @attempt.attempt_guid
+          )
+        }
+        class="block"
+      >
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">Attempt {@attempt.attempt_number}</h5>
+          <h5 class="mb-1">Attempt <%= @attempt.attempt_number %></h5>
           <span>Submitted</span>
         </div>
-        <p class="mb-1 text-muted">Submitted: {Utils.render_date(@attempt, :date_submitted, @ctx)} ({Utils.render_relative_date(@attempt, :date_submitted, @ctx)})</p>
-        <small class="text-muted">Time elapsed: {duration(@attempt.inserted_at, @attempt.date_submitted)}.</small>
+        <p class="mb-1 text-muted">
+          Submitted: <%= Utils.render_date(@attempt, :date_submitted, @ctx) %> (<%= Utils.render_relative_date(
+            @attempt,
+            :date_submitted,
+            @ctx
+          ) %>)
+        </p>
+        <small class="text-muted">
+          Time elapsed: <%= duration(@attempt.inserted_at, @attempt.date_submitted) %>.
+        </small>
       </a>
     </div>
     """
