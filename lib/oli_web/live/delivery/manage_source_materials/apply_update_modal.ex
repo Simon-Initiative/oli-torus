5d0f3ca92b75633c2e173ba98bc5e9c77d5457c7
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-08-14 09:13:55 -0300

    migrate OliWeb.Delivery.ManageSourceMaterials.ProjectInfo, OliWeb.Delivery.ManageSourceMaterials, OliWeb.Delivery.ManageSourceMaterials.ApplyUpdateModal and OliWeb.Delivery.ManageSourceMaterials.ProjectCard

--- lib/oli_web/live/delivery/manage_source_materials/apply_update_modal.ex
+++ lib/oli_web/live/delivery/manage_source_materials/apply_update_modal.ex
@@ -1,70 +1,86 @@
 defmodule OliWeb.Delivery.ManageSourceMaterials.ApplyUpdateModal do
-  use Surface.Component
+  use OliWeb, :html
 
   import OliWeb.Common.FormatDateTime
 
   alias OliWeb.Common.Utils
 
-  prop changes, :map, required: true
-  prop current_publication, :struct, required: true
-  prop id, :string, required: true
-  prop newest_publication, :struct, required: true
-  prop project_id, :integer, required: true
-  prop updates, :map, required: true
+  attr(:changes, :map, required: true)
+  attr(:current_publication, :map, required: true)
+  attr(:id, :string, required: true)
+  attr(:newest_publication, :map, required: true)
+  attr(:project_id, :integer, required: true)
+  attr(:updates, :map, required: true)
 
   def render(assigns) do
-    ~F"""
-      <div class="modal fade show" id={@id} tabindex="-1" role="dialog" aria-hidden="true" phx-hook="ModalLaunch">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
+    ~H"""
+    <div
+      class="modal fade show"
+      id={@id}
+      tabindex="-1"
+      role="dialog"
+      aria-hidden="true"
+      phx-hook="ModalLaunch"
+    >
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Apply Update - <%= @newest_publication.project.title %></h5>
+            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+              <i class="fa-solid fa-xmark fa-xl"></i>
+            </button>
+          </div>
 
-            <div class="modal-header">
-              <h5 class="modal-title">Apply Update - {@newest_publication.project.title}</h5>
-              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                <i class="fa-solid fa-xmark fa-xl"></i>
-              </button>
-            </div>
+          <div class="modal-body pb-0">
+            <h6 class="mb-3">
+              Do you want to apply this update from
+              <strong>
+                <%= Utils.render_version(
+                  @current_publication.edition,
+                  @current_publication.major,
+                  @current_publication.minor
+                ) %>
+              </strong>
+              to <strong> <%= Utils.render_version(@newest_publication.edition, @newest_publication.major, @newest_publication.minor) %></strong>?
+            </h6>
 
-            <div class="modal-body pb-0">
+            <small>Latest publication description</small>
 
-              <h6 class="mb-3">Do you want to apply this update from
-                <strong>{Utils.render_version(@current_publication.edition, @current_publication.major, @current_publication.minor)}</strong>
-                to
-                <strong>{Utils.render_version(@newest_publication.edition, @newest_publication.major, @newest_publication.minor)}</strong>?
-              </h6>
-
-              <small>Latest publication description</small>
-
-              <div class="alert alert-secondary" role="alert">
-                <div class="d-flex justify-content-between align-items-center">
-                  <p class="mb-auto">{@newest_publication.description}</p>
-                  <small>Published {date(@newest_publication.published, precision: :relative)}</small>
-                </div>
+            <div class="alert alert-secondary" role="alert">
+              <div class="d-flex justify-content-between align-items-center">
+                <p class="mb-auto"><%= @newest_publication.description %></p>
+                <small>
+                  Published <%= date(@newest_publication.published, precision: :relative) %>
+                </small>
               </div>
-
-              <hr class="bg-light">
-              <h6 class="my-3">The following materials from this project will be updated to match the latest publication</h6>
-
-              <ul class="my-3">
-                {#for {status, %{revision: revision}} <- Map.values(@changes)}
-                  <li>
-                    <span class={"badge badge-secondary badge-#{status} mr-2"}>{status}</span>
-                    <span>{revision.title}</span>
-                  </li>
-                {/for}
-              </ul>
-
-              <hr class="bg-light">
             </div>
 
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-              <button class="btn btn-warning ml-2" phx-click="apply_update" phx-key="enter">Apply Update</button>
-            </div>
+            <hr class="bg-light" />
+            <h6 class="my-3">
+              The following materials from this project will be updated to match the latest publication
+            </h6>
 
+            <ul class="my-3">
+              <%= for {status, %{revision: revision}} <- Map.values(@changes) do %>
+                <li>
+                  <span class={"badge badge-secondary badge-#{status} mr-2"}><%= status %></span>
+                  <span><%= revision.title %></span>
+                </li>
+              <% end %>
+            </ul>
+
+            <hr class="bg-light" />
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            <button class="btn btn-warning ml-2" phx-click="apply_update" phx-key="enter">
+              Apply Update
+            </button>
           </div>
         </div>
       </div>
+    </div>
     """
   end
 end
--- lib/oli_web/live/delivery/manage_source_materials/project_card.ex
+++ lib/oli_web/live/delivery/manage_source_materials/project_card.ex
@@ -1,22 +1,29 @@
 defmodule OliWeb.Delivery.ManageSourceMaterials.ProjectCard do
-  use Surface.Component
+  use OliWeb, :html
 
-  prop id, :string, required: true
-  prop title, :string, required: true
-  prop tooltip, :string, required: true
-  slot default
+  attr(:id, :string, required: true)
+  attr(:title, :string, required: true)
+  attr(:tooltip, :string, required: true)
+  slot(:inner_block, required: true)
 
   def render(assigns) do
-    ~F"""
-      <div class="card my-4">
-        <div class="card-header d-flex align-items-center" id={@id} phx-update="ignore">
-          <h6 class="mb-0 mr-2">{@title}</h6>
-          <i class="fa fa-info-circle" aria-hidden="true" data-bs-toggle="tooltip" data-placement="right" title={@tooltip}></i>
-        </div>
-        <div class="card-body overflow-auto" style="max-height: 38rem">
-          <#slot />
-        </div>
+    ~H"""
+    <div class="card my-4">
+      <div class="card-header d-flex align-items-center" id={@id} phx-update="ignore">
+        <h6 class="mb-0 mr-2"><%= @title %></h6>
+        <i
+          class="fa fa-info-circle"
+          aria-hidden="true"
+          data-bs-toggle="tooltip"
+          data-placement="right"
+          title={@tooltip}
+        >
+        </i>
       </div>
+      <div class="card-body overflow-auto" style="max-height: 38rem">
+        <%= render_slot(@inner_block) %>
+      </div>
+    </div>
     """
   end
 end
