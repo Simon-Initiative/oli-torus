79332e21aa3de4c06bc595c7edb657786cdfc984
Augusto Alonso <augusto1024@gmail.com>
2023-08-15 15:34:27 -0300

    Remove surface dependency from .formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,12 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix, :surface],
+  import_deps: [:ecto, :ecto_sql, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"],
-  inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}", "storybook/**/*.exs"],
+  inputs: [
+    "*.{heex,ex,exs}",
+    "priv/*/seeds.exs",
+    "{config,lib,test}/**/*.{heex,ex,exs}",
+    "storybook/**/*.exs"
+  ],
   subdirectories: ["priv/*/migrations"]
 ]
