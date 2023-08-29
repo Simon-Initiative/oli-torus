dd4d9d583ecac7b098cec0a3f47e58744783ae00
Marc Hughes <marc.hughes@gmail.com>
2023-08-29 13:56:23 -0400

    Targeted feedback content editor

--- assets/src/components/activities/common/responses/ResponseCard.tsx
+++ assets/src/components/activities/common/responses/ResponseCard.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Descendant } from 'slate';
+import { useAuthoringElementContext } from 'components/activities/AuthoringElementProvider';
 import { RemoveButtonConnected } from 'components/activities/common/authoring/RemoveButton';
 import { Response } from 'components/activities/types';
-import { RichTextEditorConnected } from 'components/content/RichTextEditor';
+import { SlateOrMarkdownEditor } from 'components/editing/SlateOrMarkdownEditor';
 import { Card } from 'components/misc/Card';
 import { ID } from 'data/content/model/other';
+import { DEFAULT_EDITOR, EditorType } from 'data/content/resource';
 import { AuthoringCheckboxConnected } from '../authoring/AuthoringCheckbox';
 
 interface Props {
   title: React.ReactNode;
   response: Response;
+  updateFeedbackEditor: (responseId: ID, editor: EditorType) => void;
   updateFeedback: (responseId: ID, content: Descendant[]) => void;
   updateCorrectness: (responseId: ID, correct: boolean) => void;
   removeResponse: (responseId: ID) => void;
 }
 export const ResponseCard: React.FC<Props> = (props) => {
+  const { projectSlug } = useAuthoringElementContext();
   return (
     <Card.Card>
       <Card.Title>
@@ -29,11 +33,18 @@ export const ResponseCard: React.FC<Props> = (props) => {
         <RemoveButtonConnected onClick={() => props.removeResponse(props.response.id)} />
       </Card.Title>
       <Card.Content>
-        <RichTextEditorConnected
+        <SlateOrMarkdownEditor
           placeholder="Explain why the student might have arrived at this answer"
-          value={props.response.feedback.content}
+          content={props.response.feedback.content}
           onEdit={(content) => props.updateFeedback(props.response.id, content)}
+          onEditorTypeChange={(editor) => props.updateFeedbackEditor(props.response.id, editor)}
+          allowBlockElements={true}
+          editorType={props.response.feedback.editor || DEFAULT_EDITOR}
+          editMode={true}
+          projectSlug={projectSlug}
+          initialHeight={100}
         />
+
         {props.children}
       </Card.Content>
     </Card.Card>
