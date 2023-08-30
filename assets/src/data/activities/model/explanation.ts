7582094b7199d8315e658c8db7ff410053e6756a
Marc Hughes <marc.hughes@gmail.com>
2023-08-30 14:13:30 -0400

    Fixed new explanations having correct default editor

--- assets/src/data/activities/model/explanation.ts
+++ assets/src/data/activities/model/explanation.ts
@@ -1,5 +1,6 @@
-import { HasParts, PostUndoable, makeFeedback } from 'components/activities/types';
-import { DEFAULT_EDITOR, EditorType } from 'data/content/resource';
+import { HasParts, Part, PostUndoable, makeFeedback } from 'components/activities/types';
+import { getDefaultEditor } from 'components/editing/markdown_editor/markdown_util';
+import { EditorType } from 'data/content/resource';
 import { getPartById } from './utils';
 
 export const getExplanation = (model: HasParts, partId: string) => {
@@ -13,25 +14,29 @@ export const getExplanationContent = (model: HasParts, partId: string) => {
 
 export const getExplanationEditor = (model: HasParts, partId: string): EditorType => {
   const explanation = getExplanation(model, partId);
-  return explanation?.editor || DEFAULT_EDITOR;
+  return explanation?.editor || getDefaultEditor();
+};
+
+type PartWithExplanation = Required<Pick<Part, 'explanation'>> & Part;
+
+const initializeExplanation = (part: Part): PartWithExplanation => {
+  if (part.explanation == undefined) {
+    part.explanation = makeFeedback('');
+    part.explanation.editor = getDefaultEditor();
+  }
+  return part as PartWithExplanation;
 };
 
 export const setExplanationContent = (partId: string, content: any) => {
   return (model: HasParts, _post: PostUndoable) => {
     const part = getPartById(model, partId);
-    if (part.explanation == undefined) {
-      part.explanation = makeFeedback('');
-    }
-    part.explanation.content = content;
+    initializeExplanation(part).explanation.content = content;
   };
 };
 
 export const setExplanationEditor = (partId: string, editor: EditorType) => {
   return (model: HasParts, _post: PostUndoable) => {
     const part = getPartById(model, partId);
-    if (part.explanation == undefined) {
-      part.explanation = makeFeedback('');
-    }
-    part.explanation.editor = editor;
+    initializeExplanation(part).explanation.editor = editor;
   };
 };
