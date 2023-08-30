ebd0b270c17bbec34397eec35776a2352958339c
Augusto Alonso <augusto1024@gmail.com>
2023-08-30 16:14:16 -0300

    Run linter in Oli.Interop.Export

--- lib/oli/interop/export.ex
+++ lib/oli/interop/export.ex
@@ -103,47 +103,51 @@ defmodule Oli.Interop.Export do
   end
 
   defp rewire_activity_elements(nil, _), do: nil
+
   defp rewire_activity_elements(content_as_list, project) when is_map(content_as_list) do
-
     case Map.get(content_as_list, "content") do
-
       list when is_list(list) ->
         adjusted_content = %{"type" => "content", "children" => list}
         {results, _} = rewire_elements(adjusted_content, project)
         Map.put(content_as_list, "content", results["children"])
 
       map when is_map(map) ->
-
         list = map["model"]
         adjusted_content = %{"type" => "content", "children" => list}
         {results, _} = rewire_elements(adjusted_content, project)
         content = Map.put(map, "model", results["children"])
         Map.put(content_as_list, "content", content)
-
     end
   end
 
   defp rewire_activity_elements(other, _), do: other
 
   defp rewire_elements(content, project) do
-    Oli.Resources.PageContent.visit_children(content, {:ok, []}, fn c, {status, []}, _tr_context ->
+    Oli.Resources.PageContent.visit_children(content, {:ok, []}, fn c,
+                                                                    {status, []},
+                                                                    _tr_context ->
       case Map.get(c, "type") do
         "cite" ->
           {Map.put(c, "bibref", "#{Map.get(c, "bibref")}"), {status, []}}
+
         "page_link" ->
           {Map.put(c, "idref", "#{Map.get(c, "idref")}"), {status, []}}
+
         "a" ->
           case Map.get(c, "href") do
             "/course/link/" <> slug ->
               case Oli.Publishing.AuthoringResolver.from_revision_slug(project.slug, slug) do
                 nil ->
                   {c, {status, []}}
+
                 %Oli.Resources.Revision{resource_id: resource_id} ->
                   {Map.put(c, "idref", "#{resource_id}"), {status, []}}
               end
+
             _ ->
               {c, {status, []}}
           end
+
         _ ->
           {c, {status, []}}
       end
@@ -154,72 +158,85 @@ defmodule Oli.Interop.Export do
   # stem, choices, explanation, hints, feedback.  This will take care of
   # re-wiring all of the links to pages and any bib citations.
   defp rewire_activity_content(content, project) do
+    content =
+      case Map.get(content, "stem") do
+        nil -> content
+        stem -> Map.put(content, "stem", rewire_activity_elements(stem, project))
+      end
 
-    content = case Map.get(content, "stem") do
-      nil -> content
-      stem -> Map.put(content, "stem", rewire_activity_elements(stem, project))
-    end
+    content =
+      case Map.get(content, "choices") do
+        nil ->
+          content
 
-    content = case Map.get(content, "choices") do
-      nil -> content
-      choices ->
-        choices = Enum.map(choices, fn choice ->
-          rewire_activity_elements(choice, project)
-        end)
+        choices ->
+          choices =
+            Enum.map(choices, fn choice ->
+              rewire_activity_elements(choice, project)
+            end)
 
-        Map.put(content, "choices", choices)
-    end
+          Map.put(content, "choices", choices)
+      end
 
     if Map.has_key?(content, "authoring") and Map.has_key?(Map.get(content, "authoring"), "parts") do
-      parts = content["authoring"]["parts"]
-      |> Enum.map(fn part ->
-
-        part = if Map.has_key?(part, "explanation") do
-          Map.put(part, "explanation", rewire_activity_elements(part["explanation"], project))
-        else
-          part
-        end
-
-        part = if Map.has_key?(part, "hints") and Map.get(part, "hints") != nil do
-          hints = Enum.map(part["hints"], fn hint ->
-            rewire_activity_elements(hint, project)
-          end)
-          Map.put(part, "hints", hints)
-        else
-          part
-        end
-
-        if Map.has_key?(part, "responses") and Map.get(part, "responses") != nil do
-          responses = Enum.map(part["responses"], fn response ->
-            if Map.has_key?(response, "feedback") do
-              Map.put(response, "feedback", rewire_activity_elements(response["feedback"], project))
+      parts =
+        content["authoring"]["parts"]
+        |> Enum.map(fn part ->
+          part =
+            if Map.has_key?(part, "explanation") do
+              Map.put(part, "explanation", rewire_activity_elements(part["explanation"], project))
             else
-              response
+              part
             end
-          end)
-          Map.put(part, "responses", responses)
-        else
-          part
-        end
 
-      end)
+          part =
+            if Map.has_key?(part, "hints") and Map.get(part, "hints") != nil do
+              hints =
+                Enum.map(part["hints"], fn hint ->
+                  rewire_activity_elements(hint, project)
+                end)
+
+              Map.put(part, "hints", hints)
+            else
+              part
+            end
+
+          if Map.has_key?(part, "responses") and Map.get(part, "responses") != nil do
+            responses =
+              Enum.map(part["responses"], fn response ->
+                if Map.has_key?(response, "feedback") do
+                  Map.put(
+                    response,
+                    "feedback",
+                    rewire_activity_elements(response["feedback"], project)
+                  )
+                else
+                  response
+                end
+              end)
+
+            Map.put(part, "responses", responses)
+          else
+            part
+          end
+        end)
 
       Map.put(content, "authoring", Map.put(content["authoring"], "parts", parts))
     else
       content
     end
-
   end
 
   def rewire(content, project) do
-
     {content, _} =
       Oli.Resources.PageContent.map_reduce(content, {:ok, []}, fn e, {status, []}, _tr_context ->
         case e do
           %{"type" => "content"} = ref ->
             rewire_elements(ref, project)
+
           %{"type" => "alternatives"} = ref ->
             {Map.put(ref, "group", "#{Map.get(ref, "alternatives_id")}"), {status, []}}
+
           other ->
             {other, {status, []}}
         end
@@ -229,7 +246,6 @@ defmodule Oli.Interop.Export do
       [] -> content
       integer_ids -> Map.put(content, "bibrefs", Enum.map(integer_ids, fn id -> "#{id}" end))
     end
-
   end
 
   # create entries for all pages
