172c8695fd844cbc01159b11590fd79452005db9
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-09-06 09:57:09 -0300

    Add revision history links into the 'Objectives' LiveView

--- lib/oli_web/live/objectives/objectives.ex
+++ lib/oli_web/live/objectives/objectives.ex
@@ -66,7 +66,13 @@ defmodule OliWeb.ObjectivesLive.Objectives do
        query: "",
        offset: 0,
        limit: 20
-     )}
+     )
+     |> attach_hook(:has_show_links_uri_hash, :handle_params, fn _params, uri, socket ->
+       {:cont,
+        assign_new(socket, :has_show_links_uri_hash, fn ->
+          String.contains?(uri, "#show_links")
+        end)}
+     end)}
   end
 
   defp build_objectives(project, objectives_attachments, flash_fn, first_load \\ false) do
@@ -178,7 +184,12 @@ defmodule OliWeb.ObjectivesLive.Objectives do
           additional_table_class="table-sm text-center"
           with_body={true}
         >
-          <Listing.render rows={@table_model.rows} selected={@selected} project_slug={@project.slug} />
+          <Listing.render
+            revision_history_link={@has_show_links_uri_hash and Accounts.is_admin?(@author)}
+            rows={@table_model.rows}
+            selected={@selected}
+            project_slug={@project.slug}
+          />
         </Table.render>
       </div>
     </div>
