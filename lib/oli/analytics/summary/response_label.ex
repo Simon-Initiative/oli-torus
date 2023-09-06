f78afab58ef471d1a168f5ef04f65646c3c71065
Darren Siegel <siegel.darren@gmail.com>
2023-09-06 11:01:01 -0400

    response label extraction

--- lib/oli/analytics/summary/response_label.ex
+++ lib/oli/analytics/summary/response_label.ex
@@ -5,44 +5,64 @@ defmodule Oli.Analytics.Summary.ResponseLabel do
     :label
   ]
 
-  def build(part_attempt, "oli_file_upload") do
-    tbd()
-  end
-
-  def build(part_attempt, "oli_adaptive") do
-    tbd()
-  end
-
-
-  def build(part_attempt, "oli_custom_dnd") do
-    tbd()
-  end
-
-  def build(part_attempt, "oli_image_coding") do
-    tbd()
-  end
-
-  def build(part_attempt, "oli_image_hotspot") do
-    tbd()
-  end
-
-  def build(part_attempt, "oli_likert") do
-    tbd()
-  end
-
-  def build(part_attempt, "oli_multi_input") do
-    tbd()
-  end
+  def build(_part_attempt, "oli_file_upload"), do: unsupported()
+  def build(_part_attempt, "oli_adaptive"), do: unsupported()
+  def build(_part_attempt, "oli_embedded"), do: unsupported()
+  def build(_part_attempt, "oli_image_coding"), do: unsupported()
 
   def build(part_attempt, "oli_multiple_choice"), do: from_choices(part_attempt)
   def build(part_attempt, "oli_ordering"), do: from_choices(part_attempt)
   def build(part_attempt, "oli_check_all_that_apply"), do: from_choices(part_attempt)
+  def build(part_attempt, "oli_image_hotspot"), do: from_choices(part_attempt)
 
-  def build(part_attempt, "oli_embedded") do
-    tbd()
+  def build(part_attempt, "oli_custom_dnd") do
+    parts_as_choices = part_attempt.activity_revision.content
+    |> Map.get("authoring", %{})
+    |> Map.get("parts", [])
+
+    from_choices_helper(parts_as_choices, part_attempt, &letters_from_choices/2)
   end
 
+  def build(part_attempt, "oli_likert") do
 
+    order_descending = part_attempt.activity_revision.content
+    |> Map.get("orderDescending", false)
+
+    from_choices(part_attempt, fn choices, ids ->
+      if order_descending do
+        numbers_from_choices(choices, Enum.reverse(ids))
+      else
+        numbers_from_choices(choices, ids)
+      end
+    end)
+
+  end
+
+  def build(part_attempt, "oli_multi_input") do
+    case Enum.find(part_attempt.activity_revision.content["inputs"], fn input -> input["partId"] == part_attempt.part_id end) do
+      %{"inputType" => "text"} ->
+        build(part_attempt, "oli_short_answer")
+      %{"inputType" => "dropdown"} = input ->
+
+        choices_for_this_part = Map.get(input, "choiceIds", []) |> MapSet.new()
+
+        part_attempt.activity_revision.content
+        |> Map.get("choices", [])
+        |> Enum.filter(fn c -> MapSet.member?(choices_for_this_part, c["id"]) end)
+        |> from_choices_helper(part_attempt, &letters_from_choices/2)
+
+      %{"inputType" => "math"} ->
+        build(part_attempt, "oli_short_answer")
+      %{"inputType" => "numeric"} ->
+        build(part_attempt, "oli_short_answer")
+      _ ->
+        unsupported()
+    end
+  end
+
+  def build(part_attempt, "oli_vlab") do
+    build(part_attempt, "oli_multi_input")
+  end
 
   def build(part_attempt, "oli_short_answer") do
     case part_attempt.response do
@@ -53,21 +73,13 @@ defmodule Oli.Analytics.Summary.ResponseLabel do
     end
   end
 
-  def build(part_attempt, "oli_vlab") do
-    tbd()
-  end
-
-  defp select_model(attempt, revision) do
-    case attempt.transformed_model do
-      nil -> revision.content
-      transformed_model -> transformed_model
-    end
-  end
-
   defp from_choices(part_attempt, labeller \\ &letters_from_choices/2) do
-    choices = part_attempt.activity_revision.content
+    part_attempt.activity_revision.content
     |> Map.get("choices")
+    |> from_choices_helper(part_attempt, labeller)
+  end
 
+  defp from_choices_helper(choices, part_attempt, labeller) do
      case part_attempt.response do
       %{"input" => nil} ->
         to_struct("No answer", "#{part_attempt.activity_revision.resource_id}")
@@ -118,7 +130,7 @@ defmodule Oli.Analytics.Summary.ResponseLabel do
     }
   end
 
-  defp tbd(), do: to_struct("TBD", "TBD")
+  defp unsupported(), do: to_struct("unsupported", "unsupported")
   defp empty(), do: to_struct("Empty", "")
 
 end
