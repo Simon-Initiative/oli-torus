45afaed631579f9da6afeca77b579116d189fe40
Darren Siegel <siegel.darren@gmail.com>
2023-09-14 06:55:47 -0400

    change timestamp format

--- lib/oli/analytics/xapi/uploader.ex
+++ lib/oli/analytics/xapi/uploader.ex
@@ -7,8 +7,11 @@ defmodule Oli.Analytics.XAPI.Uploader do
   def upload(%StatementBundle{partition: partition, partition_id: partition_id, category: category, bundle_id: bundle_id, body: body}) do
     bucket_name = Application.fetch_env!(:oli, :s3_xapi_bucket_name)
 
-    now = System.monotonic_time()
-    upload_path = "#{partition}/#{partition_id}/#{category}/#{now}_#{bundle_id}.jsonl"
+    {:ok, datetime} = DateTime.now("Etc/UTC")
+    timestamp = DateTime.to_iso8601(datetime)
+    |> String.replace(":", "-")
+
+    upload_path = "#{partition}/#{partition_id}/#{category}/#{timestamp}_#{bundle_id}.jsonl"
 
     S3.put_object(bucket_name, upload_path, body, [])
     |> HTTP.aws().request()
