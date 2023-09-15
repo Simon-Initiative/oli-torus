c9ddf1150063dc29c44d145dcb57a6db97affa72
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-09-15 09:21:19 -0300

    set default time value in migration

--- priv/repo/migrations/20230914120406_add_preferred_time_to_sections.exs
+++ priv/repo/migrations/20230914120406_add_preferred_time_to_sections.exs
@@ -3,7 +3,7 @@ defmodule Oli.Repo.Migrations.AddPreferredTimeToSections do
 
   def change do
     alter table(:sections) do
-      add(:preferred_scheduling_time, :time, null: true)
+      add(:preferred_scheduling_time, :time, default: "23:59:59")
     end
   end
 end
