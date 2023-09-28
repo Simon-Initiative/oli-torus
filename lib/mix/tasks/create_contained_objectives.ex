1247781843237d69f85f529051df6b7c8366c63b
Gastón Abellá <gaston@wyeworks.com>
2023-09-28 12:29:38 -0300

    [MER-2573] Task to create contained objectives for existing sections (#4238)
    * [MER-2573] Create contained objectives table and schema
    
    * [MER-2573] Add v25_migration field to sections
    
    * [MER-2573] Define task and worker to create all contained objectives
    
    * [MER-2573] Add tests
    
    * [MER-2573] Let :done be the default migration state on sections
    
    * [MER-2573] Remove useless doc
    
    * [MER-2573] Make task available on iex shell

---
+++ lib/mix/tasks/create_contained_objectives.ex
@@ -0,0 +1,52 @@
+defmodule Mix.Tasks.CreateContainedObjectives do
+  @shortdoc "Create contained objectives for all sections that were not migrated"
+
+  use Mix.Task
+
+  alias Oli.Delivery.Sections
+  alias Oli.Repo
+  alias Oli.Delivery.Sections.{ContainedObjectivesBuilder, Section}
+  alias Ecto.Multi
+
+  require Logger
+
+  import Ecto.Query, only: [from: 2]
+
+  @impl Mix.Task
+  def run(_args) do
+    Mix.Task.run("app.start")
+
+    run_now()
+  end
+
+  def run_now() do
+    Logger.info("Start enqueueing contained objectives creation")
+
+    Multi.new()
+    |> Multi.run(:sections, &get_not_started_sections(&1, &2))
+    |> Oban.insert_all(:jobs, &build_contained_objectives_jobs(&1))
+    |> Ecto.Multi.update_all(
+      :update_all_sections,
+      fn _ -> from(Section, where: [v25_migration: :not_started]) end,
+      set: [v25_migration: :pending]
+    )
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{jobs: jobs}} ->
+        Logger.info("#{Enum.count(jobs)} jobs enqueued for contained objectives creation")
+
+        :ok
+
+      {:error, _, changeset, _} ->
+        Logger.error("Error enqueuing jobs: #{inspect(changeset)}")
+
+        :error
+    end
+  end
+
+  defp get_not_started_sections(_repo, _changes),
+    do: {:ok, Sections.get_sections_by([v25_migration: :not_started], [:slug])}
+
+  defp build_contained_objectives_jobs(%{sections: sections}),
+    do: Enum.map(sections, &ContainedObjectivesBuilder.new(%{section_slug: &1.slug}))
+end
