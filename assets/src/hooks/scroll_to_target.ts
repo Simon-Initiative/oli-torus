9f79ed12de03f36a4f6d2ccb7c9c90c625acf6d9
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-10-25 16:21:06 -0300

    reimplement smooth scrolling

--- assets/src/hooks/scroll_to_target.ts
+++ assets/src/hooks/scroll_to_target.ts
@@ -3,7 +3,7 @@ export const ScrollToTarget = {
     window.addEventListener('phx:scroll_to_target', (e: Event) => {
       const el = document.getElementById((e as CustomEvent).detail.id);
       if (el) {
-        el.scrollIntoView({ block: 'start' });
+        el.scrollIntoView({ block: 'start', behavior: 'smooth' });
       }
     });
   },
