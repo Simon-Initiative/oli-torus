4007cfbfd934b884cefea0272721567340222b31
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-10-30 12:00:37 -0300

    rebuild full_hierarchy on remix and section creation

--- lib/oli_web/live/new_course/new_course.ex
+++ lib/oli_web/live/new_course/new_course.ex
@@ -375,6 +375,7 @@ defmodule OliWeb.Delivery.NewCourse do
            {:ok, section} <- Sections.create_section_resources(section, publication),
            {:ok, _} <- Sections.rebuild_contained_pages(section),
            {:ok, _} <- Sections.rebuild_contained_objectives(section),
+           {:ok, _} <- Sections.rebuild_full_hierarchy(section),
            {:ok, _enrollment} <- enroll(socket, section),
            {:ok, updated_section} <-
              Oli.Delivery.maybe_update_section_contains_explorations(section) do
@@ -391,6 +392,7 @@ defmodule OliWeb.Delivery.NewCourse do
       with {:ok, section} <- Oli.Delivery.Sections.Blueprint.duplicate(blueprint, section_params),
            {:ok, _} <- Sections.rebuild_contained_pages(section),
            {:ok, _} <- Sections.rebuild_contained_objectives(section),
+           {:ok, _} <- Sections.rebuild_full_hierarchy(section),
            {:ok, _maybe_enrollment} <- enroll(socket, section) do
         section
       else
