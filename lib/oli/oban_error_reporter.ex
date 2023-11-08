16b48120385038ad8a72df1fbf0f11c1667f47f9
Eli Knebel <eliknebel@cmu.edu>
2023-11-07 19:54:03 -0500

    custom oban logging

--- lib/oli/oban_error_reporter.ex
+++ lib/oli/oban_error_reporter.ex
@@ -1,9 +1,48 @@
 defmodule Oli.ObanErrorReporter do
   require Logger
 
+  @doc false
+  @spec handle_event([atom()], map(), map(), Keyword.t()) :: :ok
+  def handle_event([:oban, :job, event], measure, meta, opts) do
+    Logger.error(opts, fn ->
+      details = Map.take(meta.job, ~w(attempt args id max_attempts meta queue tags worker)a)
+
+      extra =
+        case event do
+          :start ->
+            %{event: "job:start", system_time: measure.system_time}
+
+          :stop ->
+            %{
+              duration: convert(measure.duration),
+              event: "job:stop",
+              queue_time: convert(measure.queue_time),
+              state: meta.state
+            }
+
+          :exception ->
+            %{
+              error: Exception.format_banner(meta.kind, meta.reason, meta.stacktrace),
+              event: "job:exception",
+              duration: convert(measure.duration),
+              queue_time: convert(measure.queue_time),
+              state: meta.state
+            }
+        end
+
+      if details[:attempt] >= details[:max_attempts] do
+        IO.inspect(Map.merge(details, extra), label: "Oban job exceeded max attempts")
+      end
+
+      Map.merge(details, extra)
+    end)
+  end
+
   def handle_event([:oban, :job, :exception], _, meta, _) do
     context = Map.take(meta, [:id, :args, :queue, :worker, :stacktrace])
 
     Logger.error("Oban job failed", context)
   end
+
+  defp convert(value), do: System.convert_time_unit(value, :native, :microsecond)
 end
