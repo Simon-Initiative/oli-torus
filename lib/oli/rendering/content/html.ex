2e94995d0e70ded581201065b08834f13e03e849
Filip Moroz <filip.moroz@gogoapps.io>
2023-11-30 00:50:07 +0100

    chore: applied autoformatting

--- lib/oli/rendering/content/html.ex
+++ lib/oli/rendering/content/html.ex
@@ -53,9 +53,14 @@ defmodule Oli.Rendering.Content.Html do
   end
 
   def img(%Context{} = context, _, %{"src" => src} = attrs) do
-    captioned_content(context, attrs, [
-      ~s|<img class="figure-img img-fluid"#{maybeAlt(attrs)}#{maybeWidth(attrs)} src="#{escape_xml!(src)}"/>\n|
-    ], "image")
+    captioned_content(
+      context,
+      attrs,
+      [
+        ~s|<img class="figure-img img-fluid"#{maybeAlt(attrs)}#{maybeWidth(attrs)} src="#{escape_xml!(src)}"/>\n|
+      ],
+      "image"
+    )
   end
 
   def img(%Context{} = _context, _, _e), do: ""
@@ -866,7 +871,7 @@ defmodule Oli.Rendering.Content.Html do
       end
     )
   end
-  
+
   defp maybe_content_type(content_type) do
     if content_type != "" do
       " caption-wrapper-#{content_type}"
@@ -878,9 +883,15 @@ defmodule Oli.Rendering.Content.Html do
   defp captioned_content(context, attrs, content, content_type \\ "")
 
   # Accessible captions are created using a combination of the <figure /> and <figcaption /> elements.
-  defp captioned_content(_context, %{"caption" => ""} = _attrs, content, _content_type), do: content
+  defp captioned_content(_context, %{"caption" => ""} = _attrs, content, _content_type),
+    do: content
 
-  defp captioned_content(%Context{} = context, %{"caption" => caption_content} = _attrs, content, content_type) do
+  defp captioned_content(
+         %Context{} = context,
+         %{"caption" => caption_content} = _attrs,
+         content,
+         content_type
+       ) do
     [~s|<div class="caption-wrapper#{maybe_content_type(content_type)}">|] ++
       [~s|<figure class="figure embed-responsive">|] ++
       content ++
