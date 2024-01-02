843b4a9864e186a89e2e702fd53a86030197ec54
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-01-02 17:24:43 -0300

    improve tooltip hook

--- assets/src/hooks/tooltip.ts
+++ assets/src/hooks/tooltip.ts
@@ -11,20 +11,19 @@ export const TooltipInit = {
 
 export const TooltipWithTarget = {
   // This hook is used to show a tooltip when the user hovers over the element that has this hook attached.
-  // The tooltip_target_id is the id of the tooltip element to be shown or hidden on mouseenter and mouseout.
-  // The tooltip_delay is the delay in milliseconds before the tooltip is shown. It is optional and defaults to 0.
+  // The tooltip_target_id is the id of the tooltip element to be shown on mouseover.
+  // The tooltip_delay is the delay in milliseconds before the tooltip is hidden again. It is optional and defaults to 2000ms.
   mounted() {
     const tooltip = document.getElementById(this.el.dataset['tooltipTargetId']);
-    const delay = this.el.dataset['tooltipDelay'] || 0;
+    const delay = this.el.dataset['tooltipDelay'] || 2000;
 
-    this.el.addEventListener('mouseenter', () => {
-      setTimeout(() => {
-        tooltip!.classList.remove('hidden');
-      }, delay);
-    });
-
-    this.el.addEventListener('mouseout', () => {
-      tooltip!.classList.add('hidden');
+    this.el.addEventListener('mouseover', () => {
+      if (tooltip?.classList.contains('hidden')) {
+        tooltip.classList.remove('hidden');
+        setTimeout(() => {
+          tooltip.classList.add('hidden');
+        }, delay);
+      }
     });
   },
 };
