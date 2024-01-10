7de08c88d78f764cc88d8c4342520333a1ee94f8
Darren Siegel <siegel.darren@gmail.com>
2024-01-10 12:07:54 -0500

    fix test

--- lib/oli_web/controllers/page_delivery_controller.ex
+++ lib/oli_web/controllers/page_delivery_controller.ex
@@ -289,6 +289,7 @@ defmodule OliWeb.PageDeliveryController do
           numbered_revisions = Sections.get_revision_indexes(section.slug)
 
           render(conn, "container.html",
+            user: user,
             scripts: [],
             section: section,
             title: title,
@@ -446,6 +447,7 @@ defmodule OliWeb.PageDeliveryController do
     numbered_revisions = Sections.get_revision_indexes(section.slug)
 
     render(conn, "prologue.html", %{
+      user: user,
       resource_access: resource_access,
       section_slug: section_slug,
       scripts: Activities.get_activity_scripts(),
@@ -935,6 +937,7 @@ defmodule OliWeb.PageDeliveryController do
             |> render(
               "instructor_page_preview.html",
               %{
+                user: nil,
                 summary: %{title: section.title},
                 section_slug: section_slug,
                 scripts: [],
@@ -972,6 +975,7 @@ defmodule OliWeb.PageDeliveryController do
             |> put_root_layout({OliWeb.LayoutView, "chromeless.html"})
             |> put_view(OliWeb.ResourceView)
             |> render("advanced_page_preview.html",
+              user: user,
               additional_stylesheets: Map.get(revision.content, "additionalStylesheets", []),
               activity_types: activity_types,
               scripts: Activities.get_activity_scripts(:delivery_script),
@@ -1097,6 +1101,7 @@ defmodule OliWeb.PageDeliveryController do
     |> render(
       "instructor_page_preview.html",
       %{
+        user: if is_nil(conn.assigns.current_user) do nil else conn.assigns.current_user end,
         summary: %{title: section.title},
         section_slug: section_slug,
         scripts: Enum.map(all_activities, fn a -> a.authoring_script end),
