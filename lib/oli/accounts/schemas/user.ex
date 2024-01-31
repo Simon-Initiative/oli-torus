d4b39467bc53ef216f85d450854d5e32af674186
Francisco-Castro <47334502+Francisco-Castro@users.noreply.github.com>
2024-01-31 13:00:07 -0500

    [MER-2953] Upgrade user edit form to be a form component (#4608)
    * [MER-2953] Upgrade user edit form tobe a  form component
    
    * [MER-2953] Add missing attributes during casting
    
    * [MER-2953] Add testing for update_changeset_for_admin changeset
    
    * [MER-2953] Remove inspects

--- lib/oli/accounts/schemas/user.ex
+++ lib/oli/accounts/schemas/user.ex
@@ -170,11 +170,17 @@ defmodule Oli.Accounts.User do
     |> maybe_name_from_given_and_family()
   end
 
-  def update_changeset_for_admin(user, attrs \\ %{}) do
+  def update_changeset_for_admin(%__MODULE__{} = user, attrs \\ %{}) do
     user
-    |> noauth_changeset(attrs)
+    |> cast(attrs, [:given_name, :family_name, :independent_learner, :can_create_sections, :email])
+    |> validate_required([:given_name, :family_name])
+    |> maybe_name_from_given_and_family()
+    |> lowercase_email()
     |> pow_user_id_field_changeset(attrs)
-    |> unique_constraint(:email, name: :users_email_independent_learner_index)
+    |> unique_constraint(:email,
+      name: :users_email_independent_learner_index,
+      message: "Email has already been taken by another independent learner"
+    )
   end
 
   @doc """
