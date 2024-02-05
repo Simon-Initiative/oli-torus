0014befd3bef4b3772b49418e4b17feeb88fb8f5
Eli Knebel <eliknebel@cmu.edu>
2024-02-04 20:45:55 -0500

    add active week styles

--- lib/oli_web/components/delivery/schedule.ex
+++ lib/oli_web/components/delivery/schedule.ex
@@ -4,29 +4,46 @@ defmodule OliWeb.Components.Delivery.Schedule do
   alias OliWeb.Common.SessionContext
 
   attr(:ctx, SessionContext, required: true)
-  attr(:week_number, :any, required: true)
+  attr(:week_number, :integer, required: true)
+  attr(:is_active, :boolean, required: true)
+  attr(:is_current_week, :boolean, required: true)
   attr(:schedule_ranges, :any, required: true)
   attr(:section_slug, :string, required: true)
 
   def week(assigns) do
     ~H"""
     <div class="flex flex-row">
-      <div class="mr-8 uppercase font-bold text-gray-500 whitespace-nowrap">
-        Week <%= @week_number %>:
+      <div class={[
+        "uppercase font-bold whitespace-nowrap mr-4 md:w-32 md:border-l",
+        if(@is_active,
+          do: "text-gray-600 dark:text-gray-300 md:border-gray-500",
+          else: "text-gray-300 dark:text-gray-700 md:border-gray-300 dark:border-gray-700"
+        )
+      ]}>
+        <div class="inline-flex items-center">
+          <.maybe_current_week_indicator is_current_week={@is_current_week} />
+          <div>Week <%= @week_number %>:</div>
+        </div>
       </div>
 
       <div class="flex-1 flex flex-col">
         <%= for {date_range, container_groups} <- @schedule_ranges do %>
-          <div class="flex-1 flex flex-col mb-4">
-            <div class="font-bold text-gray-500">
+          <div class={[
+            "flex-1 flex flex-col mb-4 group",
+            if(start_or_end_date_past?(date_range),
+              do: "past-start text-gray-300 dark:text-gray-700",
+              else: ""
+            )
+          ]}>
+            <div class="font-bold text-gray-500 group-[.past-start]:text-gray-300 dark:group-[.past-start]:text-gray-700">
               <%= render_date_range(date_range, @ctx) %>
             </div>
 
             <%= for {{resource_label, graded}, scheduled_resources} <- container_groups do %>
-              <div class="flex flex-row">
-                <div class="basis-1/4 flex flex-col mr-4">
+              <div class="flex flex-row text-gray-600 dark:text-gray-300 group-[.past-start]:text-gray-300 dark:group-[.past-start]:text-gray-700">
+                <div class="flex flex-col mr-4 md:w-64">
                   <div><%= page_or_assessment_label(graded) %></div>
-                  <div class="text-gray-500">
+                  <div>
                     <%= resource_label %>
                   </div>
                 </div>
@@ -41,7 +58,7 @@ defmodule OliWeb.Components.Delivery.Schedule do
                           <%= resource.title %>
                         </.link>
                       </div>
-                      <div class="text-sm text-gray-500">
+                      <div class="text-sm">
                         <%= resource_scheduling_label(resource.scheduling_type) %> <%= date(
                           resource.end_date,
                           ctx: @ctx,
@@ -51,13 +68,26 @@ defmodule OliWeb.Components.Delivery.Schedule do
                     </div>
                   <% end %>
                 </div>
-                <div class="basis-1/4 flex flex-col"></div>
+                <div class="flex flex-col"></div>
               </div>
             <% end %>
           </div>
         <% end %>
       </div>
     </div>
+    """
+  end
+
+  attr :is_current_week, :boolean, default: false
+
+  defp maybe_current_week_indicator(assigns) do
+    ~H"""
+    <div
+      :if={@is_current_week}
+      class="w-0 h-0 border-[8px] border-solid border-transparent border-l-gray-600 dark:border-l-gray-300"
+    >
+    </div>
+    <div :if={!@is_current_week} class="ml-[16px]"></div>
     """
   end
 
@@ -82,6 +112,18 @@ defmodule OliWeb.Components.Delivery.Schedule do
 
       true ->
         "#{date(start_date, ctx: ctx, precision: :day)} — #{date(end_date, ctx: ctx, precision: :day)}"
+    end
+  end
+
+  defp start_or_end_date_past?({start_date, end_date}) do
+    today = DateTime.utc_now()
+
+    dbg({today, start_date, end_date, today > start_date, today > end_date})
+
+    if is_nil(start_date) do
+      DateTime.after?(today, end_date)
+    else
+      DateTime.after?(today, start_date)
     end
   end
 end
--- lib/oli_web/live/delivery/student/schedule_live.ex
+++ lib/oli_web/live/delivery/student/schedule_live.ex
@@ -3,18 +3,24 @@ defmodule OliWeb.Delivery.Student.ScheduleLive do
 
   alias OliWeb.Common.SessionContext
   alias Oli.Delivery.Sections
-  alias OliWeb.Components.Delivery.Schedule
+  alias OliWeb.Components.Delivery.{Schedule, Utils}
 
   def mount(_params, _session, socket) do
     section = socket.assigns[:section]
 
     schedule = Sections.get_ordered_schedule(section)
 
+    current_datetime = DateTime.utc_now()
+    current_week = Utils.week_number(section.start_date, current_datetime)
+    current_month = current_datetime.month
+
     {:ok,
      assign(socket,
        active_tab: :schedule,
        schedule: schedule,
-       section_slug: section.slug
+       section_slug: section.slug,
+       current_week: current_week,
+       current_month: current_month
      )}
   end
 
@@ -25,7 +31,13 @@ defmodule OliWeb.Delivery.Student.ScheduleLive do
     </.hero_banner>
 
     <div class="container mx-auto">
-      <.schedule ctx={@ctx} schedule={@schedule} section_slug={@section_slug} />
+      <.schedule
+        ctx={@ctx}
+        schedule={@schedule}
+        section_slug={@section_slug}
+        current_week={@current_week}
+        current_month={@current_month}
+      />
     </div>
     """
   end
@@ -33,27 +45,46 @@ defmodule OliWeb.Delivery.Student.ScheduleLive do
   attr :ctx, SessionContext, required: true
   attr :schedule, :any, required: true
   attr :section_slug, :string, required: true
+  attr :current_week, :integer, required: true
+  attr :current_month, :integer, required: true
 
   def schedule(assigns) do
     ~H"""
     <div class="my-8 px-16">
       <div class="mb-8">
-        Scroll though your course schedule to find all critical due dates and when assignments are due. Use this schedule view to see which activities you've completed throughout your time in the course.
+        Scroll though your course schedule to find all critical due dates and when assignments are due.
+        Use this schedule view to see which activities you've completed throughout your time in the course.
       </div>
 
       <div class="flex flex-col">
         <%= for {{month, _year}, weekly_schedule} <- @schedule do %>
           <div class="flex flex-col md:flex-row">
-            <div class="w-full md:w-32 uppercase font-bold text-gray-500 p-1 mb-3 border-b md:border-b-none border-gray-300">
-              <%= month_name(month) %>
+            <div class={[
+              "w-full md:w-32 uppercase font-bold border-b md:border-b-0 border-gray-300",
+              if(month_active?(month, @current_month),
+                do: "text-gray-500",
+                else: "text-gray-300 dark:text-gray-700"
+              )
+            ]}>
+              <div class="my-[0.35rem]">
+                <%= month_name(month) %>
+              </div>
             </div>
 
             <div class="flex-1 flex flex-col">
               <%= for {week, schedule_ranges} <- weekly_schedule do %>
-                <div class="flex flex-row md:border-l md:border-gray-300 dark:border-gray-700 md:px-4">
+                <div class="flex flex-row">
                   <Schedule.week
                     ctx={@ctx}
                     week_number={week}
+                    is_active={
+                      week_active?(week, @current_week) &&
+                        month_active?(month, @current_month)
+                    }
+                    is_current_week={
+                      is_current_week?(week, @current_week) &&
+                        is_current_month?(month, @current_month)
+                    }
                     schedule_ranges={schedule_ranges}
                     section_slug={@section_slug}
                   />
@@ -71,5 +102,21 @@ defmodule OliWeb.Delivery.Student.ScheduleLive do
 
   defp month_name(month) do
     Timex.month_name(month)
+  end
+
+  defp week_active?(week_number, current_week) do
+    week_number >= current_week
+  end
+
+  defp is_current_week?(week_number, current_week) do
+    week_number == current_week
+  end
+
+  defp month_active?(month, current_month) do
+    month >= current_month
+  end
+
+  defp is_current_month?(month, current_month) do
+    month == current_month
   end
 end
