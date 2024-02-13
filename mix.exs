6ad3565b466ca998705cb129ca1ee2c321d98934
Darren Siegel <siegel.darren@gmail.com>
2024-02-13 16:25:37 -0500

    fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Oli.MixProject do
     [
       app: :oli,
       version: "0.26.4",
-      elixir: "~> 1.15.5",
+      elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
       compilers: Mix.compilers(),
