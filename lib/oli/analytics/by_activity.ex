1816edd9731fbc614f04004fa91e95e91c931641
tomasferok <tomaasfer11@gmail.com>
2024-02-26 07:27:24 -0300

    [MER-2932] filter by section

--- lib/oli/analytics/by_activity.ex
+++ lib/oli/analytics/by_activity.ex
@@ -1,10 +1,11 @@
 defmodule Oli.Analytics.ByActivity do
   import Ecto.Query, warn: false
+  alias Oli.Delivery.Sections.SectionResource
   alias Oli.Repo
   alias Oli.Analytics.Common
   alias Oli.Publishing
 
-  def query_against_project_slug(project_slug) do
+  def query_against_project_slug(project_slug, []) do
     Repo.all(
       from activity in subquery(
              Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
@@ -25,13 +26,18 @@ defmodule Oli.Analytics.ByActivity do
   def query_against_project_slug(project_slug, filtered_sections) do
     Repo.all(
       from activity in subquery(
-             Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
+             Publishing.query_unpublished_revisions_by_type_and_section(
+               project_slug,
+               "activity",
+               filtered_sections
+             )
            ),
-           join: resource in assoc(activity, :resource_id),
-           left_join: resource_summary in assoc(resource, :id),
-           join: section in assoc(resource_summary, :section_id),
+           join: resource in assoc(activity, :resource),
+           left_join: section_resource in SectionResource,
+           on: resource.id == section_resource.resource_id,
            left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
-           on: activity.resource_id == analytics.activity_id and section.id in ^filtered_sections,
+           on: activity.resource_id == analytics.activity_id,
+           where: section_resource.section_id in ^filtered_sections,
            select: %{
              slice: activity,
              eventually_correct: analytics.eventually_correct,
