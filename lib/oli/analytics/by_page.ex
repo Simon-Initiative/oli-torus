cd6c094ea1441806763724a8ceff2c99f545e48e
tomasferok <tomaasfer11@gmail.com>
2024-02-27 15:42:09 -0300

    [MER-2932] refactor query by_page

--- lib/oli/analytics/by_page.ex
+++ lib/oli/analytics/by_page.ex
@@ -7,70 +7,45 @@ defmodule Oli.Analytics.ByPage do
   alias Oli.Publishing
   alias Oli.Authoring.Course.Project
 
-  def query_against_project_slug(project_slug, []) do
-    activity_pages = get_activity_pages(project_slug)
+  def query_against_project_slug(project_slug, filtered_sections) do
+    base_query = get_base_query(project_slug, get_activity_pages(project_slug))
 
-    Repo.all(
-      from(
-        page in subquery(Publishing.query_unpublished_revisions_by_type(project_slug, "page")),
-        left_join: pairing in subquery(activity_pages),
-        on: page.resource_id == pairing.page_id,
-        left_join:
-          activity in subquery(
-            Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
-          ),
-        on: pairing.activity_id == activity.resource_id,
-        left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
-        on: pairing.activity_id == analytics.activity_id,
-        select: %{
-          slice: page,
-          activity: activity,
-          eventually_correct: analytics.eventually_correct,
-          first_try_correct: analytics.first_try_correct,
-          number_of_attempts: analytics.number_of_attempts,
-          relative_difficulty: analytics.relative_difficulty
-        },
-        preload: [:resource_type]
-      )
-    )
+    case filtered_sections do
+      [] -> base_query
+      _filtered_sections -> get_query_with_join_filter(base_query, filtered_sections)
+    end
+    |> Repo.all()
   end
 
-  def query_against_project_slug(project_slug, filtered_sections) do
-    activity_pages = get_activity_pages(project_slug)
+  defp get_query_with_join_filter(query, filter_list) do
+    from page in query,
+      join: resource in assoc(page, :resource),
+      left_join: section_resource in SectionResource,
+      on: resource.id == section_resource.resource_id,
+      where: section_resource.section_id in ^filter_list
+  end
 
-    Repo.all(
-      from(
-        page in subquery(
-          Publishing.query_unpublished_revisions_by_type_and_section(
-            project_slug,
-            "page",
-            filtered_sections
-          )
+  defp get_base_query(project_slug, activity_pages) do
+    from(
+      page in subquery(Publishing.query_unpublished_revisions_by_type(project_slug, "page")),
+      left_join: pairing in subquery(activity_pages),
+      on: page.resource_id == pairing.page_id,
+      left_join:
+        activity in subquery(
+          Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
         ),
-        left_join: pairing in subquery(activity_pages),
-        on: page.resource_id == pairing.page_id,
-        left_join:
-          activity in subquery(
-            Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
-          ),
-        on: pairing.activity_id == activity.resource_id,
-        join: resource in assoc(page, :resource),
-        left_join: section_resource in SectionResource,
-        on: resource.id == section_resource.resource_id,
-        left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
-        on: pairing.activity_id == analytics.activity_id,
-        where: section_resource.section_id in ^filtered_sections,
-        select: %{
-          slice: page,
-          activity: activity,
-          eventually_correct: analytics.eventually_correct,
-          first_try_correct: analytics.first_try_correct,
-          number_of_attempts: analytics.number_of_attempts,
-          relative_difficulty: analytics.relative_difficulty
-        },
-        distinct: true,
-        preload: [:resource_type]
-      )
+      on: pairing.activity_id == activity.resource_id,
+      left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
+      on: pairing.activity_id == analytics.activity_id,
+      select: %{
+        slice: page,
+        activity: activity,
+        eventually_correct: analytics.eventually_correct,
+        first_try_correct: analytics.first_try_correct,
+        number_of_attempts: analytics.number_of_attempts,
+        relative_difficulty: analytics.relative_difficulty
+      },
+      preload: [:resource_type]
     )
   end
 
