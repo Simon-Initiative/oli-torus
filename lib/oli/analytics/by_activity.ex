5ef62bcb265cfca6bd0523e646d3f56fb332875a
tomasferok <tomaasfer11@gmail.com>
2024-02-27 15:40:14 -0300

    [MER-2932] refactor query by_activity)

--- lib/oli/analytics/by_activity.ex
+++ lib/oli/analytics/by_activity.ex
@@ -5,47 +5,40 @@ defmodule Oli.Analytics.ByActivity do
   alias Oli.Analytics.Common
   alias Oli.Publishing
 
-  def query_against_project_slug(project_slug, []) do
-    Repo.all(
-      from activity in subquery(
-             Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
-           ),
-           left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
-           on: activity.resource_id == analytics.activity_id,
-           select: %{
-             slice: activity,
-             eventually_correct: analytics.eventually_correct,
-             first_try_correct: analytics.first_try_correct,
-             number_of_attempts: analytics.number_of_attempts,
-             relative_difficulty: analytics.relative_difficulty
-           },
-           preload: [:resource_type]
-    )
+  def query_against_project_slug(project_slug, filtered_sections) do
+    base_query = get_base_query(project_slug)
+
+    case filtered_sections do
+      [] ->
+        base_query
+
+      _filtered_sections ->
+        get_query_with_join_filter(base_query, filtered_sections)
+    end
+    |> Repo.all()
   end
 
-  def query_against_project_slug(project_slug, filtered_sections) do
-    Repo.all(
-      from activity in subquery(
-             Publishing.query_unpublished_revisions_by_type_and_section(
-               project_slug,
-               "activity",
-               filtered_sections
-             )
-           ),
-           join: resource in assoc(activity, :resource),
-           left_join: section_resource in SectionResource,
-           on: resource.id == section_resource.resource_id,
-           left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
-           on: activity.resource_id == analytics.activity_id,
-           where: section_resource.section_id in ^filtered_sections,
-           select: %{
-             slice: activity,
-             eventually_correct: analytics.eventually_correct,
-             first_try_correct: analytics.first_try_correct,
-             number_of_attempts: analytics.number_of_attempts,
-             relative_difficulty: analytics.relative_difficulty
-           },
-           preload: [:resource_type]
-    )
+  defp get_base_query(project_slug) do
+    from activity in subquery(
+           Publishing.query_unpublished_revisions_by_type(project_slug, "activity")
+         ),
+         left_join: analytics in subquery(Common.analytics_by_activity(project_slug)),
+         on: activity.resource_id == analytics.activity_id,
+         select: %{
+           slice: activity,
+           eventually_correct: analytics.eventually_correct,
+           first_try_correct: analytics.first_try_correct,
+           number_of_attempts: analytics.number_of_attempts,
+           relative_difficulty: analytics.relative_difficulty
+         },
+         preload: [:resource_type]
+  end
+
+  defp get_query_with_join_filter(query, filter_list) do
+    from activity in query,
+      join: resource in assoc(activity, :resource),
+      left_join: section_resource in SectionResource,
+      on: resource.id == section_resource.resource_id,
+      where: section_resource.section_id in ^filter_list
   end
 end
