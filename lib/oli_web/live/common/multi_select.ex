fd963e25221d5625efdb646e8528f5f8a9c6a28b
tomasferok <tomaasfer11@gmail.com>
2024-02-29 12:37:56 -0300

    [MER-2932] multiselect background modified

--- lib/oli_web/live/common/multi_select.ex
+++ lib/oli_web/live/common/multi_select.ex
@@ -1,58 +1,57 @@
 defmodule OliWeb.Common.MultiSelect do
   use OliWeb, :live_component
+  alias Phoenix.LiveView.JS
 
   def update(params, socket) do
-    %{options: options, form: form, id: id} = params
+    %{options: options, form: form, id: id, label: label} = params
 
     socket =
       socket
-      |> assign(:id, id)
-      |> assign(:selectable_options, options)
+      |> assign(id: id)
       |> assign(:form, form)
       |> assign(:section_ids, nil)
       |> assign(:product_ids, nil)
+      |> assign(:label, label)
+      |> assign(:disabled, options == [])
 
     {:ok, socket}
   end
 
+  attr :label, :string, default: "Select an option"
+
   def render(assigns) do
     ~H"""
-    <div class="multiselect">
-      <div class="fake_select_tag" id={"#{@id}-selected-options-container"}>
-        <div class="icon">
-          <!-- remove the space between arrow and multiselect  -->
-        <!-- arrow to hide multiselect content  -->
-          <svg
-            id={"#{@id}-down-icon"}
-            phx-click={
-              Phoenix.LiveView.JS.toggle()
-              |> Phoenix.LiveView.JS.toggle(to: "##{@id}-up-icon")
-              |> Phoenix.LiveView.JS.toggle(to: "##{@id}-options-container")
-            }
-          >
-            <path
-              fill-rule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-            />
-          </svg>
-          <!-- arrow to show multiselect content this has to contain an atribute hidden or something similar  -->
-          <svg
-            class="hidden"
-            id={"#{@id}-up-icon" }
-            phx-click={
-              Phoenix.LiveView.JS.toggle()
-              |> Phoenix.LiveView.JS.toggle(to: "##{@id}-down-icon")
-              |> Phoenix.LiveView.JS.toggle(to: "##{@id}-options-container")
-            }
-          >
-            <path
-              fill-rule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-            />
-          </svg>
+    <div class="flex flex-col border relative">
+      <div
+        phx-click={
+          if(!@disabled,
+            do:
+              JS.toggle(to: "##{@id}-options-container")
+              |> JS.toggle(to: "##{@id}-down-icon")
+              |> JS.toggle(to: "##{@id}-up-icon")
+          )
+        }
+        class={[
+          "flex justify-between h-10 items-center p-2 w-96 hover:cursor-pointer",
+          if(@disabled, do: "bg-gray-300 hover:cursor-not-allowed")
+        ]}
+        id={"#{@id}-selected-options-container"}
+      >
+        <span><%= @label %></span>
+        <div id={"#{@id}-down-icon"}>
+          <i class="fa-solid fa-chevron-up rotate-180"></i>
+        </div>
+        <div class="hidden" id={"#{@id}-up-icon"}>
+          <i class="fa-solid fa-chevron-up"></i>
         </div>
       </div>
-      <div id={"#{@id}-options-container"}>
+      <div
+        class="p-4 hidden absolute mt-10 dark:bg-gray-700 bg-white w-96 border max-h-56 overflow-y-scroll"
+        id={"#{@id}-options-container"}
+        phx-click-away={
+          JS.hide() |> JS.hide(to: "##{@id}-up-icon") |> JS.show(to: "##{@id}-down-icon")
+        }
+      >
         <.inputs_for :let={opt} field={@form[:options]}>
           <.input
             value={opt.data.selected}
