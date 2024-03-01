eea9a69de1731a072305db9ec0780a5f772c2958
Francisco-Castro <catrapato@hotmail.com>
2024-03-01 13:49:24 -0500

    [MER-2748] Add CreativeCommons module

---
+++ lib/oli/authoring/course/creative_commons.ex
@@ -0,0 +1,46 @@
+defmodule Oli.Authoring.Course.CreativeCommons do
+  @cc_options [
+    %{
+      id: :none,
+      text: "Non-CC / Copyrighted / Other",
+      url: ""
+    },
+    %{
+      id: :cc_by,
+      text: "CC BY: Attribution",
+      url: "https://creativecommons.org/licenses/by/4.0/"
+    },
+    %{
+      id: :cc_by_sa,
+      text: "CC BY-SA: Attribution-ShareAlike",
+      url: "https://creativecommons.org/licenses/by-sa/4.0/"
+    },
+    %{
+      id: :cc_by_nd,
+      text: "CC BY-ND: Attribution-NoDerivatives",
+      url: "https://creativecommons.org/licenses/by-nd/4.0/"
+    },
+    %{
+      id: :cc_by_nc,
+      text: "CC BY-NC: Attribution-NonCommercial",
+      url: "https://creativecommons.org/licenses/by-nc/4.0/"
+    },
+    %{
+      id: :cc_by_nc_sa,
+      text: "CC BY-NC-SA: Attribution-NonCommercial-ShareAlike",
+      url: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+    },
+    %{
+      id: :cc_by_nc_nd,
+      text: "CC BY-NC-ND: Attribution-NonCommercial-NoDerivatives",
+      url: "https://creativecommons.org/licenses/by-nc-nd/4.0/"
+    },
+    %{
+      id: :custom,
+      text: "Custom",
+      url: ""
+    }
+  ]
+
+  def cc_options(), do: @cc_options
+end
