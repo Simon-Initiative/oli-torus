c91de90304c25485fad0c3d5da04363e495cae2f
Darren Siegel <siegel.darren@gmail.com>
2024-03-22 09:04:51 -0400

    render link only when details are a url

--- test/oli_web/views/layout_view_test.exs
+++ test/oli_web/views/layout_view_test.exs
@@ -4,6 +4,7 @@ defmodule OliWeb.LayoutViewTest do
   import Oli.Factory
 
   alias OliWeb.AuthoringView
+  alias OliWeb.LayoutView
 
   describe "authoring view" do
     test "renders author info" do
@@ -30,5 +31,15 @@ defmodule OliWeb.LayoutViewTest do
       assert user
       assert user.id == user_associated.id
     end
+
+    test "is_only_url?/1" do
+      assert LayoutView.is_only_url?("http://foo")
+      assert LayoutView.is_only_url?("https://foo")
+      assert LayoutView.is_only_url?(" https://foo ")
+      refute LayoutView.is_only_url?("foo")
+      refute LayoutView.is_only_url?("foo.com")
+      refute LayoutView.is_only_url?("https://foo.com bar")
+    end
+
   end
 end
