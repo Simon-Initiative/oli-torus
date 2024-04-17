ea516a3f8277e68470910be1e03d156bc986c5c9
Darren Siegel <siegel.darren@gmail.com>
2024-04-17 09:25:55 -0400

    set progress complete

--- lib/oli/delivery/attempts/page_lifecycle/ungraded.ex
+++ lib/oli/delivery/attempts/page_lifecycle/ungraded.ex
@@ -60,18 +60,24 @@ defmodule Oli.Delivery.Attempts.PageLifecycle.Ungraded do
       }) do
     now = DateTime.utc_now()
 
-    # mark resource attempt as evaluated
-    update_resource_attempt(resource_attempt, %{
-      date_evaluated: now,
-      date_submitted: now,
-      lifecycle_state: :evaluated
-    })
+    {:ok, {:ok, resource_access}} = Repo.transaction(fn ->
+
+      {:ok, _} = update_resource_attempt(resource_attempt, %{
+        date_evaluated: now,
+        date_submitted: now,
+        lifecycle_state: :evaluated
+      })
+
+      get_resource_access(resource_attempt.resource_access_id)
+      |> Oli.Delivery.Metrics.mark_progress_completed()
+
+    end)
 
     {:ok,
      %FinalizationSummary{
        graded: false,
        lifecycle_state: :evaluated,
-       resource_access: nil,
+       resource_access: resource_access,
        part_attempt_guids: nil,
        effective_settings: effective_settings
      }}
