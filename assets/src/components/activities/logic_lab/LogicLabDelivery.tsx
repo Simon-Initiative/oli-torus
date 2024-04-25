ad5afc4e3d2fba5b473b3c4a2b1cad6bb0731d64
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2024-04-25 11:05:19 -0400

    [BUG FIX] [MER-3201] LogicLab activity: adds improved error detection and handling of missing URL.  #4744
    Improves handling of unset environment variable.  It also removes the default url, thus requiring setting the environment variable to use LogicLab.
    Adds nice error message if the lab server is unreachable in the delivery context.

--- assets/src/components/activities/logic_lab/LogicLabDelivery.tsx
+++ assets/src/components/activities/logic_lab/LogicLabDelivery.tsx
@@ -54,92 +54,96 @@ const LogicLab: React.FC<LogicLabDeliveryProps> = () => {
     let partGuid = activityState.parts[0].attemptGuid; // Moving to higher scope which helps state saving to work.
 
     const onMessage = async (e: MessageEvent) => {
-      const lab = new URL(getLabServer(context));
-      const origin = new URL(e.origin);
-      // filter so we do not process torus events.
-      if (origin.host === lab.host) {
-        const msg = e.data;
-        // only lab messages from this activity for eventual support of multiple problems on a page.
-        if (isLabMessage(msg) && msg.attemptGuid === activityState.attemptGuid) {
-          const attemptGuid = activityState.attemptGuid;
-          switch (msg.messageType) {
-            // respond to lab score request.
-            case 'score':
-              if (mode === 'delivery') {
-                // only when in delivery
-                try {
-                  // .dateEvaluated seems to not work as a check to see if part is evaluatable
-                  // Always resetting and saving seems to fix the issue with a second attempt
-                  // not registering grading.
-                  // if (activityState.parts[0].dateEvaluated) {
-                  // if the part has already been evaluated, then
-                  // it is necessary to reset the part to get a new
-                  // partGuid as there can only ever be one evaluation
-                  // per partGuid.
-                  const partResponse = await onResetPart(attemptGuid, partGuid);
-                  partGuid = partResponse.attemptState.attemptGuid;
-                  // import state to new part, luckily the current state
-                  // is already included in the score message so no need
-                  // to maintain it in state.
-                  await onSaveActivity(attemptGuid, [
-                    {
-                      attemptGuid: partGuid,
-                      response: { input: msg.score.input },
-                    },
-                  ]);
-                  onSubmitEvaluations(attemptGuid, [
-                    {
-                      score: msg.score.score,
-                      outOf: msg.score.outOf,
-                      feedback: model.feedback[Number(msg.score.complete)],
-                      response: { input: msg.score.input },
-                      attemptGuid: partGuid,
-                    },
-                  ]);
-                } catch (err) {
-                  console.error(err);
-                }
-              }
-              break;
-            // respond to lab request to save state.
-            case 'save':
-              // it seems to only save/restore properly with score
-              if (mode === 'delivery') {
-                // only update in delivery mode.
-                try {
-                  await onSaveActivity(attemptGuid, [
-                    {
-                      attemptGuid: partGuid,
-                      response: {
-                        input: msg.state,
+      try {
+        const lab = new URL(getLabServer(context));
+        const origin = new URL(e.origin);
+        // filter so we do not process torus events.
+        if (origin.host === lab.host) {
+          const msg = e.data;
+          // only lab messages from this activity for eventual support of multiple problems on a page.
+          if (isLabMessage(msg) && msg.attemptGuid === activityState.attemptGuid) {
+            const attemptGuid = activityState.attemptGuid;
+            switch (msg.messageType) {
+              // respond to lab score request.
+              case 'score':
+                if (mode === 'delivery') {
+                  // only when in delivery
+                  try {
+                    // .dateEvaluated seems to not work as a check to see if part is evaluatable
+                    // Always resetting and saving seems to fix the issue with a second attempt
+                    // not registering grading.
+                    // if (activityState.parts[0].dateEvaluated) {
+                    // if the part has already been evaluated, then
+                    // it is necessary to reset the part to get a new
+                    // partGuid as there can only ever be one evaluation
+                    // per partGuid.
+                    const partResponse = await onResetPart(attemptGuid, partGuid);
+                    partGuid = partResponse.attemptState.attemptGuid;
+                    // import state to new part, luckily the current state
+                    // is already included in the score message so no need
+                    // to maintain it in state.
+                    await onSaveActivity(attemptGuid, [
+                      {
+                        attemptGuid: partGuid,
+                        response: { input: msg.score.input },
                       },
-                    },
-                  ]);
-                } catch (err) {
-                  console.error(err);
+                    ]);
+                    onSubmitEvaluations(attemptGuid, [
+                      {
+                        score: msg.score.score,
+                        outOf: msg.score.outOf,
+                        feedback: model.feedback[Number(msg.score.complete)],
+                        response: { input: msg.score.input },
+                        attemptGuid: partGuid,
+                      },
+                    ]);
+                  } catch (err) {
+                    console.error(err);
+                  }
                 }
-              }
-              break;
-            // lab is requesting activity state
-            case 'load':
-              if (mode !== 'preview') {
-                const saved = activityState?.parts[0].response?.input;
-                if (saved && e.source) {
-                  // post saved state back to lab.
+                break;
+              // respond to lab request to save state.
+              case 'save':
+                // it seems to only save/restore properly with score
+                if (mode === 'delivery') {
+                  // only update in delivery mode.
+                  try {
+                    await onSaveActivity(attemptGuid, [
+                      {
+                        attemptGuid: partGuid,
+                        response: {
+                          input: msg.state,
+                        },
+                      },
+                    ]);
+                  } catch (err) {
+                    console.error(err);
+                  }
+                }
+                break;
+              // lab is requesting activity state
+              case 'load':
+                if (mode !== 'preview') {
+                  const saved = activityState?.parts[0].response?.input;
+                  if (saved && e.source) {
+                    // post saved state back to lab.
 
-                  e.source.postMessage(saved, { targetOrigin: lab.origin });
-                }
-              } // TODO if in preview, load appropriate content
-              // Preview feature in lab servlet is not complete.
-              break;
-            case 'log':
-              // Currently logging to console, TODO link into torus/oli logging
-              console.log(msg.content);
-              break;
-            default:
-              console.warn('Unknown message type, skipped...', e);
+                    e.source.postMessage(saved, { targetOrigin: lab.origin });
+                  }
+                } // TODO if in preview, load appropriate content
+                // Preview feature in lab servlet is not complete.
+                break;
+              case 'log':
+                // Currently logging to console, TODO link into torus/oli logging
+                console.log(msg.content);
+                break;
+              default:
+                console.warn('Unknown message type, skipped...', e);
+            }
           }
         }
+      } catch (err) {
+        console.error(err);
       }
     };
     window.addEventListener('message', onMessage);
@@ -147,19 +151,59 @@ const LogicLab: React.FC<LogicLabDeliveryProps> = () => {
     return () => window.removeEventListener('message', onMessage);
   }, [activityState, model, context]);
 
-  const url = new URL(`api/v1/activities/lab/${activity}`, getLabServer(context));
-  // url.searchParams.append('activity', activity); // Used in development environment
-  url.searchParams.append('mode', mode);
-  url.searchParams.append('attemptGuid', activityState.attemptGuid);
+  const [loading, setLoading] = useState<'loading' | 'loaded' | 'error'>('loading');
+  const [baseUrl, setBaseUrl] = useState<string>('');
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    try {
+      const server = getLabServer(context);
+      const url = new URL(`api/v1/activities/lab/${activity}`, server);
+      url.searchParams.append('activity', activity);
+      url.searchParams.append('mode', mode);
+      url.searchParams.append('attemptGuid', activityState.attemptGuid);
+      // Using promise because react's useEffect does not handle async.
+      // toString because tsc does not accept the valid URL.
+      fetch(url.toString(), { signal, method: 'HEAD' })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          setLoading('loaded');
+          setBaseUrl(url.toString());
+        })
+        .catch(() => setLoading('error'));
+    } catch (err) {
+      console.error(err);
+      setLoading('error');
+    }
+    return () => controller.abort();
+  }, [context, activity, mode, activityState]);
+
   return (
-    <iframe
-      title={`LogicLab Activity ${modelContext?.title}`}
-      src={url.toString()}
-      allow="fullscreen"
-      height="800"
-      width="100%"
-      data-activity-mode={mode}
-    ></iframe>
+    <>
+      {loading === 'loading' && (
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden sr-only">Loading...</span>
+        </div>
+      )}
+      {loading === 'error' && (
+        <div className="alert alert-danger">
+          The LogicLab server is unreachable or not properly configured. Please contact support if
+          this issue persists.
+        </div>
+      )}
+      {loading === 'loaded' && (
+        <iframe
+          title={`LogicLab Activity ${modelContext?.title}`}
+          src={baseUrl}
+          allow="fullscreen"
+          height="800"
+          width="100%"
+          data-activity-mode={mode}
+        ></iframe>
+      )}
+    </>
   );
 };
 
