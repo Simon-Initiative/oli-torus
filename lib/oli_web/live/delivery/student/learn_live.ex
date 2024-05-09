2b59c9010d905d3f90364470c9b5f4b12dddf26a
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-05-09 13:05:44 -0300

    update card labels

--- lib/oli_web/live/delivery/student/learn_live.ex
+++ lib/oli_web/live/delivery/student/learn_live.ex
@@ -1750,7 +1750,7 @@ defmodule OliWeb.Delivery.Student.LearnLive do
     end
   end
 
-  attr :title, :string, default: "Intro"
+  attr :title, :string, default: "INTRO"
   attr :video_url, :string
   attr :card_resource_id, :string
   attr :resource_id, :string
@@ -1927,7 +1927,6 @@ defmodule OliWeb.Delivery.Student.LearnLive do
           />
         </div>
       </div>
-      <.page_icon :if={@is_page} graded={@card["graded"]} />
 
       <div class="h-[170px] w-[294px]">
         <div
@@ -1938,8 +1937,8 @@ defmodule OliWeb.Delivery.Student.LearnLive do
         >
           <span class="pointer-events-none text-[12px] leading-[16px] font-bold opacity-60 text-white dark:text-opacity-50">
             <%= if @is_page,
-              do: Phoenix.HTML.raw("&nbsp;"),
-              else: "#{@unit_numbering_index}.#{@module_index}" %>
+              do: "PAGE",
+              else: "MODULE #{@module_index}" %>
           </span>
           <h5 class="pointer-events-none text-[18px] leading-[25px] font-bold text-white z-10">
             <%= @card["title"] %>
