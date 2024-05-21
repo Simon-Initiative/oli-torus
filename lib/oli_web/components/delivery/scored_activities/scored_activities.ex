c97782c2bf17257fea91611ffbf5cf0f608cb99c
Santiago Simoncelli <simoncellisantiago@gmail.com>
2024-05-21 11:27:28 -0300

    [MER-3217] Apply Nico's feedback

--- lib/oli_web/components/delivery/scored_activities/scored_activities.ex
+++ lib/oli_web/components/delivery/scored_activities/scored_activities.ex
@@ -136,18 +136,21 @@ defmodule OliWeb.Components.Delivery.ScoredActivities do
   def render(assigns) do
     ~H"""
     <div>
-      <%= unless is_nil(@current_assessment) do %>
-        <button class="whitespace-nowrap" phx-click="back" phx-target={@myself}>
-          <div class="w-36 h-9 justify-start items-start gap-3.5 inline-flex">
-            <div class="px-1.5 py-2 border-zinc-700 justify-start items-center gap-1 flex">
-              <Icons.chevron_down class="fill-blue-400 rotate-90" />
-              <div class="text-zinc-700 text-sm font-semibold tracking-tight">
-                Back to Activities
-              </div>
+      <button
+        :if={!is_nil(@current_assessment)}
+        class="whitespace-nowrap"
+        phx-click="back"
+        phx-target={@myself}
+      >
+        <div class="w-36 h-9 justify-start items-start gap-3.5 inline-flex">
+          <div class="px-1.5 py-2 border-zinc-700 justify-start items-center gap-1 flex">
+            <Icons.chevron_down class="fill-blue-400 rotate-90" />
+            <div class="text-zinc-700 text-sm font-semibold tracking-tight">
+              Back to Activities
             </div>
           </div>
-        </button>
-      <% end %>
+        </div>
+      </button>
       <.loader if={!@table_model} />
       <div :if={@table_model} class="bg-white shadow-sm dark:bg-gray-800 dark:text-white">
         <div class="flex flex-col space-y-4 lg:space-y-0 lg:flex-row lg:justify-between px-9">
