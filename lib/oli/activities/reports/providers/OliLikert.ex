85f4d199a721f9ff35444d419b9cf6c1ced9e12a
rgachuhi <rgachuhi@users.noreply.github.com>
2024-06-13 16:08:41 +0000

    Auto format

--- lib/oli/activities/reports/providers/OliLikert.ex
+++ lib/oli/activities/reports/providers/OliLikert.ex
@@ -71,7 +71,8 @@ defmodule Oli.Activities.Reports.Providers.OliLikert do
         activity_attempt.transformed_model
       end
 
-      IO.inspect(content)
+    IO.inspect(content)
+
     data =
       Map.get(content, "items")
       |> Enum.with_index()
