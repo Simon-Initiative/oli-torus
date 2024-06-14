226dff4eea6b89029d759e8978700e553adbc25f
Santiago Simoncelli <simoncellisantiago@gmail.com>
2024-06-14 16:18:25 -0300

    [MER-3183] Fix test

--- test/oli_web/live/delivery/instructor_dashboard/insights/students_tab_test.exs
+++ test/oli_web/live/delivery/instructor_dashboard/insights/students_tab_test.exs
@@ -769,7 +769,9 @@ defmodule OliWeb.Delivery.InstructorDashboard.StudentsTabTest do
         sort_by: :numbering_index,
         sort_order: :asc,
         container_filter_by: :units,
-        selected_card_value: :zero_student_progress
+        selected_card_value: :zero_student_progress,
+        progress_percentage: 100,
+        selected_proficiency_ids: Jason.encode!([])
       }
 
       ## Check that the page is back to the units/modules view
