940e1471c5a43b86193ca4746c56d83fa6943ec1
Anders Weinstein <andersw@cs.cmu.edu>
2024-06-25 15:11:27 -0400

    style initial video play button to ensure visibility

--- assets/src/components/video_player/InitialPlayButton.tsx
+++ assets/src/components/video_player/InitialPlayButton.tsx
@@ -4,6 +4,7 @@ import { ControlButtonProps } from './ControlBar';
 export const InitialPlayButton: React.FC<ControlButtonProps> = ({ actions, player }) => {
   if (player?.hasStarted) return null;
   return (
+    // style to ensure visibility against light or dark backgrounds
     <button className="big-play-button" onClick={actions?.play}>
       <i className="fa-solid fa-circle-play"></i>
     </button>
