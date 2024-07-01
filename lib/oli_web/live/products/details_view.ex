c99769af2143bb7b207305d66fce4196e0700b94
Anders Weinstein <andersw@cs.cmu.edu>
2024-07-01 17:28:42 -0400

    correct for updates w/no welcome_title

--- lib/oli_web/live/products/details_view.ex
+++ lib/oli_web/live/products/details_view.ex
@@ -302,10 +302,11 @@ defmodule OliWeb.Products.DetailsView do
     ext
   end
 
-  defp decode_welcome_title(%{"welcome_title" => welcome_title} = project_params) do
-    cond do
-      welcome_title in [nil, ""] -> %{project_params | "welcome_title" => nil}
-      true -> Map.update(project_params, "welcome_title", nil, &Poison.decode!(&1))
-    end
-  end
+  defp decode_welcome_title(%{"welcome_title" => nil} = project_params), do: project_params
+
+  defp decode_welcome_title(%{"welcome_title" => ""} = project_params),
+    do: %{project_params | "welcome_title" => nil}
+
+  defp decode_welcome_title(project_params),
+    do: Map.update(project_params, "welcome_title", nil, &Poison.decode!(&1))
 end
