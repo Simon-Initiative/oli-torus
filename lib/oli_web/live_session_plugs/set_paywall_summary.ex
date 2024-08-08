ac284d468cad88faa86d99a04bc54726e91b900e
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-08-08 17:59:45 -0300

    create SetPaywallSummary live session plug

---
+++ lib/oli_web/live_session_plugs/set_paywall_summary.ex
@@ -0,0 +1,21 @@
+defmodule OliWeb.LiveSessionPlugs.SetPaywallSummary do
+  import Phoenix.Component, only: [assign: 2]
+
+  alias Oli.Delivery.Paywall
+
+  def on_mount(
+        :default,
+        _params,
+        _session,
+        %{assigns: %{current_user: current_user, section: section}} = socket
+      )
+      when not is_nil(section) and not is_nil(current_user) do
+    {:cont,
+     assign(socket,
+       paywall_summary:
+         Paywall.summarize_access(current_user, section) |> IO.inspect(label: "summary!!!!")
+     )}
+  end
+
+  def on_mount(:default, _params, _session, socket), do: {:cont, socket}
+end
