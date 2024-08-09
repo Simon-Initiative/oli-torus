0eeea87e86fca53190cd22e1241de4b25aa277e8
Santiago Simoncelli <simoncellisantiago@gmail.com>
2024-08-09 11:32:54 -0300

    [MER-3303] Menu for sidebar project

---
+++ lib/oli_web/live/workspaces/sub_menu_item.ex
@@ -0,0 +1,32 @@
+defmodule OliWeb.Workspace.SubMenuItem do
+  @moduledoc """
+    Struct for a sub-menu item.
+  """
+
+  @enforce_keys [
+    :text,
+    :icon,
+    :view,
+    :parent_view,
+    :children,
+    :class
+  ]
+
+  defstruct [
+    :text,
+    :icon,
+    :view,
+    :parent_view,
+    :children,
+    :class,
+  ]
+
+  @type t() :: %__MODULE__{
+          text: String.t(),
+          icon: String.t(),
+          view: atom(),
+          parent_view: atom(),
+          children: list(%__MODULE__{}),
+          class: String.t(),
+        }
+end
