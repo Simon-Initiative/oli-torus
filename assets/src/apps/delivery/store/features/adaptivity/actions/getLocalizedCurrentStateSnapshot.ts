b8fd63761fcfbd6aa38b89118999aa27221258bf
Devesh Tiwari <dtiwari@ats360.com>
2024-08-26 14:22:43 +0530

    more changes

--- assets/src/apps/delivery/store/features/adaptivity/actions/getLocalizedCurrentStateSnapshot.ts
+++ assets/src/apps/delivery/store/features/adaptivity/actions/getLocalizedCurrentStateSnapshot.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getLocalizedStateSnapshot } from 'adaptivity/scripting';
+import { defaultGlobalEnv, getLocalizedStateSnapshot, getValue } from 'adaptivity/scripting';
 import { DeliveryRootState } from 'apps/delivery/store/rootReducer';
 import { selectCurrentActivityTree } from '../../groups/selectors/deck';
 import AdaptivitySlice from '../name';
@@ -14,8 +14,12 @@ export const getLocalizedCurrentStateSnapshot = createAsyncThunk(
     if (!currentActivityTree?.length) {
       return { snapshot: {} };
     }
-    const currentActivityIds = currentActivityTree[currentActivityTree.length - 1].id;
-    // Since we no longer save the values to its owener, we only need snapshot of current activity
+    let currentActivityIds: any = currentActivityTree.map((a) => a.id);
+    const attempType = getValue('app.attempType', defaultGlobalEnv);
+    if (attempType == 'New') {
+      // Since we no longer save the values to its owener, we only need snapshot of current activity
+      currentActivityIds = currentActivityTree[currentActivityTree.length - 1].id;
+    }
     const snapshot = getLocalizedStateSnapshot([currentActivityIds]);
 
     return { snapshot };
