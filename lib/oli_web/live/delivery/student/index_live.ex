d53683a495c29ec285f7b4f7bfdddf64a5342c09
Eli Knebel <eliknebel@cmu.edu>
2024-08-26 11:00:18 -0400

    handle case where lesson end date might be nil

--- lib/oli_web/live/delivery/student/index_live.ex
+++ lib/oli_web/live/delivery/student/index_live.ex
@@ -522,7 +522,11 @@ defmodule OliWeb.Delivery.Student.IndexLive do
       <div class="pr-2 pl-1 self-end">
         <div class="flex items-end gap-1">
           <div class="text-right dark:text-white text-opacity-90 text-xs font-semibold">
-            <%= Utils.days_difference(@lesson.end_date, @ctx) %>
+            <%= case @lesson do %>
+              <% %{end_date: nil} -> %>
+              <% %{end_date: end_date} -> %>
+                <%= Utils.days_difference(end_date, @ctx) %>
+            <% end %>
           </div>
         </div>
       </div>
