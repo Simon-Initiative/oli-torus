12a742b32ecdfa7e21554d1e80db67f0ab3632ee
Francisco-Castro <catrapato@hotmail.com>
2024-08-27 08:22:20 -0400

    [MER-3596] Handle case where project is not found

--- lib/oli_web/live_session_plugs/set_project.ex
+++ lib/oli_web/live_session_plugs/set_project.ex
@@ -1,25 +1,33 @@
 defmodule OliWeb.LiveSessionPlugs.SetProject do
+  use OliWeb, :verified_routes
+
   import Phoenix.Component, only: [assign: 2]
+  import Phoenix.LiveView, only: [redirect: 2, put_flash: 3]
+
+  alias Oli.Authoring.Course
+  alias Oli.Authoring.Course.Project
 
   def on_mount(:default, %{"project_id" => project_id}, _session, socket) do
-    project = Oli.Authoring.Course.get_project_by_slug(project_id)
+    project = Course.get_project_by_slug(project_id)
 
-    project =
-      case project.required_survey_resource_id do
-        nil ->
-          Map.put(project, :required_survey, nil)
+    case project do
+      nil ->
+        halt("Project not found", socket)
 
-        _ ->
-          required_survey = Oli.Authoring.Course.get_project_survey(project.id)
-          Map.put(project, :required_survey, required_survey)
-      end
+      %Project{required_survey_resource_id: nil} = project ->
+        {:cont, assign(socket, project: %{project | required_survey: nil})}
 
-    socket = assign(socket, project: project)
-
-    {:cont, socket}
+      %Project{required_survey_resource_id: _} = project ->
+        required_survey = Course.get_project_survey(project.id)
+        {:cont, assign(socket, project: %{project | required_survey: required_survey})}
+    end
   end
 
   def on_mount(:default, _params, _session, socket) do
     {:cont, socket}
+  end
+
+  defp halt(message, socket) do
+    {:halt, socket |> put_flash(:error, message) |> redirect(to: ~p"/workspaces/course_author")}
   end
 end
