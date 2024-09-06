aebf44758102c383ed4e0c7fc016c900253a5be5
Francisco-Castro <catrapato@hotmail.com>
2024-09-06 10:02:28 -0400

    [MER-3602] Add testing for ProductsLive module

--- lib/oli_web/live/workspaces/course_author/products_live.ex
+++ lib/oli_web/live/workspaces/course_author/products_live.ex
@@ -17,33 +17,20 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
   alias OliWeb.Common.Table.SortableTableModel
   alias OliWeb.Products.ProductsTableModel
 
-  @limit 20
+  @max_items_per_page 20
+  @initial_offset 0
+  @initial_create_form to_form(%{"product_title" => ""}, as: "product_form")
 
   @impl Phoenix.LiveView
   def mount(params, session, socket) do
     project = socket.assigns.project
     include_archived = Params.get_boolean_param(params, "include_archived", false)
 
-    products =
-      Blueprint.browse(
-        %Paging{offset: 0, limit: @limit},
-        %Sorting{direction: :asc, field: :title},
-        include_archived: include_archived,
-        project_id: project.id
-      )
+    products = get_products(socket.assigns)
 
-    total_count = determine_total(products)
     ctx = SessionContext.init(socket, session)
-
     {:ok, table_model} = ProductsTableModel.new(products, ctx, project.slug)
-
-    published? =
-      case project do
-        nil -> true
-        _ -> Publishing.project_published?(project.slug)
-      end
-
-    create_product_form = to_form(%{"product_title" => ""}, as: "product_title")
+    published? = Publishing.project_published?(project.slug)
 
     {:ok,
      assign(socket,
@@ -54,45 +41,36 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
        published?: published?,
        is_admin_view: false,
        include_archived: include_archived,
-       total_count: total_count,
-       limit: @limit,
-       offset: 0,
+       limit: @max_items_per_page,
+       offset: @initial_offset,
        table_model: table_model,
-       creation_title: "",
-       create_product_form: create_product_form,
+       create_product_form: @initial_create_form,
        ctx: ctx
      )}
   end
 
   @impl Phoenix.LiveView
   def handle_params(params, _, socket) do
+    # If the sidebar was toggled, we don't need to update the table model
     sidebar_was_toggled = Map.keys(socket.assigns.__changed__) == [:sidebar_expanded]
 
     if sidebar_was_toggled do
-      {:noreply, assign(socket, url_params: url_params(socket.assigns))}
+      {:noreply, socket}
     else
       table_model =
         SortableTableModel.update_from_params(socket.assigns.table_model, params)
 
-      offset = Params.get_int_param(params, "offset", 0)
+      offset = Params.get_int_param(params, "offset", @initial_offset)
       include_archived = Params.get_boolean_param(params, "include_archived", false)
 
-      products =
-        Blueprint.browse(
-          %Paging{offset: offset, limit: @limit},
-          %Sorting{direction: table_model.sort_order, field: table_model.sort_by_spec.name},
-          include_archived: include_archived,
-          project_id: socket.assigns.project && socket.assigns.project.id
-        )
+      products = get_products(socket.assigns)
 
       table_model = Map.put(table_model, :rows, products)
-      total_count = determine_total(products)
 
       {:noreply,
        assign(socket,
          offset: offset,
          table_model: table_model,
-         total_count: total_count,
          include_archived: include_archived
        )}
     end
@@ -132,7 +110,7 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
         <PagedTable.render
           page_change="paged_table_page_change"
           sort="paged_table_sort"
-          total_count={@total_count}
+          total_count={Enum.count(@table_model.rows)}
           limit={@limit}
           offset={@offset}
           table_model={@table_model}
@@ -158,12 +136,18 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
         labels -> Map.from_struct(labels)
       end
 
-    case Blueprint.create_blueprint(project_slug, product_title, customizations) do
-      {:ok, blueprint} ->
+    blueprint = Blueprint.create_blueprint(project_slug, product_title, customizations)
+
+    case blueprint do
+      {:ok, _blueprint} ->
+        products = get_products(socket.assigns)
+
+        {:ok, table_model} = ProductsTableModel.new(products, socket.assigns.ctx, project_slug)
+
         {:noreply,
          socket
          |> put_flash(:info, "Product successfully created.")
-         |> redirect(to: ~p"/workspaces/course_author/#{project_slug}/products/#{blueprint.slug}")}
+         |> assign(table_model: table_model)}
 
       {:error, _} ->
         {:noreply, put_flash(socket, :error, "Could not create product")}
@@ -185,14 +169,12 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
         project_id: project_id
       )
 
-    total_count = length(products)
     table_model = Map.put(table_model, :rows, products)
 
     socket =
       assign(socket,
         include_archived: include_archived,
         products: products,
-        total_count: total_count,
         table_model: table_model
       )
 
@@ -229,6 +211,21 @@ defmodule OliWeb.Workspaces.CourseAuthor.ProductsLive do
     }
   end
 
-  defp determine_total([]), do: 0
-  defp determine_total([hd | _]), do: hd.total_count
+  defp get_products(assigns) do
+    offset = assigns[:offset] || @initial_offset
+    limit = assigns[:limit] || @max_items_per_page
+
+    table_model = assigns[:table_model]
+    direction = if table_model, do: assigns.table_model.sort_order, else: :asc
+    field = if table_model, do: assigns.table_model.sort_by_spec.name, else: :title
+
+    include_archived = get_in(assigns, [:include_archived]) || false
+
+    Blueprint.browse(
+      %Paging{offset: offset, limit: limit},
+      %Sorting{direction: direction, field: field},
+      include_archived: include_archived,
+      project_id: assigns.project.id
+    )
+  end
 end
