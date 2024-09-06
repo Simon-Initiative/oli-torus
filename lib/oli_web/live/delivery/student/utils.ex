d9b9202032fdb3ac110c15a7e0cc708379c80f02
Darren Siegel <siegel.darren@gmail.com>
2024-09-06 13:32:13 -0400

    always show reset activiites, even when none exist

--- lib/oli_web/live/delivery/student/utils.ex
+++ lib/oli_web/live/delivery/student/utils.ex
@@ -256,7 +256,7 @@ defmodule OliWeb.Delivery.Student.Utils do
   def reset_attempts_button(assigns) do
     ~H"""
     <button
-      :if={@activity_count > 0 && @page_context.review_mode == false && not @advanced_delivery}
+      :if={@page_context.review_mode == false && not @advanced_delivery}
       id="reset_answers"
       class="btn btn-link btn-sm text-center mb-10"
       onClick={"window.OLI.finalize('#{@section_slug}', '#{@page_context.page.slug}', '#{hd(@page_context.resource_attempts).attempt_guid}', false, 'reset_answers')"}
