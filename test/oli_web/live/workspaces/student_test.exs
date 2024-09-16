ae068a7f395595a41e9092776bd0a49cb1ac56e8
Eli Knebel <eliknebel@cmu.edu>
2024-09-16 11:50:44 -0400

    fix test

--- test/oli_web/live/workspaces/student_test.exs
+++ test/oli_web/live/workspaces/student_test.exs
@@ -83,25 +83,6 @@ defmodule OliWeb.Workspaces.StudentTest do
       refute has_element?(view, "div[role='account label']")
     end
 
-    test "sees linked account email on user menu", %{conn: conn, user: user} do
-      section_1 = insert(:section, %{open_and_free: true, title: "The best course ever!"})
-      section_2 = insert(:section, %{open_and_free: true, title: "Maths"})
-
-      Sections.enroll(user.id, section_1.id, [ContextRoles.get_role(:context_learner)])
-      Sections.enroll(user.id, section_2.id, [ContextRoles.get_role(:context_instructor)])
-
-      author = insert(:author)
-      Accounts.link_user_author_account(user, author)
-
-      {:ok, view, _html} = live(conn, ~p"/workspaces/student")
-
-      assert has_element?(
-               view,
-               "div[id='workspace-user-menu-dropdown'] div[role='linked authoring account email']",
-               author.email
-             )
-    end
-
     test "can access student workspace when not enrolled to any section", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/workspaces/student")
 
