f44f5faa40bdb273c9719d4e1c122cd03221d9e4
Darren Siegel <siegel.darren@gmail.com>
2024-09-17 07:24:09 -0400

    add partial index

---
+++ priv/repo/migrations/20240917111312_add_lifecycle_index.exs
@@ -0,0 +1,12 @@
+defmodule Oli.Repo.Migrations.AddLifecycleIndex do
+  use Ecto.Migration
+
+  def up do
+    # Creates a partial index on the lifecycle_state column where the value is 'submitted'
+    execute("CREATE INDEX activity_attempts_submitted ON activity_attempts (lifecycle_state) WHERE lifecycle_state = 'submitted';")
+  end
+
+  def down do
+    execute("DROP INDEX activity_attempts_submitted;")
+  end
+end
