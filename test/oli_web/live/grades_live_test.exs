9ffcd59033c28b67693f1afecb5d6d99cb7ea0f3
Gastón Abellá <gaston@wyeworks.com>
2024-09-18 10:06:01 -0300

    [BUGFIX] [MER-3613] Grade Passback Issue (#5107)
    * [MER-3613] Fix Liveview console log error
    
    * [MER-3613] Disable Synchronize Grades button when the process is in progress
    
    * [MER-3613] Add throttle value to avoid rate limit error when fetching the LTI access token
    
    * [MER-3613] Remove unused access token call
    
    * [MER-3613] Add tests

--- test/oli_web/live/grades_live_test.exs
+++ test/oli_web/live/grades_live_test.exs
@@ -271,6 +271,30 @@ defmodule OliWeb.GradesLiveTest do
       |> render_click()
 
       assert has_element?(view, "p", "Pending grade updates: 1")
+
+      # Button disabled until it is finished
+      assert view
+             |> has_element?(
+               "a[phx-click=\"send_grades\"][disabled]",
+               "Synchronize Grades"
+             )
+
+      payload = %Oli.Delivery.Attempts.PageLifecycle.GradeUpdatePayload{
+        resource_access_id: resource_access.id,
+        job: %{id: 1},
+        status: :success,
+        details: nil
+      }
+
+      # Send success grade update message
+      send(view.pid, {:lms_grade_update_result, payload})
+
+      # Button is enabled again once the sync is finished
+      refute view
+             |> has_element?(
+               "a[phx-click=\"send_grades\"][disabled]",
+               "Synchronize Grades"
+             )
     end
 
     test "sync grades - select other resource", %{
@@ -455,26 +479,6 @@ defmodule OliWeb.GradesLiveTest do
       |> render_click()
 
       assert has_element?(view, "div#flash", "Error getting LMS access token")
-    end
-
-    test "sync grades - shows error on failure to obtain access token",
-         %{
-           conn: conn,
-           section: section
-         } do
-      user = insert(:user)
-      enroll_user_to_section(user, section, :context_learner)
-
-      {:ok, view, _html} = live(conn, live_view_grades_route(section.slug))
-
-      view
-      |> element(
-        "a[phx-click=\"send_grades\"]",
-        "Synchronize Grades"
-      )
-      |> render_click()
-
-      assert has_element?(view, "div#flash", "error fetching access token")
     end
   end
 end
