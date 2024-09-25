231a822d5645b5c104a16bd1fe3c20cc3199a888
Santiago Simoncelli <simoncellisantiago@gmail.com>
2024-09-25 15:13:30 -0300

    [MER-3535] Applies Nico's feedback

--- lib/oli_web/icons.ex
+++ lib/oli_web/icons.ex
@@ -1473,9 +1473,8 @@ defmodule OliWeb.Icons do
     ~H"""
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        class="hover:stroke-zinc-300"
         d="M5 12H19M19 12L15 16M19 12L15 8"
-        stroke="white"
+        stroke="currentColor"
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
