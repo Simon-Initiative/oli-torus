179375bb1d640e9d77b1c2ae9bc26b551b2c7b4f
Anders Weinstein <andersw@cs.cmu.edu>
2024-09-25 22:40:41 -0400

    delint

--- assets/src/components/activities/short_answer/sections/MathInput.tsx
+++ assets/src/components/activities/short_answer/sections/MathInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuthoringElementContext } from 'components/activities/AuthoringElementProvider';
 import { MathLive } from 'components/common/MathLive';
 import { InputText } from 'data/activities/model/rules';
