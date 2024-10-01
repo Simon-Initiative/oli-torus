9f2888cb308bbbcd619e6c00cc7bbaff87fa65e4
Anders Weinstein <andersweinstein@users.noreply.github.com>
2024-10-01 18:26:08 -0400

    [BUG FIX] [MER-3853] ensure MathLive keyboard shows in authoring (#5136)
    * ensure MathLive keyboard shows in authoring
    
    * delint
    
    ---------
    
    Co-authored-by: Anders Weinstein <andersw@cs.cmu.edu>

--- assets/src/components/activities/short_answer/sections/MathInput.tsx
+++ assets/src/components/activities/short_answer/sections/MathInput.tsx
@@ -16,6 +16,10 @@ export const MathInput: React.FC<MathInputProps> = ({ input, onEditInput }) => {
         value={input.value}
         options={{
           readOnly: !editMode,
+          // need this because while keyboard toggle gets hidden if readonly, it apparently
+          // isn't automatically restored on change back to nonReadOnly, and authoring context
+          // routinely passes through readonly renders before becoming editable
+          virtualKeyboardMode: editMode ? 'manual' : 'off',
         }}
         onChange={(latex: string) => onEditInput({ ...input, value: latex, operator: 'equals' })}
       />
