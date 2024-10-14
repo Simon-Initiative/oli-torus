cc1a6c6900cfb777ed4530b53f666413309ad454
Darren Siegel <siegel.darren@gmail.com>
2024-10-14 16:42:25 -0400

    fix resetting

--- lib/oli/delivery/attempts/page_lifecycle/ungraded.ex
+++ lib/oli/delivery/attempts/page_lifecycle/ungraded.ex
@@ -132,12 +132,12 @@ defmodule Oli.Delivery.Attempts.PageLifecycle.Ungraded do
     Oli.Delivery.Metrics.mark_progress_completed(resource_access)
   end
 
-  defp do_update_progress(%{progress: nil}, _) do
+  defp do_update_progress(%{progress: nil} = resource_access, _) do
     Oli.Delivery.Metrics.reset_progress(resource_access)
   end
 
-  defp do_update_progress(r, _) do
-    {:ok, r}
+  defp do_update_progress(resource_access, _) do
+    {:ok, resource_access}
   end
 
   # We need a new attempt when:
