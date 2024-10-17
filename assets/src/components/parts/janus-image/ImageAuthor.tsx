17382003f2bcd612a0453ca964c2658b497da9ad
Devesh Tiwari <dtiwari@ats360.com>
2024-10-17 13:54:38 +0530

    MER-3412

--- assets/src/components/parts/janus-image/ImageAuthor.tsx
+++ assets/src/components/parts/janus-image/ImageAuthor.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
-import React, { CSSProperties, useEffect, useState } from 'react';
+import React, { CSSProperties, useEffect, useRef, useState } from 'react';
+import { clone } from 'utils/common';
 import { AuthorPartComponentProps } from '../types/parts';
 import { ImageModel } from './schema';
 
 const ImageAuthor: React.FC<AuthorPartComponentProps<ImageModel>> = (props) => {
-  const { model } = props;
+  const { model, onSaveConfigure } = props;
   const [ready, setReady] = useState<boolean>(false);
   const id: string = props.id;
 
@@ -24,8 +25,40 @@ const ImageAuthor: React.FC<AuthorPartComponentProps<ImageModel>> = (props) => {
     width,
     height,
   };
-
-  return ready ? <img draggable="false" alt={alt} src={src} style={imageStyles} /> : null;
+  const imageContainerRef = useRef<HTMLImageElement>(null);
+    if (!imageContainerRef?.current) {
+      return;
+    const naturalWidth = imageContainerRef.current.naturalWidth;
+    const naturalHeight = imageContainerRef.current.naturalHeight;
+    const ratioWidth = naturalWidth / imageContainerRef.current.width;
+    const ratioHeight = naturalHeight / imageContainerRef.current.height;
+    let newAdjustedHeight = imageContainerRef.current.height;
+    let newAdjustedWidth = imageContainerRef.current.width;
+    if (ratioWidth > ratioHeight) {
+      const newHeight = Number(naturalHeight / ratioWidth).toFixed();
+      newAdjustedHeight = parseInt(newHeight);
+    } else {
+      const newWidth = Number(naturalWidth / ratioHeight).toFixed();
+      newAdjustedWidth = parseInt(newWidth);
+    }
+    const modelClone = clone(model);
+    modelClone.height = newAdjustedHeight;
+    modelClone.width = newAdjustedWidth;
+    if (newAdjustedHeight != height || newAdjustedWidth != width) {
+      //console.log('I AM SETTING NOW ->', { newAdjustedHeight, newAdjustedWidth, model });
+      onSaveConfigure({ id, snapshot: modelClone });
+    }
+  };
+  return ready ? (
+    <img
+      ref={imageContainerRef}
+      onLoad={manipulateImageSize}
+      draggable="false"
+      alt={alt}
+      src={src}
+      style={imageStyles}
+    />
+  ) : null;
 };
 
 export const tagName = 'janus-image';
