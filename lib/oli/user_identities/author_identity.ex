aa11992c9b2b82e5cf8efecda2938238dc1a72ec
Eli Knebel <eliknebel@cmu.edu>
2024-10-22 09:14:29 -0400

    restore user and author identity schema details

--- lib/oli/user_identities/author_identity.ex
+++ lib/oli/user_identities/author_identity.ex
@@ -3,7 +3,22 @@ defmodule Oli.UserIdentities.AuthorIdentity do
 
   schema "author_identities" do
     # MER-3835 TODO
+    field :provider, :string
+    field :uid, :string
+
+    belongs_to :user, Oli.Accounts.Author
 
     timestamps(type: :utc_datetime)
+  end
+
+  @doc """
+  Validates an author identity.
+  """
+  def changeset(author_identity_or_changeset, attrs) do
+    author_identity_or_changeset
+    |> Changeset.cast(attrs, [:provider, :uid, :user_id])
+    |> Changeset.validate_required([:provider, :uid])
+    |> Changeset.assoc_constraint(:user)
+    |> Changeset.unique_constraint(:uid_provider, name: :author_identities_uid_provider_index)
   end
 end
--- lib/oli/user_identities/user_identity.ex
+++ lib/oli/user_identities/user_identity.ex
@@ -3,7 +3,22 @@ defmodule Oli.UserIdentities.UserIdentity do
 
   schema "user_identities" do
     # MER-3835 TODO
+    field :provider, :string
+    field :uid, :string
+
+    belongs_to :user, Oli.Accounts.User
 
     timestamps(type: :utc_datetime)
+  end
+
+  @doc """
+  Validates a user identity.
+  """
+  def changeset(user_identity_or_changeset, attrs) do
+    user_identity_or_changeset
+    |> Changeset.cast(attrs, [:provider, :uid, :user_id])
+    |> Changeset.validate_required([:provider, :uid])
+    |> Changeset.assoc_constraint(:user)
+    |> Changeset.unique_constraint(:uid_provider, name: :user_identities_uid_provider_index)
   end
 end
