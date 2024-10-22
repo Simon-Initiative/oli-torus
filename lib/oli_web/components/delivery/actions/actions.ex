10acc0ea3e4ea5a1fde7dfe6d9264d3824f74a85
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-10-22 09:46:55 -0300

    rearrange modals in template - avoids a small gap when opening modals

--- lib/oli_web/components/delivery/actions/actions.ex
+++ lib/oli_web/components/delivery/actions/actions.ex
@@ -54,131 +54,135 @@ defmodule OliWeb.Components.Delivery.Actions do
 
   def render(assigns) do
     ~H"""
-    <div
-      id="student_actions"
-      class="mx-10 mb-10 bg-white dark:bg-gray-800 shadow-sm px-14 py-7 flex flex-col gap-6"
-    >
-      <%= if @is_suspended? do %>
-        <div id="unenrolled_student_actions">
-          <Modal.modal
-            id="re_enroll_user_modal"
-            class="w-5/6"
-            on_confirm={
-              JS.push("re_enroll", target: @myself) |> Modal.hide_modal("re_enroll_user_modal")
-            }
-          >
-            <:title>Re-enroll student</:title>
-            <%= "Are you sure you want to re-enroll #{@user.name} in the course #{@section.title}" %>
-            <:confirm>Confirm</:confirm>
-          </Modal.modal>
-          <div class="ml-auto">
-            <button phx-click={Modal.show_modal("re_enroll_user_modal")} class="btn btn-primary">
-              Re-enroll
-            </button>
-          </div>
-        </div>
-      <% else %>
-        <div id="enrolled_student_actions" class="flex flex-col gap-6">
-          <Modal.modal
-            id="unenroll_user_modal"
-            class="w-5/6"
-            on_confirm={
-              JS.push("unenroll", target: @myself) |> Modal.hide_modal("unenroll_user_modal")
-            }
-          >
-            <:title>Unenroll student</:title>
-            <%= "Are you sure you want to unenroll #{@user.name} from the course #{@section.title}" %>
-            <:confirm>Confirm</:confirm>
-          </Modal.modal>
+    <div>
+      <div id="modals">
+        <Modal.modal
+          id="unenroll_user_modal"
+          class="w-5/6"
+          on_confirm={JS.push("unenroll", target: @myself) |> Modal.hide_modal("unenroll_user_modal")}
+        >
+          <:title>Unenroll student</:title>
+          <%= "Are you sure you want to unenroll #{@user.name} from the course #{@section.title}" %>
+          <:confirm>Confirm</:confirm>
+        </Modal.modal>
 
-          <Modal.modal
-            id="payment_status_modal"
-            class="w-5/6"
-            on_confirm={
-              JS.push("invalidate_payment", target: @myself)
-              |> Modal.hide_modal("payment_status_modal")
-            }
-            on_cancel={
-              JS.push("force_reset_payment_status", target: @myself)
-              |> Modal.hide_modal("payment_status_modal")
-            }
-          >
-            <:title>Update payment status</:title>
-            <%= "Are you sure you want to change the payment status of #{@user.name} in the course #{@section.title} to NOT PAID?" %>
-            <:confirm>Confirm</:confirm>
-          </Modal.modal>
-          <div class="flex flex-col sm:flex-row sm:items-end instructor_dashboard_table">
-            <h4 class="torus-h4 !py-0 mr-auto dark:text-white">Actions</h4>
-          </div>
+        <Modal.modal
+          id="re_enroll_user_modal"
+          class="w-5/6"
+          on_confirm={
+            JS.push("re_enroll", target: @myself) |> Modal.hide_modal("re_enroll_user_modal")
+          }
+        >
+          <:title>Re-enroll student</:title>
+          <%= "Are you sure you want to re-enroll #{@user.name} in the course #{@section.title}" %>
+          <:confirm>Confirm</:confirm>
+        </Modal.modal>
 
-          <div class="flex justify-between items-center">
-            <div class="flex flex-col">
-              <span class="dark:text-white">Change enrolled user role</span>
-              <span class="text-xs text-gray-400 dark:text-gray-950">
-                Select the role to change for the user in this section.
-              </span>
-            </div>
-            <form phx-change="display_confirm_modal" phx-target={@myself}>
-              <select class="torus-select pr-32" name="filter_by_role_id">
-                <%= for elem <- @user_role_data do %>
-                  <option selected={elem.id == @user_role_id} value={elem.id}>
-                    <%= elem.title %>
-                  </option>
-                <% end %>
-              </select>
-            </form>
-          </div>
-          <%= if @section.requires_payment and @is_admin do %>
-            <div class="flex justify-between items-center">
-              <div class="flex flex-col">
-                <span class="dark:text-black">Bypass payment</span>
-                <span class="text-xs text-gray-400 dark:text-gray-950">Apply bypass payment</span>
-              </div>
-              <button
-                class="torus-button flex justify-center primary h-9 w-48"
-                disabled={@has_payment}
-                phx-click="display_bypass_modal"
-                phx-target={@myself}
-              >
-                Apply Bypass Payment
+        <Modal.modal
+          id="payment_status_modal"
+          class="w-5/6"
+          on_confirm={
+            JS.push("invalidate_payment", target: @myself)
+            |> Modal.hide_modal("payment_status_modal")
+          }
+          on_cancel={
+            JS.push("force_reset_payment_status", target: @myself)
+            |> Modal.hide_modal("payment_status_modal")
+          }
+        >
+          <:title>Update payment status</:title>
+          <%= "Are you sure you want to change the payment status of #{@user.name} in the course #{@section.title} to NOT PAID?" %>
+          <:cancel>Cancel</:cancel>
+          <:confirm>Confirm</:confirm>
+        </Modal.modal>
+      </div>
+      <div
+        id="student_actions"
+        class="mx-10 mb-10 bg-white dark:bg-gray-800 shadow-sm px-14 py-7 flex flex-col gap-6"
+      >
+        <%= if @is_suspended? do %>
+          <div id="unenrolled_student_actions">
+            <div class="ml-auto">
+              <button phx-click={Modal.show_modal("re_enroll_user_modal")} class="btn btn-primary">
+                Re-enroll
               </button>
             </div>
-            <div :if={@has_payment} class="flex justify-between items-center">
+          </div>
+        <% else %>
+          <div id="enrolled_student_actions" class="flex flex-col gap-6">
+            <div class="flex flex-col sm:flex-row sm:items-end instructor_dashboard_table">
+              <h4 class="torus-h4 !py-0 mr-auto dark:text-white">Actions</h4>
+            </div>
+
+            <div class="flex justify-between items-center">
               <div class="flex flex-col">
-                <span class="dark:text-white">Update payment status</span>
+                <span class="dark:text-white">Change enrolled user role</span>
+                <span class="text-xs text-gray-400 dark:text-gray-950">
+                  Select the role to change for the user in this section.
+                </span>
               </div>
-              <form
-                id={"payment_status_form_#{@payment_status_uuid}"}
-                phx-change={Modal.show_modal("payment_status_modal")}
-              >
+              <form phx-change="display_confirm_modal" phx-target={@myself}>
                 <select class="torus-select pr-32" name="filter_by_role_id">
-                  <option selected={true} value={:paid}>
-                    Paid
-                  </option>
-                  <option selected={false} value={:unpaid}>
-                    Not Paid
-                  </option>
+                  <%= for elem <- @user_role_data do %>
+                    <option selected={elem.id == @user_role_id} value={elem.id}>
+                      <%= elem.title %>
+                    </option>
+                  <% end %>
                 </select>
               </form>
             </div>
-          <% end %>
+            <%= if @section.requires_payment and @is_admin do %>
+              <div class="flex justify-between items-center">
+                <div class="flex flex-col">
+                  <span class="dark:text-black">Bypass payment</span>
+                  <span class="text-xs text-gray-400 dark:text-gray-950">Apply bypass payment</span>
+                </div>
+                <button
+                  class="torus-button flex justify-center primary h-9 w-48"
+                  disabled={@has_payment}
+                  phx-click="display_bypass_modal"
+                  phx-target={@myself}
+                >
+                  Apply Bypass Payment
+                </button>
+              </div>
+              <div :if={@has_payment} class="flex justify-between items-center">
+                <div class="flex flex-col">
+                  <span class="dark:text-white">Update payment status</span>
+                </div>
+                <form
+                  id={"payment_status_form_#{@payment_status_uuid}"}
+                  phx-change={Modal.show_modal("payment_status_modal")}
+                >
+                  <select class="torus-select pr-32" name="filter_by_role_id">
+                    <option selected={true} value={:paid}>
+                      Paid
+                    </option>
+                    <option selected={false} value={:unpaid}>
+                      Not Paid
+                    </option>
+                  </select>
+                </form>
+              </div>
+            <% end %>
 
-          <%= if @is_admin do %>
-            <.live_component
-              id="transfer_enrollment"
-              module={OliWeb.Delivery.Actions.TransferEnrollment}
-              section={@section}
-              user={@user}
-            />
-          <% end %>
+            <%= if @is_admin do %>
+              <.live_component
+                id="transfer_enrollment"
+                module={OliWeb.Delivery.Actions.TransferEnrollment}
+                section={@section}
+                user={@user}
+              />
+            <% end %>
 
-          <div class="ml-auto">
-            <button phx-click={Modal.show_modal("unenroll_user_modal")} class="btn btn-danger">
-              Unenroll
-            </button>
+            <div class="ml-auto">
+              <button phx-click={Modal.show_modal("unenroll_user_modal")} class="btn btn-danger">
+                Unenroll
+              </button>
+            </div>
           </div>
-        </div>
-      <% end %>
+        <% end %>
+      </div>
     </div>
     """
   end
