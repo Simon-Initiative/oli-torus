abafd8f8682bea156af61c59010a67d3fe7319ec
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-11-06 16:17:21 -0300

    add hero banner

--- lib/oli_web/live/delivery/student/assignments_live.ex
+++ lib/oli_web/live/delivery/student/assignments_live.ex
@@ -7,6 +7,17 @@ defmodule OliWeb.Delivery.Student.AssignmentsLive do
 
   def render(assigns) do
     ~H"""
+    <div
+      role="hero banner"
+      class="w-full bg-cover bg-center bg-no-repeat h-[247px]"
+      style="background-image: url('/images/gradients/assignments-bg.png');"
+    >
+      <div class="h-[247px] bg-gradient-to-r from-[#e4e4ea] dark:from-[#0a0b11] to-transparent">
+        <h1 class="py-20 pl-[76px] text-4xl md:text-6xl font-normal tracking-tight dark:text-white">
+          Assignments
+        </h1>
+      </div>
+    </div>
     Placeholder for new Assignments Live View
     """
   end
