eca2e7f7e7b198232f0ed88b5bbf27b2390dddd1
Francisco-Castro <catrapato@hotmail.com>
2024-11-28 15:41:41 -0500

    [MER-4012] Use handle_continue to dont block the app initialization

--- lib/oli/delivery/depot_warmer.ex
+++ lib/oli/delivery/depot_warmer.ex
@@ -1,14 +1,18 @@
 defmodule Oli.Delivery.DepotWarmer do
   use GenServer
 
-  alias Oli.Delivery.Sections.SectionResourceDepot
   alias Oli.Delivery.DistributedDepotCoordinator
+  alias Oli.Delivery.Sections.SectionResourceDepot
 
   def start_link(init_args) do
     GenServer.start_link(__MODULE__, init_args, name: __MODULE__)
   end
 
   def init(_) do
+    {:ok, [], {:continue, :init_depot_warmer}}
+  end
+
+  def handle_continue(:init_depot_warmer, state) do
     depot_desc = SectionResourceDepot.depot_desc()
     sections_ids = SectionResourceDepot.fetch_recently_active_sections()
 
@@ -16,6 +20,6 @@ defmodule Oli.Delivery.DepotWarmer do
       DistributedDepotCoordinator.init_if_necessary(depot_desc, section_id, SectionResourceDepot)
     end)
 
-    {:ok, []}
+    {:noreply, state}
   end
 end
