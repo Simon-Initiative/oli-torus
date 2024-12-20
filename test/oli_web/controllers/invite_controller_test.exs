a295ec4e4c76ac3ea2da4412891900363ca2c842
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2024-12-20 15:34:14 -0300

    add more InviteController tests

--- test/oli_web/controllers/invite_controller_test.exs
+++ test/oli_web/controllers/invite_controller_test.exs
@@ -3,6 +3,7 @@ defmodule OliWeb.InviteControllerTest do
   import Swoosh.TestAssertions
 
   import Oli.Factory
+  import Ecto.Query, warn: false
 
   alias Oli.Repo
   alias Oli.Accounts
@@ -11,8 +12,9 @@ defmodule OliWeb.InviteControllerTest do
   @invite_email "invite@example.com"
   setup [:create_admin]
 
-  describe "Create invitation" do
-    test "deliver new instructor invitation", %{conn: conn} do
+  describe "`create_bulk` action" do
+    test "for a new instructor: creates new user, enrolls user to the given section, creates invitation token and delivers email invitation",
+         %{conn: conn} do
       expect_recaptcha_http_post()
       stub_real_current_time()
       section = insert(:section)
@@ -27,15 +29,41 @@ defmodule OliWeb.InviteControllerTest do
         )
       )
 
-      assert Accounts.get_user_by(email: @invite_email)
+      # user is created
+      new_user = Accounts.get_user_by(email: @invite_email)
+      assert new_user
 
+      # user is enrolled to the section as an instructor with :pending_confirmation status
+      enrollment =
+        Oli.Delivery.Sections.get_enrollment(section.slug, new_user.id, filter_by_status: false)
+        |> Repo.preload(:context_roles)
+
+      assert enrollment.section_id == section.id
+
+      assert enrollment.status == :pending_confirmation
+
+      assert hd(enrollment.context_roles).uri ==
+               "http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor"
+
+      # invitation user_token is created
+      context = "enrollment_invitation:#{section.slug}"
+
+      assert from(ut in Oli.Accounts.UserToken,
+               where:
+                 ut.user_id == ^new_user.id and ut.context == ^context and
+                   ut.sent_to == @invite_email
+             )
+             |> Repo.one()
+
+      # and email is sent
       assert_email_sent(
         to: @invite_email,
         subject: "You were invited as an instructor to \"#{section.title}\""
       )
     end
 
-    test "deliver new student invitation", %{conn: conn} do
+    test "for a new student: creates new user, enrolls user to the given section, creates invitation token and delivers email invitation",
+         %{conn: conn} do
       expect_recaptcha_http_post()
       stub_real_current_time()
       section = insert(:section)
@@ -50,7 +78,33 @@ defmodule OliWeb.InviteControllerTest do
         )
       )
 
-      assert Accounts.get_user_by(email: @invite_email)
+      # user is created
+      new_user = Accounts.get_user_by(email: @invite_email)
+      assert new_user
+
+      # user is enrolled to the section as a student with :pending_confirmation status
+      enrollment =
+        Oli.Delivery.Sections.get_enrollment(section.slug, new_user.id, filter_by_status: false)
+        |> Repo.preload(:context_roles)
+
+      assert enrollment.section_id == section.id
+
+      assert enrollment.status == :pending_confirmation
+
+      assert hd(enrollment.context_roles).uri ==
+               "http://purl.imsglobal.org/vocab/lis/v2/membership#Learner"
+
+      # invitation user_token is created
+      context = "enrollment_invitation:#{section.slug}"
+
+      assert from(ut in Oli.Accounts.UserToken,
+               where:
+                 ut.user_id == ^new_user.id and ut.context == ^context and
+                   ut.sent_to == @invite_email
+             )
+             |> Repo.one()
+
+      # and email is sent
 
       assert_email_sent(
         to: @invite_email,
@@ -58,11 +112,12 @@ defmodule OliWeb.InviteControllerTest do
       )
     end
 
-    test "deliver existing instructor invitation", %{conn: conn} do
+    test "for an existing instructor: enrolls user to the given section, creates invitation token and delivers email invitation",
+         %{conn: conn} do
       expect_recaptcha_http_post()
       stub_real_current_time()
       section = insert(:section)
-      insert(:user, email: @invite_email)
+      existing_instructor = insert(:user, email: @invite_email)
 
       post(
         conn,
@@ -74,19 +129,43 @@ defmodule OliWeb.InviteControllerTest do
         )
       )
 
-      assert Accounts.get_user_by(email: @invite_email)
+      # user is enrolled to the section as an instructor with :pending_confirmation status
+      enrollment =
+        Oli.Delivery.Sections.get_enrollment(section.slug, existing_instructor.id,
+          filter_by_status: false
+        )
+        |> Repo.preload(:context_roles)
 
+      assert enrollment.section_id == section.id
+
+      assert enrollment.status == :pending_confirmation
+
+      assert hd(enrollment.context_roles).uri ==
+               "http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor"
+
+      # invitation user_token is created
+      context = "enrollment_invitation:#{section.slug}"
+
+      assert from(ut in Oli.Accounts.UserToken,
+               where:
+                 ut.user_id == ^existing_instructor.id and ut.context == ^context and
+                   ut.sent_to == @invite_email
+             )
+             |> Repo.one()
+
+      # and email is sent
       assert_email_sent(
         to: @invite_email,
         subject: "You were invited as an instructor to \"#{section.title}\""
       )
     end
 
-    test "deliver existing student invitation", %{conn: conn} do
+    test "for an existing student: enrolls user to the given section, creates invitation token and delivers email invitation",
+         %{conn: conn} do
       expect_recaptcha_http_post()
       stub_real_current_time()
       section = insert(:section)
-      insert(:user, email: @invite_email)
+      existing_student = insert(:user, email: @invite_email)
 
       post(
         conn,
@@ -98,12 +177,72 @@ defmodule OliWeb.InviteControllerTest do
         )
       )
 
-      assert Accounts.get_user_by(email: @invite_email)
+      # user is enrolled to the section as a student with :pending_confirmation status
+      enrollment =
+        Oli.Delivery.Sections.get_enrollment(section.slug, existing_student.id,
+          filter_by_status: false
+        )
+        |> Repo.preload(:context_roles)
+
+      assert enrollment.section_id == section.id
+
+      assert enrollment.status == :pending_confirmation
+
+      assert hd(enrollment.context_roles).uri ==
+               "http://purl.imsglobal.org/vocab/lis/v2/membership#Learner"
+
+      # invitation user_token is created
+      context = "enrollment_invitation:#{section.slug}"
+
+      assert from(ut in Oli.Accounts.UserToken,
+               where:
+                 ut.user_id == ^existing_student.id and ut.context == ^context and
+                   ut.sent_to == @invite_email
+             )
+             |> Repo.one()
+
+      # and email is sent
 
       assert_email_sent(
         to: @invite_email,
         subject: "You were invited as a student to \"#{section.title}\""
       )
+    end
+  end
+
+  describe "accept_user_invitation action" do
+    setup [:create_section_and_user]
+
+    test "logs in the user and redirects to the section", %{
+      conn: conn,
+      section: section,
+      user: user
+    } do
+      conn =
+        post(
+          conn,
+          ~p"/users/accept_invitation?email=#{user.email}&section_slug=#{section.slug}",
+          %{"user" => %{"password" => "hello world!", "remember_me" => "false"}}
+        )
+
+      assert conn.resp_body ==
+               "<html><body>You are being <a href=\"/sections/#{section.slug}\">redirected</a>.</body></html>"
+    end
+
+    test "does not log in the user and redirects to the section if the provided password is invalid",
+         %{
+           conn: conn,
+           section: section,
+           user: user
+         } do
+      conn =
+        post(
+          conn,
+          ~p"/users/accept_invitation?email=#{user.email}&section_slug=#{section.slug}",
+          %{"user" => %{"password" => "invalid_password", "remember_me" => "false"}}
+        )
+
+      assert conn.private.plug_session["phoenix_flash"]["error"] == "Invalid email or password"
     end
   end
 
@@ -122,5 +261,9 @@ defmodule OliWeb.InviteControllerTest do
       log_in_author(conn, author)
 
     {:ok, conn: conn, author: author}
+  end
+
+  defp create_section_and_user(%{conn: conn}) do
+    {:ok, conn: conn, section: insert(:section), user: user_fixture()}
   end
 end
