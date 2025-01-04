1674e5eb2527681539ea6f8de9c858eb9a6699b8
Darren Siegel <siegel.darren@gmail.com>
2025-01-04 09:50:04 -0500

    db transaction, add lti_institution_id

--- lib/oli_web/controllers/lti_controller.ex
+++ lib/oli_web/controllers/lti_controller.ex
@@ -48,19 +48,29 @@ defmodule OliWeb.LtiController do
 
     case Lti_1p3.Tool.LaunchValidation.validate(params, session_state) do
       {:ok, lti_params} ->
-        # cache user lti params and store the id in the current session
-        case LtiParams.create_or_update_lti_params(lti_params) do
-          {:ok, %{id: lti_params_id}} ->
-            conn = LtiSession.put_session_lti_params(conn, lti_params_id)
 
-            # handle the valid lti launch
-            handle_valid_lti_1p3_launch(conn, lti_params)
+        result = Oli.Repo.transaction(fn ->
 
-          _ ->
-            {_error_id, error_msg} =
-              log_error("An error occurred while creating/updating LTI params")
+          # cache user lti params and store the id in the current session
+          case LtiParams.create_or_update_lti_params(lti_params) do
+            {:ok, %{id: lti_params_id}} ->
+              conn = LtiSession.put_session_lti_params(conn, lti_params_id)
 
-            throw(error_msg)
+              # handle the valid lti launch
+              handle_valid_lti_1p3_launch(conn, lti_params)
+
+            _ ->
+              {_error_id, error_msg} =
+                log_error("An error occurred while creating/updating LTI params")
+
+              throw(error_msg)
+          end
+
+        end)
+
+        case result do
+          {:ok, conn} -> conn
+          e -> e
         end
 
       {:error, %{reason: :invalid_registration, msg: _msg, issuer: issuer, client_id: client_id}} ->
@@ -353,6 +363,7 @@ defmodule OliWeb.LtiController do
   end
 
   defp handle_valid_lti_1p3_launch(conn, lti_params) do
+
     issuer = lti_params["iss"]
     client_id = LtiParams.peek_client_id(lti_params)
     deployment_id = lti_params["https://purl.imsglobal.org/spec/lti/claim/deployment_id"]
@@ -383,7 +394,8 @@ defmodule OliWeb.LtiController do
                  locale: lti_params["locale"],
                  phone_number: lti_params["phone_number"],
                  phone_number_verified: lti_params["phone_number_verified"],
-                 address: lti_params["address"]
+                 address: lti_params["address"],
+                 lti_institution_id: institution.id
                },
                institution.id
              ) do
