51cfd596231ee73c39bad5091b9daba2b941d9b3
Darren Siegel <siegel.darren@gmail.com>
2025-01-05 12:30:35 -0500

    prep 29.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oli.MixProject do
   def project do
     [
       app: :oli,
-      version: "0.29.3",
+      version: "0.29.4",
       elixir: "~> 1.17.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
