de46a5668a0da654ebee236fb00514a3984cb000
Raphael <rgachuhi@users.noreply.github.com>
2025-01-06 14:24:46 -0500

    [FEATURE] [MER-3366] bulk import questions from CSV file into question bank (#5292)
    * added support for csv file bulk question import into bank
    
    * add activity in bulk endpoint
    
    * exposes api for bulk activity creation
    
    * refactored activity editor
    
    * adds alert for file import errors
    
    * Auto format
    
    * cleanup
    
    ---------
    
    Co-authored-by: Raphael Gachuhi <rgachuhi@cmu.edu>

--- assets/src/components/activities/check_all_that_apply/utils.ts
+++ assets/src/components/activities/check_all_that_apply/utils.ts
@@ -1,7 +1,12 @@
 import { CATASchema as CATA } from 'components/activities/check_all_that_apply/schema';
 import {
+  Choice,
+  CreationData,
+  Hint,
+  Part,
   Transform,
   makeChoice,
+  makeFeedback,
   makeHint,
   makePart,
   makeResponse,
@@ -35,6 +40,85 @@ export const defaultCATAModel = (): CATA => {
         ),
       ],
       correct: [[correctChoice.id], correctResponse.id],
+      targeted: [],
+      transformations: [makeTransformation('choices', Transform.shuffle, true)],
+      previewText: '',
+    },
+  };
+};
+
+export const cATAModel = (creationData: CreationData): CATA => {
+  const choices: Map<string, Choice> = new Map();
+  Object.entries(creationData)
+    .filter(([key, value]) => key.startsWith('choice') && value)
+    .map(([key, value]) => {
+      const choice = makeChoice(value.toString());
+      choices.set(key, choice);
+      return choice;
+    });
+
+  if (choices.size === 0) {
+    throw new Error(`No choices provided for ${creationData.title}`);
+  }
+
+  const hints: Hint[] = Object.entries(creationData)
+    .filter(([key, _value]) => key.startsWith('hint'))
+    .map(([_key, value]) => {
+      if (value) {
+        return makeHint(value.toString());
+      }
+      return makeHint('');
+    });
+
+  const correctChoices: string[] = creationData.answer
+    .split(',')
+    .map((choice) => `choice${choice}`);
+
+  if (correctChoices.length === 0) {
+    throw new Error(`No correct choices provided for ${creationData.title}`);
+  }
+
+  const answers: Choice[] = [choices.get('choiceA') as Choice];
+  choices.forEach((value: Choice, key: string) => {
+    if (correctChoices.includes(key)) {
+      answers.push(value);
+    }
+  });
+
+  if (answers.length === 0) {
+    throw new Error(`No correct choices found for ${creationData.title}`);
+  }
+
+  const correctFeedback = creationData.correct_feedback ? creationData.correct_feedback : 'Correct';
+  const incorrectFeedback = creationData.incorrect_feedback
+    ? creationData.incorrect_feedback
+    : 'Incorrect';
+
+  const correctResponse = makeResponse(
+    matchListRule(
+      [...choices.values()].map((choice: Choice) => choice.id),
+      answers.map((choice) => choice.id),
+    ),
+    1,
+    correctFeedback,
+    true,
+  );
+
+  const part: Part = makePart([correctResponse, Responses.catchAll(incorrectFeedback)], hints, '1');
+
+  if (creationData.explanation) {
+    part.explanation = makeFeedback(creationData.explanation);
+  }
+
+  const stem = creationData.stem ? creationData.stem : '';
+
+  return {
+    stem: makeStem(stem),
+    choices: [...choices.values()],
+    authoring: {
+      version: 2,
+      parts: [part],
+      correct: [answers.map((choice) => choice.id), correctResponse.id],
       targeted: [],
       transformations: [makeTransformation('choices', Transform.shuffle, true)],
       previewText: '',
--- lib/oli_web/router.ex
+++ lib/oli_web/router.ex
@@ -532,6 +532,8 @@ defmodule OliWeb.Router do
 
     post("/:project/activity/:activity_type", Api.ActivityController, :create)
 
+    post("/:project/create/activity/bulk", Api.ActivityController, :create_bulk)
+
     put("/test/evaluate", Api.ActivityController, :evaluate)
     put("/test/transform", Api.ActivityController, :transform)
 
