96481c58251e4816babd35584f230980ad1d6796
Darren Siegel <siegel.darren@gmail.com>
2025-01-06 10:19:35 -0500

    refine conflict graded page handling

--- lib/oli_web/live/admin/restore_user_progress.ex
+++ lib/oli_web/live/admin/restore_user_progress.ex
@@ -1,4 +1,5 @@
 defmodule OliWeb.Admin.RestoreUserProgress do
+  alias Oli.Delivery.Attempts.Core.ResourceAttempt
   use OliWeb, :live_view
 
   import Ecto.Query
@@ -14,6 +15,7 @@ defmodule OliWeb.Admin.RestoreUserProgress do
      assign(socket,
        title: "Restore User Progress",
        email: nil,
+       result: "",
        true_user: nil,
        changes: []
      )}
@@ -37,6 +39,17 @@ defmodule OliWeb.Admin.RestoreUserProgress do
         Commit Changes
       </button>
 
+      <div><%= @result %></div>
+
+      <h3>True User</h3>
+      <div><%= if @true_user do @true_user.id else nil end %></div>
+
+      <h3>Changes</h3>
+
+      <%= for change <- @changes do %>
+        <div><%= inspect(change) %></div>
+      <% end %>
+
     </div>
     """
   end
@@ -47,14 +60,18 @@ defmodule OliWeb.Admin.RestoreUserProgress do
 
   def handle_event("preview", _, socket) do
 
-    {true_user, changes} = preview(socket.email)
+    {true_user, changes} = preview(socket.assigns.email)
     {:noreply, assign(socket, true_user: true_user, changes: changes)}
   end
 
   def handle_event("commit", _, socket) do
 
     result = case Repo.transaction(fn -> process(socket.assigns.true_user, socket.assigns.changes) end) do
-      {:ok, :ok} -> "success!"
+      {:ok, items} ->
+        case Enum.all?(items, fn {result, _} -> result == :ok end) do
+          true -> "success!"
+          false -> "failed: #{inspect(items)}"
+        end
       {_, reason} -> "failed: #{inspect(reason)}"
     end
 
@@ -63,15 +80,21 @@ defmodule OliWeb.Admin.RestoreUserProgress do
 
   defp preview(email) do
 
+    # Get all of the LMS users for this email, sorting so the most recently
+    # created user is first. This represents the true user, the user record
+    # created at last launch.
     [true_user | rest] = fetch_users(email)
     all_users = [true_user | rest]
-    enrollments = fetch_enrollments(all_users)
 
+    enrollments = Enum.map(all_users, & &1.id) |> fetch_enrollments()
+
+    # We process the progress restoration on a per section basis, being careful
+    # to only consider the users that are actually enrolled in the section.
     changes = Enum.group_by(enrollments, & &1.section_id)
     |> Enum.map(fn {section_id, enrollments} ->
 
-      # If the true user is not enrolled in the section OR if there is a single enrollment
-      # there is nothing to restore, as this is likely a previous semester section.
+      # If the true user is not enrolled in the section OR if there is only a single enrollment
+      # then there is nothing to restore, as this is likely a previous semester section.
       case Enum.any?(enrollments, fn e -> e.user_id == true_user.id end) and Enum.count(enrollments) > 1 do
         true ->
 
@@ -84,7 +107,7 @@ defmodule OliWeb.Admin.RestoreUserProgress do
             end
           end)
 
-          fetch_resource_accesses(section_id, enrolled_users)
+          fetch_resource_accesses(section_id, Enum.map(enrolled_users, & &1.id))
           |> preview_section_changes(true_user)
 
         false ->
@@ -94,7 +117,6 @@ defmodule OliWeb.Admin.RestoreUserProgress do
     end)
     |> List.flatten()
     |> Enum.filter(& &1 != nil)
-    |> Enum.sort()
 
     {true_user, changes}
 
@@ -111,23 +133,34 @@ defmodule OliWeb.Admin.RestoreUserProgress do
         is_nil(access_for_true) or ra.id != access_for_true.id
       end)
 
+      # Here now is the core logic for progress restoration.  We handle graded and
+      # practice pages differently but both driven from an initial step where we look
+      # to see if the true user has a resource access record for this resource.  For graded
+      # pages, having a resource access can simply mean that they visited the prologue page - but
+      # of course it can also mean that they have completed one or more attempts OR even that
+      # they have their first attempt in progress. For practice pages, having a resource access
+      # simply means that they have viewed the page.
+
       if is_graded do
 
         # Is there a resource access for the true user?
         case access_for_true do
 
-          # No access for the true user, so we need to find the most recently scored access
+          # Case 1: No access for the true user, so we need to find the most recently scored access
           # and make it the true user's access
           nil -> get_most_recently_scored(other_accesses)
 
-          # There is an access for the true user, but there isn't a score.  In this case
-          # we need to find the most recently scored access and make it the true user's access,
-          # but we also need to detach the resource access from the true user.  We indicate
-          # this detachment action by returning a negative id.
-          %{score: nil, id: to_nil} -> [get_most_recently_scored(other_accesses), -to_nil]
+          # Case 2: There is an access for the true user, but there isn't a score.  In this case
+          # we need to check if another access does have a score and if so, we need to shift
+          # the score and all resource attempts to the true user's access record.
+          %{score: nil, id: to_shift_to} ->
+            case get_most_recently_scored(other_accesses) do
+              nil -> nil
+              to_shift_from -> {to_shift_from, to_shift_to}
+            end
 
-          # This must be a case where there is a score on the true user's access
-          # we do nothing
+          # Case 3: This must be a case where there is a score on the true user's access
+          # so we do nothing
           _ -> nil
 
         end
@@ -142,9 +175,10 @@ defmodule OliWeb.Admin.RestoreUserProgress do
         # Is there a resource access for the true user?
         case access_for_true do
 
-          # No access for the true user, so we take the most recent other access
+          # Case 1: No access for the true user, so we take the most recent other access
           nil -> most_recent_other_id
 
+          # Case 2: There is an access for the true user, so we do nothing
           _ -> nil
 
         end
@@ -157,18 +191,38 @@ defmodule OliWeb.Admin.RestoreUserProgress do
 
   defp process(true_user, changes) do
 
-    Enum.reduce(changes, :ok, fn id, _ ->
+    Enum.map(changes, fn change ->
 
-      access_to_edit = Repo.get!(ResourceAccess, abs(id))
+      case change do
 
-      changeset = cond do
-        id < 0 -> ResourceAccess.changeset(access_to_edit, %{user_id: nil})
-        id > 0 -> ResourceAccess.changeset(access_to_edit, %{user_id: true_user.id})
-      end
+        {shift_from, shift_to} ->
 
-      case Repo.update(changeset) do
-        {:ok, _struct} -> {:cont, :ok}
-        {:error, e} -> {:halt, e}
+          # We need to edit the shift_to access score and out_of to match the shift_from
+          access_to_edit = Repo.get!(ResourceAccess, shift_to)
+          access_to_read = Repo.get!(ResourceAccess, shift_from)
+          changeset = ResourceAccess.changeset(access_to_edit, %{score: access_to_read.score, out_of: access_to_read.out_of})
+
+          {:ok, _} = Repo.update(changeset)
+
+          # We also need to shift all resource attempts to the shift_to access record BUT only
+          # if there are no attempts for the shift_to access record
+          if fetch_resource_attempts(shift_to) == [] do
+
+            resource_attempts_from = fetch_resource_attempts(shift_from)
+
+            Enum.map(resource_attempts_from, fn ra ->
+              changeset = ResourceAttempt.changeset(ra, %{resource_access_id: shift_to})
+              {:ok, _} = Repo.update(changeset)
+            end)
+
+          end
+
+        id ->
+
+          access_to_edit = Repo.get!(ResourceAccess, id)
+          changeset = ResourceAccess.changeset(access_to_edit, %{user_id: true_user.id})
+
+          {:ok, _} = Repo.update(changeset)
       end
 
     end)
@@ -188,7 +242,7 @@ defmodule OliWeb.Admin.RestoreUserProgress do
 
   defp fetch_users(email) do
     query = from u in User,
-      where: u.email == ^email and u.independent_user == false,
+      where: u.email == ^email and u.independent_learner == false,
       order_by: [desc: :inserted_at]
 
     Repo.all(query)
@@ -205,6 +259,13 @@ defmodule OliWeb.Admin.RestoreUserProgress do
     query = from ra in ResourceAccess,
       where: ra.section_id == ^section_id and ra.user_id in ^user_ids,
       order_by: [desc: :inserted_at]
+
+    Repo.all(query)
+  end
+
+  defp fetch_resource_attempts(resource_access_id) do
+    query = from ra in ResourceAttempt,
+      where: ra.resource_access_id == ^resource_access_id
 
     Repo.all(query)
   end
