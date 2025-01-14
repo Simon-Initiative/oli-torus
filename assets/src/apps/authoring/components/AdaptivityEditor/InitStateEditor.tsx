df984342fca67468d3481767e19e2e32dd40bff0
Devesh Tiwari <dtiwari@ats360.com>
2025-01-14 17:29:43 +0530

    Update InitStateEditor.tsx

--- assets/src/apps/authoring/components/AdaptivityEditor/InitStateEditor.tsx
+++ assets/src/apps/authoring/components/AdaptivityEditor/InitStateEditor.tsx
@@ -53,7 +53,7 @@ const InitStateItem: React.FC<InitStateItemProps> = ({ state, onChange, onDelete
     onChange(state.id, 'target', val);
   };
 
-  // update adding operator if targetType changes from number
+  //update adding operator if targetType changes from number
   useEffect(() => {
     if (state.type !== CapiVariableTypes.NUMBER) {
       if (state.operator === 'adding') {
