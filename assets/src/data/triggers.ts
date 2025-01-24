c16d4f464f8bd175b4950a3ba46bfc1c26fa368e
Anders Weinstein <andersweinstein@users.noreply.github.com>
2025-01-24 12:52:36 -0500

    [FEATURE] [MER-4040] Initial activity trigger authoring implementation (#5370)
    * initial activity trigger authoring implementation
    
    * delint
    
    * delint
    
    * revert inadvertently included local mod
    
    ---------
    
    Co-authored-by: Anders Weinstein <andersw@cs.cmu.edu>

--- assets/src/data/triggers.ts
+++ assets/src/data/triggers.ts
@@ -39,3 +39,24 @@ export interface GroupTrigger extends IsTrigger {
 export interface ContentBlockTrigger extends IsTrigger {
   trigger_type: 'content';
 }
+
+export const makeTrigger = (trigger_type: string) => {
+  return { type: 'trigger', trigger_type, prompt: '' };
+};
+
+export const makeHintTrigger = (hint_number: number) =>
+  Object.assign(makeTrigger('hint'), { hint_number });
+
+export const makeTargetedTrigger = (response_id: string) =>
+  Object.assign(makeTrigger('targeted_feedback'), { response_id });
+
+export const sameTrigger = (t1: Trigger, t2: Trigger) => {
+  switch (t1.trigger_type) {
+    case 'hint':
+      return t2.trigger_type === 'hint' && t1.hint_number === t2.hint_number;
+    case 'targeted_feedback':
+      return t2.trigger_type === 'targeted_feedback' && t1.response_id === t2.response_id;
+    default:
+      return t1.trigger_type === t2.trigger_type;
+  }
+};
