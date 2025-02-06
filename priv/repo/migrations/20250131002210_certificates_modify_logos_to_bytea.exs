889a7066e00b1b36d478fa4b098c415329ff56d8
Martin Manelli <martin@manelli.org>
2025-02-06 02:40:09 -0300

    fix migration;

--- priv/repo/migrations/20250131002210_certificates_modify_logos_to_bytea.exs
+++ priv/repo/migrations/20250131002210_certificates_modify_logos_to_bytea.exs
@@ -1,11 +1,21 @@
 defmodule Oli.Repo.Migrations.CertificatesModifyLogosToBytea do
   use Ecto.Migration
 
-  def change do
-    alter table(:certificates) do
-      modify :logo1, :bytea
-      modify :logo2, :bytea
-      modify :logo3, :bytea
-    end
+  def up do
+    execute("""
+    ALTER TABLE certificates
+      ALTER COLUMN logo1 TYPE bytea USING logo1::bytea,
+      ALTER COLUMN logo2 TYPE bytea USING logo2::bytea,
+      ALTER COLUMN logo3 TYPE bytea USING logo3::bytea;
+    """)
+  end
+
+  def down do
+    execute("""
+    ALTER TABLE certificates
+      ALTER COLUMN logo1 TYPE text USING NULL,
+      ALTER COLUMN logo2 TYPE text USING NULL,
+      ALTER COLUMN logo3 TYPE text USING NULL;
+    """)
   end
 end
