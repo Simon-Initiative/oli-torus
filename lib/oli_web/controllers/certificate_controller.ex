b260d1d124bc5b0088aa03c51667fedd2c31077c
Martin Manelli <martin@manelli.org>
2025-02-12 21:34:56 -0300

    Display PDF

--- lib/oli_web/controllers/certificate_controller.ex
+++ lib/oli_web/controllers/certificate_controller.ex
@@ -8,7 +8,8 @@ defmodule OliWeb.CertificateController do
   def verify(conn, params) do
     if recaptcha_verified?(params) do
       certificate = GrantedCertificates.get_granted_certificate_by_guid(params["guid"]["value"])
-      render(conn, "show.html", certificate: certificate)
+      certificate_url = s3_certificate_url(certificate)
+      render(conn, "show.html", certificate: certificate, certificate_url: certificate_url)
     else
       render(conn, "index.html", recaptcha_error: "ReCaptcha failed, please try again")
     end
@@ -18,4 +19,9 @@ defmodule OliWeb.CertificateController do
     recaptcha_response = Map.get(params, "g-recaptcha-response", "")
     Oli.Utils.Recaptcha.verify(recaptcha_response) == {:success, true}
   end
+
+  defp s3_certificate_url(%{guid: guid}),
+    do: "https://torus-pdf-certificates.s3.amazonaws.com/certificates/#{guid}.pdf"
+
+  defp s3_certificate_url(_), do: nil
 end
