895ba1fbdbcd888e5dc2eb4d476e67b7e67702f6
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-02-19 14:56:23 -0300

    initial file draft

---
+++ lib/oli/delivery/sections/certificates/email_templates.ex
@@ -0,0 +1,13 @@
+defmodule Oli.Delivery.Sections.Certificates.EmailTemplates do
+  @moduledoc """
+  This module contains the email templates for the certificates email notifications.
+  """
+
+  def student_approval(assigns) do
+    # TODO
+  end
+
+  def student_denial(assigns) do
+    # TODO
+  end
+end
