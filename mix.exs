607527a8c3dc15f6f0bde0bc90e579f119f17f36
Darren Siegel <siegel.darren@gmail.com>
2025-02-22 16:06:27 -0500

    prep HF

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oli.MixProject do
   def project do
     [
       app: :oli,
-      version: "0.29.7",
+      version: "0.29.8",
       elixir: "~> 1.17.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
