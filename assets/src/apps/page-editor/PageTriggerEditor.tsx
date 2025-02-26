8b06ec5b8a50151071f369fb01820d7d63b9a31c
Darren Siegel <siegel.darren@gmail.com>
2025-02-26 13:28:35 -0500

    prettier

--- assets/src/apps/page-editor/PageTriggerEditor.tsx
+++ assets/src/apps/page-editor/PageTriggerEditor.tsx
@@ -5,33 +5,30 @@ export const PageTriggerEditor: React.FC<{
   trigger: PageTrigger | undefined;
   onEdit: (trigger: PageTrigger | undefined) => void;
 }> = React.memo(({ trigger, onEdit }) => {
+  const [promptsExpanded, setPromptsExpanded] = useState<boolean>(false);
 
-const [promptsExpanded, setPromptsExpanded] = useState<boolean>(false);
-
-const ExpandablePromptHelp = () => (
-  <div className={`mt-2 ${promptsExpanded ? 'bg-gray-100 dark:bg-gray-600 rounded-lg' : ''}`}>
-    <button
-      className="bg-slate-300 rounded-lg p-1"
-      onClick={(e) => setPromptsExpanded(!promptsExpanded)}
-    >
-      View examples of helpful prompts&nbsp;&nbsp; {promptsExpanded ? '^' : '\u22C1'}
-    </button>
-    {promptsExpanded && (
-      <ul className="list-disc list-inside py-2 ml-10">
-        <li>&quot;Highlight the most imporant concepts present on this page&quot;</li>
-        <li>
-          &quot;Welcome the student to this page and let them know that you are here to help&quot;
-        </li>
-        <li>
-          &quot;Point students towards more practice regarding the concepts on this page&apos;s learning
-          objectives&quot;
-        </li>
-
-      </ul>
-    )}
-  </div>
-);
-
+  const ExpandablePromptHelp = () => (
+    <div className={`mt-2 ${promptsExpanded ? 'bg-gray-100 dark:bg-gray-600 rounded-lg' : ''}`}>
+      <button
+        className="bg-slate-300 rounded-lg p-1"
+        onClick={(e) => setPromptsExpanded(!promptsExpanded)}
+      >
+        View examples of helpful prompts&nbsp;&nbsp; {promptsExpanded ? '^' : '\u22C1'}
+      </button>
+      {promptsExpanded && (
+        <ul className="list-disc list-inside py-2 ml-10">
+          <li>&quot;Highlight the most imporant concepts present on this page&quot;</li>
+          <li>
+            &quot;Welcome the student to this page and let them know that you are here to help&quot;
+          </li>
+          <li>
+            &quot;Point students towards more practice regarding the concepts on this page&apos;s
+            learning objectives&quot;
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 
   return trigger != undefined ? (
     <div className="mt-3 bg-white dark:bg-gray-600 rounded-lg p-3" contentEditable={false}>
