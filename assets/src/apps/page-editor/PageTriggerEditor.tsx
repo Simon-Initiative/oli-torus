94d9b5d1060b10517983e87149a293a6c96cdfaa
Darren Siegel <siegel.darren@gmail.com>
2025-02-26 13:20:12 -0500

    add dropdown

--- assets/src/apps/page-editor/PageTriggerEditor.tsx
+++ assets/src/apps/page-editor/PageTriggerEditor.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageTrigger } from 'data/triggers';
 
 export const PageTriggerEditor: React.FC<{
   trigger: PageTrigger | undefined;
   onEdit: (trigger: PageTrigger | undefined) => void;
 }> = React.memo(({ trigger, onEdit }) => {
+
+const [promptsExpanded, setPromptsExpanded] = useState<boolean>(false);
+
+const ExpandablePromptHelp = () => (
+  <div className={`mt-2 ${promptsExpanded ? 'bg-gray-100 dark:bg-gray-600 rounded-lg' : ''}`}>
+    <button
+      className="bg-slate-300 rounded-lg p-1"
+      onClick={(e) => setPromptsExpanded(!promptsExpanded)}
+    >
+      View examples of helpful prompts&nbsp;&nbsp; {promptsExpanded ? '^' : '\u22C1'}
+    </button>
+    {promptsExpanded && (
+      <ul className="list-disc list-inside py-2 ml-10">
+        <li>&quot;Highlight the most imporant concepts present on this page&quot;</li>
+        <li>
+          &quot;Welcome the student to this page and let them know that you are here to help&quot;
+        </li>
+        <li>
+          &quot;Point students towards more practice regarding the concepts on this page&apos;s learning
+          objectives&quot;
+        </li>
+
+      </ul>
+    )}
+  </div>
+);
+
+
   return trigger != undefined ? (
     <div className="mt-3 bg-white dark:bg-gray-600 rounded-lg p-3" contentEditable={false}>
       <div className="flex justify-between">
@@ -18,7 +46,7 @@ export const PageTriggerEditor: React.FC<{
       </div>
       <p className="mt-2">
         Customize a prompt for our AI assistant, DOT, to follow based on learner actions within this
-        activity.
+        page.
       </p>
 
       <h6 className="mt-2">
@@ -38,6 +66,8 @@ export const PageTriggerEditor: React.FC<{
         An AI prompt is a question or instruction given to our AI assistant, DOT, to guide its
         response, helping it generate useful feedback, explanations, or support for learners.
       </p>
+
+      <ExpandablePromptHelp />
 
       <textarea
         className="mt-2 grow w-full bg-white rounded-lg p-3 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
