e3d48bd9821a52444e7e515024ad87571c2dea3a
Francisco-Castro <47334502+Francisco-Castro@users.noreply.github.com>
2025-03-05 06:51:17 -0800

    [BUG FIX] [MER-4357] Fix issue preventing threshold checks from triggering on completed jobs (#5462)
    * [MER-4357] Fix issue preventing threshold checks from triggering on completed jobs
    
    * [MER-4357] Adjust uniqueness through the definition of the worker.

--- lib/oli/delivery/sections/certificates/workers/check_certification.ex
+++ lib/oli/delivery/sections/certificates/workers/check_certification.ex
@@ -1,7 +1,15 @@
 defmodule Oli.Delivery.Sections.Certificates.Workers.CheckCertification do
+  @moduledoc """
+  Worker responsible for checking if a user qualifies for a certificate in a specific section.
+  """
   use Oban.Worker,
     queue: :certificate_eligibility,
-    unique: [keys: [:user_id, :section_id]],
+    unique: [
+      keys: [:user_id, :section_id],
+      # Prevents duplicate jobs in these states
+      states: [:available, :scheduled, :retryable],
+      period: :infinity
+    ],
     max_attempts: 1
 
   alias Oli.Delivery.GrantedCertificates
