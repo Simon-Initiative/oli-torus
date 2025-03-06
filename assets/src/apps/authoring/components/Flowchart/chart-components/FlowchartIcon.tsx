7835839cca1fbcf99ff4aa485017b1a2ad035d7d
Devesh Tiwari <dtiwari@ats360.com>
2025-03-06 22:10:43 +0530

    Update FlowchartIcon.tsx

--- assets/src/apps/authoring/components/Flowchart/chart-components/FlowchartIcon.tsx
+++ assets/src/apps/authoring/components/Flowchart/chart-components/FlowchartIcon.tsx
@@ -8,6 +8,7 @@ export const FlowchartIcon: React.FC<{ fill?: string; stroke?: string }> = ({
     <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g
         clipPath="url(#clip0_304_15587)"
+        stroke={stroke}
         strokeWidth={1.5}
         strokeLinecap="round"
         strokeLinejoin="round"
