1426fb5bdd418b46948047c7509243a15a0e530e
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-03-24 14:54:35 -0300

    [BUG FIX] [MER-4436] 500 error when accessing student quizz attempts (#5513)
    * consider case where the feedback map has a 'model' key
    
    * add tests

---
+++ test/oli_web/live/common/utils_test.exs
@@ -0,0 +1,81 @@
+defmodule OliWeb.Common.UtilsTest do
+  use OliWeb.ConnCase
+
+  alias OliWeb.Common.Utils
+
+  import ExUnit.CaptureLog
+
+  describe "extract_feedback_text/1" do
+    test "extracts the feedback text from an attempt and logs an error if it can not be parsed" do
+      activity_attempts = [
+        %{
+          part_attempts: [
+            %{
+              feedback: %{
+                "content" => [
+                  %{
+                    "children" => [%{"text" => "First Feedback"}],
+                    "id" => "7brHHbLfce3qYbdU8rkk23",
+                    "type" => "p"
+                  }
+                ]
+              }
+            },
+            %{
+              feedback: %{
+                "content" => [
+                  %{
+                    "children" => [%{"text" => "Second Feedback"}],
+                    "id" => "7brHHbLfce3qYbdU8rkk23",
+                    "type" => "p"
+                  }
+                ]
+              }
+            },
+            %{
+              feedback: %{
+                "content" => %{
+                  "model" => [
+                    %{
+                      "children" => [%{"text" => "Third Feedback"}],
+                      "id" => "7brHHbLfce3qYbdU8rkk23",
+                      "type" => "p"
+                    }
+                  ]
+                }
+              }
+            },
+            %{
+              feedback: %{
+                "content" => %{
+                  "some_other_case" => [
+                    %{
+                      "children" => [
+                        %{
+                          "text" =>
+                            "This feedback does not match any known case, so a Log error should be triggered"
+                        }
+                      ],
+                      "id" => "7brHHbLfce3qYbdU8rkk23",
+                      "type" => "p"
+                    }
+                  ]
+                }
+              }
+            }
+          ]
+        }
+      ]
+
+      {result, log} =
+        with_log(fn ->
+          Utils.extract_feedback_text(activity_attempts)
+        end)
+
+      assert result == ["First Feedback", "Second Feedback", "Third Feedback"]
+
+      assert log =~
+               "[error] Could not parse feedback text from {\"some_other_case\", [%{\"children\" => [%{\"text\" => \"This feedback does not match any known case, so a Log error should be triggered\"}], \"id\" => \"7brHHbLfce3qYbdU8rkk23\", \"type\" => \"p\"}]}"
+    end
+  end
+end
