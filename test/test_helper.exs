3c6171071742a8fbf2b794ee1960598c38b1acc7
Martin Manelli <martin@manelli.org>
2025-03-27 18:07:13 -0300

    Undo changes to test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,3 @@
 Application.ensure_all_started(:ex_machina)
-
-ExUnit.start(
-  exclude: [:skip],
-  failures_manifest_path: "/Users/martin/work/oli-torus/failures_manifest.txt"
-)
-
+ExUnit.start(exclude: [:skip])
 Ecto.Adapters.SQL.Sandbox.mode(Oli.Repo, :manual)
