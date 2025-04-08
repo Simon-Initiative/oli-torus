52f21d1deae1911e5ba7eade9524f3cbba938255
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-04-08 17:47:12 -0300

    [BUG FIX] [MER-4035] Add support link to lesson pages - part 2 (#5537)
    * set support button width
    
    * improve mobile layout
    
    * consider button on sidebar case
    
    * improve responsiveness

--- lib/oli_web/components/delivery/layouts.ex
+++ lib/oli_web/components/delivery/layouts.ex
@@ -781,7 +781,7 @@ defmodule OliWeb.Components.Delivery.Layouts do
       id={@id}
       onclick="window.showHelpModal();"
       class={[
-        "w-full h-11 px-3 py-3 flex-col justify-center items-start inline-flex text-black/70 hover:text-black/90 dark:text-gray-400 hover:dark:text-white stroke-black/70 hover:stroke-black/90 dark:stroke-[#B8B4BF] hover:dark:stroke-white",
+        "w-auto mr-auto h-11 px-3 py-3 flex-col justify-center items-start inline-flex text-black/70 hover:text-black/90 dark:text-gray-400 hover:dark:text-white stroke-black/70 hover:stroke-black/90 dark:stroke-[#B8B4BF] hover:dark:stroke-white",
         @class
       ]}
     >
@@ -789,7 +789,9 @@ defmodule OliWeb.Components.Delivery.Layouts do
         <div class="w-5 h-5 flex items-center justify-center">
           <Icons.support class="" />
         </div>
-        <div :if={@sidebar_expanded} class="text-sm font-medium tracking-tight">Support</div>
+        <div :if={@sidebar_expanded} class="text-sm font-medium tracking-tight">
+          Support
+        </div>
       </div>
     </button>
     """
