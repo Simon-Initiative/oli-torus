f30f3ad40e33ae6a0b91aa0076b27365f3763a22
Devesh Tiwari <dtiwari@ats360.com>
2025-04-18 10:16:16 +0530

    Delete 20250416_rename_slider_part_component.exs

--- priv/repo/migrations/20250416_rename_slider_part_component.exs
+++
@@ -1,7 +0,0 @@
-defmodule Oli.Repo.Migrations.DropPublicationScope do
-  use Ecto.Migration
-
-  execute("""
-  update part_component_registrations set title='Slider (Numeric)' where slug = 'janus_slider'  and delivery_element ='janus-slider'
-  """)
-end
