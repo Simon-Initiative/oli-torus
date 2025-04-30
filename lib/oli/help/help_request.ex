e0b4593fe309d8316eda7aa074d17f1f7d909a7c
Francisco-Castro <catrapato@hotmail.com>
2025-04-30 15:34:47 -0400

    [MER-4034] Remove unnecessary code

--- lib/oli/help/help_request.ex
+++ lib/oli/help/help_request.ex
@@ -22,23 +22,3 @@ defmodule Oli.Help.HelpRequest do
     |> validate_inclusion(:subject, Oli.Help.HelpContent.list_subjects() |> Map.keys())
   end
 end
-
-# |> validate_required([:subject, :message, :location, :cookies_enabled])
-# |> cast(attrs, [:subject, :message])
-# |> cast(attrs, [:subject, :message, :current_location, :cookies_enabled, :captcha])
-# field :issuer, :string
-# field :email, :string
-# field :course_section, :string
-# field :current_location, :string
-# user_account_link
-# role
-# user_agent_string
-# browser_name
-# browser_version
-# operating_system
-# device_type, :mobile, :tablet, :desktop
-# screen_size
-# ip_address
-# browser_plugins
-# field :cookies_enabled, :boolean
-# js_enabled
