d46c84d2fbff31dd5baefcdc4523a6d02b95bedc
Gastón Abellá <gaston@wyeworks.com>
2025-05-13 15:21:04 -0300

    [MER-4294] Improve transitions and animations on toggle switch

--- lib/oli_web/components/common.ex
+++ lib/oli_web/components/common.ex
@@ -1144,7 +1144,13 @@ defmodule OliWeb.Components.Common do
             phx-hook={if @with_confirmation, do: "ConditionalToggle"}
             data-checked={"#{@checked}"}
           />
-          <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary">
+          <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
+                      peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full
+                      peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
+                      peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px]
+                      after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5
+                      after:w-5 after:transition-transform dark:border-gray-600 peer-checked:bg-primary
+                      after:duration-300 after:ease-in-out transition-colors duration-300 ease-in-out">
           </div>
           <%= case @label do %>
             <% nil -> %>
