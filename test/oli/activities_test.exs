6984dd1fd15dae059a92d6deced569f3492a29be
Santiago Simoncelli <simoncellisantiago@gmail.com>
2025-05-13 10:42:24 -0300

    [FEATURE] [MER-4308] LTI content type button (#5599)
    * [MER-4308] Add deployment_id field to ActivityRegistration and update related functions
    
    * [MER-4308] Update AddActivity to provide specific tooltip for LTI activities
    
    * [MER-4308] Remove deprecated test

--- test/oli/activities_test.exs
+++ test/oli/activities_test.exs
@@ -2,7 +2,6 @@ defmodule Oli.ActivitiesTest do
   use Oli.DataCase
   import ExUnit.Assertions
   alias Oli.Activities
-  alias Oli.Lti.PlatformExternalTools
 
   setup [:author_project_fixture]
 
@@ -71,41 +70,6 @@ defmodule Oli.ActivitiesTest do
 
       image_coding = Map.get(editor_menu_items, "oli_image_coding")
       assert !image_coding.globallyAvailable and image_coding.enabledForProject
-    end
-  end
-
-  describe "list activities" do
-    test "list_activity_registrations_with_deployment_id/0 works correctly" do
-      # register a new lti external tool activity
-      params = %{
-        "name" => "External Tool Test 1",
-        "description" => "External Tool Description",
-        "client_id" => "new_tool_client_id_1",
-        "target_link_uri" => "https://example.com/launch",
-        "login_url" => "https://example.com/login",
-        "keyset_url" => "https://example.com/jwks",
-        "redirect_uris" => "https://example.com/redirect",
-        "custom_params" => "param1=value1&param2=value2"
-      }
-
-      {:ok, {_platform_instance, activity_registration, deployment}} =
-        PlatformExternalTools.register_lti_external_tool_activity(params)
-
-      # get activity registrations with deployment id
-      activities = Activities.list_activity_registrations_with_deployment_id()
-
-      # assert that all activities have the :deployment_id field
-      assert Enum.all?(activities, fn activity ->
-               Map.has_key?(activity, :deployment_id)
-             end)
-
-      # assert that the lti external tool activity is included in the list with their deployment id
-      new_activity_registered =
-        Enum.find(activities, fn activity ->
-          activity.id == activity_registration.id
-        end)
-
-      assert new_activity_registered.deployment_id == deployment.deployment_id
     end
   end
 end
