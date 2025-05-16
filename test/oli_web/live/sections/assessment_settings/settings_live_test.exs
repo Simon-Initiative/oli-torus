9f36b2f7e3554088fab44ddf66fab0ec0e37577c
Raphael Gachuhi <rgachuhi@cmu.edu>
2025-05-16 10:45:18 -0400

    updates testcase

--- test/oli_web/live/sections/assessment_settings/settings_live_test.exs
+++ test/oli_web/live/sections/assessment_settings/settings_live_test.exs
@@ -638,7 +638,7 @@ defmodule OliWeb.Sections.AssessmentSettings.SettingsLiveTest do
       assert assessment_4 == page_4.title
 
       assert html =~
-               ~s(<a href="/sections/#{section.slug}/manage">Manage Section</a>)
+               ~s(<a href="/sections/#{section.slug}/manage">#{section.title}</a>)
     end
   end
 
@@ -671,7 +671,7 @@ defmodule OliWeb.Sections.AssessmentSettings.SettingsLiveTest do
       assert assessment_4 == page_4.title
 
       assert html =~
-               ~s(<a href="/sections/#{section.slug}/manage">Manage Section</a>)
+               ~s(<a href="/sections/#{section.slug}/manage">#{section.title}</a>)
     end
 
     test "student_exceptions view loads correctly", %{
