92ab2578ac49848c974152fa0de0d3d22dbe98cd
Eli Knebel <eliknebel@cmu.edu>
2025-06-23 20:12:34 -0400

    remove commented code

--- lib/oli_web/plugs/lti_ags_token_validator.ex
+++ lib/oli_web/plugs/lti_ags_token_validator.ex
@@ -3,26 +3,6 @@ defmodule OliWeb.Plugs.LtiAgsTokenValidator do
 
   alias Oli.Lti.Tokens
 
-  require Logger
-
-  # def init(opts), do: opts
-
-  # def call(conn, _opts) do
-  #   # Extract Bearer token from Authorization header
-  #   case get_req_header(conn, "authorization") do
-  #     ["Bearer " <> token] ->
-  #       # TODO: Validate the JWT access token (should match what /lti/auth/token issues)
-  #       # If valid, assign claims to conn; if not, halt with 401
-  #       # For now, just allow all requests (placeholder)
-  #       conn
-
-  #     _ ->
-  #       conn
-  #       |> send_resp(401, "Unauthorized: Missing or invalid access token")
-  #       |> halt()
-  #   end
-  # end
-
   def init(opts), do: opts
 
   def call(conn, _opts) do
@@ -32,7 +12,7 @@ defmodule OliWeb.Plugs.LtiAgsTokenValidator do
     else
       _ ->
         conn
-        |> send_resp(401, "Unauthorized")
+        |> send_resp(401, "Unauthorized: Missing or invalid access token")
         |> halt()
     end
   end
