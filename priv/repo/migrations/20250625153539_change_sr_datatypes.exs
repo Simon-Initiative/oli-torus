be2fd4ecfb762970baffaaa0376b0097779ba13f
Darren Siegel <siegel.darren@gmail.com>
2025-06-25 12:38:25 -0400

    [BUG FIX] [MER-4724] Fix data types of section resource fields (#5699)
    * migration to change data type
    
    * add unit test

---
+++ priv/repo/migrations/20250625153539_change_sr_datatypes.exs
@@ -0,0 +1,9 @@
+defmodule Oli.Repo.Migrations.ChangeSrDatatypes do
+  use Ecto.Migration
+
+  def change do
+    execute("ALTER TABLE section_resources ALTER COLUMN title type text")
+    execute("ALTER TABLE section_resources ALTER COLUMN poster_image type text")
+    execute("ALTER TABLE section_resources ALTER COLUMN intro_video type text")
+  end
+end
