dd9a68b8be17290676dea577fcac1ba6b91acbee
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-07-17 11:14:13 -0300

    add  to ActivityRegistrationProjectActivity

--- lib/oli/activities/activity_registration_project.ex
+++ lib/oli/activities/activity_registration_project.ex
@@ -1,4 +1,10 @@
 defmodule Oli.Activities.ActivityRegistrationProject do
+  @moduledoc """
+  This schema is used to join activity registrations to projects.
+  Activities listed here were added by an author to the project.
+  Once added, authors can enable or disable activities at the project level.
+  """
+
   use Ecto.Schema
   import Ecto.Changeset
 
@@ -7,13 +13,15 @@ defmodule Oli.Activities.ActivityRegistrationProject do
     timestamps(type: :utc_datetime)
     field :activity_registration_id, :integer, primary_key: true
     field :project_id, :integer, primary_key: true
+    field :status, Ecto.Enum, values: [:enabled, :disabled], default: :enabled
   end
 
   @doc false
   def changeset(author_project, attrs \\ %{}) do
     author_project
-    |> cast(attrs, [:activity_registration_id, :project_id])
-    |> validate_required([:activity_registration_id, :project_id])
+    |> cast(attrs, [:activity_registration_id, :project_id, :status])
+    |> validate_required([:activity_registration_id, :project_id, :status])
     |> unique_constraint(:activity_registration_id, name: :index_activity_registration_project)
+    |> validate_inclusion(:status, Ecto.Enum.values(__MODULE__, :status))
   end
 end
