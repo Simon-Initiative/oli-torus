a42535389c3857320a3dddb1316c3590ed9d7a10
Devesh Tiwari <dtiwari@ats360.com>
2025-08-12 09:30:16 +0530

    updating changes based on PR

--- priv/repo/migrations/20250811145546_updateCapiPartRegistrations.exs
+++ priv/repo/migrations/20250811145546_updateCapiPartRegistrations.exs
@@ -1,11 +1,24 @@
 defmodule Oli.Repo.Migrations.UpdateCapiPartRegistrations do
   use Ecto.Migration
 
-  def change do
-    execute "update part_component_registrations
-    set title='Iframe',description='A webpage displayed in an iframe, or a simulation,
-    virtual tour, or component that uses CAPI.'
-    where slug='janus_capi_iframe' and delivery_element='janus-capi-iframe'"
-    flush()
+  def up do
+    execute """
+    UPDATE part_component_registrations
+    SET title = 'Iframe',
+        description = 'A webpage displayed in an iframe, or a simulation, virtual tour, or component that uses CAPI.'
+    WHERE slug = 'janus_capi_iframe'
+      AND delivery_element = 'janus-capi-iframe'
+    """
+  end
+
+  def down do
+    # Replace with actual original values when known
+    execute """
+    UPDATE part_component_registrations
+    SET title = 'CAPI Simulation / VFT / Component',
+        description = 'An advanced Simulation, VFT (Virtual Field Trip), or Component that communicates dynamically through the CAPI interface.'
+    WHERE slug = 'janus_capi_iframe'
+      AND delivery_element = 'janus-capi-iframe'
+    """
   end
 end
