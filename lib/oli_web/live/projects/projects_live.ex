bb229db289b3fa3fba932468f554d3e709e975ba
darrensiegel <darrensiegel@users.noreply.github.com>
2025-08-21 14:27:53 +0000

    Auto format

--- lib/oli_web/live/projects/projects_live.ex
+++ lib/oli_web/live/projects/projects_live.ex
@@ -156,7 +156,6 @@ defmodule OliWeb.Projects.ProjectsLive do
             <i class="fa fa-plus pr-2"></i> New Project
           </button>
         </div>
-
       </div>
       <div class="projects-title-row px-4 mt-2">
         <div class="flex justify-between items-baseline">
@@ -188,7 +187,6 @@ defmodule OliWeb.Projects.ProjectsLive do
           <div class="flex-grow-1"></div>
         </div>
       </div>
-
 
       <div class="flex justify-between">
         <div class="flex w-fit gap-4 p-2 pr-8 mx-4 mt-3 mb-2 shadow-[0px_2px_6.099999904632568px_0px_rgba(0,0,0,0.10)] border border-[#ced1d9] dark:border-[#3B3740] dark:bg-[#000000]">
