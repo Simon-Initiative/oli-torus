92ce2bb4ce73f45423cbff5de29a7a5e3127e652
manelli <manelli@users.noreply.github.com>
2025-08-28 23:54:06 +0000

    Auto format

--- assets/src/hooks/copy_to_clipboard_event.ts
+++ assets/src/hooks/copy_to_clipboard_event.ts
@@ -1,11 +1,14 @@
 export const CopyToClipboardEvent = {
   mounted() {
     this.handleEvent('copy_to_clipboard', ({ text }) => {
-      navigator.clipboard.writeText(text).then(() => {
-        console.log('Text copied to clipboard:', text);
-      }).catch(err => {
-        console.error('Failed to copy text to clipboard:', err);
-      });
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          console.log('Text copied to clipboard:', text);
+        })
+        .catch((err) => {
+          console.error('Failed to copy text to clipboard:', err);
+        });
     });
   },
 };
