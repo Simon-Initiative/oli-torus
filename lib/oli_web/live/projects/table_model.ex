6b215c9685011c5920d8c94aa5f872e9c3b38259
Santiago Simoncelli <simoncellisantiago@gmail.com>
2025-08-29 11:41:57 -0300

    [MER-4667] Change projects table default sorting

--- lib/oli_web/live/projects/table_model.ex
+++ lib/oli_web/live/projects/table_model.ex
@@ -5,43 +5,60 @@ defmodule OliWeb.Projects.TableModel do
   alias OliWeb.Common.{Chip, FormatDateTime, SessionContext}
   alias OliWeb.Common.Table.{ColumnSpec, SortableTableModel}
 
-  def new(%SessionContext{} = ctx, sections) do
+  def new(%SessionContext{} = ctx, sections, opts \\ []) do
+    default_td_class = "!border-r border-Table-table-border"
+    default_th_class = "!border-r border-Table-table-border"
+
     column_specs = [
       %ColumnSpec{
         name: :title,
         label: "Title",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :tags,
         label: "Tags",
         render_fn: &custom_render/3,
-        sortable: false
+        sortable: false,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :inserted_at,
         label: "Created",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :name,
         label: "Created By",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :collaborators,
         label: "Collaborators",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :published,
         label: "Published",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :visibility,
         label: "Visibility",
-        render_fn: &custom_render/3
+        render_fn: &custom_render/3,
+        td_class: default_td_class,
+        th_class: default_th_class
       },
       %ColumnSpec{
         name: :status,
@@ -50,11 +67,19 @@ defmodule OliWeb.Projects.TableModel do
       }
     ]
 
+    sort_by = Keyword.get(opts, :sort_by_spec, :inserted_at)
+    sort_order = Keyword.get(opts, :sort_order, :asc)
+
+    sort_by_spec =
+      Enum.find(column_specs, fn spec -> spec.name == sort_by end)
+
     SortableTableModel.new(
       rows: sections,
       column_specs: column_specs,
       event_suffix: "",
       id_field: [:id],
+      sort_by_spec: sort_by_spec,
+      sort_order: sort_order,
       data: %{
         ctx: ctx
       }
