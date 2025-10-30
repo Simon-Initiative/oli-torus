49a3f761baa535d5542a1923d51bc3f8769a380b
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-10-30 10:00:45 -0300

    minor adjustment for mobile

--- lib/oli_web/components/footer.ex
+++ lib/oli_web/components/footer.ex
@@ -4,7 +4,7 @@ defmodule OliWeb.Components.Footer do
 
   def delivery_footer(assigns) do
     ~H"""
-    <footer class="w-full py-4 md:container md:mx-auto lg:px-10 text-xs bg-delivery-footer dark:bg-delivery-footer-dark">
+    <footer class="w-full py-4 md:container lg:px-10 text-xs bg-delivery-footer dark:bg-delivery-footer-dark">
       <div class="flex flex-col">
         <div class="flex flex-row gap-2">
           <.cookie_preferences />
@@ -114,7 +114,7 @@ defmodule OliWeb.Components.Footer do
       <a
         href="javascript:;"
         onclick={"OLI.selectCookiePreferences({privacyPoliciesUrl: '#{@privacy_policies_url}'})"}
-        class={@class}
+        class={[@class, "whitespace-nowrap"]}
       >
         Cookie Preferences
       </a>
