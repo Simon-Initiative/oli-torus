85a95ede348c81cc7f00179618003c10723842da
Anders Weinstein <andersw@andrew.cmu.edu>
2025-12-10 12:52:01 -0500

    fix typo

--- assets/src/components/common/SelectTimezone.tsx
+++ assets/src/components/common/SelectTimezone.tsx
@@ -55,7 +55,7 @@ export const SelectTimezone: React.FC<SelectTimezoneProps> = ({
         name="timezone[timezone]"
         className="max-w-[300px] dark:text-white text-sm font-normal font-['Roboto'] rounded-md border-gray-300 w-full disabled:bg-gray-100 disabled:text-gray-600 dark:bg-delivery-body-dark dark:border-gray-700"
         value={selectedTimezone}
-        aria-lablledBy={labelId}
+        aria-labelledby={labelId}
       >
         {timezones.map(([label, value]) => (
           <option key={value} value={value}>
