1521f1a7cb4ab7de583f8e7f29a99c9c738d646e
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2025-12-11 15:44:31 -0300

    [FEATURE] [MER-4471] Learn page expanded outline and gallery view - Part 1 (#6092)
    * device detection on live session plug
    
    * placeholder view for expanding a module on gallery view
    
    * update tidewave
    
    * remove unnecessary ToggleReadMore hook
    
    * mobile module layer for gallery view v1 - refactor to share as much components with desktop as possible
    
    * gallery view: ensure module layer is always mounted at the top scroll position
    
    * mobile - gallery view: do not indent nested resources
    
    * mobile > gallery view > module layer: implement figma design for listed pages
    
    * mobile > learn page: implement figma designs for gallery rows
    
    * fix Read more button for intro content
    
    * restore Popover hook that was erased by accident
    
    * add is_mobile flag to outline view
    
    * AI review: update device detection to consider android tablets + replace get_connect_info with case logic
    
    * AI review: consider not moounted socket case on set_device_type
    
    * add tests
    
    * AI review: add aria labels
    
    * mobile view can not be tested correctly
    
    * adjust spacing in unit/page labels and due dates

--- assets/src/hooks/index.ts
+++ assets/src/hooks/index.ts
@@ -37,9 +37,11 @@ import { FirePageTrigger } from './page_trigger';
 import { PointMarkers } from './point_markers';
 import { ProjectsTypeahead } from './projects_typeahead';
 import { ReactToLiveView } from './react_to_liveview';
+import { ReadMoreToggle } from './read_more_toggle';
 import { Recaptcha } from './recaptcha';
 import { ResizeListener } from './resize_listener';
 import { ReviewActivity } from './review_activity';
+import { ScrollToTheTop } from './scroll_to_the_top';
 import { Scroller } from './scroller';
 import { SelectListener } from './select_listener';
 import { ShowTeaser } from './show_teaser';
@@ -53,7 +55,6 @@ import { TagsComponent } from './tags_component';
 import { TextInputListener } from './text_input_listener';
 import { TextareaListener } from './textarea_listener';
 import { ThemeToggle } from './theme_toggle';
-import { ToggleReadMore } from './toggle_read_more';
 import { AutoHideTooltip, Popover, TooltipInit, TooltipWithTarget } from './tooltip';
 import { VideoPlayer } from './video_player';
 import { PauseOthersOnSelected, VideoPreview } from './video_preview';
@@ -92,7 +93,6 @@ export const Hooks = {
   LtiConnectInstructions,
   HierarchySelector,
   TextareaListener,
-  ToggleReadMore,
   LiveReact,
   SubmitForm,
   LoadSurveyScripts,
@@ -125,5 +125,7 @@ export const Hooks = {
   SyncChevronState,
   ConditionalToggle,
   CopyToClipboard,
+  ReadMoreToggle,
   TagsComponent,
+  ScrollToTheTop,
 };
