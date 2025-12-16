6afb1a27eebaf85aca5ac74356bc77600aaa165c
Anders Weinstein <andersw@andrew.cmu.edu>
2025-12-16 17:57:06 -0500

    remove unneeded aria-label (button has visible text)

--- assets/src/components/common/TechSupportButton.tsx
+++ assets/src/components/common/TechSupportButton.tsx
@@ -16,7 +16,6 @@ export const TechSupportButton = () => (
               active:text-delivery-primary-600
               active:hover:text-delivery-primary-600
             "
-    aria-label="support"
     onClick={() => (window as any).showHelpModal()}
   >
     Tech Support
