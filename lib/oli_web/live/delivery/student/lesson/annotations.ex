8b6197b6fd5d987ce3992624bc682418a0c253c6
Anders Weinstein <andersw@andrew.cmu.edu>
2025-12-17 12:26:52 -0500

    correct notes like button aria-pressed condition

--- lib/oli_web/live/delivery/student/lesson/annotations.ex
+++ lib/oli_web/live/delivery/student/lesson/annotations.ex
@@ -573,7 +573,7 @@ defmodule OliWeb.Delivery.Student.Lesson.Annotations do
             class="inline-flex gap-1 text-sm text-gray-500 bold py-1 px-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
             role="reactions"
             aria-label="thumbs up"
-            aria-pressed={to_string(get_in(@post, [:reaction_summaries, :like, :reacted]) || false)}
+            aria-pressed={to_string(get_in(@post.reaction_summaries, [:like, :reacted]) || false)}
             phx-click={@on_toggle_reaction}
             phx-value-reaction={:like}
             phx-value-post-id={assigns.post.id}
