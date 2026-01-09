7e580b398f171e279779a57a62597b6b8059da0d
Anders Weinstein <andersw@andrew.cmu.edu>
2026-01-09 10:44:07 -0500

    add header_level param to modal component

--- lib/oli_web/components/modal.ex
+++ lib/oli_web/components/modal.ex
@@ -32,6 +32,7 @@ defmodule OliWeb.Components.Modal do
   attr :header_class, :string, default: "flex items-start justify-between p-4"
   attr :body_class, :string, default: "p-6 space-y-6"
   attr :confirm_class, :string, default: "py-2 px-3"
+  attr :header_level, :integer, default: 1
 
   attr :cancel_class, :string,
     default: "bg-transparent text-blue-500 hover:underline hover:bg-transparent"
@@ -86,12 +87,12 @@ defmodule OliWeb.Components.Modal do
               <div class={@header_class}>
                 <div>
                   <div :if={@title != []}>
-                    <h1
-                      id={"#{@id}-title"}
-                      class="text-xl font-semibold text-gray-900 dark:text-white"
-                    >
-                      {render_slot(@title)}
-                    </h1>
+                    <%= Phoenix.HTML.Tag.content_tag(
+                      :"h#{@header_level}",
+                      render_slot(@title),
+                      id: "#{@id}-title",
+                      class: "text-xl font-semibold text-gray-900 dark:text-white"
+                    ) %>
                     <p
                       :if={@subtitle != []}
                       id={"#{@id}-description"}
@@ -216,7 +217,7 @@ defmodule OliWeb.Components.Modal do
                   <span class="hover:underline">Back</span>
                 </button>
               </div>
-              
+
     <!-- Modal header -->
               <div class="flex items-start justify-between px-4 sm:px-0">
                 <div :if={@title != []} class="mb-6 lg:mb-11 w-full">
