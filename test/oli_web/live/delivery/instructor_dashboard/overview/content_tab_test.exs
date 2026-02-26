ff8be8fb1cb5eaf271f5adc56e4310dc40aafc05
Santiago Simoncelli <simoncellisantiago@gmail.com>
2026-02-26 09:23:58 -0300

    [MER-4841] Add test

--- test/oli_web/live/delivery/instructor_dashboard/overview/content_tab_test.exs
+++ test/oli_web/live/delivery/instructor_dashboard/overview/content_tab_test.exs
@@ -4,6 +4,8 @@ defmodule OliWeb.Delivery.InstructorDashboard.Overview.Content do
 
   import Phoenix.LiveViewTest
 
+  alias Oli.Delivery.Sections
+
   defp instructor_course_content_path(section_slug) do
     Routes.live_path(
       OliWeb.Endpoint,
@@ -15,10 +17,12 @@ defmodule OliWeb.Delivery.InstructorDashboard.Overview.Content do
   end
 
   defp section_with_larger_hierarchy(%{instructor: instructor} = _conn) do
-    %{section: section, mod1_pages: mod1_pages} = Oli.Seeder.base_project_with_larger_hierarchy()
+    %{section: section, mod1_pages: mod1_pages, unit1_resource: unit1_resource} =
+      Oli.Seeder.base_project_with_larger_hierarchy()
+
     enroll_user_to_section(instructor, section, :context_instructor)
 
-    %{section: section, mod1_pages: mod1_pages}
+    %{section: section, mod1_pages: mod1_pages, unit1_resource: unit1_resource}
   end
 
   describe "Instructor dashboard overview - course tab" do
@@ -48,6 +52,30 @@ defmodule OliWeb.Delivery.InstructorDashboard.Overview.Content do
              )
 
       assert has_element?(view, "a", "Open as instructor")
+    end
+
+    test "shows updated section title for a unit in course content", %{
+      conn: conn,
+      section: section,
+      unit1_resource: unit1_resource
+    } do
+      unit_1_section_resource = Sections.get_section_resource(section.id, unit1_resource.id)
+      updated_title = "Updated Unit 1 Title"
+
+      assert {:ok, _updated_sr} =
+               Sections.update_section_resource(unit_1_section_resource, %{title: updated_title})
+
+      {:ok, view, _html} = live(conn, instructor_course_content_path(section.slug))
+
+      view
+      |> element("button[phx-click='next_node']")
+      |> render_click()
+
+      view
+      |> element("button[phx-click='next_node']")
+      |> render_click()
+
+      assert has_element?(view, "h4", updated_title)
     end
   end
 end
