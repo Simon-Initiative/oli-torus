c23c3eaedb7220ab7610227d752ddc23063c6f90
Raphael Gachuhi <raph@mac.mynetworksettings.com>
2026-03-31 11:17:57 -0400

    fix docker file

--- scripts/dev/ha-proxy/docker-entrypoint-regular.sh
+++ scripts/dev/ha-proxy/docker-entrypoint-regular.sh
@@ -7,12 +7,13 @@ set -eu
 TORUS_BACKEND_HOST="${TORUS_BACKEND_HOST:-host.docker.internal}"
 TORUS_BACKEND_PORT="${TORUS_BACKEND_PORT:-${HTTP_PORT:-8080}}"
 MEDIA_ORIGIN_PORT="${MEDIA_ORIGIN_PORT:-80}"
+RENDERED_CONFIG="${RENDERED_CONFIG:-/tmp/haproxy.cfg}"
 
 sed \
   -e "s/__TORUS_BACKEND_HOST__/${TORUS_BACKEND_HOST}/g" \
   -e "s/__TORUS_BACKEND_PORT__/${TORUS_BACKEND_PORT}/g" \
   -e "s/__MEDIA_ORIGIN_HOST__/${MEDIA_ORIGIN_HOST}/g" \
   -e "s/__MEDIA_ORIGIN_PORT__/${MEDIA_ORIGIN_PORT}/g" \
-  /usr/local/etc/haproxy/haproxy.cfg.template >/usr/local/etc/haproxy/haproxy.cfg
+  /usr/local/etc/haproxy/haproxy.cfg.template >"$RENDERED_CONFIG"
 
-exec haproxy -W -db -f /usr/local/etc/haproxy/haproxy.cfg
+exec haproxy -W -db -f "$RENDERED_CONFIG"
