3b6d0c18ff8bb400e9437f8adb1e2d2cfcfb3ad4
andersweinstein <andersweinstein@users.noreply.github.com>
2026-04-07 15:33:58 +0000

    Auto format

--- test/oli_web/views/page_delivery_view_test.exs
+++ test/oli_web/views/page_delivery_view_test.exs
@@ -10,13 +10,15 @@ defmodule OliWeb.PageDeliveryViewTest do
       prev_html =
         render_component(&PageDeliveryView.prev_link/1, %{
           to: "/previous",
-          title: "A very long page title that should truncate instead of pushing the arrow off screen"
+          title:
+            "A very long page title that should truncate instead of pushing the arrow off screen"
         })
 
       next_html =
         render_component(&PageDeliveryView.next_link/1, %{
           to: "/next",
-          title: "Another very long page title that should truncate instead of pushing the arrow off screen"
+          title:
+            "Another very long page title that should truncate instead of pushing the arrow off screen"
         })
 
       assert prev_html =~ ~s(class="flex flex-col text-right overflow-hidden flex-1 min-w-0")
