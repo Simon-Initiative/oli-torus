20c25798b9694ef950a57630e708847e969493ac
gastonabella <gastonabella@users.noreply.github.com>
2026-05-29 19:29:34 +0000

    Auto format

--- assets/src/components/parts/janus-video/Video.tsx
+++ assets/src/components/parts/janus-video/Video.tsx
@@ -268,8 +268,8 @@ const Video: React.FC<PartComponentProps<VideoModel>> = (props) => {
     Array.isArray(subtitles)
       ? subtitles
       : subtitles && typeof subtitles === 'object'
-        ? [subtitles]
-        : []
+      ? [subtitles]
+      : []
   )
     .filter((s: any) => s?.src)
     .map((s: any) => ({
