64b81b1fc11d31fce76d914f1b389ded9b3e62c9
Darren Siegel <darren@Darrens-MacBook-Pro.local>
2026-06-05 09:23:05 -0400

    remove tests

--- .github/actions/amazon-linux-builder/entrypoint.sh
+++ .github/actions/amazon-linux-builder/entrypoint.sh
@@ -23,12 +23,3 @@ cd gleam && gleam build --target erlang && cd ..
 MIX_ENV=prod SHA=$RELEASE_SHA mix compile
 MIX_ENV=prod mix assets.deploy
 MIX_ENV=prod SHA=$RELEASE_SHA mix release
-DATABASE_URL=ecto://postgres:postgres@localhost/oli \
-  SECRET_KEY_BASE=0000000000000000000000000000000000000000000000000000000000000000 \
-  LIVE_VIEW_SALT=00000000000000000000000000000000 \
-  HOST=localhost \
-  S3_MEDIA_BUCKET_NAME=torus-media \
-  S3_XAPI_BUCKET_NAME=torus-xapi \
-  MEDIA_URL=http://localhost/torus-media \
-  CLOAK_VAULT_KEY=HXCdm5z61eNgUpnXObJRv94k3JnKSrnfwppyb60nz6w= \
-  _build/prod/rel/oli/bin/oli eval 'case Oli.Math.Gleam.call(:torus_math, :decode_match_config, ["{\"version\":1,\"type\":\"always\"}"]) do {:ok, _} -> IO.puts("server Gleam smoke test passed"); other -> raise "server Gleam smoke test failed: #{inspect(other)}" end'
