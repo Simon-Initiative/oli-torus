4640cb459d2958a4507c87a219aee9d5c722f7de
Eli Knebel <eliknebel@cmu.edu>
2026-06-05 07:55:48 -0400

    [DEVOPS] [MER-0000] Fix package workflow CI mix gleam issue attempt 2 (#6636)
    * fix package workflow
    
    * fix
    
    * adjust approach
    
    * fix

--- .github/actions/amazon-linux-builder/entrypoint.sh
+++ .github/actions/amazon-linux-builder/entrypoint.sh
@@ -14,13 +14,12 @@ mix local.rebar --force
 mix archive.install hex mix_gleam 0.6.2 --force
 # mix archive.install hex phx_new 1.5.9 --force
 
-mix deps.get
-cd gleam && gleam build --target erlang && cd ..
-MIX_ENV=prod SHA=$RELEASE_SHA mix compile
-
+MIX_ENV=prod mix deps.get --only prod
 yarn --cwd ./assets
 NODE_ENV=production npm run deploy --prefix ./assets
 NODE_ENV=production npm run deploy-node --prefix ./assets
 
+cd gleam && gleam build --target erlang && cd ..
+MIX_ENV=prod SHA=$RELEASE_SHA mix compile
 MIX_ENV=prod mix assets.deploy
 MIX_ENV=prod SHA=$RELEASE_SHA mix release
