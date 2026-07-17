e0a69bbceef084d22e5dc6cd23493ff7dfbbec77
Kevin Segovia <kjsegovi@icloud.com>
2026-07-17 09:59:53 -0700

    fixed svg component based from gh reviewer.

--- assets/src/apps/authoring/components/Flowchart/toolbar/FlashcardIcon.tsx
+++ assets/src/apps/authoring/components/Flowchart/toolbar/FlashcardIcon.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 
 export const FlashcardIcon: React.FC<{ stroke?: string; fill?: string }> = ({
-  stroke: _stroke,
-  fill: _fill,
+  stroke = '#222439',
+  fill = '#F3F5F8',
   ...props
 }) => (
-  <img
-    src="/images/icons/icon-part-flashcards.svg"
+  <svg
     width={24}
     height={24}
-    alt=""
-    draggable={false}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
-  />
+  >
+    <rect width={24} height={24} rx={3} fill={fill} />
+    <g fill={stroke} fillRule="evenodd">
+      <rect x={8} y={2} width={12} height={16} rx={1.5} opacity={0.35} />
+      <rect x={6} y={4} width={12} height={16} rx={1.5} opacity={0.6} />
+      <rect x={4} y={6} width={12} height={16} rx={1.5} />
+      <rect x={7} y={10} width={6} height={1.25} rx={0.625} />
+      <rect x={7} y={13} width={4.5} height={1.25} rx={0.625} />
+      <rect x={7} y={16} width={5.5} height={1.25} rx={0.625} />
+    </g>
+  </svg>
 );
