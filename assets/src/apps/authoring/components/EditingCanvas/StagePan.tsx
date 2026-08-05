80a81c4843fd731ceb93ebc433abf5defb5cca46
Devesh Tiwari <dtiwari@ats360.com>
2026-08-05 13:22:50 +0530

    Update StagePan.tsx

--- assets/src/apps/authoring/components/EditingCanvas/StagePan.tsx
+++ assets/src/apps/authoring/components/EditingCanvas/StagePan.tsx
@@ -40,7 +40,7 @@ export const StagePan: React.FC = ({ children }) => {
       }}
       onMouseMove={onMove}
       onMouseDown={(e) => {
-        /*  console.log('pan event', e); */
+        /*  console.log('pan events', e); */
         const clickTarget = e.target;
         if (
           (clickTarget as HTMLElement).classList.contains('aa-stage-pan') ||
