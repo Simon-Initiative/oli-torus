ea3f620cfc2f6563eeb7850f68ae89b39e1b2469
Martin Manelli <martin.manelli@wyeworks.com>
2026-08-13 18:53:22 -0300

    fix test

--- lib/oli_web/live/delivery/student_onboarding/intro.ex
+++ lib/oli_web/live/delivery/student_onboarding/intro.ex
@@ -5,6 +5,7 @@ defmodule OliWeb.Delivery.StudentOnboarding.Intro do
   alias OliWeb.Components.Delivery.Student
 
   attr :section, :map, required: true
+  attr :id_prefix, :string, default: "onboarding"
 
   @doc """
   Renders the section-defined onboarding welcome message or the default course welcome title.
@@ -18,27 +19,27 @@ defmodule OliWeb.Delivery.StudentOnboarding.Intro do
     <div class="flex flex-col gap-3 px-[50px] hvsm:px-[70px] hvxl:px-[84px] py-9 dark:text-white">
       <%= if custom_message?(@section.description) do %>
         <h2
-          id="onboarding-welcome-title"
+          id={"#{@id_prefix}-welcome-title"}
           class="font-semibold text-[18xl] leading-[24px] hvsm:text-[30px] hvsm:leading-[40px] hvxl:text-[40px] hvxl:leading-[54px] tracking-[0.02px]"
         >
           <Student.welcome_title title={@section.welcome_title} />
         </h2>
         <p
           :if={custom_message?(@section.encouraging_subtitle)}
-          id="onboarding-welcome-subtitle"
+          id={"#{@id_prefix}-welcome-subtitle"}
           class="font-semibold text-[16px] leading-6 tracking-[0.02px] dark:text-opacity-80"
         >
           {@section.encouraging_subtitle}
         </p>
         <p
-          id="onboarding-welcome-description"
+          id={"#{@id_prefix}-welcome-description"}
           class="whitespace-pre-line text-[14px] leading-5 tracking-[0.02px] dark:text-opacity-80"
         >
           {@section.description}
         </p>
       <% else %>
         <h2
-          id="onboarding-welcome-title"
+          id={"#{@id_prefix}-welcome-title"}
           class="font-semibold text-[18xl] leading-[24px] hvsm:text-[30px] hvsm:leading-[40px] hvxl:text-[40px] hvxl:leading-[54px] tracking-[0.02px]"
         >
           Welcome to {@section.title}
