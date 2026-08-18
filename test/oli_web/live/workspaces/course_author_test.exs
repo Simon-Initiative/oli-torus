cf238da96d92043540ee19fb5db01ed70ff97076
Santiago Simoncelli <simoncellisantiago@gmail.com>
2026-08-18 08:26:23 -0300

    [ENHANCEMENT] [MER-5792] Update learning objectives authoring description (#6778)

--- test/oli_web/live/workspaces/course_author_test.exs
+++ test/oli_web/live/workspaces/course_author_test.exs
@@ -374,8 +374,26 @@ defmodule OliWeb.Workspaces.CourseAuthorTest do
 
       assert has_element?(
                view,
+               "p.text-Text-text-high",
+               "Learning objectives define the knowledge and skills students should demonstrate throughout your course."
+             )
+
+      assert has_element?(
+               view,
                "p",
-               "Learning objectives help you to organize course content and determine appropriate assessments and instructional strategies."
+               "Use this page to organize objectives and review coverage of formative (practice) and summative (scored) activities and pages."
+             )
+
+      assert has_element?(
+               view,
+               ~s|a.external.text-Text-text-button[href="https://www.cmu.edu/teaching/designteach/design/learningobjectives.html"][rel="noopener"][target="_blank"]|,
+               "CMU Eberly Center guide on learning objectives"
+             )
+
+      assert has_element?(
+               view,
+               "p",
+               "to learn more about the importance of attaching learning objectives to pages and activities."
              )
 
       assert has_element?(view, "button", "Create new Objective")
