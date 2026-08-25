2724e85c98c598d5dda290ec93cb2dddd61c84c8
Raphael Gachuhi <raph@Raph-Mac-Pro.local>
2026-08-25 16:54:54 -0400

    match ui to figma design

--- test/oli_web/live/workspaces/course_author_test.exs
+++ test/oli_web/live/workspaces/course_author_test.exs
@@ -375,18 +375,18 @@ defmodule OliWeb.Workspaces.CourseAuthorTest do
       assert has_element?(
                view,
                "p.text-Text-text-high",
-               "Learning objectives define the knowledge and skills students should demonstrate throughout your course."
+               "Learning objectives help you to organize course content and determine appropriate assessments and instructional strategies."
              )
 
       assert has_element?(
                view,
                "p",
-               "Use this page to organize objectives and review coverage of formative (practice) and summative (scored) activities and pages."
+               "Learning objectives help you to organize course content and determine appropriate assessments and instructional strategies."
              )
 
       assert has_element?(
                view,
-               ~s|a.external.text-Text-text-button[href="https://www.cmu.edu/teaching/designteach/design/learningobjectives.html"][rel="noopener"][target="_blank"]|,
+               ~s|a[href="https://www.cmu.edu/teaching/designteach/design/learningobjectives.html"][rel="noopener"][target="_blank"]|,
                "CMU Eberly Center guide on learning objectives"
              )
 
