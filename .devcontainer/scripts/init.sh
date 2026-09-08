919ad47244b60032c1fd33472d5d80f69551fb4a
Eli Knebel <eliknebel@cmu.edu>
2026-09-08 13:14:54 +0000

    fix dev container

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -2,7 +2,8 @@ if [ ! -f oli.env ]; then
   cp oli.example.env oli.env;
 
   # use postgres container
-  sed -i '' 's/DB_HOST=/# DB_HOST=/g' oli.env
+  sed -i.bak 's/DB_HOST=/# DB_HOST=/g' oli.env
+  rm -f oli.env.bak
   printf "\n\nDB_HOST=postgres" >> oli.env
 else
   echo "Config file oli.env already exists. Skipping configuration."
