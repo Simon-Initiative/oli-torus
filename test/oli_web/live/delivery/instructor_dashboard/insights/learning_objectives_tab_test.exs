ce5198e74e956bba77e9cb72e93f05a9a407c9b4
Anders Weinstein <andersweinstein@users.noreply.github.com>
2026-09-09 22:03:14 -0400

    Restore proficiency distribution gaps (#6842)
    Co-authored-by: Anders Weinstein <andersw@andrew.cmu.edu>

--- test/oli_web/live/delivery/instructor_dashboard/insights/learning_objectives_tab_test.exs
+++ test/oli_web/live/delivery/instructor_dashboard/insights/learning_objectives_tab_test.exs
@@ -409,6 +409,17 @@ defmodule OliWeb.Delivery.InstructorDashboard.LearningObjectivesTabTest do
       for color <- ~w(#CED1D9 #CE2C31 #BF5B13 #218358 #353740 #FF8787 #FFB387 #39E581) do
         assert html =~ color
       end
+
+      chart_props =
+        html
+        |> Floki.parse_fragment!()
+        |> Floki.find("#proficiency-data-bar-chart-for-objective-#{obj_revision_1.resource_id}")
+        |> Floki.attribute("data-live-react-props")
+        |> hd()
+        |> Jason.decode!()
+
+      assert chart_props["spec"]["mark"] == %{"type" => "bar", "binSpacing" => 2}
+      assert chart_props["spec"]["encoding"]["x"]["bin"] == "binned"
     end
   end
 
